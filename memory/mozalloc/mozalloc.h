8dbecac3a47ba311212c80ad78f22cd5c28a0f77
Riccardo Mottola <multix@iBookG4.westernesse>
2018-11-19 00:48:55 +0100

    backport fix for C++11

--- memory/mozalloc/mozalloc.h
+++ memory/mozalloc/mozalloc.h
@@ -204,6 +204,12 @@ MOZALLOC_EXPORT void* moz_valloc(size_t size)
  */
 #define MOZALLOC_THROW_IF_HAS_EXCEPTIONS
 #define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS
+#elif __cplusplus >= 201103
+/*
+ * C++11 has deprecated exception-specifications in favour of |noexcept|.
+ */
+#define MOZALLOC_THROW_IF_HAS_EXCEPTIONS noexcept(true)
+#define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS noexcept(false)
 #else
 #define MOZALLOC_THROW_IF_HAS_EXCEPTIONS throw()
 #define MOZALLOC_THROW_BAD_ALLOC_IF_HAS_EXCEPTIONS throw(std::bad_alloc)
