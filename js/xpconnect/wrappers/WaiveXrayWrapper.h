fe5f9f3c08eef2742cc09165db308504f6c60690
Riccardo Mottola <rm@gnu.org>
2019-02-11 23:14:55 +0100

    const override -> const MOZ_OVERRIDE

--- js/xpconnect/wrappers/WaiveXrayWrapper.h
+++ js/xpconnect/wrappers/WaiveXrayWrapper.h
@@ -25,17 +25,17 @@ class WaiveXrayWrapper : public js::CrossCompartmentWrapper {
     virtual bool get(JSContext *cx, JS::Handle<JSObject*> wrapper, JS::Handle<JSObject*> receiver,
                      JS::Handle<jsid> id, JS::MutableHandle<JS::Value> vp) const MOZ_OVERRIDE;
     virtual bool call(JSContext *cx, JS::Handle<JSObject*> wrapper,
-                      const JS::CallArgs& args) const override;
+                      const JS::CallArgs& args) const MOZ_OVERRIDE;
     virtual bool construct(JSContext* cx, JS::Handle<JSObject*> wrapper,
-                           const JS::CallArgs& args) const override;
+                           const JS::CallArgs& args) const MOZ_OVERRIDE;
 
     virtual bool enumerate(JSContext* cx, JS::Handle<JSObject*> proxy,
-                           JS::MutableHandle<JSObject*> objp) const override;
+                           JS::MutableHandle<JSObject*> objp) const MOZ_OVERRIDE;
     virtual bool nativeCall(JSContext* cx, JS::IsAcceptableThis test,
-                            JS::NativeImpl impl, const JS::CallArgs& args) const override;
+                            JS::NativeImpl impl, const JS::CallArgs& args) const MOZ_OVERRIDE;
     virtual bool getPropertyDescriptor(JSContext* cx, JS::Handle<JSObject*> wrapper,
                                        JS::Handle<jsid> id,
-                                       JS::MutableHandle<JSPropertyDescriptor> desc) const override;
+                                       JS::MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
 
     static const WaiveXrayWrapper singleton;
 };
