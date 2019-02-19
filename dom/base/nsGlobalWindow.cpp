0f1ad05549fab5da17f4004a405e344e7a7c7367
Riccardo Mottola <rm@gnu.org>
2019-02-19 08:31:15 +0100

    Bug 1356843 - Fix -Wcomma warnings in dom/base/ and dom/xml/. clang's -Wcomma warning warns about suspicious use of the comma operator such as between two statements or to call a function for side effects within an expression.
    This warning indicates that nsXMLFragmentContentSink::CloseElement() has been calling aContent->IsHTMLElement() but not using its return value to determine whether to call PreventExecution(). This was a regression from bug 1134280 back in 2015.
    
    dom/base/nsGlobalWindow.cpp:9344:55 [-Wcomma] possible misuse of comma operator here
    dom/xml/nsXMLFragmentContentSink.cpp:227:50 [-Wcomma] possible misuse of comma operator here

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -8663,11 +8663,13 @@ nsGlobalWindow::ReallyCloseWindow()
            from the list of browsers) (and has an unload handler
            that closes the window). */
         // XXXbz now that we have mHavePendingClose, is this needed?
-        bool isTab = false;
+        bool isTab;
         if (rootWin == this ||
-            !bwin || (bwin->IsTabContentWindow(GetOuterWindowInternal(),
-                                               &isTab), isTab))
+            !bwin ||
+            (NS_SUCCEEDED(bwin->IsTabContentWindow(GetOuterWindowInternal(),
+                                                   &isTab)) && isTab)) {
           treeOwnerAsWin->Destroy();
+        }
       }
     }
 
