98f1b9c377d6b64003c77b9a9b40b0a7d6f79918
Riccardo Mottola <rm@gnu.org>
2019-03-25 11:19:21 +0100

    Bug 1136906 part 3. Remove the parent arg from JS_NewObject.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -1925,13 +1925,12 @@ JS_FireOnNewGlobalObject(JSContext* cx, JS::HandleObject global)
     Debugger::onNewGlobalObject(cx, globalObject);
 }
 
-JS_PUBLIC_API(JSObject*)
-JS_NewObject(JSContext* cx, const JSClass* jsclasp, HandleObject parent)
+JS_PUBLIC_API(JSObject *)
+JS_NewObject(JSContext *cx, const JSClass *jsclasp)
 {
     MOZ_ASSERT(!cx->runtime()->isAtomsCompartment(cx->compartment()));
     AssertHeapIsIdle(cx);
     CHECK_REQUEST(cx);
-    assertSameCompartment(cx, parent);
 
     const Class* clasp = Valueify(jsclasp);
     if (!clasp)
@@ -1940,7 +1939,7 @@ JS_NewObject(JSContext* cx, const JSClass* jsclasp, HandleObject parent)
     MOZ_ASSERT(clasp != &JSFunction::class_);
     MOZ_ASSERT(!(clasp->flags & JSCLASS_IS_GLOBAL));
 
-    JSObject* obj = NewObjectWithClassProto(cx, clasp, NullPtr(), parent);
+    JSObject *obj = NewObjectWithClassProto(cx, clasp, NullPtr(), NullPtr());
     MOZ_ASSERT_IF(obj, obj->getParent());
     return obj;
 }
--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -2456,8 +2456,8 @@ JS_GlobalObjectTraceHook(JSTracer* trc, JSObject* global);
 extern JS_PUBLIC_API(void)
 JS_FireOnNewGlobalObject(JSContext* cx, JS::HandleObject global);
 
-extern JS_PUBLIC_API(JSObject*)
-JS_NewObject(JSContext* cx, const JSClass* clasp, JS::Handle<JSObject*> parent = JS::NullPtr());
+extern JS_PUBLIC_API(JSObject *)
+JS_NewObject(JSContext *cx, const JSClass *clasp);
 
 /* Queries the [[Extensible]] property of the object. */
 extern JS_PUBLIC_API(bool)
