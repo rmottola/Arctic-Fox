e91a020d19dddb90663cf3ba99fcede81c82fb64
Riccardo Mottola <rm@gnu.org>
2019-04-06 23:05:54 +0200

    Bug 1135042: Inline SIMD stores in Ion

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -9047,6 +9047,7 @@ class MStoreTypedArrayElement
     public StoreTypedArrayPolicy::Data
 {
     Scalar::Type arrayType_;
+    Scalar::Type writeType_;
     bool requiresBarrier_;
     int32_t offsetAdjustment_;
 
@@ -9058,6 +9059,7 @@ class MStoreTypedArrayElement
                             int32_t offsetAdjustment)
       : MTernaryInstruction(elements, index, value),
         arrayType_(arrayType),
+        writeType_(arrayType),
         requiresBarrier_(requiresBarrier == DoesRequireMemoryBarrier),
         offsetAdjustment_(offsetAdjustment),
         racy_(false)
@@ -9083,8 +9085,17 @@ class MStoreTypedArrayElement
                                                   requiresBarrier, offsetAdjustment);
     }
 
+    void setWriteType(Scalar::Type type) {
+        writeType_ = type;
+    }
+    Scalar::Type writeType() const {
+        return writeType_;
+    }
     Scalar::Type arrayType() const {
         return arrayType_;
+    }
+    bool isSimdWrite() const {
+        return Scalar::isSimdType(writeType());
     }
     bool isByteArray() const {
         return arrayType_ == Scalar::Int8 ||
