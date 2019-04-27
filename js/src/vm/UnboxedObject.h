5f184fb4a647afb036aff173cebea773c42492f4
Riccardo Mottola <rm@gnu.org>
2019-04-27 22:00:06 +0200

    Bug 1134150 - Don't check type sweeping generation when accessing an unboxed object's layout during nursery collections

--- js/src/vm/UnboxedObject.h
+++ js/src/vm/UnboxedObject.h
@@ -206,6 +206,10 @@ class UnboxedPlainObject : public JSObject
         return group()->unboxedLayout();
     }
 
+    const UnboxedLayout &layoutDontCheckGeneration() const {
+        return group()->unboxedLayoutDontCheckGeneration();
+    }
+
     uint8_t *data() {
         return &data_[0];
     }
