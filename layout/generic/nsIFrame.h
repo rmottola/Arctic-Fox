2c69cf891e4aa636f883fad00c34c5966cb1d5d0
Riccardo Mottola <rm@gnu.org>
2019-05-22 23:44:46 +0200

    Bug 1143513 - Make nsFrameList compatible with range-based syntax and utils.

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -3365,6 +3365,27 @@ nsFrameList::FrameLinkEnumerator::Next()
   Enumerator::Next();
 }
 
+// Operators of nsFrameList::Iterator
+// ---------------------------------------------------
+
+inline nsFrameList::Iterator&
+nsFrameList::Iterator::operator++()
+{
+  mCurrent = mCurrent->GetNextSibling();
+  return *this;
+}
+
+inline nsFrameList::Iterator&
+nsFrameList::Iterator::operator--()
+{
+  if (!mCurrent) {
+    mCurrent = mList.LastChild();
+  } else {
+    mCurrent = mCurrent->GetPrevSibling();
+  }
+  return *this;
+}
+
 // Helper-functions for nsIFrame::SortFrameList()
 // ---------------------------------------------------
 
