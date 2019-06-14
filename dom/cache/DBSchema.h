82d81819a78981dc1338ab854ba1ad985f8f41cb
Riccardo Mottola <rm@gnu.org>
2019-06-14 06:20:02 +0200

    Bug 1133763 - Part 3: Wipe out the cache directory when detecting a change in the DB schema;

--- dom/cache/DBSchema.h
+++ dom/cache/DBSchema.h
@@ -89,6 +89,9 @@ public:
                                  Namespace aNamespace,
                                  nsTArray<nsString>& aKeysOut);
 
+  // We will wipe out databases with a schema versions less than this.
+  static const int32_t kMaxWipeSchemaVersion;
+
 private:
   typedef int32_t EntryId;
 
