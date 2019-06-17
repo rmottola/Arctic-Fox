0f4842e41c3cccfdd399935b8e3aa915bbab38be
Riccardo Mottola <rm@gnu.org>
2019-06-17 22:51:16 +0200

    Bug 1142124 - Never revalidate cache entries for synthesized responses.

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -3266,6 +3266,10 @@ nsHttpChannel::OnCacheEntryCheck(nsICacheEntry* entry, nsIApplicationCache* appC
             mRedirectedCachekeys->AppendElement(cacheKey);
     }
 
+    if (doValidation && mInterceptCache == INTERCEPTED) {
+        doValidation = false;
+    }
+
     mCachedContentIsValid = !doValidation;
 
     if (doValidation) {
