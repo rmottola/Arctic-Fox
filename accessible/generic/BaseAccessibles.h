4285a88c338edb51df2299148e395d783656a127
Riccardo Mottola <rm@gnu.org>
2019-07-03 12:28:00 +0200

    Bug 1139049 - turn EnumRoleAccessible into template

--- accessible/generic/BaseAccessibles.h
+++ accessible/generic/BaseAccessibles.h
@@ -93,21 +93,21 @@ protected:
 /**
  * A simple accessible that gets its enumerated role passed into constructor.
  */
-class EnumRoleAccessible : public AccessibleWrap
+template<a11y::role R>
+class RoleTAccessible : public AccessibleWrap
 {
 public:
-  EnumRoleAccessible(nsIContent* aContent, DocAccessible* aDoc, 
-                     a11y::role aRole);
+  RoleTAccessible(nsIContent* aContent, DocAccessible* aDoc) :
+    AccessibleWrap(aContent, aDoc) { }
 
-  NS_DECL_ISUPPORTS_INHERITED
+  NS_IMETHOD QueryInterface(REFNSIID aIID, void** aPtr) override
+    { return Accessible::QueryInterface(aIID, aPtr); }
 
   // Accessible
-  virtual a11y::role NativeRole() override;
+  virtual a11y::role NativeRole() override { return R; }
 
 protected:
-  virtual ~EnumRoleAccessible() { }
-
-  a11y::role mRole;
+  virtual ~RoleTAccessible() { }
 };
 
 
