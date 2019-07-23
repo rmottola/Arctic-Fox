43525c5ad28432b721b0cbf08b8597cd44c90443
wicknix <39230578+wicknix@users.noreply.github.com>
2019-07-23 00:17:08 -0500

    Add space in MOZ_APP_DISPLAYNAME
    Changes menu bar from displaying ArcticFox to Arctic Fox

--- browser/branding/arcticfox/configure.sh
+++ browser/branding/arcticfox/configure.sh
@@ -2,5 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=ArcticFox
+MOZ_APP_DISPLAYNAME="Arctic Fox"
 # MOZ_UA_BUILDID=20100101
