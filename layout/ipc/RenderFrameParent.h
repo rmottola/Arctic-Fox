fda5e8176523bb1dc8a59735ea6b1c4d059a5a5a
Riccardo Mottola <rm@gnu.org>
2019-08-08 10:21:05 +0200

    Bug 1134493 - Ensure we don't set the force-empty-hit-region flag when a subdocument has mozpasspointerevents. r=botond

--- layout/ipc/RenderFrameParent.h
+++ layout/ipc/RenderFrameParent.h
@@ -162,7 +162,7 @@ class nsDisplayRemote : public nsDisplayItem
   typedef mozilla::layout::RenderFrameParent RenderFrameParent;
 
 public:
-  nsDisplayRemote(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame,
+  nsDisplayRemote(nsDisplayListBuilder* aBuilder, nsSubDocumentFrame* aFrame,
                   RenderFrameParent* aRemoteFrame);
 
   virtual LayerState GetLayerState(nsDisplayListBuilder* aBuilder,
