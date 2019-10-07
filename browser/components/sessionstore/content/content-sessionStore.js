fdd1311ecaa9edbba33b52d9469f7e5ff8ed85b7
Riccardo Mottola <rm@gnu.org>
2019-10-07 09:35:36 +0200

    Bug 910646 - Collect docShell capabilities from content script (r=ttaubert)

--- browser/components/sessionstore/content/content-sessionStore.js
+++ browser/components/sessionstore/content/content-sessionStore.js
@@ -15,6 +15,8 @@ let Cr = Components.results;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm", this);
 
+XPCOMUtils.defineLazyModuleGetter(this, "DocShellCapabilities",
+  "resource:///modules/sessionstore/DocShellCapabilities.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "SessionHistory",
   "resource:///modules/sessionstore/SessionHistory.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "SessionStorage",
@@ -75,7 +77,8 @@ let MessageListener = {
 
   MESSAGES: [
     "SessionStore:collectSessionHistory",
-    "SessionStore:collectSessionStorage"
+    "SessionStore:collectSessionStorage",
+    "SessionStore:collectDocShellCapabilities"
   ],
 
   init: function () {
@@ -91,6 +94,10 @@ let MessageListener = {
       case "SessionStore:collectSessionStorage":
         let storage = SessionStorage.serialize(docShell);
         sendAsyncMessage(name, {id: id, data: storage});
+        break;
+      case "SessionStore:collectDocShellCapabilities":
+        let disallow = DocShellCapabilities.collect(docShell);
+        sendAsyncMessage(name, {id: id, data: disallow});
         break;
       default:
         debug("received unknown message '" + name + "'");
