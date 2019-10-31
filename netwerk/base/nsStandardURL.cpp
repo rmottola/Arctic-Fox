84b653c37ca1bd99bb2ce65886c1ffd1b86950e1
Riccardo Mottola <rm@gnu.org>
2019-10-31 16:55:17 +0100

    Bug 1135354 - Crash/Hang when clicking URL with a huge length r=mcmanus

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -843,6 +843,10 @@ nsStandardURL::ParseURL(const char *spec, int32_t specLen)
 {
     nsresult rv;
 
+    if (specLen > net_GetURLMaxLength()) {
+        return NS_ERROR_MALFORMED_URI;
+    }
+
     //
     // parse given URL string
     //
@@ -886,6 +890,10 @@ nsresult
 nsStandardURL::ParsePath(const char *spec, uint32_t pathPos, int32_t pathLen)
 {
     LOG(("ParsePath: %s pathpos %d len %d\n",spec,pathPos,pathLen));
+
+    if (pathLen > net_GetURLMaxLength()) {
+        return NS_ERROR_MALFORMED_URI;
+    }
 
     nsresult rv = mParser->ParsePath(spec + pathPos, pathLen,
                                      &mFilepath.mPos, &mFilepath.mLen,
@@ -1248,6 +1256,10 @@ nsStandardURL::SetSpec(const nsACString &input)
         if (NS_FAILED(rv)) {
             return rv;
         }
+    }
+
+    if (input.Length() > (uint32_t) net_GetURLMaxLength()) {
+        return NS_ERROR_MALFORMED_URI;
     }
 
     // Make a backup of the curent URL
@@ -2849,6 +2861,10 @@ nsStandardURL::Init(uint32_t urlType,
                     nsIURI *baseURI)
 {
     ENSURE_MUTABLE();
+
+    if (spec.Length() > (uint32_t) net_GetURLMaxLength()) {
+        return NS_ERROR_MALFORMED_URI;
+    }
 
     InvalidateCache();
 
