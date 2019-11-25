4b7ed93ae8958b8bd5ce92ba8052c6ab22e215c9
Riccardo Mottola <rm@gnu.org>
2019-11-25 19:59:33 +0100

    Bug 931571, part 2 - Convert Cleanup() and FreeArray() to methods. r=froydnj

--- js/xpconnect/src/XPCVariant.cpp
+++ js/xpconnect/src/XPCVariant.cpp
@@ -58,7 +58,7 @@ XPCTraceableVariant::~XPCTraceableVariant()
 
     MOZ_ASSERT(val.isGCThing(), "Must be traceable or unlinked");
 
-    nsVariant::Cleanup(&mData);
+    mData.Cleanup();
 
     if (!val.isNull())
         RemoveFromRootSet();
@@ -93,7 +93,7 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(XPCVariant)
     JS::Value val = tmp->GetJSValPreserveColor();
 
-    nsVariant::Cleanup(&tmp->mData);
+    tmp->mData.Cleanup();
 
     if (val.isMarkable()) {
         XPCTraceableVariant* v = static_cast<XPCTraceableVariant*>(tmp);
@@ -611,7 +611,7 @@ XPCVariant::VariantDataToJS(nsIVariant* variant,
                 case nsIDataType::VTYPE_EMPTY:
                 default:
                     NS_ERROR("bad type in array!");
-                    nsVariant::Cleanup(&du);
+                    du.Cleanup();
                     return false;
             }
 
@@ -621,7 +621,7 @@ XPCVariant::VariantDataToJS(nsIVariant* variant,
                                            conversionType, pid,
                                            du.u.array.mArrayCount, pErr);
 
-            nsVariant::Cleanup(&du);
+            du.Cleanup();
             return success;
         }
         case nsIDataType::VTYPE_EMPTY_ARRAY:
