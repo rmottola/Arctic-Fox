b1b1611b05bf6f0c643893d66a3d3cb576e48895
Riccardo Mottola <rm@gnu.org>
2020-01-19 16:01:10 +0100

    fix redeclaration of cc

--- browser/base/content/safeMode.js
+++ browser/base/content/safeMode.js
@@ -2,9 +2,9 @@
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let Cc = Components.classes;
-let Ci = Components.interfaces;
-let Cu = Components.utils;
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cu = Components.utils;
 
 const appStartup = Services.startup;
 
