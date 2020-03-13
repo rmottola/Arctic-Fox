96d8f1eb773b6fbb18d1b15f91e5d88bd2fa151e
Riccardo Mottola <rm@gnu.org>
2020-03-13 20:37:59 +0100

    Bug 1147522 - Move SessionRecorder to Toolkit/modules. r=gfritzsche

--- services/datareporting/DataReportingService.js
+++ services/datareporting/DataReportingService.js
@@ -432,6 +432,6 @@ this.NSGetFactory = XPCOMUtils.generateNSGetFactory([DataReportingService]);
 ;
 #include policy.jsm
 ;
-#include sessions.jsm
+#include ../../toolkit/modules/SessionRecorder.jsm
 ;
 
