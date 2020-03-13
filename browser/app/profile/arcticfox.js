30ee856a94e6efa4ee282a2613dc16a6c66c6b33
Riccardo Mottola <rm@gnu.org>
2020-03-14 00:16:30 +0100

    remove also padlock related preferences

--- browser/app/profile/arcticfox.js
+++ browser/app/profile/arcticfox.js
@@ -1167,19 +1167,6 @@ pref("network.disable.ipc.security", true);
 // Determines if Telemetry pings can be archived locally.
 pref("toolkit.telemetry.archive.enabled", true);
 
-//Arctic Fox padlock overlay preferences
-pref("browser.padlock.shown", true);
-/* Where to show the padlock
-   1 = inside identity button, right side
-   2 = inside identity button, left side
-   3 = urlbar, right side (next to bookmark star)
-   4 = statusbar
-   5 = tabs bar, right side 
-   6-10 = same locations, classic style padlock */
-pref("browser.padlock.style", 1);
-// address bar border, 0 = no border, 1 = border, 2 = border only on secure sites
-pref("browser.padlock.urlbar_background", 2);
-
 //Arctic Fox standalone image background color
 pref("browser.display.standalone_images.background_color", "#2E3B41");
 
