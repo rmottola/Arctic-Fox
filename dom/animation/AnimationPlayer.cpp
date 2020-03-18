7cc762bbb37a0baabf49e3e9979db709fc83ab9d
Riccardo Mottola <rm@gnu.org>
2020-03-18 13:54:28 +0100

    Bug 1145246, part 7 - Add more CSS animation tests to check currentTime clamping. r=birtles

--- dom/animation/AnimationPlayer.cpp
+++ dom/animation/AnimationPlayer.cpp
@@ -622,7 +622,9 @@ AnimationPlayer::UpdateFinishedState(bool aSeekFlag)
     mFinished = nullptr;
   }
   mIsPreviousStateFinished = currentFinishedState;
-  mPreviousCurrentTime = currentTime;
+  // We must recalculate the current time to take account of any mHoldTime
+  // changes the code above made.
+  mPreviousCurrentTime = GetCurrentTime();
 }
 
 void
