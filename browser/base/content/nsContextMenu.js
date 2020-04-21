60b552127cee0efad1cdb43780e50c5239b281a8
Riccardo Mottola <rm@gnu.org>
2020-04-21 11:59:55 +0200

    Bug 1134222 - Save a reference to the ownerDocument during the sync contextmenu message, and use it for saveLink(). r=gabor

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -518,6 +518,7 @@ nsContextMenu.prototype = {
     this.focusedElement = elt;
 
     let ownerDoc = this.target.ownerDocument;
+    this.ownerDoc = ownerDoc;
 
     // If this is a remote context menu event, use the information from
     // gContextMenuContentData instead.
@@ -1267,9 +1268,8 @@ nsContextMenu.prototype = {
 
   // Save URL of clicked-on link.
   saveLink: function() {
-    var doc =  this.target.ownerDocument;
     urlSecurityCheck(this.linkURL, this.principal);
-    this.saveHelper(this.linkURL, this.linkText, null, true, doc,
+    this.saveHelper(this.linkURL, this.linkText, null, true, this.ownerDoc,
                     this.linkDownload);
   },
 
