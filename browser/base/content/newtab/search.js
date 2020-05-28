985c07d920ae167323505958f080cf0abfdab68d
Riccardo Mottola <rm@gnu.org>
2020-05-28 12:16:51 +0200

    Bug 1019989 - ContentSearch should pass images as shared ArrayBuffers instead of data URI strings. r=felipe

--- browser/base/content/newtab/search.js
+++ browser/base/content/newtab/search.js
@@ -82,8 +82,17 @@ let gSearch = {
     this._initWhenInitalStateReceived();
   },
 
+  onCurrentState: function (data) {
+    if (this._initialStateReceived) {
+      this._makePanel(data.engines);
+      this._setCurrentEngine(data.currentEngine);
+    }
+  },
+
   onCurrentEngine: function (engineName) {
-    this._setCurrentEngine(engineName);
+    if (this._initialStateReceived) {
+      this._setCurrentEngine(engineName);
+    }
   },
 
   onFocusInput: function () {
@@ -104,6 +113,7 @@ let gSearch = {
     this._nodes.form.addEventListener("submit", e => this.search(e));
     this._nodes.logo.addEventListener("click", e => this.showPanel());
     this._nodes.manage.addEventListener("click", e => this.manageEngines());
+    this._initialStateReceived = true;
     this._initWhenInitalStateReceived = function () {};
   },
 
@@ -150,8 +160,9 @@ let gSearch = {
     });
 
     let image = document.createElementNS(XUL_NAMESPACE, "image");
-    if (engine.iconURI) {
-      image.setAttribute("src", engine.iconURI);
+    if (engine.iconBuffer) {
+      let blob = new Blob([engine.iconBuffer]);
+      image.setAttribute("src", URL.createObjectURL(blob));
     }
     box.appendChild(image);
 
@@ -166,11 +177,12 @@ let gSearch = {
     this.currentEngineName = engine.name;
 
     // Set the logo.
-    let logoURI = window.devicePixelRatio == 2 ? engine.logo2xURI :
-                  engine.logoURI;
-    if (logoURI) {
+    let logoBuf = window.devicePixelRatio == 2 ? engine.logo2xBuffer :
+                  engine.logoBuffer || engine.logo2xBuffer;
+    if (logoBuf) {
       this._nodes.logo.hidden = false;
-      this._nodes.logo.style.backgroundImage = "url(" + logoURI + ")";
+      let uri = URL.createObjectURL(new Blob([logoBuf]));
+      this._nodes.logo.style.backgroundImage = "url(" + uri + ")";
       this._nodes.text.placeholder = "";
     }
     else {
