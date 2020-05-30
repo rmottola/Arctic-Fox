ba586d37d6e85a07342c0cb4badc74c7b66fc23a
Riccardo Mottola <rm@gnu.org>
2020-05-30 12:16:22 +0200

    Bug 1062462 - Copy Icc Info from the message delivered by ril_worker instead of the one in rilContext. r=echen.

--- dom/system/gonk/RadioInterfaceLayer.js
+++ dom/system/gonk/RadioInterfaceLayer.js
@@ -2251,7 +2251,8 @@ RadioInterface.prototype = {
     gMessageManager.sendIccMessage("RIL:IccInfoChanged",
                                    this.clientId,
                                    message.iccid ? message : null);
-    gIccService.notifyIccInfoChanged(this.clientId, this.rilContext.iccInfo);
+    gIccService.notifyIccInfoChanged(this.clientId,
+                                     message.iccid ? message : null);
 
     // Update lastKnownHomeNetwork.
     if (message.mcc && message.mnc) {
