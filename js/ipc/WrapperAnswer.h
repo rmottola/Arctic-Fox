96c997639fbcbf83f40c6a8537d49744588c1f65
Riccardo Mottola <rm@gnu.org>
2020-06-06 11:51:41 +0200

    Bug 1148962 - Use TakeOwnershipOfErrorReporting in CPOW code (r=bholley)

--- js/ipc/WrapperAnswer.h
+++ js/ipc/WrapperAnswer.h
@@ -11,6 +11,11 @@
 #include "JavaScriptShared.h"
 
 namespace mozilla {
+
+namespace dom {
+class AutoJSAPI;
+}
+
 namespace jsipc {
 
 class WrapperAnswer : public virtual JavaScriptShared
@@ -62,9 +67,9 @@ class WrapperAnswer : public virtual JavaScriptShared
     bool RecvDropObject(const ObjectId& objId);
 
   private:
-    bool fail(JSContext* cx, ReturnStatus* rs);
-    bool ok(ReturnStatus *rs);
-    bool ok(ReturnStatus *rs, const JS::ObjectOpResult &result);
+    bool fail(dom::AutoJSAPI& jsapi, ReturnStatus* rs);
+    bool ok(ReturnStatus* rs);
+    bool ok(ReturnStatus* rs, const JS::ObjectOpResult& result);
 };
 
 } // namespace jsipc
