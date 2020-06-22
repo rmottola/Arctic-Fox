2e750c9605707e22786583411aea7eb2400888e2
Riccardo Mottola <rm@gnu.org>
2020-06-22 15:16:05 +0200

    rearrange code

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -338,24 +338,6 @@ ErrorResult::ReportNotEnoughArgsError(JSContext* cx,
   ThrowErrorMessage(cx, dom::MSG_MISSING_ARGUMENTS, errorMessage.get());
 }
 
-void
-ErrorResult::SuppressException()
-{
-  WouldReportJSException();
-  if (IsErrorWithMessage()) {
-    ClearMessage();
-  } else if (IsJSException()) {
-    JSContext* cx = nsContentUtils::GetDefaultJSContextForThread();
-    // Just steal it into a stack value (unrooting it in the process)
-    // that we then allow to die.
-    JS::Rooted<JS::Value> temp(cx);
-    StealJSException(cx, &temp);
-  }
-  // We don't use AssignErrorCode, because we want to override existing error
-  // states, which AssignErrorCode is not allowed to do.
-  mResult = NS_OK;
-}
-
 ErrorResult&
 ErrorResult::operator=(ErrorResult&& aRHS)
 {
@@ -392,6 +374,24 @@ ErrorResult::operator=(ErrorResult&& aRHS)
   mResult = aRHS.mResult;
   aRHS.mResult = NS_OK;
   return *this;
+}
+
+void
+ErrorResult::SuppressException()
+{
+  WouldReportJSException();
+  if (IsErrorWithMessage()) {
+    ClearMessage();
+  } else if (IsJSException()) {
+    JSContext* cx = nsContentUtils::GetDefaultJSContextForThread();
+    // Just steal it into a stack value (unrooting it in the process)
+    // that we then allow to die.
+    JS::Rooted<JS::Value> temp(cx);
+    StealJSException(cx, &temp);
+  }
+  // We don't use AssignErrorCode, because we want to override existing error
+  // states, which AssignErrorCode is not allowed to do.
+  mResult = NS_OK;
 }
 
 namespace dom {
