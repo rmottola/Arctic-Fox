c2e6d90704c23cfaac623df6254c0e6ea5990109
Riccardo Mottola <rm@gnu.org>
2020-06-23 10:02:05 +0200

    Backed out changeset 1b5fc672f0d4 (bug 1159082) for B2G ICS Emulator opt R6 test failures on a CLOSED TREE

--- dom/base/nsDOMMutationObserver.cpp
+++ dom/base/nsDOMMutationObserver.cpp
@@ -328,7 +328,7 @@ void
 nsAnimationReceiver::RecordAnimationMutation(Animation* aAnimation,
                                              AnimationMutation aMutationType)
 {
-  KeyframeEffectReadOnly* effect = aAnimation->GetEffect();
+  KeyframeEffectReadonly* effect = aAnimation->GetEffect();
   if (!effect) {
     return;
   }
