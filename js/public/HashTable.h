353f6e1e3c3109e728ef044852bfd424f153af08
Riccardo Mottola <rm@gnu.org>
2020-07-23 17:53:54 +0200

    Bug 1139217 - Make js::HashSet<T> work with move-only T types; r=luke

--- js/public/HashTable.h
+++ js/public/HashTable.h
@@ -1358,7 +1358,8 @@ class HashTable : private AllocPolicy
         for (Entry* src = oldTable, *end = src + oldCap; src < end; ++src) {
             if (src->isLive()) {
                 HashNumber hn = src->getKeyHash();
-                findFreeEntry(hn).setLive(hn, mozilla::Move(src->get()));
+                findFreeEntry(hn).setLive(
+                    hn, mozilla::Move(const_cast<typename Entry::NonConstT&>(src->get())));
                 src->destroy();
             }
         }
