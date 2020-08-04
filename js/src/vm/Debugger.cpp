12d7d6f1996e41b20e0a9f22619d28ec1d6f68c5
Riccardo Mottola <rm@gnu.org>
2020-08-04 23:41:20 +0200

    Bug 1146743. Remove use of compileAndGo in JS debugger. r=shu

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -1575,10 +1575,7 @@ Debugger::onSingleStep(JSContext* cx, MutableHandleValue vp)
                 }
             }
         }
-        if (trappingScript->compileAndGo())
-            MOZ_ASSERT(stepperCount == trappingScript->stepModeCount());
-        else
-            MOZ_ASSERT(stepperCount <= trappingScript->stepModeCount());
+        MOZ_ASSERT(stepperCount == trappingScript->stepModeCount());
     }
 #endif
 
