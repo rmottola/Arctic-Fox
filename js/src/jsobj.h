07fbcfdaffe6250c4f1b50940b7cd8284fbc3bc1
Riccardo Mottola <rm@gnu.org>
2020-08-11 20:00:25 +0200

    Bug 1153959 - Use RAII to manage tracing details; r=jonco

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -1253,9 +1253,6 @@ XDRObjectLiteral(XDRState<mode>* xdr, MutableHandleObject obj);
 extern JSObject*
 CloneObjectLiteral(JSContext* cx, HandleObject srcObj);
 
-extern void
-GetObjectSlotName(JSTracer* trc, char* buf, size_t bufsize);
-
 extern bool
 ReportGetterOnlyAssignment(JSContext* cx, bool strict);
 
