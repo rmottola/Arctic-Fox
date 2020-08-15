d0c09e4ecbdfaac98602ac68ca31e0aafe7b9aa1
Riccardo Mottola <rm@gnu.org>
2020-08-15 20:05:03 +0200

    pointer style

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -1548,14 +1548,14 @@ IonBuilder::inlineConstantStringSplit(CallInfo& callInfo)
     if (templateObject->getDenseInitializedLength() != initLength)
         return InliningStatus_NotInlined;
 
-    JSContext *cx = GetJitContext()->cx;
+    JSContext* cx = GetJitContext()->cx;
     Vector<MConstant*, 0, SystemAllocPolicy> arrayValues;
     for (uint32_t i = 0; i < initLength; i++) {
         JSAtom* str = js::AtomizeString(cx, templateObject->getDenseElement(i).toString());
         if (!str)
             return InliningStatus_Error;
 
-        MConstant *value = MConstant::New(alloc(), StringValue(str), constraints());
+        MConstant* value = MConstant::New(alloc(), StringValue(str), constraints());
         if (!TypeSetIncludes(key.maybeTypes(), value->type(), value->resultTypeSet()))
             return InliningStatus_NotInlined;
 
