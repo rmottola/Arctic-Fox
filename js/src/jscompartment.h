632e06739130105d667b7ff3b2b942a34c0fcd0e
Riccardo Mottola <rm@gnu.org>
2020-08-22 18:42:30 +0200

    Bug 1153963 - Add telemetry for regexp.hasOwnProperty("source") and Object.getOwnPropertyDescriptor(regexp, "source") on non-release build. r=till

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -588,6 +588,7 @@ struct JSCompartment
         // No longer using 5 (was: let expressions)
         DeprecatedNoSuchMethod = 6,         // JS 1.7+
         DeprecatedFlagsArgument = 7,        // JS 1.3 or older
+        RegExpSourceProperty = 8,           // ES5
         DeprecatedLanguageExtensionCount
     };
 
