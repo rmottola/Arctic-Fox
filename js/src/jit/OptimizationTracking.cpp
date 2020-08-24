d29b2ac28d106dcb1c31927157580e607962fe32
Riccardo Mottola <rm@gnu.org>
2020-08-24 19:36:09 +0200

    pointer style

--- js/src/jit/OptimizationTracking.cpp
+++ js/src/jit/OptimizationTracking.cpp
@@ -445,11 +445,11 @@ IonTrackedOptimizationsRegion::RangeIterator::readNext(uint32_t* startOffset, ui
 }
 
 Maybe<uint8_t>
-JitcodeGlobalEntry::IonEntry::trackedOptimizationIndexAtAddr(void *ptr, uint32_t *entryOffsetOut)
+JitcodeGlobalEntry::IonEntry::trackedOptimizationIndexAtAddr(void* ptr, uint32_t* entryOffsetOut)
 {
     MOZ_ASSERT(hasTrackedOptimizations());
     MOZ_ASSERT(containsPointer(ptr));
-    uint32_t ptrOffset = ((uint8_t *) ptr) - ((uint8_t *) nativeStartAddr());
+    uint32_t ptrOffset = ((uint8_t*) ptr) - ((uint8_t*) nativeStartAddr());
     Maybe<IonTrackedOptimizationsRegion> region = optsRegionTable_->findRegion(ptrOffset);
     if (region.isNothing())
         return Nothing();
@@ -491,7 +491,7 @@ IonTrackedOptimizationsTypeInfo::forEach(ForEachOp& op, const IonTrackedTypeVect
 }
 
 Maybe<uint8_t>
-IonTrackedOptimizationsRegion::findIndex(uint32_t offset, uint32_t *entryOffsetOut) const
+IonTrackedOptimizationsRegion::findIndex(uint32_t offset, uint32_t* entryOffsetOut) const
 {
     if (offset <= startOffset_ || offset > endOffset_)
         return Nothing();
@@ -1133,11 +1133,11 @@ IonBuilder::trackInlineSuccessUnchecked(InliningStatus status)
 }
 
 JS_PUBLIC_API(void)
-JS::ForEachTrackedOptimizationAttempt(JSRuntime *rt, void *addr, uint8_t index,
-                                      ForEachTrackedOptimizationAttemptOp &op,
-                                      JSScript **scriptOut, jsbytecode **pcOut)
+JS::ForEachTrackedOptimizationAttempt(JSRuntime* rt, void* addr, uint8_t index,
+                                      ForEachTrackedOptimizationAttemptOp& op,
+                                      JSScript** scriptOut, jsbytecode** pcOut)
 {
-    JitcodeGlobalTable *table = rt->jitRuntime()->getJitcodeGlobalTable();
+    JitcodeGlobalTable* table = rt->jitRuntime()->getJitcodeGlobalTable();
     JitcodeGlobalEntry entry;
     table->lookupInfallible(addr, &entry, rt);
     entry.youngestFrameLocationAtAddr(rt, addr, scriptOut, pcOut);
@@ -1175,7 +1175,7 @@ FunctionFromTrackedType(const IonTrackedTypeWithAddendum& tracked)
 }
 
 void
-IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedTypeWithAddendum &tracked)
+IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedTypeWithAddendum& tracked)
 {
     TypeSet::Type ty = tracked.type;
 
@@ -1187,7 +1187,7 @@ IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedType
     char buf[512];
     const uint32_t bufsize = mozilla::ArrayLength(buf);
 
-    if (JSFunction *fun = FunctionFromTrackedType(tracked)) {
+    if (JSFunction* fun = FunctionFromTrackedType(tracked)) {
         if (fun->isNative()) {
             //
             // Print out the absolute address of the function pointer.
@@ -1201,7 +1201,7 @@ IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedType
             // for use with utilities like `addr2line` on Linux and `atos` on
             // OS X. Converting to an offset may be done via dladdr():
             //
-            //   void *addr = JS_FUNC_TO_DATA_PTR(void *, fun->native());
+            //   void* addr = JS_FUNC_TO_DATA_PTR(void*, fun->native());
             //   uintptr_t offset;
             //   Dl_info info;
             //   if (dladdr(addr, &info) != 0)
@@ -1214,18 +1214,18 @@ IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::readType(const IonTrackedType
         }
 
         PutEscapedString(buf, bufsize, fun->displayAtom(), 0);
-        const char *filename;
+        const char* filename;
         unsigned lineno;
         InterpretedFunctionFilenameAndLineNumber(fun, &filename, &lineno);
         op_.readType(tracked.constructor ? "constructor" : "function", buf, filename, lineno);
         return;
     }
 
-    const char *className = ty.objectKey()->clasp()->name;
+    const char* className = ty.objectKey()->clasp()->name;
     JS_snprintf(buf, bufsize, "[object %s]", className);
 
     if (tracked.hasAllocationSite()) {
-        JSScript *script = tracked.script;
+        JSScript* script = tracked.script;
         op_.readType("alloc site", buf,
                      script->maybeForwardedScriptSource()->filename(),
                      PCToLineNumber(script, script->offsetToPC(tracked.offset)));
@@ -1248,10 +1248,10 @@ IonTrackedOptimizationsTypeInfo::ForEachOpAdapter::operator()(JS::TrackedTypeSit
 }
 
 JS_PUBLIC_API(void)
-JS::ForEachTrackedOptimizationTypeInfo(JSRuntime *rt, void *addr, uint8_t index,
-                                       ForEachTrackedOptimizationTypeInfoOp &op)
+JS::ForEachTrackedOptimizationTypeInfo(JSRuntime* rt, void* addr, uint8_t index,
+                                       ForEachTrackedOptimizationTypeInfoOp& op)
 {
-    JitcodeGlobalTable *table = rt->jitRuntime()->getJitcodeGlobalTable();
+    JitcodeGlobalTable* table = rt->jitRuntime()->getJitcodeGlobalTable();
     JitcodeGlobalEntry entry;
     table->lookupInfallible(addr, &entry, rt);
     IonTrackedOptimizationsTypeInfo::ForEachOpAdapter adapter(op);
@@ -1259,9 +1259,9 @@ JS::ForEachTrackedOptimizationTypeInfo(JSRuntime *rt, void *addr, uint8_t index,
 }
 
 JS_PUBLIC_API(Maybe<uint8_t>)
-JS::TrackedOptimizationIndexAtAddr(JSRuntime *rt, void *addr, void **entryAddr)
+JS::TrackedOptimizationIndexAtAddr(JSRuntime* rt, void* addr, void** entryAddr)
 {
-    JitcodeGlobalTable *table = rt->jitRuntime()->getJitcodeGlobalTable();
+    JitcodeGlobalTable* table = rt->jitRuntime()->getJitcodeGlobalTable();
     JitcodeGlobalEntry entry;
     table->lookupInfallible(addr, &entry, rt);
     if (!entry.hasTrackedOptimizations())
@@ -1269,6 +1269,6 @@ JS::TrackedOptimizationIndexAtAddr(JSRuntime *rt, void *addr, void **entryAddr)
     uint32_t entryOffset = 0;
     Maybe<uint8_t> index = entry.trackedOptimizationIndexAtAddr(addr, &entryOffset);
     if (index.isSome())
-        *entryAddr = (void *)(((uint8_t *) entry.nativeStartAddr()) + entryOffset);
+        *entryAddr = (void*)(((uint8_t*) entry.nativeStartAddr()) + entryOffset);
     return index;
 }
