eab962f01b01e89401afe5f86e6d61fcb4847639
Riccardo Mottola <rm@gnu.org>
2020-08-25 00:51:48 +0200

    Bug 1141817 - Followup to fix additional intermittents like bug 1132370, r=vp

--- toolkit/devtools/performance/modules/front.js
+++ toolkit/devtools/performance/modules/front.js
@@ -357,8 +357,12 @@ PerformanceFront.prototype = {
    * them to consumers.
    */
   _pullAllocationSites: Task.async(function *() {
+    let isDetached = (yield this._request("memory", "getState")) !== "attached";
+    if (isDetached) {
+      return;
+    }
+
     let memoryData = yield this._request("memory", "getAllocations");
-    let isStillAttached = (yield this._request("memory", "getState")) === "attached";
 
     this.emit("allocations", {
       sites: memoryData.allocations,
@@ -367,10 +371,8 @@ PerformanceFront.prototype = {
       counts: memoryData.counts
     });
 
-    if (isStillAttached) {
-      let delay = DEFAULT_ALLOCATION_SITES_PULL_TIMEOUT;
-      this._sitesPullTimeout = setTimeout(this._pullAllocationSites, delay);
-    }
+    let delay = DEFAULT_ALLOCATION_SITES_PULL_TIMEOUT;
+    this._sitesPullTimeout = setTimeout(this._pullAllocationSites, delay);
   }),
 
   /**
