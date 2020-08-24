d5ab6dff8b032f4285b22a35a72333ecfcda8fe1
Riccardo Mottola <rm@gnu.org>
2020-08-24 16:28:22 +0200

    Bug 1146718 - Split the 'J' tag in the profiler into 'J' (JIT frames without opt info) and 'O' (with opt info). (r=djvj)

--- tools/profiler/ProfileEntry.cpp
+++ tools/profiler/ProfileEntry.cpp
@@ -337,7 +337,7 @@ void UniqueJITOptimizations::stream(JSStreamWriter& b, JSRuntime* rt)
 }
 
 void ProfileBuffer::StreamSamplesToJSObject(JSStreamWriter& b, int aThreadId, JSRuntime* rt,
-                                            UniqueJITOptimizations &aUniqueOpts)
+                                            UniqueJITOptimizations& aUniqueOpts)
 {
   b.BeginArray();
 
@@ -462,7 +462,8 @@ void ProfileBuffer::StreamSamplesToJSObject(JSStreamWriter& b, int aThreadId, JS
                       }
                       readAheadPos = (framePos + incBy) % mEntrySize;
                       if (readAheadPos != mWritePos &&
-                          mEntries[readAheadPos].mTagName == 'J') {
+                          (mEntries[readAheadPos].mTagName == 'J' ||
+                           mEntries[readAheadPos].mTagName == 'O')) {
                         void* pc = mEntries[readAheadPos].mTagPtr;
 
                         // TODOshu: cannot stream tracked optimization info if
@@ -480,11 +481,16 @@ void ProfileBuffer::StreamSamplesToJSObject(JSStreamWriter& b, int aThreadId, JS
                           // Sampled JIT optimizations are deduplicated by
                           // aUniqueOpts to save space. Stream an index that
                           // references into the optimizations array.
-                          Maybe<unsigned> optsIndex = aUniqueOpts.getIndex(pc, rt);
-                          if (optsIndex.isSome()) {
-                            b.NameValue("optsIndex", optsIndex.value());
+                          bool mightHaveTrackedOpts = mEntries[readAheadPos].mTagName == 'O';
+                          if (mightHaveTrackedOpts) {
+                            Maybe<unsigned> optsIndex = aUniqueOpts.getIndex(pc, rt);
+                            if (optsIndex.isSome()) {
+                              b.NameValue("optsIndex", optsIndex.value());
+                            }
                           }
                         }
+
+                        incBy++;
                       }
                     b.EndObject();
                   }
