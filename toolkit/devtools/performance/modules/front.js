35c015dd07354fbb4cd8848f554b3342f3adce3f
Riccardo Mottola <rm@gnu.org>
2020-08-25 21:56:58 +0200

    Bug 1147656 - Remove duplicate profiler defaults from the front end and just use on the server. r=vp

--- toolkit/devtools/performance/modules/front.js
+++ toolkit/devtools/performance/modules/front.js
@@ -297,8 +297,10 @@ PerformanceFront.prototype = {
       return profilerStatus.currentTime;
     }
 
-    // Extend the profiler options so that protocol.js doesn't modify the original.
-    let profilerOptions = extend({}, this._customProfilerOptions);
+    // If this._customProfilerOptions is defined, use those to pass in
+    // to the profiler actor. The profiler actor handles all the defaults
+    // now, so this should only be used for tests.
+    let profilerOptions = this._customProfilerOptions || {};
     yield this._request("profiler", "startProfiler", profilerOptions);
 
     this.emit("profiler-activated");
@@ -322,29 +324,51 @@ PerformanceFront.prototype = {
   }),
 
   /**
-   * Starts recording allocations in the memory actor, if necessary.
+   * Starts polling for allocations from the memory actor, if necessary.
    */
   _startMemory: Task.async(function *(options) {
     if (!options.withAllocations) {
       return 0;
     }
-    yield this._request("memory", "attach");
-    let memoryStartTime = yield this._request("memory", "startRecordingAllocations", {
-      probability: options.allocationsSampleProbability,
-      maxLogLength: options.allocationsMaxLogLength
-    });
+    let memoryStartTime = yield this._startRecordingAllocations(options);
     yield this._pullAllocationSites();
     return memoryStartTime;
   }),
 
   /**
-   * Stops recording allocations in the memory actor, if necessary.
+   * Stops polling for allocations from the memory actor, if necessary.
    */
   _stopMemory: Task.async(function *(options) {
     if (!options.withAllocations) {
       return 0;
     }
+    // Since `_pullAllocationSites` is usually running inside a timeout, and
+    // it's performing asynchronous requests to the server, a recording may
+    // be stopped before that method finishes executing. Therefore, we need to
+    // wait for the last request to `getAllocations` to finish before actually
+    // stopping recording allocations.
+    yield this._lastPullAllocationSitesFinished;
     clearTimeout(this._sitesPullTimeout);
+
+    return yield this._stopRecordingAllocations();
+  }),
+
+  /**
+   * Starts recording allocations in the memory actor.
+   */
+  _startRecordingAllocations: Task.async(function*(options) {
+    yield this._request("memory", "attach");
+    let memoryStartTime = yield this._request("memory", "startRecordingAllocations", {
+      probability: options.allocationsSampleProbability,
+      maxLogLength: options.allocationsMaxLogLength
+    });
+    return memoryStartTime;
+  }),
+
+  /**
+   * Stops recording allocations in the memory actor.
+   */
+  _stopRecordingAllocations: Task.async(function*() {
     let memoryEndTime = yield this._request("memory", "stopRecordingAllocations");
     yield this._request("memory", "detach");
     return memoryEndTime;
@@ -375,19 +399,6 @@ PerformanceFront.prototype = {
       this._sitesPullTimeout = setTimeout(this._pullAllocationSites, delay);
     }
   }),
-
-  /**
-   * Overrides the options sent to the built-in profiler module when activating,
-   * such as the maximum entries count, the sampling interval etc.
-   *
-   * Used in tests and for older backend implementations.
-   */
-  _customProfilerOptions: {
-    entries: 1000000,
-    interval: 1,
-    features: ["js"],
-    threadFilters: ["GeckoMain"]
-  },
 
   /**
    * Returns an object indicating if mock actors are being used or not.
