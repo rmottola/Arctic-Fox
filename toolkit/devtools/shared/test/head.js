61a9064cea582383e9b5c30e18c7cc7261cf22e4
Riccardo Mottola <rm@gnu.org>
2020-09-01 23:40:07 +0200

    Bug 1154115 - Fix devtools tests to use the new profiler JSON format. (r=jsantell)

--- toolkit/devtools/shared/test/head.js
+++ toolkit/devtools/shared/test/head.js
@@ -150,6 +150,24 @@ let createHost = Task.async(function*(type = "bottom", src = "data:text/html;cha
 });
 
 /**
+ * Synthesize a profile for testing.
+ */
+function synthesizeProfileForTest(samples) {
+  const { RecordingUtils } = devtools.require("devtools/performance/recording-utils");
+
+  samples.unshift({
+    time: 0,
+    frames: []
+  });
+
+  let uniqueStacks = new RecordingUtils.UniqueStacks();
+  return RecordingUtils.deflateThread({
+    samples: samples,
+    markers: []
+  }, uniqueStacks);
+}
+
+/**
  * Open and close the toolbox in the current browser tab, several times, waiting
  * some amount of time in between.
  * @param {Number} nbOfTimes
