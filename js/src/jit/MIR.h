c14a28de1ca229d0feab553208c1a661f625b38f
Riccardo Mottola <rm@gnu.org>
2020-09-15 14:06:20 +0200

    Bug 1147403 part 5 - Add Debugger::onIonCompilation hook. r=shu

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -748,9 +748,7 @@ class MDefinition : public MNode
 
     void setVirtualRegister(uint32_t vreg) {
         virtualRegister_ = vreg;
-#ifdef DEBUG
         setLoweredUnchecked();
-#endif
     }
     uint32_t virtualRegister() const {
         MOZ_ASSERT(isLowered());
