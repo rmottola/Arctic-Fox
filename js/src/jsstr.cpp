6dad527a9355abab464e0e2366223ed786d95c71
Riccardo Mottola <rm@gnu.org>
2020-10-07 18:33:12 +0200

    1156886 - Optimize toLowerCase and toUpperCase on ASCII characters. r=luke

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -612,7 +612,7 @@ ToLowerCase(JSContext* cx, JSLinearString* str)
         size_t i = 0;
         for (; i < length; i++) {
             char16_t c = chars[i];
-            if (unicode::ToLowerCase(c) != c)
+            if (unicode::CanLowerCase(c))
                 break;
         }
 
@@ -731,7 +731,7 @@ ToUpperCase(JSContext* cx, JSLinearString* str)
         size_t i = 0;
         for (; i < length; i++) {
             char16_t c = chars[i];
-            if (unicode::ToUpperCase(c) != c)
+            if (unicode::CanUpperCase(c))
                 break;
         }
 
