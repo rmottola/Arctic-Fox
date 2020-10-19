fa28f7568d0adeb8ef710ada4ca7a5015c53a2df
Riccardo Mottola <rm@gnu.org>
2020-10-19 20:37:01 +0200

    Bug 1248416 - add symbols for bad_function_call exception for C++ runtimes. r=nfroyd

--- memory/mozalloc/throw_gcc.h
+++ memory/mozalloc/throw_gcc.h
@@ -73,6 +73,12 @@ __throw_bad_typeid(void)
     mozalloc_abort("fatal: STL threw bad_typeid");
 }
 
+// used by <functional>
+MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_THROW_INLINE void
+__throw_bad_function_call(void) {
+  mozalloc_abort("fatal: STL threw bad_function_call");
+}
+
 MOZ_THROW_NORETURN MOZ_THROW_EXPORT MOZ_ALWAYS_INLINE void
 __throw_logic_error(const char* msg)
 {
