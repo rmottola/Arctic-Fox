4b76de8d8f1bd33e27b151cb5696d33dac9d31b9
Riccardo Mottola <rm@gnu.org>
2020-11-06 12:55:19 +0100

    Files as of Bug 1138721 - Application reputation should check mac file extensions. r=mmc
    Specify the download type when doing a remote lookup.

---
+++ toolkit/components/downloads/generate_csd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# A script to generate toolkit/components/downloads/csd.pb.{cc,h} for use in
+# nsIApplicationReputationQuery. This script assumes you have downloaded and
+# installed the protocol buffer compiler.
+# As of June 26 2014, csd.proto contains many protobufs that are currently
+# unused by ApplicationReputation. You may want to strip csd.proto of these
+# before running the protocol compiler on it.
+if [ -n $PROTOC_PATH ]; then
+  PROTOC_PATH=/usr/local/bin/protoc
+fi
+
+echo "Using $PROTOC_PATH as protocol compiler"
+
+if [ ! -e $PROTOC_PATH ]; then
+  echo "You must install the protocol compiler from " \
+       "https://code.google.com/p/protobuf/downloads/list"
+  exit 1
+fi
+
+# Get the protocol buffer and compile it
+CMD='wget http://src.chromium.org/chrome/trunk/src/chrome/common/safe_browsing/csd.proto -O csd.proto'
+OUTPUT_PATH=toolkit/components/downloads
+
+$CMD
+$PROTOC_PATH csd.proto --cpp_out=$OUTPUT_PATH
