1d01ee87815122046885be6101a9f5dd4edf1ef5
Riccardo Mottola <rm@gnu.org>
2020-11-09 23:51:22 +0100

    Bug 1190002 - Don't compile baseline stubs for sets to unboxed arrays when there is no JIT FPU support, r=jandem.

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -4775,6 +4775,10 @@ CanOptimizeDenseOrUnboxedArraySetElem(JSObject* obj, uint32_t index,
     if (initLength < oldInitLength || capacity < oldCapacity)
         return false;
 
+    // Unboxed arrays need to be able to emit floating point code.
+    if (obj->is<UnboxedArrayObject>() && !obj->runtimeFromMainThread()->jitSupportsFloatingPoint)
+        return false;
+
     Shape* shape = obj->maybeShape();
 
     // Cannot optimize if the shape changed.
