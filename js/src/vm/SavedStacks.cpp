dc780d45530d5de9b8dc3bef092690a0ba3d36ac
Riccardo Mottola <rm@gnu.org>
2020-11-16 14:22:37 +0100

    Bug 1167468: Assert that JSObjects and their metadata are always in the same compartment. r=fitzgen

--- js/src/vm/SavedStacks.cpp
+++ js/src/vm/SavedStacks.cpp
@@ -28,6 +28,7 @@
 #include "js/Vector.h"
 #include "vm/Debugger.h"
 #include "vm/StringBuffer.h"
+#include "vm/WrapperObject.h"
 
 #include "jscntxtinlines.h"
 
@@ -1226,6 +1227,7 @@ SavedStacksMetadataCallback(JSContext* cx, JSObject* target)
     if (!Debugger::onLogAllocationSite(cx, obj, frame, PRMJ_Now()))
         CrashAtUnhandlableOOM("SavedStacksMetadataCallback");
 
+    MOZ_ASSERT_IF(frame, !frame->is<WrapperObject>());
     return frame;
 }
 
