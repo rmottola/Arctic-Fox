bd23dafcee230f163576cdbec65d295a056b31c2
Riccardo Mottola <rm@gnu.org>
2020-11-16 14:51:16 +0100

    Bug 1172138 - Call PR_LogInit before profiler_init. r=froydnj

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -447,6 +447,8 @@ NS_InitXPCOM2(nsIServiceManager** aResult,
 
   mozPoisonValueInit();
 
+  NS_LogInit();
+
   char aLocal;
   profiler_init(&aLocal);
   nsresult rv = NS_OK;
@@ -468,8 +470,6 @@ NS_InitXPCOM2(nsIServiceManager** aResult,
   nsSystemInfo::gUserUmask = ::umask(0777);
   ::umask(nsSystemInfo::gUserUmask);
 #endif
-
-  NS_LogInit();
 
   // Set up chromium libs
   NS_ASSERTION(!sExitManager && !sMessageLoop, "Bad logic!");
