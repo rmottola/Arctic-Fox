be105dff449a9d118e96709ecb5506d66fa84490
Riccardo Mottola <rm@gnu.org>
2020-11-17 02:39:36 +0100

    Bug 1182730 - Followup: only mark the JitcodeGlobalMap when profiling is on. (r=djvj)

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -621,7 +621,8 @@ JitRuntime::Mark(JSTracer* trc)
 /* static */ void
 JitRuntime::MarkJitcodeGlobalTableUnconditionally(JSTracer* trc)
 {
-    if (trc->runtime()->hasJitRuntime() &&
+    if (trc->runtime()->spsProfiler.enabled() &&
+        trc->runtime()->hasJitRuntime() &&
         trc->runtime()->jitRuntime()->hasJitcodeGlobalTable())
     {
         trc->runtime()->jitRuntime()->getJitcodeGlobalTable()->markUnconditionally(trc);
@@ -631,7 +632,8 @@ JitRuntime::MarkJitcodeGlobalTableUnconditionally(JSTracer* trc)
 /* static */ bool
 JitRuntime::MarkJitcodeGlobalTableIteratively(JSTracer* trc)
 {
-    if (trc->runtime()->hasJitRuntime() &&
+    if (trc->runtime()->spsProfiler.enabled() &&
+        trc->runtime()->hasJitRuntime() &&
         trc->runtime()->jitRuntime()->hasJitcodeGlobalTable())
     {
         return trc->runtime()->jitRuntime()->getJitcodeGlobalTable()->markIteratively(trc);
