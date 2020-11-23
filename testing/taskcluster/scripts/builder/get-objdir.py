ffe9814ca97497616c13737e2872bebf4bd4312f
Riccardo Mottola <rm@gnu.org>
2020-11-23 20:15:57 +0100

    Bug 1132562 - Use workspace directory so autoclobber can function with caches r=wcosta
     - Move builder scripts into tree proper instead of baked into docker images

--- testing/taskcluster/scripts/builder/get-objdir.py
+++ testing/taskcluster/scripts/builder/get-objdir.py
@@ -7,16 +7,14 @@ import json
 import subprocess
 from StringIO import StringIO
 
-DEFAULT_OBJDIR = "/home/worker/object-folder"
-
-goanna_dir = sys.argv[1]
-os.chdir(goanna_dir)
+gecko_dir = sys.argv[1]
+os.chdir(gecko_dir)
 
 result = subprocess.check_output(["./mach", "environment", "--format", "json"])
 environment = json.load(StringIO(result))
 
 topobjdir = environment["mozconfig"]["topobjdir"]
 if topobjdir is None:
-    topobjdir = DEFAULT_OBJDIR
+    topobjdir = sys.argv[2]
 
 print(topobjdir)
