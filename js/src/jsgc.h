d95e1726642331c396ad8928d999cb24434d767c
Riccardo Mottola <rm@gnu.org>
2020-11-25 23:53:24 +0100

    Bug 1151981 - Properly check for MSVC (mingw fixup).

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -187,7 +187,7 @@ CanBeFinalizedInBackground(AllocKind kind, const Class* clasp)
 // GCC and Clang require an explicit template declaration in front of the
 // specialization of operator() because it is a dependent template. MSVC, on
 // the other hand, gets very confused if we have a |template| token there.
-#ifdef XP_WIN
+#ifdef _MSC_VER
 # define DEPENDENT_TEMPLATE_HINT
 #else
 # define DEPENDENT_TEMPLATE_HINT template
