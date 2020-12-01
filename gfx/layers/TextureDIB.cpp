f78a07160ee9ccc320a8c80deb6644420c74f719
Riccardo Mottola <rm@gnu.org>
2020-12-01 23:42:20 +0100

    Bug 1133007 - Send the content of a texture only if its was altered since previous transmission. r=kamidphish, r=boris

--- gfx/layers/TextureDIB.cpp
+++ gfx/layers/TextureDIB.cpp
@@ -298,7 +298,7 @@ DIBTextureHost::DIBTextureHost(TextureFlags aFlags,
 }
 
 void
-DIBTextureHost::Updated(const nsIntRegion* aRegion)
+DIBTextureHost::UpdatedInternal(const nsIntRegion* aRegion)
 {
   if (!mCompositor) {
     // This can happen if we send textures to a compositable that isn't yet
@@ -332,7 +332,7 @@ TextureHostFileMapping::~TextureHostFileMapping()
 }
 
 void
-TextureHostFileMapping::Updated(const nsIntRegion* aRegion)
+TextureHostFileMapping::UpdatedInternal(const nsIntRegion* aRegion)
 {
   if (!mCompositor) {
     // This can happen if we send textures to a compositable that isn't yet
