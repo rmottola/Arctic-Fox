720d3b3b448fe0e585aa065faabf53c47fabf0e8
Riccardo Mottola <rm@gnu.org>
2020-12-07 21:07:32 +0100

    Bug 1158089 - Remove LAYERS_D3D10 enum value since it's unused. r=Bas

--- dom/media/wmf/WMFReader.cpp
+++ dom/media/wmf/WMFReader.cpp
@@ -104,7 +104,6 @@ WMFReader::InitializeDXVA()
 
   LayersBackend backend = layerManager->GetCompositorBackendType();
   if (backend != LayersBackend::LAYERS_D3D9 &&
-      backend != LayersBackend::LAYERS_D3D10 &&
       backend != LayersBackend::LAYERS_D3D11) {
     return false;
   }
