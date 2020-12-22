7dc6bd220a2dd7bf87a447a4674a14a61e743522
Riccardo Mottola <rm@gnu.org>
2020-12-22 21:59:23 +0100

    Bug 1159433 - Part 4: Mark nsTAutoString_CharT and nsAutoArrayBase as non-memmovable; r=froydnj

--- xpcom/glue/nsTArray.h
+++ xpcom/glue/nsTArray.h
@@ -2128,7 +2128,7 @@ public:
 // You shouldn't use this class directly.
 //
 template<class TArrayBase, size_t N>
-class nsAutoArrayBase : public TArrayBase
+class MOZ_NON_MEMMOVABLE nsAutoArrayBase : public TArrayBase
 {
   static_assert(N != 0, "nsAutoArrayBase<TArrayBase, 0> should be specialized");
 public:
