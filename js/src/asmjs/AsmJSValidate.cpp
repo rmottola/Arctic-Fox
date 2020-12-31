b299ad8ef2adfc0daba02149b3a6bb547c802d1b
Riccardo Mottola <rm@gnu.org>
2020-12-31 21:45:19 +0100

    Bug 1184965 part 1 - Factor MacroAssembler::callJit. r=bbouvier

--- js/src/asmjs/AsmJSValidate.cpp
+++ js/src/asmjs/AsmJSValidate.cpp
@@ -11407,7 +11407,7 @@ GenerateFFIIonExit(ModuleCompiler& m, const ModuleCompiler::ExitDescriptor& exit
     }
 
     AssertStackAlignment(masm, JitStackAlignment, sizeOfRetAddr);
-    masm.callJitFromAsmJS(callee);
+    masm.callJitNoProfiler(callee);
     AssertStackAlignment(masm, JitStackAlignment, sizeOfRetAddr);
 
     {
