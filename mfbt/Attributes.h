49ecade5d2c95dd8d22c2072c53c1b9a771178a4
Riccardo Mottola <rm@gnu.org>
2021-01-01 19:11:25 +0100

    remove obsolete MOZ_MUST_USE stuff

--- mfbt/Attributes.h
+++ mfbt/Attributes.h
@@ -289,26 +289,6 @@
 #endif
 
 /**
- * MOZ_MUST_USE tells the compiler to emit a warning if a function's
- * return value is not used by the caller.
- *
- * Place this attribute at the very beginning of a function declaration. For
- * example, write
- *
- *   MOZ_MUST_USE int foo();
- *
- * or
- *
- *   MOZ_MUST_USE int foo() { return 42; }
- */
-#if defined(__GNUC__) || defined(__clang__)
-#  define MOZ_MUST_USE __attribute__ ((warn_unused_result))
-#else
-#  define MOZ_MUST_USE
-#endif
-
-
-/**
  * MOZ_ALLOCATOR tells the compiler that the function it marks returns either a
  * "fresh", "pointer-free" block of memory, or nullptr. "Fresh" means that the
  * block is not pointed to by any other reachable pointer in the program.
