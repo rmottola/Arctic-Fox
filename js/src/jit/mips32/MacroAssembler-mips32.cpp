acf17d8e02240f58cdf06d202ee49c809386b7bb
Riccardo Mottola <rm@gnu.org>
2021-01-04 10:26:27 +0000

    Bug 1195163 - IonMonkey: MIPS32: Fix boolValueToDouble and boolValueToFloat32. r=rankov

--- js/src/jit/mips32/MacroAssembler-mips32.cpp
+++ js/src/jit/mips32/MacroAssembler-mips32.cpp
@@ -2453,7 +2453,7 @@ MacroAssemblerMIPSCompat::boxNonDouble(JSValueType type, Register src,
 void
 MacroAssemblerMIPSCompat::boolValueToDouble(const ValueOperand& operand, FloatRegister dest)
 {
-    convertBoolToInt32(ScratchRegister, operand.payloadReg());
+    convertBoolToInt32(operand.payloadReg(), ScratchRegister);
     convertInt32ToDouble(ScratchRegister, dest);
 }
 
@@ -2469,7 +2469,7 @@ MacroAssemblerMIPSCompat::boolValueToFloat32(const ValueOperand& operand,
                                              FloatRegister dest)
 {
 
-    convertBoolToInt32(ScratchRegister, operand.payloadReg());
+    convertBoolToInt32(operand.payloadReg(), ScratchRegister);
     convertInt32ToFloat32(ScratchRegister, dest);
 }
 
