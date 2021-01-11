c4ea9cf33652529c9d9c4e178373fd648796eb50
Riccardo Mottola <rm@gnu.org>
2021-01-11 21:14:29 +0000

    Bug 1166598 (part 3) - Use PLDHashTable2 in nsCommandParams. r=froydnj.

--- embedding/components/commandhandler/nsCommandParams.h
+++ embedding/components/commandhandler/nsCommandParams.h
@@ -20,8 +20,6 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSICOMMANDPARAMS
 
-  nsresult Init();
-
 protected:
   virtual ~nsCommandParams();
 
@@ -129,7 +127,7 @@ protected:
 
   static void HashClearEntry(PLDHashTable* aTable, PLDHashEntryHdr* aEntry);
 
-  PLDHashTable mValuesHash;
+  PLDHashTable2 mValuesHash;
 
   static const PLDHashTableOps sHashOps;
 };
