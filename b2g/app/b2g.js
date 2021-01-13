570fc0e984c9019db719dad884d12f927851dd6c
Riccardo Mottola <rm@gnu.org>
2021-01-13 19:23:24 +0100

    Bug 1037329 - Part 2: Enable SystemUpdate API on b2g. r=fabrice

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -543,6 +543,9 @@ pref("app.update.socket.maxErrors", 20);
 // Enable update logging for now, to diagnose growing pains in the
 // field.
 pref("app.update.log", true);
+
+// SystemUpdate API
+pref("dom.system_update.active", "@mozilla.org/updates/update-prompt;1");
 #else
 // Explicitly disable the shutdown watchdog.  It's enabled by default.
 // When the updater is disabled, we want to know about shutdown hangs.
@@ -1033,6 +1036,9 @@ pref("dom.mapped_arraybuffer.enabled", true);
 
 // BroadcastChannel API
 pref("dom.broadcastChannel.enabled", true);
+
+// SystemUpdate API
+pref("dom.system_update.enabled", true);
 
 // UDPSocket API
 pref("dom.udpsocket.enabled", true);
