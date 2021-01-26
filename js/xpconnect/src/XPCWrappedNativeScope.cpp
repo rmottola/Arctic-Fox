0503b28cf1cfd1030e342b4bc6860d20f51b8f96
Riccardo Mottola <rm@gnu.org>
2021-01-26 18:19:08 +0100

    Bug 1174594 (part 3) - Remove ShutdownData. r=mrbkap.
    It's written to but never read from.

--- js/xpconnect/src/XPCWrappedNativeScope.cpp
+++ js/xpconnect/src/XPCWrappedNativeScope.cpp
@@ -618,22 +618,11 @@ XPCWrappedNativeScope::KillDyingScopes()
     gDyingScopes = nullptr;
 }
 
-struct ShutdownData
-{
-    ShutdownData()
-        : wrapperCount(0),
-          protoCount(0) {}
-    int wrapperCount;
-    int protoCount;
-};
-
 //static
 void
 XPCWrappedNativeScope::SystemIsBeingShutDown()
 {
     int liveScopeCount = 0;
-
-    ShutdownData data;
 
     XPCWrappedNativeScope* cur;
 
@@ -663,7 +652,6 @@ XPCWrappedNativeScope::SystemIsBeingShutDown()
         for (auto i = cur->mWrappedNativeProtoMap->RemovingIter(); !i.Done(); i.Next()) {
             auto entry = static_cast<ClassInfo2WrappedNativeProtoMap::Entry*>(i.Get());
             entry->value->SystemIsBeingShutDown();
-            data.protoCount++;
             i.Remove();
         }
         for (auto i = cur->mWrappedNativeMap->RemovingIter(); !i.Done(); i.Next()) {
@@ -671,7 +659,6 @@ XPCWrappedNativeScope::SystemIsBeingShutDown()
             XPCWrappedNative* wrapper = entry->value;
             if (wrapper->IsValid()) {
                 wrapper->SystemIsBeingShutDown();
-                data.wrapperCount++;
             }
             i.Remove();
         }
