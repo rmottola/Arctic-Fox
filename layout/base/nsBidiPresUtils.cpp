3ed20e1afcafd39d3912797c77b10a5cc1ee6511
Riccardo Mottola <rm@gnu.org>
2021-01-27 15:39:53 +0100

    Bug 1149009 - Suppress line break inside text frame if it is directly contained by ruby content box. r=dbaron

--- layout/base/nsBidiPresUtils.cpp
+++ layout/base/nsBidiPresUtils.cpp
@@ -1051,7 +1051,8 @@ nsBidiPresUtils::TraverseFrames(nsBlockFrame*              aBlockFrame,
       if (nsGkAtoms::textFrame == frameType) {
         if (content != aBpd->mPrevContent) {
           aBpd->mPrevContent = content;
-          if (!frame->StyleText()->NewlineIsSignificant(frame)) {
+          if (!frame->StyleText()->NewlineIsSignificant(
+                static_cast<nsTextFrame*>(frame))) {
             content->AppendTextTo(aBpd->mBuffer);
           } else {
             /*
