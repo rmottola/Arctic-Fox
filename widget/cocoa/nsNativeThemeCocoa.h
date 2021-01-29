60ecc9d4f231d733c4a2d1eb8f79ee118b063deb
Riccardo Mottola <rm@gnu.org>
2021-01-29 21:48:21 +0100

    Bug 889085 - Make sheets styled with '-moz-appearance: dialog;' vibrant/semi-transparent. r=mstange, roc.

--- widget/cocoa/nsNativeThemeCocoa.h
+++ widget/cocoa/nsNativeThemeCocoa.h
@@ -38,6 +38,7 @@ public:
     eThemeGeometryTypeVibrancyLight,
     eThemeGeometryTypeVibrancyDark,
     eThemeGeometryTypeTooltip,
+    eThemeGeometryTypeSheet,
   };
 
   nsNativeThemeCocoa();
@@ -74,7 +75,8 @@ public:
   bool ThemeDrawsFocusForWidget(uint8_t aWidgetType) override;
   bool ThemeNeedsComboboxDropmarker() override;
   virtual bool WidgetAppearanceDependsOnWindowFocus(uint8_t aWidgetType) override;
-  virtual bool NeedToClearBackgroundBehindWidget(uint8_t aWidgetType) override;
+  virtual bool NeedToClearBackgroundBehindWidget(nsIFrame* aFrame,
+                                                 uint8_t aWidgetType) override;
   virtual bool WidgetProvidesFontSmoothingBackgroundColor(nsIFrame* aFrame, uint8_t aWidgetType,
                                                           nscolor* aColor) override;
   virtual ThemeGeometryType ThemeGeometryTypeForWidget(nsIFrame* aFrame,
@@ -95,6 +97,7 @@ protected:
   nsIFrame* SeparatorResponsibility(nsIFrame* aBefore, nsIFrame* aAfter);
   CGRect SeparatorAdjustedRect(CGRect aRect, nsIFrame* aLeft,
                                nsIFrame* aCurrent, nsIFrame* aRight);
+  bool IsWindowSheet(nsIFrame* aFrame);
 
   // HITheme drawing routines
   void DrawFrame(CGContextRef context, HIThemeFrameKind inKind,
