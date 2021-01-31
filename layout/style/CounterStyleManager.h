331397d0178b1845fbdb7e7e30ee460ea68e9a03
Riccardo Mottola <rm@gnu.org>
2021-01-31 19:05:28 +0100

    Bug 1144607 part 1 - Remove CounterStyleManager::BuildCounterStyle for anonymous counter styles. r=dbaron

--- layout/style/CounterStyleManager.h
+++ layout/style/CounterStyleManager.h
@@ -156,7 +156,6 @@ public:
   }
 
   CounterStyle* BuildCounterStyle(const nsSubstring& aName);
-  CounterStyle* BuildCounterStyle(const nsCSSValue::Array* aParams);
 
   static CounterStyle* GetBuiltinStyle(int32_t aStyle);
   static CounterStyle* GetNoneStyle()
