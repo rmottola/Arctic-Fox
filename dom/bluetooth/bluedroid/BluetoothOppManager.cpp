ed2a7922328b7727a4c083d8528372d45d60276e
Riccardo Mottola <rm@gnu.org>
2021-02-08 21:35:14 +0100

    Bug 1146355: Prepare Bluetooth OPP manager, r=joliu

--- dom/bluetooth/bluedroid/BluetoothOppManager.cpp
+++ dom/bluetooth/bluedroid/BluetoothOppManager.cpp
@@ -88,11 +88,16 @@ BluetoothOppManager::Observe(nsISupports* aSubject,
                              const char16_t* aData)
 {
   MOZ_ASSERT(sBluetoothOppManager);
+
+#ifdef MOZ_B2G_BT_API_V2
+  // Removed in bluetooth2
+#else
   // if state of any volume was changed
   if (!strcmp(aTopic, NS_VOLUME_STATE_CHANGED)) {
     HandleVolumeStateChanged(aSubject);
-    return NS_OK; 
+    return NS_OK;
   }
+#endif
 
   if (!strcmp(aTopic, NS_XPCOM_SHUTDOWN_OBSERVER_ID)) {
     HandleShutdown();
@@ -221,9 +226,13 @@ BluetoothOppManager::~BluetoothOppManager()
     BT_WARNING("Failed to remove shutdown observer!");
   }
 
+#ifdef MOZ_B2G_BT_API_V2
+  // Removed in bluetooth2
+#else
   if (NS_FAILED(obs->RemoveObserver(this, NS_VOLUME_STATE_CHANGED))) {
     BT_WARNING("Failed to remove volume observer!");
   }
+#endif
 }
 
 bool
@@ -236,10 +245,14 @@ BluetoothOppManager::Init()
     return false;
   }
 
+#ifdef MOZ_B2G_BT_API_V2
+  // Removed in bluetooth2
+#else
   if (NS_FAILED(obs->AddObserver(this, NS_VOLUME_STATE_CHANGED, false))) {
     BT_WARNING("Failed to add ns volume observer!");
     return false;
   }
+#endif
 
   /**
    * We don't start listening here as BluetoothServiceBluedroid calls Listen()
@@ -308,6 +321,9 @@ BluetoothOppManager::HandleShutdown()
   sBluetoothOppManager = nullptr;
 }
 
+#ifdef MOZ_B2G_BT_API_V2
+  // Removed in bluetooth2
+#else
 void
 BluetoothOppManager::HandleVolumeStateChanged(nsISupports* aSubject)
 {
@@ -341,6 +357,7 @@ BluetoothOppManager::HandleVolumeStateChanged(nsISupports* aSubject)
     Disconnect(nullptr);
   }
 }
+#endif
 
 bool
 BluetoothOppManager::Listen()
