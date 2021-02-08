bbcf9b61753d0121804fe0f8498c7d82a0697966
Riccardo Mottola <rm@gnu.org>
2021-02-08 17:08:18 +0100

    Bug 1147156 - Re-enable e10s by default for Nightly users who may have disabled it. r=poirot,mconley

--- browser/app/profile/arcticfox.js
+++ browser/app/profile/arcticfox.js
@@ -1305,5 +1305,9 @@ pref("status4evar.status.popup.invertMirror", false);
 pref("status4evar.status.popup.mouseMirror", true);
 
 #ifdef NIGHTLY_BUILD
-pref("browser.tabs.remote.autostart.1", true);
+// At the moment, autostart.2 is used, while autostart.1 is unused.
+// We leave it here set to false to reset users' defaults and allow
+// us to change everybody to true in the future, when desired.
+pref("browser.tabs.remote.autostart.1", false);
+pref("browser.tabs.remote.autostart.2", true);
 #endif
