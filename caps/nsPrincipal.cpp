73ef73a920734262cfb5f025648d08802cb14aa6
Riccardo Mottola <rm@gnu.org>
2021-02-10 00:37:44 +0100

    Bug 1164292 - Twiddle format of GetScriptLocation to match what we use for GetOrigin. r=gabor

--- caps/nsPrincipal.cpp
+++ caps/nsPrincipal.cpp
@@ -925,9 +925,7 @@ nsExpandedPrincipal::IsOnCSSUnprefixingWhitelist()
 void
 nsExpandedPrincipal::GetScriptLocation(nsACString& aStr)
 {
-  aStr.Assign(EXPANDED_PRINCIPAL_SPEC);
-  aStr.AppendLiteral(" (");
-
+  aStr.Assign("[Expanded Principal [");
   for (size_t i = 0; i < mPrincipals.Length(); ++i) {
     if (i != 0) {
       aStr.AppendLiteral(", ");
@@ -939,7 +937,7 @@ nsExpandedPrincipal::GetScriptLocation(nsACString& aStr)
     aStr.Append(spec);
 
   }
-  aStr.Append(")");
+  aStr.Append("]]");
 }
 
 //////////////////////////////////////////
