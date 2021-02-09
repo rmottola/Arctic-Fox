a81e4ed81e9bf6a12b7e7332dcdf925ebb8d5ed1
Riccardo Mottola <rm@gnu.org>
2021-02-09 16:26:37 +0100

    Bug 1158818: Only store Bluetooth result runnable after command has been sent successfully, r=shuang
    With the current code, the Bluetooth result runnable is saved for
    receiving before a command has been sent successfully. If sending
    fails afterwards, the saved result runnable will still sit in the
    result queue, and interfere with later, successful commands.
    
    With this patch, the result runnable is saved only if the sending
    was successful.

--- dom/bluetooth/bluedroid/BluetoothDaemonInterface.cpp
+++ dom/bluetooth/bluedroid/BluetoothDaemonInterface.cpp
@@ -1509,6 +1509,7 @@ BluetoothDaemonProtocol::Send(BluetoothDaemonPDU* aPDU, void* aUserData)
   MOZ_ASSERT(mConnection);
   MOZ_ASSERT(aPDU);
 
+  aPDU->SetConsumer(this);
   aPDU->SetUserData(aUserData);
   aPDU->UpdateHeader();
   return mConnection->Send(aPDU); // Forward PDU to command channel
