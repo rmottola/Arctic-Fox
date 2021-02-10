1067d0fbaed401de8b52ec22ac3c519fd1906cce
Riccardo Mottola <rm@gnu.org>
2021-02-10 15:36:35 +0100

    Bug 1173525 - Add JS::Construct overload for specifying new.target. (r=Waldo)

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -3964,6 +3964,12 @@ extern JS_PUBLIC_API(bool)
 Construct(JSContext* cx, JS::HandleValue fun,
           const JS::HandleValueArray& args,
           MutableHandleValue rval);
+
+extern JS_PUBLIC_API(bool)
+Construct(JSContext* cx, JS::HandleValue fun,
+          HandleObject newTarget, const JS::HandleValueArray &args,
+          MutableHandleValue rval);
+
 } /* namespace JS */
 
 extern JS_PUBLIC_API(bool)
