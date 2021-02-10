9ea0b73e030da3430ef3f9171f713f0a42756dbd
Riccardo Mottola <rm@gnu.org>
2021-02-10 20:47:22 +0100

    Bug 1147967 - Remove CreateStackFrameLocation. r=bholley
    This method is defined on nsIXPConnect and StackFrame, but is not used.
    
    Also the non-default constructor that for StackFrame that is no longer used.

--- dom/bindings/Exceptions.cpp
+++ dom/bindings/Exceptions.cpp
@@ -218,12 +218,6 @@ public:
   NS_DECL_CYCLE_COLLECTION_CLASS(StackFrame)
   NS_DECL_NSISTACKFRAME
 
-  StackFrame(uint32_t aLanguage,
-             const char* aFilename,
-             const char* aFunctionName,
-             int32_t aLineNumber,
-             nsIStackFrame* aCaller);
-
   StackFrame()
     : mLineno(0)
     , mColNo(0)
@@ -231,12 +225,6 @@ public:
   {
   }
 
-  static already_AddRefed<nsIStackFrame>
-  CreateStackFrameLocation(uint32_t aLanguage,
-                           const char* aFilename,
-                           const char* aFunctionName,
-                           int32_t aLineNumber,
-                           nsIStackFrame* aCaller);
 protected:
   virtual ~StackFrame();
 
@@ -264,19 +252,6 @@ protected:
   int32_t mColNo;
   uint32_t mLanguage;
 };
-
-StackFrame::StackFrame(uint32_t aLanguage,
-                       const char* aFilename,
-                       const char* aFunctionName,
-                       int32_t aLineNumber,
-                       nsIStackFrame* aCaller)
-  : mCaller(aCaller)
-  , mLineno(aLineNumber)
-  , mLanguage(aLanguage)
-{
-  CopyUTF8toUTF16(aFilename, mFilename);
-  CopyUTF8toUTF16(aFunctionName, mFunname);
-}
 
 StackFrame::~StackFrame()
 {
@@ -857,34 +832,10 @@ JSStackFrame::CreateStack(JSContext* aCx, int32_t aMaxDepth)
   return first.forget();
 }
 
-/* static */ already_AddRefed<nsIStackFrame>
-StackFrame::CreateStackFrameLocation(uint32_t aLanguage,
-                                     const char* aFilename,
-                                     const char* aFunctionName,
-                                     int32_t aLineNumber,
-                                     nsIStackFrame* aCaller)
-{
-  nsRefPtr<StackFrame> self =
-    new StackFrame(aLanguage, aFilename, aFunctionName, aLineNumber, aCaller);
-  return self.forget();
-}
-
 already_AddRefed<nsIStackFrame>
 CreateStack(JSContext* aCx, int32_t aMaxDepth)
 {
   return JSStackFrame::CreateStack(aCx, aMaxDepth);
-}
-
-already_AddRefed<nsIStackFrame>
-CreateStackFrameLocation(uint32_t aLanguage,
-                         const char* aFilename,
-                         const char* aFunctionName,
-                         int32_t aLineNumber,
-                         nsIStackFrame* aCaller)
-{
-  return StackFrame::CreateStackFrameLocation(aLanguage, aFilename,
-                                              aFunctionName, aLineNumber,
-                                              aCaller);
 }
 
 } // namespace exceptions
