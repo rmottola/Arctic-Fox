559bfa7a5245f672235834334bd88be1799a7261
Riccardo Mottola <rm@gnu.org>
2021-02-15 14:39:18 +0100

    Bug 1161183: Don't show the add-on version in the list view. r=dao

--- toolkit/mozapps/extensions/test/browser/browser_bug596336.js
+++ toolkit/mozapps/extensions/test/browser/browser_bug596336.js
@@ -8,19 +8,12 @@
 var gManagerWindow;
 var gCategoryUtilities;
 
-function test() {
+add_task(function* test() {
   waitForExplicitFinish();
 
-  open_manager("addons://list/extension", function(aWindow) {
-    gManagerWindow = aWindow;
-    gCategoryUtilities = new CategoryUtilities(gManagerWindow);
-    run_next_test();
-  });
-}
-
-function end_test() {
-  close_manager(gManagerWindow, finish);
-}
+  gManagerWindow = yield open_manager("addons://list/extension");
+  gCategoryUtilities = new CategoryUtilities(gManagerWindow);
+});
 
 function get_list_item_count() {
   return get_test_items_in_list(gManagerWindow).length;
@@ -34,21 +27,23 @@ function get_class_node(parent, cls) {
   return parent.ownerDocument.getAnonymousElementByAttribute(parent, "class", cls);
 }
 
-function install_addon(aXpi, aCallback) {
-  AddonManager.getInstallForURL(TESTROOT + "addons/" + aXpi + ".xpi",
-                                function(aInstall) {
-    aInstall.addListener({
-      onInstallEnded: function(aInstall) {
-        executeSoon(aCallback);
-      }
-    });
-    aInstall.install();
-  }, "application/x-xpinstall");
+function install_addon(aXpi) {
+  return new Promise(resolve => {
+    AddonManager.getInstallForURL(TESTROOT + "addons/" + aXpi + ".xpi",
+                                  function(aInstall) {
+      aInstall.addListener({
+        onInstallEnded: function(aInstall) {
+          resolve();
+        }
+      });
+      aInstall.install();
+    }, "application/x-xpinstall");
+  });
 }
 
-function check_addon(aAddon, version) {
+let check_addon = Task.async(function*(aAddon, aVersion) {
   is(get_list_item_count(), 1, "Should be one item in the list");
-  is(aAddon.version, version, "Add-on should have the right version");
+  is(aAddon.version, aVersion, "Add-on should have the right version");
 
   let item = get_addon_element(gManagerWindow, "addon1@tests.mozilla.org");
   ok(!!item, "Should see the add-on in the list");
@@ -147,34 +142,33 @@ add_test(function() {
 
 // Install version 1, disable it, click the remove button and then upgrade to
 // version 2 with the manager open
-add_test(function() {
-  install_addon("browser_bug596336_1", function() {
-    AddonManager.getAddonByID("addon1@tests.mozilla.org", function(aAddon) {
-      aAddon.userDisabled = true;
-      check_addon(aAddon, "1.0");
-      ok(aAddon.userDisabled, "Add-on should be disabled");
+add_task(function() {
+  yield install_addon("browser_bug596336_1");
+  let [aAddon] = yield promiseAddonsByIDs(["addon1@tests.mozilla.org"]);
+  aAddon.userDisabled = true;
+  yield check_addon(aAddon, "1.0");
+  ok(aAddon.userDisabled, "Add-on should be disabled");
 
-      let item = get_addon_element(gManagerWindow, "addon1@tests.mozilla.org");
-      EventUtils.synthesizeMouseAtCenter(get_node(item, "remove-btn"), { }, gManagerWindow);
+  let item = get_addon_element(gManagerWindow, "addon1@tests.mozilla.org");
+  EventUtils.synthesizeMouseAtCenter(get_node(item, "remove-btn"), { }, gManagerWindow);
 
-      // Force XBL to apply
-      item.clientTop;
+  // Force XBL to apply
+  item.clientTop;
 
-      ok(!!(aAddon.pendingOperations & AddonManager.PENDING_UNINSTALL), "Add-on should be pending uninstall");
-      is_element_visible(get_class_node(item, "pending"), "Pending message should be visible");
+  ok(aAddon.userDisabled, "Add-on should be disabled");
+  ok(!aAddon.pendingUninstall, "Add-on should not be pending uninstall");
+  is_element_visible(get_class_node(item, "pending"), "Pending message should be visible");
 
-      install_addon("browser_bug596336_2", function() {
-        AddonManager.getAddonByID("addon1@tests.mozilla.org", function(aAddon) {
-          check_addon(aAddon, "2.0");
-          ok(aAddon.userDisabled, "Add-on should be disabled");
+  yield install_addon("browser_bug596336_2");
+  [aAddon] = yield promiseAddonsByIDs(["addon1@tests.mozilla.org"]);
+  yield check_addon(aAddon, "2.0");
+  ok(aAddon.userDisabled, "Add-on should be disabled");
 
-          aAddon.uninstall();
+  aAddon.uninstall();
 
-          is(get_list_item_count(), 0, "Should be no items in the list");
+  is(get_list_item_count(), 0, "Should be no items in the list");
+});
 
-          run_next_test();
-        });
-      });
-    });
-  });
+add_task(function end_test() {
+  close_manager(gManagerWindow, finish);
 });
