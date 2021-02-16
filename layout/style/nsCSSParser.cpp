d4103b7991647abc3da57dafe43a1edecbbd6531
Riccardo Mottola <rm@gnu.org>
2021-02-16 11:50:54 +0100

    Bug 1152033 - Expose CSS lexer to js. r=heycam, r=bz

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -2743,7 +2743,9 @@ CSSParserImpl::GetToken(bool aSkipWS)
       return true;
     }
   }
-  return mScanner->Next(mToken, aSkipWS);
+  return mScanner->Next(mToken, aSkipWS ?
+                        eCSSScannerExclude_WhitespaceAndComments :
+                        eCSSScannerExclude_Comments);
 }
 
 void
