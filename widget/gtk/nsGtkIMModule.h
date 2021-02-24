f4c677871d303d2235edde9509acf72e2312b29a
Riccardo Mottola <rm@gnu.org>
2021-02-24 17:01:05 +0100

    Bug 1143197 part.1 Use current IM context at handling key events rather than active IM context r=m_kato

--- widget/gtk/nsGtkIMModule.h
+++ widget/gtk/nsGtkIMModule.h
@@ -156,9 +156,19 @@ protected:
     };
     eCompositionState mCompositionState;
 
-    bool IsComposing()
+    bool IsComposing() const
     {
         return (mCompositionState != eCompositionState_NotComposing);
+    }
+
+    bool IsComposingOn(GtkIMContext* aContext) const
+    {
+        return IsComposing() && mComposingContext == aContext;
+    }
+
+    bool IsComposingOnCurrentContext() const
+    {
+        return IsComposingOn(GetCurrentContext());
     }
 
     bool EditorHasCompositionString()
