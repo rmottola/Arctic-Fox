6978581c72b248d4c1f5592fdd6c19f6395ea21c
Riccardo Mottola <rm@gnu.org>
2021-03-01 09:30:01 +0100

    Bug 1176954 part.7 IMENotification::TextChangeData should have a method to merge with another instance and IMENotification::MergeWith() should use it r=smaug

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -665,16 +665,7 @@ struct IMENotification
         break;
       case NOTIFY_IME_OF_TEXT_CHANGE:
         MOZ_ASSERT(aNotification.mMessage == NOTIFY_IME_OF_TEXT_CHANGE);
-        // TODO: Needs to merge the ranges rather than overwriting.
-        mTextChangeData.mStartOffset =
-          aNotification.mTextChangeData.mStartOffset;
-        mTextChangeData.mRemovedEndOffset =
-          aNotification.mTextChangeData.mRemovedEndOffset;
-        mTextChangeData.mAddedEndOffset =
-          aNotification.mTextChangeData.mAddedEndOffset;
-        mTextChangeData.mCausedByComposition =
-          mTextChangeData.mCausedByComposition &&
-            aNotification.mTextChangeData.mCausedByComposition;
+        mTextChangeData += aNotification.mTextChangeData;
         break;
       case NOTIFY_IME_OF_COMPOSITION_UPDATE:
         MOZ_ASSERT(aNotification.mMessage == NOTIFY_IME_OF_COMPOSITION_UPDATE);
@@ -768,6 +759,17 @@ struct IMENotification
       mStartOffset = UINT32_MAX;
       mRemovedEndOffset = mAddedEndOffset = 0;
     }
+
+    void MergeWith(const TextChangeDataBase& aOther);
+    TextChangeDataBase& operator+=(const TextChangeDataBase& aOther)
+    {
+      MergeWith(aOther);
+      return *this;
+    }
+
+#ifdef DEBUG
+    void Test();
+#endif // #ifdef DEBUG
   };
 
   // TextChangeDataBase cannot have constructors because they are used in union.
