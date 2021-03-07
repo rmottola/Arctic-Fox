f0eb9653b848f9f725feb91dde8dd5e59b11c0db
Riccardo Mottola <rm@gnu.org>
2021-03-07 10:13:49 +0100

    Bug 1169514 - Part 3: Use a shared preference style sheet across all pres contexts. r=jwatt

--- layout/base/nsPresShell.h
+++ layout/base/nsPresShell.h
@@ -89,7 +89,7 @@ public:
   virtual void Destroy() override;
   virtual void MakeZombie() override;
 
-  virtual nsresult SetPreferenceStyleRules(bool aForceReflow) override;
+  virtual void UpdatePreferenceStyles() override;
 
   NS_IMETHOD GetSelection(SelectionType aType, nsISelection** aSelection) override;
   virtual mozilla::dom::Selection* GetCurrentSelection(SelectionType aType) override;
@@ -515,14 +515,7 @@ protected:
 
   void RecordStyleSheetChange(nsIStyleSheet* aStyleSheet);
 
-    /**
-    * methods that manage rules that are used to implement the associated preferences
-    *  - initially created for bugs 31816, 20760, 22963
-    */
-  nsresult ClearPreferenceStyleRules(void);
-  nsresult CreatePreferenceStyleSheet(void);
-  nsresult SetPrefLinkRules(void);
-  nsresult SetPrefFocusRules(void);
+  void RemovePreferenceStyles();
 
   // methods for painting a range to an offscreen buffer
 
