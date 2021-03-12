a5aecf9de87fb62605c82eebb76ef99156d47de4
Riccardo Mottola <rm@gnu.org>
2021-03-12 09:51:08 +0100

    Bug 1130096 - Convert embedding/components/commandhandler/ to Gecko style. r=mccr8

--- embedding/components/commandhandler/nsCommandParams.h
+++ embedding/components/commandhandler/nsCommandParams.h
@@ -30,11 +30,11 @@ protected:
     uint8_t mEntryType;
     union
     {
-      bool        mBoolean;
-      int32_t     mLong;
-      double      mDouble;
-      nsString*   mString;
-      nsCString*  mCString;
+      bool mBoolean;
+      int32_t mLong;
+      double mDouble;
+      nsString* mString;
+      nsCString* mCString;
     } mData;
 
     nsCOMPtr<nsISupports> mISupports;
@@ -69,17 +69,14 @@ protected:
           mData.mCString = new nsCString(*aRHS.mData.mCString);
           break;
         case eISupportsType:
-          mISupports = aRHS.mISupports.get();    // additional addref
+          mISupports = aRHS.mISupports.get();
           break;
         default:
           NS_ERROR("Unknown type");
       }
     }
 
-    ~HashEntry()
-    {
-      Reset(eNoType);
-    }
+    ~HashEntry() { Reset(eNoType); }
 
     void Reset(uint8_t aNewType)
     {
@@ -100,7 +97,7 @@ protected:
           mData.mString = nullptr;
           break;
         case eISupportsType:
-          mISupports = nullptr;   // clear the nsCOMPtr
+          mISupports = nullptr;
           break;
         case eStringType:
           delete mData.mCString;
@@ -120,7 +117,7 @@ protected:
   static PLDHashNumber HashKey(PLDHashTable* aTable, const void* aKey);
 
   static bool HashMatchEntry(PLDHashTable* aTable,
-                             const PLDHashEntryHdr *aEntry, const void* aKey);
+                             const PLDHashEntryHdr* aEntry, const void* aKey);
 
   static void HashMoveEntry(PLDHashTable* aTable, const PLDHashEntryHdr* aFrom,
                             PLDHashEntryHdr* aTo);
