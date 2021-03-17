40c902a36100306bbcc7d4c549bbf2fd49682bce
Riccardo Mottola <rm@gnu.org>
2021-03-17 15:48:00 +0100

    Bug 1143575. Move mLayer from ImageClientBridge up into its superclass ImageClient. r=nical
    This simplifies code slightly.

--- gfx/layers/client/ClientImageLayer.cpp
+++ gfx/layers/client/ClientImageLayer.cpp
@@ -77,6 +77,7 @@ protected:
   void DestroyBackBuffer()
   {
     if (mImageClient) {
+      mImageClient->SetLayer(nullptr);
       mImageClient->OnDetach();
       mImageClient = nullptr;
     }
@@ -149,13 +150,10 @@ ClientImageLayer::RenderLayer()
     mImageClient = ImageClient::CreateImageClient(type,
                                                   ClientManager()->AsShadowForwarder(),
                                                   flags);
-    if (type == CompositableType::IMAGE_BRIDGE) {
-      static_cast<ImageClientBridge*>(mImageClient.get())->SetLayer(this);
-    }
-
     if (!mImageClient) {
       return;
     }
+    mImageClient->SetLayer(this);
     if (HasShadow() && !mContainer->IsAsync()) {
       mImageClient->Connect();
       ClientManager()->AsShadowForwarder()->Attach(mImageClient, this);
