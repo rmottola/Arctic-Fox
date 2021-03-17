34d67fcc02c4fee70c4e7b1735a8c59efb65b334
Riccardo Mottola <rm@gnu.org>
2021-03-17 15:12:40 +0100

    Bug 1143575. Rename ImageBridgeChild's AutoRemoteTextures to AutoRemoveTexturesFromImageBridge to avoid clashes with later work. r=nical

--- gfx/layers/ipc/ImageBridgeChild.cpp
+++ gfx/layers/ipc/ImageBridgeChild.cpp
@@ -450,13 +450,13 @@ ImageBridgeChild::BeginTransaction()
   mTxn->Begin();
 }
 
-class MOZ_STACK_CLASS AutoRemoveTextures
+class MOZ_STACK_CLASS AutoRemoveTexturesFromImageBridge
 {
 public:
-  explicit AutoRemoveTextures(ImageBridgeChild* aImageBridge)
+  explicit AutoRemoveTexturesFromImageBridge(ImageBridgeChild* aImageBridge)
     : mImageBridge(aImageBridge) {}
 
-  ~AutoRemoveTextures()
+  ~AutoRemoveTexturesFromImageBridge()
   {
     mImageBridge->RemoveTexturesIfNecessary();
   }
@@ -471,7 +471,7 @@ ImageBridgeChild::EndTransaction()
   MOZ_ASSERT(!mTxn->Finished(), "forgot BeginTransaction?");
 
   AutoEndTransaction _(mTxn);
-  AutoRemoveTextures autoRemoveTextures(this);
+  AutoRemoveTexturesFromImageBridge autoRemoveTextures(this);
 
   if (mTxn->IsEmpty()) {
     return;
