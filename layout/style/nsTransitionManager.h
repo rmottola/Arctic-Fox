f37e36e8a380f748e38555f79ba2d8afed869946
Riccardo Mottola <rm@gnu.org>
2021-03-23 23:55:26 +0100

    Bug 1004383 follow-up: Mark ElementPropertyTransition::AsTransition() as override

--- layout/style/nsTransitionManager.h
+++ layout/style/nsTransitionManager.h
@@ -39,8 +39,8 @@ struct ElementPropertyTransition : public dom::KeyframeEffectReadOnly
                                   aTiming, EmptyString())
   { }
 
-  virtual ElementPropertyTransition* AsTransition() { return this; }
-  virtual const ElementPropertyTransition* AsTransition() const { return this; }
+  virtual ElementPropertyTransition* AsTransition() override { return this; }
+  virtual const ElementPropertyTransition* AsTransition() const override { return this; }
 
   virtual const nsString& Name() const override;
 
