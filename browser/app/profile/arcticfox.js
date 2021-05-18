0131c940db828cc3434d239e9d5cb18ecf7e1ba9
Riccardo Mottola <rm@gnu.org>
2021-05-18 22:11:01 +0200

    Bug 1160313 - Remove temporary retro-mode for performance tools. r=vp

--- browser/app/profile/arcticfox.js
+++ browser/app/profile/arcticfox.js
@@ -1124,14 +1124,6 @@ pref("devtools.performance.timeline.hidden-markers", "[]");
 pref("devtools.performance.profiler.buffer-size", 10000000);
 pref("devtools.performance.profiler.sample-frequency-khz", 1);
 pref("devtools.performance.ui.show-jit-optimizations", false);
-// If in aurora (40.0, will revert for 40.1), set default
-// to retro mode.
-// TODO bug 1160313
-#if MOZ_UPDATE_CHANNEL == aurora
-  pref("devtools.performance.ui.retro-mode", true);
-#else
-  pref("devtools.performance.ui.retro-mode", false);
-#endif
 
 // Default theme ("dark" or "light")
 #ifdef MOZ_DEV_EDITION
