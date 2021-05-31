851e371acfe5ebbce5c19af700e4b0a795589b6d
Riccardo Mottola <rm@gnu.org>
2021-05-31 23:34:30 +0200

    Bug 1181619. Make sure we've entered a microtask before we call nsJSUtils::EvaluateString, and put those microtasks outside the relevant AutoEntryScripts so we report any possible exceptions before doing the microtask checkpoint. r=bholley

--- dom/plugins/base/nsNPAPIPlugin.cpp
+++ dom/plugins/base/nsNPAPIPlugin.cpp
@@ -1504,6 +1504,7 @@ _evaluate(NPP npp, NPObject* npobj, NPString *script, NPVariant *result)
     return false;
   }
 
+  nsAutoMicroTask mt;
   dom::AutoEntryScript aes(win, "NPAPI NPN_evaluate");
   aes.TakeOwnershipOfErrorReporting();
   JSContext* cx = aes.cx();
