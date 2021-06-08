e2e290a670182528380a17dd7d4365584b1da1fc
Riccardo Mottola <rm@gnu.org>
2021-06-08 20:59:41 +0200

    Bug 1135191 - Add runtime panel with toggle. r=jryans

--- toolkit/devtools/webide/test/sidebars/browser_tabs.js
+++ toolkit/devtools/webide/test/sidebars/browser_tabs.js
@@ -2,7 +2,7 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 "use strict";
 
-const TEST_URI = "http://example.com/browser/browser/devtools/webide/test/doc_tabs.html";
+const TEST_URI = "http://example.com/browser/toolkit/devtools/webide/test/doc_tabs.html";
 
 function test() {
   waitForExplicitFinish();
@@ -11,6 +11,8 @@ function test() {
   Task.spawn(function() {
     const { DebuggerServer } =
       Cu.import("resource://gre/modules/devtools/dbg-server.jsm", {});
+
+    Services.prefs.setBoolPref("devtools.webide.sidebars", true);
 
     // Since we test the connections set below, destroy the server in case it
     // was left open.
@@ -21,19 +23,24 @@ function test() {
     let tab = yield addTab(TEST_URI);
 
     let win = yield openWebIDE();
+    let docProject = getProjectDocument(win);
+    let docRuntime = getRuntimeDocument(win);
 
-    yield connectToLocal(win);
+    yield connectToLocal(win, docRuntime);
 
     is(Object.keys(DebuggerServer._connections).length, 1, "Locally connected");
 
-    yield selectTabProject(win);
+    yield selectTabProject(win, docProject);
+
+    ok(win.UI.toolboxPromise, "Toolbox promise exists");
+    yield win.UI.toolboxPromise;
 
     let project = win.AppManager.selectedProject;
     is(project.location, TEST_URI, "Location is correct");
     is(project.name, "example.com: Test Tab", "Name is correct");
 
     // Ensure tab list changes are noticed
-    let tabsNode = win.document.querySelector("#project-panel-tabs");
+    let tabsNode = docProject.querySelector("#project-panel-tabs");
     is(tabsNode.querySelectorAll(".panel-item").length, 2, "2 tabs available");
     yield removeTab(tab);
     yield waitForUpdate(win, "project");
@@ -47,21 +54,22 @@ function test() {
   }).then(finish, handleError);
 }
 
-function connectToLocal(win) {
+function connectToLocal(win, docRuntime) {
   let deferred = promise.defer();
   win.AppManager.connection.once(
       win.Connection.Events.CONNECTED,
       () => deferred.resolve());
-  win.document.querySelectorAll(".runtime-panel-item-other")[1].click();
+  docRuntime.querySelectorAll(".runtime-panel-item-other")[1].click();
   return deferred.promise;
 }
 
-function selectTabProject(win) {
+function selectTabProject(win, docProject) {
   return Task.spawn(function() {
-    yield win.Cmds.showProjectPanel();
     yield waitForUpdate(win, "runtime-targets");
-    let tabsNode = win.document.querySelector("#project-panel-tabs");
+    let tabsNode = docProject.querySelector("#project-panel-tabs");
     let tabNode = tabsNode.querySelectorAll(".panel-item")[1];
+    let project = waitForUpdate(win, "project");
     tabNode.click();
+    yield project;
   });
 }
