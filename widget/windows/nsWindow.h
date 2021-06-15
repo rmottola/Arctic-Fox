b1b8e10c691591ced3d3a7187be51742abf29cea
Riccardo Mottola <rm@gnu.org>
2021-06-15 15:10:20 +0200

    Move compositor backend decisions into gfxPlatform. (bug 1179051 part 3, r=mattwoodrow)

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -244,6 +244,7 @@ public:
    * Misc.
    */
   virtual bool            AutoErase(HDC dc);
+  bool ComputeShouldAccelerate() override;
 
   static void             ClearCompositor(nsWindow* aWindow);
 
@@ -281,8 +282,6 @@ public:
   void                    PickerClosed();
 
   bool                    const DestroyCalled() { return mDestroyCalled; }
-
-  virtual void GetPreferredCompositorBackends(nsTArray<mozilla::layers::LayersBackend>& aHints);
 
   bool IsPopup();
   virtual bool ShouldUseOffMainThreadCompositing();
