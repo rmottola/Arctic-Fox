ea188ea159adeb2b688a60689366216b9fcc0034
Riccardo Mottola <rm@gnu.org>
2021-07-16 13:16:38 +0200

    Bug 968923 - part 3d - record use counter information from the CSS parser; r=dbaron

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -111,6 +111,8 @@ public:
 
   nsresult SetStyleSheet(CSSStyleSheet* aSheet);
 
+  nsIDocument* GetDocument();
+
   nsresult SetQuirkMode(bool aQuirkMode);
 
   nsresult SetChildLoader(mozilla::css::Loader* aChildLoader);
@@ -1323,6 +1325,15 @@ CSSParserImpl::SetStyleSheet(CSSStyleSheet* aSheet)
   return NS_OK;
 }
 
+nsIDocument*
+CSSParserImpl::GetDocument()
+{
+  if (!mSheet) {
+    return nullptr;
+  }
+  return mSheet->GetDocument();
+}
+
 nsresult
 CSSParserImpl::SetQuirkMode(bool aQuirkMode)
 {
@@ -1675,7 +1686,8 @@ CSSParserImpl::ParseProperty(const nsCSSProperty aPropID,
       aDeclaration->ExpandTo(&mData);
       *aChanged = mData.TransferFromBlock(mTempData, aPropID,
                                           PropertyEnabledState(), aIsImportant,
-                                          true, false, aDeclaration);
+                                          true, false, aDeclaration,
+                                          GetDocument());
       aDeclaration->CompressFrom(&mData);
     }
     CLEAR_ERROR();
@@ -6888,7 +6900,7 @@ CSSParserImpl::ParseDeclaration(css::Declaration* aDeclaration,
                                          PropertyEnabledState(),
                                          status == ePriority_Important,
                                          false, aMustCallValueAppended,
-                                         aDeclaration);
+                                         aDeclaration, GetDocument());
   }
 
   return true;
