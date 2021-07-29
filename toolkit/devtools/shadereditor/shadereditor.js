c98f20c30a229705af3e53e6b7f7770dca897985
Riccardo Mottola <rm@gnu.org>
2021-07-29 19:44:12 +0200

    Bug 1168077 - Remove remaining spidermonkey js specific syntax from browser/devtools; r=miker

--- toolkit/devtools/shadereditor/shadereditor.js
+++ toolkit/devtools/shadereditor/shadereditor.js
@@ -618,4 +618,4 @@ EventEmitter.decorate(this);
 /**
  * DOM query helper.
  */
-function $(selector, target = document) target.querySelector(selector);
+let $ = (selector, target = document) => target.querySelector(selector);
