4161f7d988330bc854ffb21eca303c96978a938f
Riccardo Mottola <rm@gnu.org>
2021-08-16 08:36:58 +0200

    Bug 1180448 - Add APIs to XUL browser for muting and unmuting audio playback in the document loaded inside it; r=mconley

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -693,6 +693,7 @@ let MediaPlaybackListener = {
 
   init() {
     Services.obs.addObserver(this, "media-playback", false);
+    addMessageListener("MediaPlaybackMute", this);
     addEventListener("unload", () => {
       MediaPlaybackListener.uninit();
     });
@@ -700,6 +701,7 @@ let MediaPlaybackListener = {
 
   uninit() {
     Services.obs.removeObserver(this, "media-playback");
+    removeMessageListener("MediaPlaybackMute", this);
   },
 
   observe(subject, topic, data) {
@@ -709,6 +711,14 @@ let MediaPlaybackListener = {
         name += (data === "active") ? "Start" : "Stop";
         sendAsyncMessage(name);
       }
+    }
+  },
+
+  receiveMessage(msg) {
+    if (msg.name == "MediaPlaybackMute") {
+      let utils = global.content.QueryInterface(Ci.nsIInterfaceRequestor)
+                                .getInterface(Ci.nsIDOMWindowUtils);
+      utils.audioMuted = msg.data.type === "mute";
     }
   },
 };
