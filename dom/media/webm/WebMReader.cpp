b623b2959b0775a8101a5f4009ace8925be3dfdc
Riccardo Mottola <rm@gnu.org>
2021-08-17 17:30:46 +0200

    correct comment

--- dom/media/webm/WebMReader.cpp
+++ dom/media/webm/WebMReader.cpp
@@ -280,7 +280,8 @@ nsresult WebMReader::ReadMetadata(MediaInfo* aInfo,
                                   MetadataTags** aTags)
 {
   // We can't use OnTaskQueue() here because of the wacky initialization task
-  // queue that TrackBuffer uses.
+  // queue that TrackBuffer uses. We should be able to fix this when we do
+  // bug 1148234.
   MOZ_ASSERT(mDecoder->OnDecodeTaskQueue());
 
   nestegg_io io;
