8759dad4077b36b1fd0e7338e0ee812aef176ba4
Riccardo Mottola <rm@gnu.org>
2021-08-19 00:10:53 +0200

    Bug 554186 - Part 1: Unimplement NPN_Status API. r=josh

--- dom/plugins/ipc/PluginModuleChild.cpp
+++ dom/plugins/ipc/PluginModuleChild.cpp
@@ -1291,9 +1291,7 @@ void
 _status(NPP aNPP,
         const char* aMessage)
 {
-    PLUGIN_LOG_DEBUG_FUNCTION;
-    ENSURE_PLUGIN_THREAD_VOID();
-    NS_WARNING("Not yet implemented!");
+    // NPN_Status is no longer supported.
 }
 
 void
