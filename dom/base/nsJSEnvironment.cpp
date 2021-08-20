d94f76a45ce9e7f2900a04130f00edc6297e8d05
Riccardo Mottola <rm@gnu.org>
2021-08-20 14:58:11 +0200

    Bug 1186544 - A better test suite for StructuredCloneAlgorithm, r=smaug
    --HG--
    rename : dom/base/test/iframe_cloning_fileList.html => dom/base/test/iframe_postMessages.html
    rename : dom/base/test/script_cloning_fileList.js => dom/base/test/script_postmessages_fileList.js

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -2495,7 +2495,13 @@ NS_DOMReadStructuredClone(JSContext* cx,
 {
   if (tag == SCTAG_DOM_IMAGEDATA) {
     return ReadStructuredCloneImageData(cx, reader);
-  } else if (tag == SCTAG_DOM_WEBCRYPTO_KEY) {
+  }
+
+  if (tag == SCTAG_DOM_WEBCRYPTO_KEY) {
+    if (!NS_IsMainThread()) {
+      return nullptr;
+    }
+
     nsIGlobalObject *global = xpc::NativeGlobal(JS::CurrentGlobalOrNull(cx));
     if (!global) {
       return nullptr;
@@ -2512,9 +2518,15 @@ NS_DOMReadStructuredClone(JSContext* cx,
       }
     }
     return result;
-  } else if (tag == SCTAG_DOM_NULL_PRINCIPAL ||
-             tag == SCTAG_DOM_SYSTEM_PRINCIPAL ||
-             tag == SCTAG_DOM_CONTENT_PRINCIPAL) {
+  }
+
+  if (tag == SCTAG_DOM_NULL_PRINCIPAL ||
+      tag == SCTAG_DOM_SYSTEM_PRINCIPAL ||
+      tag == SCTAG_DOM_CONTENT_PRINCIPAL) {
+    if (!NS_IsMainThread()) {
+      return nullptr;
+    }
+
     mozilla::ipc::PrincipalInfo info;
     if (tag == SCTAG_DOM_SYSTEM_PRINCIPAL) {
       info = mozilla::ipc::SystemPrincipalInfo();
@@ -2552,8 +2564,14 @@ NS_DOMReadStructuredClone(JSContext* cx,
     }
 
     return result.toObjectOrNull();
-  } else if (tag == SCTAG_DOM_NFC_NDEF) {
+  }
+
 #ifdef MOZ_NFC
+  if (tag == SCTAG_DOM_NFC_NDEF) {
+    if (!NS_IsMainThread()) {
+      return nullptr;
+    }
+
     nsIGlobalObject *global = xpc::NativeGlobal(JS::CurrentGlobalOrNull(cx));
     if (!global) {
       return nullptr;
@@ -2567,13 +2585,15 @@ NS_DOMReadStructuredClone(JSContext* cx,
                ndefRecord->WrapObject(cx, nullptr) : nullptr;
     }
     return result;
-#else
-    return nullptr;
-#endif
   }
+#endif
 
-  if (tag == SCTAG_DOM_RTC_CERTIFICATE) {
 #ifdef MOZ_WEBRTC
+  if (tag == SCTAG_DOM_RTC_CERTIFICATE) {
+    if (!NS_IsMainThread()) {
+      return nullptr;
+    }
+
     nsIGlobalObject *global = xpc::NativeGlobal(JS::CurrentGlobalOrNull(cx));
     if (!global) {
       return nullptr;
@@ -2590,10 +2610,8 @@ NS_DOMReadStructuredClone(JSContext* cx,
       }
     }
     return result;
-#else
-    return nullptr;
-#endif
   }
+#endif
 
   // Don't know what this is. Bail.
   xpc::Throw(cx, NS_ERROR_DOM_DATA_CLONE_ERR);
@@ -2615,6 +2633,7 @@ NS_DOMWriteStructuredClone(JSContext* cx,
   // Handle Key cloning
   CryptoKey* key;
   if (NS_SUCCEEDED(UNWRAP_OBJECT(CryptoKey, obj, key))) {
+    MOZ_ASSERT(NS_IsMainThread(), "This object should not be exposed outside the main-thread.");
     return JS_WriteUint32Pair(writer, SCTAG_DOM_WEBCRYPTO_KEY, 0) &&
            key->WriteStructuredClone(writer);
   }
@@ -2623,12 +2642,13 @@ NS_DOMWriteStructuredClone(JSContext* cx,
   // Handle WebRTC Certificate cloning
   RTCCertificate* cert;
   if (NS_SUCCEEDED(UNWRAP_OBJECT(RTCCertificate, obj, cert))) {
+    MOZ_ASSERT(NS_IsMainThread(), "This object should not be exposed outside the main-thread.");
     return JS_WriteUint32Pair(writer, SCTAG_DOM_RTC_CERTIFICATE, 0) &&
            cert->WriteStructuredClone(writer);
   }
 #endif
 
-  if (xpc::IsReflector(obj)) {
+  if (NS_IsMainThread() && xpc::IsReflector(obj)) {
     nsCOMPtr<nsISupports> base = xpc::UnwrapReflectorToISupports(obj);
     nsCOMPtr<nsIPrincipal> principal = do_QueryInterface(base);
     if (principal) {
@@ -2656,6 +2676,7 @@ NS_DOMWriteStructuredClone(JSContext* cx,
 #ifdef MOZ_NFC
   MozNDEFRecord* ndefRecord;
   if (NS_SUCCEEDED(UNWRAP_OBJECT(MozNDEFRecord, obj, ndefRecord))) {
+    MOZ_ASSERT(NS_IsMainThread(), "This object should not be exposed outside the main-thread.");
     return JS_WriteUint32Pair(writer, SCTAG_DOM_NFC_NDEF, 0) &&
            ndefRecord->WriteStructuredClone(cx, writer);
   }
