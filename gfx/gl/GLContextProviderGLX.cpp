4f7c00f01d707b844d40a5fe60d6b952b22ecbc9
Riccardo Mottola <rm@gnu.org>
2021-08-23 19:14:36 +0200

    Bug 1180498 - Tolerate a different major libGL.so on NetBSD as well r=jgilbert

--- gfx/gl/GLContextProviderGLX.cpp
+++ gfx/gl/GLContextProviderGLX.cpp
@@ -81,7 +81,7 @@ GLXLibrary::EnsureInitialized()
         // see e.g. bug 608526: it is intrinsically interesting to know whether we have dynamically linked to libGL.so.1
         // because at least the NVIDIA implementation requires an executable stack, which causes mprotect calls,
         // which trigger glibc bug http://sourceware.org/bugzilla/show_bug.cgi?id=12225
-#ifdef __OpenBSD__
+#if defined(__OpenBSD__) || defined(__NetBSD__)
         libGLfilename = "libGL.so";
 #else
         libGLfilename = "libGL.so.1";
