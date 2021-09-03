d75e7d34d71b36921426db31bb8c1fe2d4e3ffd4
Riccardo Mottola <rm@gnu.org>
2021-09-03 18:44:01 +0200

    Bug 1044721 - Part 1: Add setSmscAddress API in MozMobileMessageManager Web IDL interface, and corresponding implementation in MobileMessageManager class. r=hsinyi

--- dom/mobilemessage/MobileMessageManager.h
+++ dom/mobilemessage/MobileMessageManager.h
@@ -14,6 +14,7 @@
 class nsISmsService;
 class nsIDOMMozSmsMessage;
 class nsIDOMMozMmsMessage;
+class Promise;
 
 namespace mozilla {
 namespace dom {
@@ -25,6 +26,7 @@ struct MmsSendParameters;
 struct MobileMessageFilter;
 class OwningLongOrMozSmsMessageOrMozMmsMessage;
 struct SmsSendParameters;
+struct SmscAddress;
 
 class MobileMessageManager final : public DOMEventTargetHelper
                                  , public nsIObserver
@@ -114,6 +116,11 @@ public:
 
   already_AddRefed<DOMRequest>
   GetSmscAddress(const Optional<uint32_t>& aServiceId,
+                 ErrorResult& aRv);
+
+  already_AddRefed<Promise>
+  SetSmscAddress(const SmscAddress& aSmscAddress,
+                 const Optional<uint32_t>& aServiceId,
                  ErrorResult& aRv);
 
   IMPL_EVENT_HANDLER(received)
