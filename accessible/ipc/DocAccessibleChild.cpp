be2eb3eb7efc8f7b263d8712779ccab2abda2ee8
Riccardo Mottola <rm@gnu.org>
2021-09-20 23:25:13 +0200

    bug 1162543 - make DocAccessibleChild::IDToAccessible correctly get the document r=davidb
    DocAccessible::mAccessibleCache doesn't contain an entry for the document
    itself so we need to handle it separately.  The document always has ID 0, so we
    can just check if that is the ID we are getting and if so just return the
    document.

--- accessible/ipc/DocAccessibleChild.cpp
+++ accessible/ipc/DocAccessibleChild.cpp
@@ -52,6 +52,9 @@ SerializeTree(Accessible* aRoot, nsTArray<AccessibleData>& aTree)
 Accessible*
 DocAccessibleChild::IdToAccessible(const uint64_t& aID) const
 {
+  if (!aID)
+    return mDoc;
+
   return mDoc->GetAccessibleByUniqueID(reinterpret_cast<void*>(aID));
 }
 
