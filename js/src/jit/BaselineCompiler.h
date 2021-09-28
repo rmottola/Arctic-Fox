8215c953bd2b57d3bba3d56e011522284218185a
Riccardo Mottola <rm@gnu.org>
2021-09-28 08:48:25 +0200

    Bug 1180184 - Support JSOP_TOSTRING used by template strings in baseline JIT. r=jandem

--- js/src/jit/BaselineCompiler.h
+++ js/src/jit/BaselineCompiler.h
@@ -184,6 +184,7 @@ namespace jit {
     _(JSOP_RUNONCE)            \
     _(JSOP_REST)               \
     _(JSOP_TOID)               \
+    _(JSOP_TOSTRING)           \
     _(JSOP_TABLESWITCH)        \
     _(JSOP_ITER)               \
     _(JSOP_MOREITER)           \
