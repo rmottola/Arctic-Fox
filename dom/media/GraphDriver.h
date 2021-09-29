0faaf6db8ea9a049929dad47fa6e0cf566102444
Riccardo Mottola <rm@gnu.org>
2021-09-30 00:43:20 +0200

    bug 1190285 move GetIntervalForIteration() from base class to ThreadedDriver, where it is used r=padenot

--- dom/media/GraphDriver.h
+++ dom/media/GraphDriver.h
@@ -69,10 +69,6 @@ public:
   explicit GraphDriver(MediaStreamGraphImpl* aGraphImpl);
 
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(GraphDriver);
-  /* When the graph wakes up to do an iteration, this returns the range of time
-   * that will be processed. */
-  virtual void GetIntervalForIteration(GraphTime& aFrom,
-                                       GraphTime& aTo) = 0;
   /* For real-time graphs, this waits until it's time to process more data. For
    * offline graphs, this is a no-op. */
   virtual void WaitForNextIteration() = 0;
@@ -254,6 +250,10 @@ public:
 
   virtual bool OnThread() override { return !mThread || NS_GetCurrentThread() == mThread; }
 
+  /* When the graph wakes up to do an iteration, implementations return the
+   * range of time that will be processed. */
+  virtual void GetIntervalForIteration(GraphTime& aFrom,
+                                       GraphTime& aTo) = 0;
 protected:
   nsCOMPtr<nsIThread> mThread;
 };
@@ -349,8 +349,6 @@ public:
   virtual void Stop() override;
   virtual void Resume() override;
   virtual void Revive() override;
-  virtual void GetIntervalForIteration(GraphTime& aFrom,
-                                       GraphTime& aTo) override;
   virtual void WaitForNextIteration() override;
   virtual void WakeUp() override;
 
