c00adba10088d93cdbda8b5778c50bae4e6426e0
Riccardo Mottola <rm@gnu.org>
2021-10-11 23:45:02 +0200

    Bug 1190950: Check mappings and surfaces for UpdateFromSurface more robustly. r=mattwoodrow

--- gfx/layers/d3d11/TextureD3D11.h
+++ gfx/layers/d3d11/TextureD3D11.h
@@ -117,7 +117,7 @@ public:
 
   virtual gfx::DrawTarget* BorrowDrawTarget() override;
 
-  virtual void UpdateFromSurface(gfx::DataSourceSurface* aSurface) override;
+  virtual void UpdateFromSurface(gfx::SourceSurface* aSurface) override;
 
   virtual bool AllocateForSurface(gfx::IntSize aSize,
                                   TextureAllocationFlags aFlags = ALLOC_DEFAULT) override;
