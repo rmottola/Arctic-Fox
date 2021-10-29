f9cf4b1945589ae1a9d87eb2ce1bb127a91349c3
Riccardo Mottola <rm@gnu.org>
2021-10-29 11:02:13 +0200

    Bug 1155618 - Fix some more places where we didn't report OOM to the context r=terrence

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -750,8 +750,10 @@ ToDisassemblySource(JSContext* cx, HandleValue v, JSAutoByteString* bytes)
         if (!nbytes)
             return false;
         nbytes = JS_sprintf_append(nullptr, "%s", nbytes);
-        if (!nbytes)
+        if (!nbytes) {
+            ReportOutOfMemory(cx);
             return false;
+        }
         bytes->initBytes(nbytes);
         return true;
     }
@@ -759,8 +761,10 @@ ToDisassemblySource(JSContext* cx, HandleValue v, JSAutoByteString* bytes)
     JSRuntime* rt = cx->runtime();
     if (rt->isHeapBusy() || !rt->gc.isAllocAllowed()) {
         char* source = JS_sprintf_append(nullptr, "<value>");
-        if (!source)
+        if (!source) {
+            ReportOutOfMemory(cx);
             return false;
+        }
         bytes->initBytes(source);
         return true;
     }
@@ -770,8 +774,10 @@ ToDisassemblySource(JSContext* cx, HandleValue v, JSAutoByteString* bytes)
         if (obj.is<StaticBlockObject>()) {
             Rooted<StaticBlockObject*> block(cx, &obj.as<StaticBlockObject>());
             char* source = JS_sprintf_append(nullptr, "depth %d {", block->localOffset());
-            if (!source)
+            if (!source) {
+                ReportOutOfMemory(cx);
                 return false;
+            }
 
             Shape::Range<CanGC> r(cx, block->lastProperty());
 
@@ -790,13 +796,17 @@ ToDisassemblySource(JSContext* cx, HandleValue v, JSAutoByteString* bytes)
                                            bytes.ptr(),
                                            block->shapeToIndex(*shape),
                                            !r.empty() ? ", " : "");
-                if (!source)
+                if (!source) {
+                    ReportOutOfMemory(cx);
                     return false;
+                }
             }
 
             source = JS_sprintf_append(source, "}");
-            if (!source)
+            if (!source) {
+                ReportOutOfMemory(cx);
                 return false;
+            }
             bytes->initBytes(source);
             return true;
         }
