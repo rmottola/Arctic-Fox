e5239585f0de0cf80b765c59812c9eb74beb47ea
Riccardo Mottola <rm@gnu.org>
2021-11-01 09:08:11 +0100

    Bug 1164543 - Make plugin shutdown async in e10s (r=jimm)

--- dom/plugins/ipc/PluginModuleChild.h
+++ dom/plugins/ipc/PluginModuleChild.h
@@ -161,6 +161,8 @@ public:
 
     void CleanUp();
 
+    NPError NP_Shutdown();
+
     const char* GetUserAgent();
 
     static const NPNetscapeFuncs sBrowserFuncs;
@@ -310,6 +312,7 @@ private:
     int mQuirks;
 
     bool mIsChrome;
+    bool mHasShutdown; // true if NP_Shutdown has run
     Transport* mTransport;
 
     // we get this from the plugin
