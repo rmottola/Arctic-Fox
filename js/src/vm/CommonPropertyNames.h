5a420105f2cae2ef8754ce4e39e9dd07597cf617
Riccardo Mottola <rm@gnu.org>
2021-11-02 09:06:29 +0100

    Bug 1197970 - Expose the GC cycle's number through Debugger.Memory's onGarbageCollection hook. r=sfink

--- js/src/vm/CommonPropertyNames.h
+++ js/src/vm/CommonPropertyNames.h
@@ -97,6 +97,7 @@
     macro(format, format, "format") \
     macro(frame, frame, "frame") \
     macro(from, from, "from") \
+    macro(gcCycleNumber, gcCycleNumber, "gcCycleNumber") \
     macro(GeneratorFunction, GeneratorFunction, "GeneratorFunction") \
     macro(get, get, "get") \
     macro(getInternals, getInternals, "getInternals") \
