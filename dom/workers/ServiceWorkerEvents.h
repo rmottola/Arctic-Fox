0f463a834e2c1de67d5eaa4e0fcb40afc3ad0cb9
Riccardo Mottola <rm@gnu.org>
2021-11-08 15:47:40 +0100

    Bug 983301 part 2. Change Promise to not be distinguishable from any other type. r=peterv

--- dom/workers/ServiceWorkerEvents.h
+++ dom/workers/ServiceWorkerEvents.h
@@ -90,7 +90,7 @@ public:
   }
 
   void
-  RespondWith(const ResponseOrPromise& aArg, ErrorResult& aRv);
+  RespondWith(Promise& aArg, ErrorResult& aRv);
 
   already_AddRefed<Promise>
   ForwardTo(const nsAString& aUrl);
