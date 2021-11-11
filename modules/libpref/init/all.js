742017ac18bec857989364f030c3ea9175d2c493
Riccardo Mottola <rm@gnu.org>
2021-11-11 16:40:55 +0100

    smoe more jetpack removal and Bug 1149702 - Display a note about add-ons that aren't properly signed in the add-ons manager. r=mossop

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4535,6 +4535,7 @@ pref("browser.history.maxStateObjectSize", 655360);
 pref("xpinstall.whitelist.required", true);
 // Only Firefox requires add-on signatures
 pref("xpinstall.signatures.required", false);
+pref("xpinstall.signatures.infoURL", "https://wiki.mozilla.org/Addons/Extension_Signing");
 pref("extensions.alwaysUnpack", false);
 pref("extensions.minCompatiblePlatformVersion", "1.8");
 
