c26cb3a1c37ff24055fcf1e10bb59ea7b6e4c6d6
Riccardo Mottola <rm@gnu.org>
2021-11-30 02:11:20 +0100

    Bug 996872 - Reduce calls to getXPCOMStatusFromNSS() in PSM xpcshell tests. r=keeler relanding on a CLOSED TREE

--- security/manager/ssl/tests/unit/test_cert_overrides.js
+++ security/manager/ssl/tests/unit/test_cert_overrides.js
@@ -15,7 +15,7 @@ do_get_profile();
 
 function add_non_overridable_test(aHost, aExpectedError) {
   add_connection_test(
-    aHost, getXPCOMStatusFromNSS(aExpectedError), null,
+    aHost, aExpectedError, null,
     function (securityInfo) {
       // bug 754369 - no SSLStatus probably means this is a non-overridable
       // error, which is what we're testing (although it would be best to test
@@ -82,44 +82,41 @@ function run_test() {
 function add_simple_tests() {
   add_cert_override_test("expired.example.com",
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SEC_ERROR_EXPIRED_CERTIFICATE));
+                         SEC_ERROR_EXPIRED_CERTIFICATE);
   add_cert_override_test("notyetvalid.example.com",
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(
-                           MOZILLA_PKIX_ERROR_NOT_YET_VALID_CERTIFICATE));
+                         MOZILLA_PKIX_ERROR_NOT_YET_VALID_CERTIFICATE);
   add_cert_override_test("before-epoch.example.com",
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SEC_ERROR_INVALID_TIME));
+                         SEC_ERROR_INVALID_TIME);
   add_cert_override_test("selfsigned.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
   add_cert_override_test("unknownissuer.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
   add_cert_override_test("expiredissuer.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_EXPIRED_ISSUER_CERTIFICATE));
+                         SEC_ERROR_EXPIRED_ISSUER_CERTIFICATE);
   add_cert_override_test("notyetvalidissuer.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(
-                           MOZILLA_PKIX_ERROR_NOT_YET_VALID_ISSUER_CERTIFICATE));
+                         MOZILLA_PKIX_ERROR_NOT_YET_VALID_ISSUER_CERTIFICATE);
   add_cert_override_test("before-epoch-issuer.example.com",
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SEC_ERROR_INVALID_TIME));
+                         SEC_ERROR_INVALID_TIME);
   add_cert_override_test("md5signature.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(
-                            SEC_ERROR_CERT_SIGNATURE_ALGORITHM_DISABLED));
+                         SEC_ERROR_CERT_SIGNATURE_ALGORITHM_DISABLED);
   add_cert_override_test("mismatch.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH,
-                         getXPCOMStatusFromNSS(SSL_ERROR_BAD_CERT_DOMAIN));
+                         SSL_ERROR_BAD_CERT_DOMAIN);
 
   // A Microsoft IIS utility generates self-signed certificates with
   // properties similar to the one this "host" will present (see
   // tlsserver/generate_certs.sh).
   add_cert_override_test("selfsigned-inadequateEKU.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
 
   add_non_overridable_test("inadequatekeyusage.example.com",
                            SEC_ERROR_INADEQUATE_KEY_USAGE);
@@ -145,17 +142,17 @@ function add_simple_tests() {
   // is a scenario in which an override is allowed.
   add_cert_override_test("self-signed-end-entity-with-cA-true.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
 
   add_cert_override_test("ca-used-as-end-entity.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_CA_CERT_USED_AS_END_ENTITY));
+                         MOZILLA_PKIX_ERROR_CA_CERT_USED_AS_END_ENTITY);
 
   // If an X.509 version 1 certificate is not a trust anchor, we will
   // encounter an overridable error.
   add_cert_override_test("end-entity-issued-by-v1-cert.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_V1_CERT_USED_AS_CA));
+                         MOZILLA_PKIX_ERROR_V1_CERT_USED_AS_CA);
   // If we make that certificate a trust anchor, the connection will succeed.
   add_test(function() {
     let certOverrideService = Cc["@mozilla.org/security/certoverride;1"]
@@ -166,7 +163,8 @@ function add_simple_tests() {
     clearSessionCache();
     run_next_test();
   });
-  add_connection_test("end-entity-issued-by-v1-cert.example.com", Cr.NS_OK);
+  add_connection_test("end-entity-issued-by-v1-cert.example.com",
+                      PRErrorCodeSuccess);
   // Reset the trust for that certificate.
   add_test(function() {
     let v1Cert = constructCertFromFile("tlsserver/v1Cert.der");
@@ -179,7 +177,7 @@ function add_simple_tests() {
   // certificates that are not valid CAs.
   add_cert_override_test("end-entity-issued-by-non-CA.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_CA_CERT_INVALID));
+                         SEC_ERROR_CA_CERT_INVALID);
 
   // This host presents a 1008-bit RSA key. NSS determines this key is too
   // small and terminates the connection. The error is not overridable.
@@ -191,40 +189,38 @@ function add_combo_tests() {
   add_cert_override_test("mismatch-expired.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH |
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SSL_ERROR_BAD_CERT_DOMAIN));
   add_cert_override_test("mismatch-notYetValid.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH |
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SSL_ERROR_BAD_CERT_DOMAIN));
+                         SSL_ERROR_BAD_CERT_DOMAIN);
   add_cert_override_test("mismatch-untrusted.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH |
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
   add_cert_override_test("untrusted-expired.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED |
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
   add_cert_override_test("mismatch-untrusted-expired.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH |
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED |
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                         SEC_ERROR_UNKNOWN_ISSUER);
 
   add_cert_override_test("md5signature-expired.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED |
                          Ci.nsICertOverrideService.ERROR_TIME,
-                         getXPCOMStatusFromNSS(
-                            SEC_ERROR_CERT_SIGNATURE_ALGORITHM_DISABLED));
+                         SEC_ERROR_CERT_SIGNATURE_ALGORITHM_DISABLED);
 
   add_cert_override_test("ca-used-as-end-entity-name-mismatch.example.com",
                          Ci.nsICertOverrideService.ERROR_MISMATCH |
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_CA_CERT_USED_AS_END_ENTITY));
+                         MOZILLA_PKIX_ERROR_CA_CERT_USED_AS_END_ENTITY);
 }
 
 function add_distrust_tests() {
   // Before we specifically distrust this certificate, it should be trusted.
-  add_connection_test("untrusted.example.com", Cr.NS_OK);
+  add_connection_test("untrusted.example.com", PRErrorCodeSuccess);
 
   add_distrust_test("tlsserver/default-ee.der", "untrusted.example.com",
                     SEC_ERROR_UNTRUSTED_CERT);
--- security/manager/ssl/tests/unit/test_pinning.js
+++ security/manager/ssl/tests/unit/test_pinning.js
@@ -56,23 +56,27 @@ function test_strict() {
 
   // Issued by otherCA, which is not in the pinset for pinning.example.com.
   add_connection_test("bad.include-subdomains.pinning.example.com",
-    getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
 
   // Check that using a FQDN doesn't bypass pinning.
   add_connection_test("bad.include-subdomains.pinning.example.com.",
-    getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
   // For some reason this is also navigable (see bug 1118522).
   add_connection_test("bad.include-subdomains.pinning.example.com..",
-    getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
 
   // These domains serve certs that match the pinset.
-  add_connection_test("include-subdomains.pinning.example.com", Cr.NS_OK);
-  add_connection_test("good.include-subdomains.pinning.example.com", Cr.NS_OK);
-  add_connection_test("exclude-subdomains.pinning.example.com", Cr.NS_OK);
+  add_connection_test("include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
+  add_connection_test("good.include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
+  add_connection_test("exclude-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
 
   // This domain serves a cert that doesn't match the pinset, but subdomains
   // are excluded.
-  add_connection_test("sub.exclude-subdomains.pinning.example.com", Cr.NS_OK);
+  add_connection_test("test-mode.pinning.example.com",
+                      PRErrorCodeSuccess);
 
   // This domain's pinset is exactly the same as
   // include-subdomains.pinning.example.com, serves the same cert as
@@ -94,8 +98,10 @@ function test_mitm() {
     run_next_test();
   });
 
-  add_connection_test("include-subdomains.pinning.example.com", Cr.NS_OK);
-  add_connection_test("good.include-subdomains.pinning.example.com", Cr.NS_OK);
+  add_connection_test("include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
+  add_connection_test("good.include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
 
   // Normally this is overridable. But, since we have pinning information for
   // this host, we don't allow overrides (since building a trusted chain fails,
@@ -104,7 +110,7 @@ function test_mitm() {
   add_prevented_cert_override_test(
     "unknownissuer.include-subdomains.pinning.example.com",
     Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-    getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                    SEC_ERROR_UNKNOWN_ISSUER);
   add_clear_override("unknownissuer.include-subdomains.pinning.example.com");
 
   // In this case, even though otherCA is not in the pinset, it is a
@@ -116,7 +122,7 @@ function test_mitm() {
   add_connection_test("test-mode.pinning.example.com", Cr.NS_OK);
   add_cert_override_test("unknownissuer.test-mode.pinning.example.com",
                          Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-                         getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
   add_clear_override("unknownissuer.test-mode.pinning.example.com");
 };
 
@@ -156,28 +162,32 @@ function test_enforce_test_mode() {
   add_prevented_cert_override_test(
     "unknownissuer.include-subdomains.pinning.example.com",
     Ci.nsICertOverrideService.ERROR_UNTRUSTED,
-    getXPCOMStatusFromNSS(SEC_ERROR_UNKNOWN_ISSUER));
+                    MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
   add_clear_override("unknownissuer.include-subdomains.pinning.example.com");
 
   // Issued by otherCA, which is not in the pinset for pinning.example.com.
   add_connection_test("bad.include-subdomains.pinning.example.com",
-    getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
 
   // These domains serve certs that match the pinset.
-  add_connection_test("include-subdomains.pinning.example.com", Cr.NS_OK);
-  add_connection_test("good.include-subdomains.pinning.example.com", Cr.NS_OK);
-  add_connection_test("exclude-subdomains.pinning.example.com", Cr.NS_OK);
+  add_connection_test("include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
+  add_connection_test("good.include-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
+  add_connection_test("exclude-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
 
   // This domain serves a cert that doesn't match the pinset, but subdomains
   // are excluded.
-  add_connection_test("sub.exclude-subdomains.pinning.example.com", Cr.NS_OK);
+  add_connection_test("sub.exclude-subdomains.pinning.example.com",
+                      PRErrorCodeSuccess);
 
   // This domain's pinset is exactly the same as
   // include-subdomains.pinning.example.com, serves the same cert as
   // bad.include-subdomains.pinning.example.com, is in test-mode, but we are
   // enforcing test mode pins.
   add_connection_test("test-mode.pinning.example.com",
-    getXPCOMStatusFromNSS(MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE));
+                      MOZILLA_PKIX_ERROR_KEY_PINNING_FAILURE);
   // Normally this is overridable. But, since we have pinning information for
   // this host (and since we're enforcing test mode), we don't allow overrides.
   add_prevented_cert_override_test(
