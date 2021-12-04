da289d24365809ee00ac0ddce206c5b59a4e05bc
Riccardo Mottola <rm@gnu.org>
2021-12-04 19:05:37 +0100

    Bug 1188251 part 4 - Move throttling checks to AnimationCollection::RequestRestyle; r=dholbert
    This patch moves the additional checks (beyond those of Animation::CanThrottle)
    from FlushAnimations/FlushTransitions to AnimationCollection::RequestRestyle.
    These checks are on a per-collection basis hence it makes sense for the
    collection to perform them. This also moves logic out of the managers which is
    needed if we want to support script-based animations without introducing another
    manager.

--- layout/style/AnimationCommon.h
+++ layout/style/AnimationCommon.h
@@ -278,10 +278,6 @@ struct AnimationCollection : public PRCList
 
   void EnsureStyleRuleFor(TimeStamp aRefreshTime, EnsureStyleRuleFlags aFlags);
 
-  bool CanThrottleTransformChanges(TimeStamp aTime);
-
-  bool CanThrottleAnimation(TimeStamp aTime);
-
   enum CanAnimateFlags {
     // Testing for width, height, top, right, bottom, or left.
     CanAnimate_HasGeometricProperty = 1,
@@ -305,6 +301,9 @@ private:
   CanAnimatePropertyOnCompositor(const dom::Element *aElement,
                                  nsCSSProperty aProperty,
                                  CanAnimateFlags aFlags);
+
+  bool CanThrottleAnimation(TimeStamp aTime);
+  bool CanThrottleTransformChanges(TimeStamp aTime);
 
 public:
   static bool IsCompositorAnimationDisabledForFrame(nsIFrame* aFrame);
