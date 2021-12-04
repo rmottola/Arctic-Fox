c7602af1864e71fade9038dbc3f688f567e90c7c
Riccardo Mottola <rm@gnu.org>
2021-12-04 19:29:49 +0100

    Bug 1188251 part 11 - Add RestyleType::Layer; r=dholbert
    We currently have a series of methods that clobber various bits of animation
    state to force animations on layers to be updated. This aligns closely with
    the restyle code introduced in this patch series.
    
    By re-using RequestRestyle when updating animations on layers, not only should
    we be able to simplify the code somewhat but, in future, we should also be able
    to have Animation objects use the same mechanism to update layers during
    a regular tick.
    
    For example, currently we have a bug where when an animation starts after
    a delay with the same value as the backwards fill then we don't send the
    animation to the compositor right away (see
    https://dxr.mozilla.org/mozilla-central/rev/d6ea652c579992daa9041cc9718bb7c6abefbc91/layout/style/test/test_animations_omta.html#287).
    By adding this Restyle::Layer value we should be able to fix that in future.

--- dom/animation/Animation.cpp
+++ dom/animation/Animation.cpp
@@ -355,6 +355,8 @@ Animation::Tick()
   UpdateTiming(SeekFlag::NoSeek, SyncNotifyFlag::Async);
 
   // FIXME: Detect the no-change case and don't request a restyle at all
+  // FIXME: Detect changes to IsPlaying() state and request RestyleType::Layer
+  //        so that layers get updated immediately
   AnimationCollection* collection = GetCollection();
   if (collection) {
     collection->RequestRestyle(CanThrottle() ?
@@ -924,7 +926,7 @@ Animation::PostUpdate()
 {
   AnimationCollection* collection = GetCollection();
   if (collection) {
-    collection->NotifyAnimationUpdated();
+    collection->RequestRestyle(AnimationCollection::RestyleType::Layer);
   }
 }
 
