7f54ba8d266ecfab9b68e89c2873d2eb589f0f3e
Riccardo Mottola <rm@gnu.org>
2021-12-26 16:12:52 +0100

    Bug 1019063 - Check for ::CreateDCW failing when printing. r=dvander

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -2885,6 +2885,7 @@ PresShell::CreateReferenceRenderingContext()
   if (mPresContext->IsScreen()) {
     rc = new gfxContext(gfxPlatform::GetPlatform()->ScreenReferenceDrawTarget());
   } else {
+    // We assume the devCtx has positive width and height for this call
     rc = devCtx->CreateRenderingContext();
   }
 
