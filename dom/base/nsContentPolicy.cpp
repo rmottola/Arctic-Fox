14c4d843e4ca8d6bbd727c0ab1c7ea464b50f32f
Riccardo Mottola <rm@gnu.org>
2022-01-13 21:44:25 +0100

    add internal CSP preload check from TFF

--- dom/base/nsContentPolicy.cpp
+++ dom/base/nsContentPolicy.cpp
@@ -145,11 +145,25 @@ nsContentPolicy::CheckPolicy(CPMethod          policyMethod,
         /* check the appropriate policy */
         // Send the internal content policy type to the mixed content blocker
         // which needs to know about TYPE_INTERNAL_WORKER,
-        // TYPE_INTERNAL_SHARED_WORKER and TYPE_INTERNAL_SERVICE_WORKER.
+        // TYPE_INTERNAL_SHARED_WORKER and TYPE_INTERNAL_SERVICE_WORKER
+        // and also preloads: TYPE_INTERNAL_SCRIPT_PRELOAD,
+        // TYPE_INTERNAL_IMAGE_PRELOAD, TYPE_INTERNAL_STYLESHEET_PRELOAD
         bool isMixedContentBlocker = mixedContentBlocker == entries[i];
         nsContentPolicyType type = externalType;
         if (isMixedContentBlocker) {
             type = externalTypeOrMCBInternal;
+        }
+        // Send the internal content policy type for CSP which needs to
+        // know about preloads and workers, in particular:
+        // * TYPE_INTERNAL_SCRIPT_PRELOAD
+        // * TYPE_INTERNAL_IMAGE_PRELOAD
+        // * TYPE_INTERNAL_STYLESHEET_PRELOAD
+        // * TYPE_INTERNAL_WORKER
+        // * TYPE_INTERNAL_SHARED_WORKER
+        // * TYPE_INTERNAL_SERVICE_WORKER
+        bool isCSP = cspService == entries[i];
+        if (isCSP) {
+          type = externalTypeOrCSPInternal;
         }
         rv = (entries[i]->*policyMethod)(type, contentLocation,
                                          requestingLocation, requestingContext,
