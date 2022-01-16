6b787252a50bc9445288e2877868df860fac8de9
Riccardo Mottola <rm@gnu.org>
2022-01-17 00:46:54 +0100

    Bug 1153255 - set a build configuration flag when MOZ_TSAN is enabled; r=jorendorff
    We will want to query this just like MOZ_ASAN.

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -129,6 +129,14 @@ GetBuildConfiguration(JSContext* cx, unsigned argc, Value* vp)
     if (!JS_SetProperty(cx, info, "asan", value))
         return false;
 
+#ifdef MOZ_TSAN
+    value = BooleanValue(true);
+#else
+    value = BooleanValue(false);
+#endif
+    if (!JS_SetProperty(cx, info, "tsan", value))
+        return false;
+
 #ifdef JS_GC_ZEAL
     value = BooleanValue(true);
 #else
