cb64cb1ff540f91811ccf09245b2c2363ae7234d
Riccardo Mottola <rm@gnu.org>
2022-01-31 09:52:58 +0100

    Bug 1178793 - Make asm.js atomic primitives return Int, not Intish. r=luke

--- js/src/asmjs/AsmJSValidate.cpp
+++ js/src/asmjs/AsmJSValidate.cpp
@@ -6012,7 +6012,7 @@ CheckAtomicsLoad(FunctionBuilder& f, ParseNode* call, Type* type)
     f.patchU8(needsBoundsCheckAt, uint8_t(needsBoundsCheck));
     f.patchU8(viewTypeAt, uint8_t(viewType));
 
-    *type = Type::Intish;
+    *type = Type::Int;
     return true;
 }
 
@@ -6109,7 +6109,7 @@ CheckAtomicsBinop(FunctionBuilder& f, ParseNode* call, Type* type, js::jit::Atom
     f.patchU8(needsBoundsCheckAt, uint8_t(needsBoundsCheck));
     f.patchU8(viewTypeAt, uint8_t(viewType));
 
-    *type = Type::Intish;
+    *type = Type::Int;
     return true;
 }
 
@@ -6184,7 +6184,7 @@ CheckAtomicsCompareExchange(FunctionBuilder& f, ParseNode* call, Type* type)
     f.patchU8(needsBoundsCheckAt, uint8_t(needsBoundsCheck));
     f.patchU8(viewTypeAt, uint8_t(viewType));
 
-    *type = Type::Intish;
+    *type = Type::Int;
     return true;
 }
 
@@ -6236,7 +6236,7 @@ CheckAtomicsExchange(FunctionBuilder& f, ParseNode* call, Type* type)
     f.patchU8(needsBoundsCheckAt, uint8_t(needsBoundsCheck));
     f.patchU8(viewTypeAt, uint8_t(viewType));
 
-    *type = Type::Intish;
+    *type = Type::Int;
     return true;
 }
 
