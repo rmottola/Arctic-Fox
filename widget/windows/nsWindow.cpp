506a177c2c4489e5d0abb9aefcf56ceeb9f141fb
Riccardo Mottola <rm@gnu.org>
2022-02-02 11:50:37 +0100

    fix missing semicolon

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -5273,7 +5273,7 @@ nsWindow::ProcessMessage(UINT msg, WPARAM& wParam, LPARAM& lParam,
           result = !Preferences::GetBool("mouse.button5.enabled", true);
           break;
         default:
-          break
+          break;
       }
       break;
 
