09793532d71840eb4ef1342f0064ee26b3e17270
Riccardo Mottola <rm@gnu.org>
2022-02-16 09:51:24 +0100

    Bug 1192412 - Part 1: Set class constructor name to class name. r=efaust

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -6189,6 +6189,10 @@ Parser<FullParseHandler>::classDefinition(YieldHandling yieldHandling,
           case PropertyType::Setter:
             funName = nullptr;
             break;
+          case PropertyType::Constructor:
+          case PropertyType::DerivedConstructor:
+            funName = name;
+            break;
           default:
             if (tokenStream.isCurrentTokenType(TOK_NAME))
                 funName = tokenStream.currentName();
