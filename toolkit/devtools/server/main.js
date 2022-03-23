26f259b441bf43b5361ee654eb3c16749074742e
Riccardo Mottola <rm@gnu.org>
2022-03-23 12:39:40 +0100

    Bug 1145049 - Stop leaking tab actors and root actor on disconnect. r=jryans

--- toolkit/devtools/server/main.js
+++ toolkit/devtools/server/main.js
@@ -1573,6 +1573,8 @@ DebuggerServerConnection.prototype = {
     this._extraPools.map(function(p) { p.cleanup(); });
     this._extraPools = null;
 
+    this.rootActor = null;
+    this._transport = null;
     DebuggerServer._connectionClosed(this);
   },
 
