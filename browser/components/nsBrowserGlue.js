ea64a1ef26f9cc4ab20c02ff2555723d887f4d25
Riccardo Mottola <rm@gnu.org>
2022-03-23 21:43:54 +0100

    Bug 1164640 - Reset the flag for disabling e10s when an a11y tool is active. r=mconley
    There's now a blacklist in place for the tools that should be disabled, so we want to give another change for users with tools that are not blacklisted to test e10s.

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2617,7 +2617,8 @@ let E10SUINotification = {
       let channelAuthorized = updateChannel == "nightly" || updateChannel == "aurora";
 
       skipE10sChecks = !channelAuthorized ||
-                       UpdateServices.prefs.getBoolPref("browser.tabs.remote.autostart.disabled-because-using-a11y");    } catch(e) {}
+                       Services.prefs.getBoolPref("browser.tabs.remote.disabled-for-a11y");
+    } catch(e) {}
 
     if (skipE10sChecks) {
       return;
@@ -2664,7 +2665,9 @@ let E10SUINotification = {
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsISupportsWeakReference]),
 
   observe: function(subject, topic, data) {
-    if (topic == "a11y-init-or-shutdown" && data == "1") {
+    if (topic == "a11y-init-or-shutdown"
+        && data == "1" &&
+        Services.appinfo.accessibilityIsBlacklistedForE10S) {
       this._showE10sAccessibilityWarning();
     }
   },
@@ -2739,7 +2742,15 @@ let E10SUINotification = {
   _warnedAboutAccessibility: false,
 
   _showE10sAccessibilityWarning: function() {
-    Services.prefs.setBoolPref("browser.tabs.remote.autostart.disabled-because-using-a11y", true);
+    try {
+      if (!Services.prefs.getBoolPref("browser.tabs.remote.disabled-for-a11y")) {
+        // Only return if the pref exists and was set to false, but not
+        // if the pref didn't exist (which will throw).
+        return;
+      }
+    } catch (e) { }
+
+    Services.prefs.setBoolPref("browser.tabs.remote.disabled-for-a11y", true);
 
     if (this._warnedAboutAccessibility) {
       return;
@@ -2757,8 +2768,8 @@ let E10SUINotification = {
 
     let promptMessage = "Multiprocess Nightly (e10s) does not yet support accessibility features. Multiprocessing will be disabled if you restart Firefox. Would you like to restart?";
     let mainAction = {
-      label: "Disable and Restart",
-      accessKey: "R",
+      label: win.gNavigatorBundle.getString("e10s.accessibilityNotice.disableAndRestart.label"),
+      accessKey: win.gNavigatorBundle.getString("e10s.accessibilityNotice.disableAndRestart.accesskey"),
       callback: function () {
         // Restart the app
         let cancelQuit = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
@@ -2770,10 +2781,10 @@ let E10SUINotification = {
     };
     let secondaryActions = [
       {
-        label: "Don't Disable",
-        accessKey: "D",
+        label: win.gNavigatorBundle.getString("e10s.accessibilityNotice.dontDisable.label"),
+        accessKey: win.gNavigatorBundle.getString("e10s.accessibilityNotice.dontDisable.accesskey"),
         callback: function () {
-          Services.prefs.setBoolPref("browser.tabs.remote.autostart.disabled-because-using-a11y", false);
+          Services.prefs.setBoolPref("browser.tabs.remote.disabled-for-a11y", false);
         }
       }
     ];
