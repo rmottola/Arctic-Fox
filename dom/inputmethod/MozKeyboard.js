764b6cedd1f45d7da44dab9f59c9a86ef68530dc
Riccardo Mottola <rm@gnu.org>
2022-03-27 23:29:22 +0200

    Bug 1137557 - Part 2: Remove sendKeyEvent from forms.js. r=masayuki, r=smaug (to make the webidl hook happy)

--- dom/inputmethod/MozKeyboard.js
+++ dom/inputmethod/MozKeyboard.js
@@ -677,13 +677,15 @@ MozInputContext.prototype = {
 
   sendKey: function ic_sendKey(keyCode, charCode, modifiers, repeat) {
     let self = this;
+
+    // XXX: modifiers are ignored in this API method.
+
     return this._sendPromise(function(resolverId) {
       cpmmSendAsyncMessageWithKbID(self, 'Keyboard:SendKey', {
         contextId: self._contextId,
         requestId: resolverId,
         keyCode: keyCode,
         charCode: charCode,
-        modifiers: modifiers,
         repeat: repeat
       });
     });
