aaed79183d0bc7036b2149c689af905f1b90978e
Riccardo Mottola <rm@gnu.org>
2022-03-31 21:36:08 +0200

    Bug 1173069 - Don't accumulate the cache hit telemetry for intercepted channels; r=mayhemer,jdm
    These channels will always have a fake cache entry associated with them,
    so they can skew our telemetry results once we ship service workers.

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -483,7 +483,10 @@ nsHttpChannel::ContinueConnect()
                 event->Revoke();
             }
 
-            AccumulateCacheHitTelemetry(kCacheHit);
+            // Don't accumulate the cache hit telemetry for intercepted channels.
+            if (mInterceptCache != INTERCEPTED) {
+                AccumulateCacheHitTelemetry(kCacheHit);
+            }
 
             return rv;
         }
