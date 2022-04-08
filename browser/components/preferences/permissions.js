47352a32e95e4281585015a34ff65b45504635aa
Riccardo Mottola <rm@gnu.org>
2022-04-08 19:45:31 +0200

    partial Bug 1039540 - In-content preferences: Disable the sorting of rows ofsub-dialogs when right clicking; r=MattN

--- browser/components/preferences/permissions.js
+++ browser/components/preferences/permissions.js
@@ -249,6 +249,20 @@ var gPermissionManager = {
     var urlLabel = document.getElementById("urlLabel");
     urlLabel.hidden = !urlFieldVisible;
 
+    let treecols = document.getElementsByTagName("treecols")[0];
+    treecols.addEventListener("click", event => {
+      if (event.target.nodeName != "treecol" || event.button != 0) {
+        return;
+      }
+
+      let sortField = event.target.getAttribute("data-field-name");
+      if (!sortField) {
+        return;
+      }
+
+      gPermissionManager.onPermissionSort(sortField);
+    });
+
     Services.obs.notifyObservers(null, NOTIFICATION_FLUSH_PERMISSIONS, this._type);
     Services.obs.addObserver(this, "perm-changed", false);
 
