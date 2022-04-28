7c067e2b1cd32fdc298cda0604538a13a5abe612
Riccardo Mottola <rm@gnu.org>
2022-04-28 13:16:57 +0200

    Bug 1200980 part.4 nsPlaintextEditor should notify editor observers of the end of edit action when NS_COMPOSITION_CHANGE isn't followed by NS_COMPOSITION_END r=smaug

--- editor/libeditor/nsEditor.cpp
+++ editor/libeditor/nsEditor.cpp
@@ -1856,6 +1856,9 @@ nsEditor::NotifyEditorObservers(NotificationForEditorObservers aNotification)
       FireInputEvent();
       break;
     case eNotifyEditorObserversOfBefore:
+      if (NS_WARN_IF(mIsInEditAction)) {
+        break;
+      }
       mIsInEditAction = true;
       for (auto& observer : observers) {
         observer->BeforeEditAction();
