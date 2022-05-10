0e843a3e0be43422d9c3a39930568cc95879bd0f
Riccardo Mottola <rm@gnu.org>
2022-05-10 07:59:04 +0200

    Bug 1128472 - Part 1. Mac and Win for model, stepping, cores, cache, cpu speed; VM max on Win only, vendor on Mac only. r=gfritzsche

--- xpcom/base/nsObjCExceptions.h
+++ xpcom/base/nsObjCExceptions.h
@@ -17,6 +17,12 @@
 #import <ExceptionHandling/NSExceptionHandler.h>
 #endif
 
+#if defined(MOZ_CRASHREPORTER) && defined(__cplusplus)
+#include "nsICrashReporter.h"
+#include "nsCOMPtr.h"
+#include "nsServiceManagerUtils.h"
+#endif
+
 #include <unistd.h>
 #include <signal.h>
 #include "nsError.h"
@@ -40,6 +46,15 @@ nsObjCExceptionLog(NSException* aException)
 {
   NSLog(@"Mozilla has caught an Obj-C exception [%@: %@]",
         [aException name], [aException reason]);
+
+#if defined(MOZ_CRASHREPORTER) && defined(__cplusplus)
+  // Attach exception info to the crash report.
+  nsCOMPtr<nsICrashReporter> crashReporter =
+    do_GetService("@mozilla.org/toolkit/crash-reporter;1");
+  if (crashReporter) {
+    crashReporter->AppendObjCExceptionInfoToAppNotes(static_cast<void*>(aException));
+  }
+#endif
 
 #ifdef DEBUG
   @try {
