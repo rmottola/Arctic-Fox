6da77e3cadb0b7b22444b9e11cf68f9b616d781c
Riccardo Mottola <rm@gnu.org>
2022-05-13 14:07:12 +0200

    Bug 1060609 (Part 1) - Disable downscale-during-decode when HQ scaling is disabled. r=tn

--- image/RasterImage.cpp
+++ image/RasterImage.cpp
@@ -1585,6 +1585,7 @@ RasterImage::CanDownscaleDuringDecode(const IntSize& aSize, uint32_t aFlags)
   // image, we have all the source data and know our size, the flags allow us to
   // do it, and a 'good' filter is being used.
   if (!mDownscaleDuringDecode || !mHasSize ||
+      !gfxPrefs::ImageHQDownscalingEnabled() ||
       !(aFlags & imgIContainer::FLAG_HIGH_QUALITY_SCALING)) {
     return false;
   }
