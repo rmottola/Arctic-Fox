4635ccf49241fe36fff659da384fa82982ddac93
Riccardo Mottola <rm@gnu.org>
2022-06-20 12:09:25 +0200

    Bug 912121 - Create shims for popular DevTools modules in add-ons. rs=devtools
    For popular modules used by many DevTools add-ons, add shim files which wrap the
    modules and make them available at their previous location.
    
    Each shim includes a deprecation warning to make devs and users aware of the
    issue.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1178,6 +1178,11 @@ pref("devtools.devices.url", "https://code.cdn.mozilla.net/devices/devices.json"
 // URL of the remote JSON catalog used for device simulation
 pref("devtools.devices.url", "https://code.cdn.mozilla.net/devices/devices.json");
 
+
+// Deprecation warnings after DevTools file migration.  Bug 1204127 tracks
+// enabling this.
+pref("devtools.migration.warnings", false);
+
 // view source
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
