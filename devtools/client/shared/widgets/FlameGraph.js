993c41aae5c344a3d7782927b27fe51e1a95fb2e
Riccardo Mottola <rm@gnu.org>
2022-06-24 00:01:21 +0200

    Bug 1141615 - Use before declaration for scaledStep in FlameGraph.jsm, r=jsantell

--- devtools/client/shared/widgets/FlameGraph.js
+++ devtools/client/shared/widgets/FlameGraph.js
@@ -937,10 +937,10 @@ FlameGraph.prototype = {
     }
 
     while (true) {
+      let scaledStep = dataScale * timingStep;
       if (++numIters > maxIters) {
         return scaledStep;
       }
-      let scaledStep = dataScale * timingStep;
       if (scaledStep < spacingMin) {
         timingStep <<= 1;
         continue;
