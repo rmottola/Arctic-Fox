f298c7691c9ce43b94c55dc944e23630eff1284a
Riccardo Mottola <rm@gnu.org>
2022-07-19 21:34:16 +0200

    missing comments and bit of Bug 1157293 - Part 3

--- devtools/client/styleinspector/computed-view.js
+++ devtools/client/styleinspector/computed-view.js
@@ -4,7 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* globals _Iterator, StopIteration */
+/* globals overlays, StyleInspectorMenu, loader, clipboardHelper,
+  _Iterator, StopIteration */
 
 "use strict";
 
