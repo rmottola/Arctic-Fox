61e1c717107caad50036bca2d3b8f51912c59300
Riccardo Mottola <rm@gnu.org>
2022-08-04 01:57:10 +0200

    Bug 1168091 - Initialize startLine and startColumn members in FunctionBox constructor, r=jorendorff.

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -656,6 +656,8 @@ FunctionBox::FunctionBox(ExclusiveContext* cx, ObjectBox* traceListHead, JSFunct
     enclosingStaticScope_(enclosingStaticScope),
     bufStart(0),
     bufEnd(0),
+    startLine(1),
+    startColumn(0),
     length(0),
     generatorKindBits_(GeneratorKindAsBits(generatorKind)),
     inGenexpLambda(false),
