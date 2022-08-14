197118cf95ba20c5f02f4b39c2fe06fcc1be4c08
Riccardo Mottola <rm@gnu.org>
2022-08-14 10:15:50 +0200

    Bug 1203920 - Get rid of JS_SetStructuredCloneCallbacks, r=sfink

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1084,9 +1084,6 @@ typedef void
 JS_FRIEND_API(void)
 SetActivityCallback(JSRuntime* rt, ActivityCallback cb, void* arg);
 
-extern JS_FRIEND_API(const JSStructuredCloneCallbacks*)
-GetContextStructuredCloneCallbacks(JSContext* cx);
-
 typedef bool
 (* DOMInstanceClassHasProtoAtDepth)(const Class* instanceClass,
                                     uint32_t protoID, uint32_t depth);
