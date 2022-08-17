30b4e9d4fa14ba5b630068c58010bd902273fd2e
Riccardo Mottola <rm@gnu.org>
2022-08-18 00:35:09 +0200

    Bug 1173939 - Race condition in browser.urlbar.suggest. preferences handlers. r=adw

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -351,19 +351,6 @@ var gPrivacyPane = {
     return getVal("default.behavior") & 3;
   },
 
-  /**
-   * Update browser.urlbar.autocomplete.enabled when a
-   * browser.urlbar.suggest.* pref is changed from the ui.
-   */
-  writeSuggestionPref: function PPP_writeSuggestionPref() {
-    let getVal = (aPref) => {
-      return document.getElementById("browser.urlbar.suggest." + aPref).value;
-    }
-    // autocomplete.enabled is true if any of the suggestions is true
-    let enabled = ["history", "bookmark", "openpage"].map(getVal).some(v => v);
-    Services.prefs.setBoolPref("browser.urlbar.autocomplete.enabled", enabled);
-  },
-
   /*
    * Preferences:
    *
