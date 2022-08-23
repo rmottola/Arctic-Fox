84838869364a986b305ca8421a0cdc444699313d
Riccardo Mottola <rm@gnu.org>
2022-08-23 15:49:20 +0200

    Bug 1193519 pt 10 - Clean up remaining mentions of 'sideways-left' in code comments. r=dholbert

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -1199,7 +1199,7 @@ nsImageFrame::DisplayAltText(nsPresContext*      aPresContext,
       nscoord x, y;
 
       if (isVertical) {
-        x = pt.x + maxDescent; // XXX will need update for sideways-left
+        x = pt.x + maxDescent;
         if (wm.IsBidiLTR()) {
           y = aRect.y;
           dir = NSBIDI_LTR;
