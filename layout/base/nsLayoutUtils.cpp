ece6e331d10a7ec860e29430f684ce5752348f8a
Riccardo Mottola <rm@gnu.org>
2022-08-23 13:44:39 +0200

    Bug 1193519 pt 5 - Expose the sideways-lr value for writing-mode to CSS. r=dholbert

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -6680,10 +6680,9 @@ nsLayoutUtils::GetTextRunOrientFlagsForStyle(nsStyleContext* aStyleContext)
       return 0;
     }
 
-  /* not yet implemented:
   case NS_STYLE_WRITING_MODE_SIDEWAYS_LR:
     return gfxTextRunFactory::TEXT_ORIENT_VERTICAL_SIDEWAYS_LEFT;
-  */
+
   case NS_STYLE_WRITING_MODE_SIDEWAYS_RL:
     return gfxTextRunFactory::TEXT_ORIENT_VERTICAL_SIDEWAYS_RIGHT;
 
