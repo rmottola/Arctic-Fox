8e9c37b5a9daf3066e0a465b06ab6fac86cd12ab
Riccardo Mottola <rm@gnu.org>
2022-09-01 12:15:33 +0200

    let-var

--- browser/components/places/content/places.js
+++ browser/components/places/content/places.js
@@ -1413,7 +1413,7 @@ var ViewMenu = {
   }
 }
 
-let ContentArea = {
+var ContentArea = {
   _specialViews: new Map(),
 
   init: function CA_init() {
@@ -1551,7 +1551,7 @@ let ContentArea = {
   }
 };
 
-let ContentTree = {
+var ContentTree = {
   init: function CT_init() {
     this._view = document.getElementById("placeContent");
   },
