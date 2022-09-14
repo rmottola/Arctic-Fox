7b3552e2baaabb01fe0067c3d557f0d70485e023
Riccardo Mottola <rm@gnu.org>
2022-09-14 14:14:46 +0200

    var/let

--- toolkit/components/viewsource/content/viewSource.js
+++ toolkit/components/viewsource/content/viewSource.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { utils: Cu, interfaces: Ci, classes: Cc } = Components;
+var { utils: Cu, interfaces: Ci, classes: Cc } = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/ViewSourceBrowser.jsm");
@@ -691,12 +691,12 @@ ViewSourceChrome.prototype = {
   },
 };
 
-let viewSourceChrome = new ViewSourceChrome();
+var viewSourceChrome = new ViewSourceChrome();
 
 /**
  * PrintUtils uses this to make Print Preview work.
  */
-let PrintPreviewListener = {
+var PrintPreviewListener = {
   _ppBrowser: null,
 
   getPrintPreviewBrowser() {
