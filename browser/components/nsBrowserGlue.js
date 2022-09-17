9073ba4d465dd279e991cb8b3cd05839e9ab1682
Riccardo Mottola <rm@gnu.org>
2022-09-17 19:58:52 +0200

    Bug 1172710: Breaking an add-on's signature doesn't show a notification on next startup. r=dao

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -940,12 +940,14 @@ BrowserGlue.prototype = {
     }
 
     let disabledAddons = AddonManager.getStartupChanges(AddonManager.STARTUP_CHANGE_DISABLED);
-    for (let id of disabledAddons) {
-      if (AddonManager.getAddonByID(id).signedState <= AddonManager.SIGNEDSTATE_MISSING) {
-        this._notifyUnsignedAddonsDisabled();
-        break;
+    AddonManager.getAddonsByIDs(disabledAddons, (addons) => {
+      for (let addon of addons) {
+        if (addon.signedState <= AddonManager.SIGNEDSTATE_MISSING) {
+          this._notifyUnsignedAddonsDisabled();
+          break;
+        }
       }
-    }
+    });
 
     this._firstWindowTelemetry(aWindow);
   },
