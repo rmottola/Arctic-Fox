8808df4ba70165d1f3eb7629540f9ec1d2837e12
Riccardo Mottola <rm@gnu.org>
2022-09-19 20:11:40 +0200

    No Bug - Followup fix for build failure on a CLOSED TREE; r=bustage, a=RyanVM

--- js/src/jit/MacroAssembler-inl.h
+++ js/src/jit/MacroAssembler-inl.h
@@ -45,7 +45,7 @@ MacroAssembler::setFramePushed(uint32_t framePushed)
 void
 MacroAssembler::adjustFrame(int32_t value)
 {
-    MOZ_ASSERT_IF(value < 0, framePushed_ >= -value);
+    MOZ_ASSERT_IF(value < 0, framePushed_ >= uint32_t(-value));
     setFramePushed(framePushed_ + value);
 }
 
