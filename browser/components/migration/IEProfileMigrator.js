5cdbb839b93a4feb9abeddb0149ce39004e58912
Riccardo Mottola <rm@gnu.org>
2022-09-26 23:42:24 +0200

    Bug 1192036 - import cookies from Edge, r=MattN

--- browser/components/migration/IEProfileMigrator.js
+++ browser/components/migration/IEProfileMigrator.js
@@ -20,116 +20,8 @@ Cu.import("resource:///modules/MSMigrationUtils.jsm");
 
 XPCOMUtils.defineLazyModuleGetter(this, "PlacesUtils",
                                   "resource://gre/modules/PlacesUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "ctypes",
-                                  "resource://gre/modules/ctypes.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "WindowsRegistry",
                                   "resource://gre/modules/WindowsRegistry.jsm");
-
-Cu.importGlobalProperties(["File"]);
-
-////////////////////////////////////////////////////////////////////////////////
-//// Helpers.
-
-let CtypesHelpers = {
-  _structs: {},
-  _functions: {},
-  _libs: {},
-
-  /**
-   * Must be invoked once before first use of any of the provided helpers.
-   */
-  initialize: function CH_initialize() {
-    const WORD = ctypes.uint16_t;
-    const DWORD = ctypes.uint32_t;
-    const BOOL = ctypes.int;
-
-    this._structs.SYSTEMTIME = new ctypes.StructType('SYSTEMTIME', [
-      {wYear: WORD},
-      {wMonth: WORD},
-      {wDayOfWeek: WORD},
-      {wDay: WORD},
-      {wHour: WORD},
-      {wMinute: WORD},
-      {wSecond: WORD},
-      {wMilliseconds: WORD}
-    ]);
-
-    this._structs.FILETIME = new ctypes.StructType('FILETIME', [
-      {dwLowDateTime: DWORD},
-      {dwHighDateTime: DWORD}
-    ]);
-
-    try {
-      this._libs.kernel32 = ctypes.open("Kernel32");
-      this._functions.FileTimeToSystemTime =
-        this._libs.kernel32.declare("FileTimeToSystemTime",
-                                    ctypes.default_abi,
-                                    BOOL,
-                                    this._structs.FILETIME.ptr,
-                                    this._structs.SYSTEMTIME.ptr);
-    } catch (ex) {
-      this.finalize();
-    }
-  },
-
-  /**
-   * Must be invoked once after last use of any of the provided helpers.
-   */
-  finalize: function CH_finalize() {
-    this._structs = {};
-    this._functions = {};
-    for each (let lib in this._libs) {
-      try {
-        lib.close();
-      } catch (ex) {}
-    }
-    this._libs = {};
-  },
-
-  /**
-   * Converts a FILETIME struct (2 DWORDS), to a SYSTEMTIME struct.
-   *
-   * @param aTimeHi
-   *        Least significant DWORD.
-   * @param aTimeLo
-   *        Most significant DWORD.
-   * @return a Date object representing the converted datetime.
-   */
-  fileTimeToDate: function CH_fileTimeToDate(aTimeHi, aTimeLo) {
-    let fileTime = this._structs.FILETIME();
-    fileTime.dwLowDateTime = aTimeLo;
-    fileTime.dwHighDateTime = aTimeHi;
-    let systemTime = this._structs.SYSTEMTIME();
-    let result = this._functions.FileTimeToSystemTime(fileTime.address(),
-                                                      systemTime.address());
-    if (result == 0)
-      throw new Error(ctypes.winLastError);
-
-    return new Date(systemTime.wYear,
-                    systemTime.wMonth - 1,
-                    systemTime.wDay,
-                    systemTime.wHour,
-                    systemTime.wMinute,
-                    systemTime.wSecond,
-                    systemTime.wMilliseconds);
-  }
-};
-
-/**
- * Checks whether an host is an IP (v4 or v6) address.
- *
- * @param aHost
- *        The host to check.
- * @return whether aHost is an IP address.
- */
-function hostIsIPAddress(aHost) {
-  try {
-    Services.eTLD.getBaseDomainFromHost(aHost);
-  } catch (e if e.result == Cr.NS_ERROR_HOST_IS_IP_ADDRESS) {
-    return true;
-  } catch (e) {}
-  return false;
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 //// Resources
@@ -224,150 +116,127 @@ History.prototype = {
   }
 };
 
-function Cookies() {
+function Settings() {
 }
 
-Cookies.prototype = {
-  type: MigrationUtils.resourceTypes.COOKIES,
+Settings.prototype = {
+  type: MigrationUtils.resourceTypes.SETTINGS,
 
-  get exists() !!this._cookiesFolder,
+  get exists() true,
 
-  __cookiesFolder: null,
-  get _cookiesFolder() {
-    // Cookies are stored in txt files, in a Cookies folder whose path varies
-    // across the different OS versions.  CookD takes care of most of these
-    // cases, though, in Windows Vista/7, UAC makes a difference.
-    // If UAC is enabled, the most common destination is CookD/Low.  Though,
-    // if the user runs the application in administrator mode or disables UAC,
-    // cookies are stored in the original CookD destination.  Cause running the
-    // browser in administrator mode is unsafe and discouraged, we just care
-    // about the UAC state.
-    if (!this.__cookiesFolder) {
-      let cookiesFolder = Services.dirsvc.get("CookD", Ci.nsIFile);
-      if (cookiesFolder.exists() && cookiesFolder.isReadable()) {
-        // Check if UAC is enabled.
-        if (Services.appinfo.QueryInterface(Ci.nsIWinAppHelper).userCanElevate) {
-          cookiesFolder.append("Low");
-        }
-        this.__cookiesFolder = cookiesFolder;
-      }
+  migrate: function S_migrate(aCallback) {
+    // Converts from yes/no to a boolean.
+    function yesNoToBoolean(v) v == "yes";
+
+    // Converts source format like "en-us,ar-kw;q=0.7,ar-om;q=0.3" into
+    // destination format like "en-us, ar-kw, ar-om".
+    // Final string is sorted by quality (q=) param.
+    function parseAcceptLanguageList(v) {
+      return v.match(/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/gi)
+              .sort(function (a , b) {
+                let qA = parseFloat(a.split(";q=")[1]) || 1.0;
+                let qB = parseFloat(b.split(";q=")[1]) || 1.0;
+                return qA < qB ? 1 : qA == qB ? 0 : -1;
+              })
+              .map(function(a) a.split(";")[0]);
     }
-    return this.__cookiesFolder;
-  },
 
-  migrate: function C_migrate(aCallback) {
-    CtypesHelpers.initialize();
+    // For reference on some of the available IE Registry settings:
+    //  * http://msdn.microsoft.com/en-us/library/cc980058%28v=prot.13%29.aspx
+    //  * http://msdn.microsoft.com/en-us/library/cc980059%28v=prot.13%29.aspx
 
-    let cookiesGenerator = (function genCookie() {
-      let success = false;
-      let entries = this._cookiesFolder.directoryEntries;
-      while (entries.hasMoreElements()) {
-        let entry = entries.getNext().QueryInterface(Ci.nsIFile);
-        // Skip eventual bogus entries.
-        if (!entry.isFile() || !/\.txt$/.test(entry.leafName))
-          continue;
+    // Note that only settings exposed in our UI should be migrated.
 
-        this._readCookieFile(entry, function(aSuccess) {
-          // Importing even a single cookie file is considered a success.
-          if (aSuccess)
-            success = true;
-          try {
-            cookiesGenerator.next();
-          } catch (ex) {}
-        });
+    this._set("Software\\Microsoft\\Internet Explorer\\International",
+              "AcceptLanguage",
+              "intl.accept_languages",
+              parseAcceptLanguageList);
+    // TODO (bug 745853): For now, only x-western font is translated.
+    this._set("Software\\Microsoft\\Internet Explorer\\International\\Scripts\\3",
+              "IEFixedFontName",
+              "font.name.monospace.x-western");
+    this._set(kMainKey,
+              "Use FormSuggest",
+              "browser.formfill.enable",
+              yesNoToBoolean);
+    this._set(kMainKey,
+              "FormSuggest Passwords",
+              "signon.rememberSignons",
+              yesNoToBoolean);
+    this._set(kMainKey,
+              "Anchor Underline",
+              "browser.underline_anchors",
+              yesNoToBoolean);
+    this._set(kMainKey,
+              "Display Inline Images",
+              "permissions.default.image",
+              function (v) yesNoToBoolean(v) ? 1 : 2);
+    this._set(kMainKey,
+              "Move System Caret",
+              "accessibility.browsewithcaret",
+              yesNoToBoolean);
+    this._set("Software\\Microsoft\\Internet Explorer\\Settings",
+              "Always Use My Colors",
+              "browser.display.document_color_use",
+              function (v) !Boolean(v) ? 0 : 2);
+    this._set("Software\\Microsoft\\Internet Explorer\\Settings",
+              "Always Use My Font Face",
+              "browser.display.use_document_fonts",
+              function (v) !Boolean(v));
+    this._set(kMainKey,
+              "SmoothScroll",
+              "general.smoothScroll",
+              Boolean);
+    this._set("Software\\Microsoft\\Internet Explorer\\TabbedBrowsing\\",
+              "WarnOnClose",
+              "browser.tabs.warnOnClose",
+              Boolean);
+    this._set("Software\\Microsoft\\Internet Explorer\\TabbedBrowsing\\",
+              "OpenInForeground",
+              "browser.tabs.loadInBackground",
+              function (v) !Boolean(v));
 
-        yield;
-      }
-
-      CtypesHelpers.finalize();
-
-      aCallback(success);
-    }).apply(this);
-    cookiesGenerator.next();
-  },
-
-  _readCookieFile: function C__readCookieFile(aFile, aCallback) {
-    let fileReader = Cc["@mozilla.org/files/filereader;1"].
-                     createInstance(Ci.nsIDOMFileReader);
-    fileReader.addEventListener("loadend", (function onLoadEnd() {
-      fileReader.removeEventListener("loadend", onLoadEnd, false);
-
-      if (fileReader.readyState != fileReader.DONE) {
-        Cu.reportError("Could not read cookie contents: " + fileReader.error);
-        aCallback(false);
-        return;
-      }
-
-      let success = true;
-      try {
-        this._parseCookieBuffer(fileReader.result);
-      } catch (ex) {
-        Components.utils.reportError("Unable to migrate cookie: " + ex);
-        success = false;
-      } finally {
-        aCallback(success);
-      }
-    }).bind(this), false);
-    fileReader.readAsText(new File(aFile));
+    aCallback(true);
   },
 
   /**
-   * Parses a cookie file buffer and returns an array of the contained cookies.
-   *
-   * The cookie file format is a newline-separated-values with a "*" used as
-   * delimeter between multiple records.
-   * Each cookie has the following fields:
-   *  - name
-   *  - value
-   *  - host/path
-   *  - flags
-   *  - Expiration time most significant integer
-   *  - Expiration time least significant integer
-   *  - Creation time most significant integer
-   *  - Creation time least significant integer
-   *  - Record delimiter "*"
-   *
-   * @note All the times are in FILETIME format.
+   * Reads a setting from the Registry and stores the converted result into
+   * the appropriate Firefox preference.
+   * 
+   * @param aPath
+   *        Registry path under HKCU.
+   * @param aKey
+   *        Name of the key.
+   * @param aPref
+   *        Firefox preference.
+   * @param [optional] aTransformFn
+   *        Conversion function from the Registry format to the pref format.
    */
-  _parseCookieBuffer: function C__parseCookieBuffer(aTextBuffer) {
-    // Note the last record is an empty string.
-    let records = [r for each (r in aTextBuffer.split("*\n")) if (r)];
-    for (let record of records) {
-      let [name, value, hostpath, flags,
-           expireTimeLo, expireTimeHi] = record.split("\n");
+  _set: function S__set(aPath, aKey, aPref, aTransformFn) {
+    let value = WindowsRegistry.readRegKey(Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+                                           aPath, aKey);
+    // Don't import settings that have never been flipped.
+    if (value === undefined)
+      return;
 
-      // IE stores deleted cookies with a zero-length value, skip them.
-      if (value.length == 0)
-        continue;
+    if (aTransformFn)
+      value = aTransformFn(value);
 
-      let hostLen = hostpath.indexOf("/");
-      let host = hostpath.substr(0, hostLen);
-      let path = hostpath.substr(hostLen);
-
-      // For a non-null domain, assume it's what Mozilla considers
-      // a domain cookie.  See bug 222343.
-      if (host.length > 0) {
-        // Fist delete any possible extant matching host cookie.
-        Services.cookies.remove(host, name, path, false);
-        // Now make it a domain cookie.
-        if (host[0] != "." && !hostIsIPAddress(host))
-          host = "." + host;
-      }
-
-      let expireTime = CtypesHelpers.fileTimeToDate(Number(expireTimeHi),
-                                                    Number(expireTimeLo));
-      Services.cookies.add(host,
-                           path,
-                           name,
-                           value,
-                           Number(flags) & 0x1, // secure
-                           false, // httpOnly
-                           false, // session
-                           expireTime);
+    switch (typeof(value)) {
+      case "string":
+        Services.prefs.setCharPref(aPref, value);
+        break;
+      case "number":
+        Services.prefs.setIntPref(aPref, value);
+        break;
+      case "boolean":
+        Services.prefs.setBoolPref(aPref, value);
+        break;
+      default:
+        throw new Error("Unexpected value type: " + typeof(value));
     }
   }
 };
-
 
 ////////////////////////////////////////////////////////////////////////////////
 //// Migrator
@@ -382,7 +251,7 @@ IEProfileMigrator.prototype.getResources = function IE_getResources() {
   let resources = [
     MSMigrationUtils.getBookmarksMigrator()
   , new History()
-  , new Cookies()
+  , MSMigrationUtils.getCookiesMigrator()
   , new Settings()
   ];
   return [r for each (r in resources) if (r.exists)];
