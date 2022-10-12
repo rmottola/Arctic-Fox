af69507f238925975963528ca8abe030fff1a909
Riccardo Mottola <rm@gnu.org>
2022-10-13 00:22:57 +0200

    Bug 1039528 - [e10s] Make "Inspect element" context menu item work (r=pbrosset)# Please enter the commit message for your changes. Lines starting

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -556,7 +556,14 @@ nsContextMenu.prototype = {
     let tt = devtools.TargetFactory.forTab(gBrowser.selectedTab);
     return gDevTools.showToolbox(tt, "inspector").then(function(toolbox) {
       let inspector = toolbox.getCurrentPanel();
-      inspector.selection.setNode(this.target, "browser-context-menu");
+      if (this.isRemote) {
+        this.browser.messageManager.sendAsyncMessage("debug:inspect", {}, {node: this.target});
+        inspector.walker.findInspectingNode().then(nodeFront => {
+          inspector.selection.setNodeFront(nodeFront, "browser-context-menu");
+        });
+      } else {
+        inspector.selection.setNode(this.target, "browser-context-menu");
+      }
     }.bind(this));
   },
 
@@ -651,7 +658,7 @@ nsContextMenu.prototype = {
     }
 
     // Check if we are in a synthetic document (stand alone image, video, etc.).
-    this.inSyntheticDoc =  ownerDoc.mozSyntheticDocument;
+    this.inSyntheticDoc = ownerDoc.mozSyntheticDocument;
     // First, do checks for nodes that never have children.
     if (this.target.nodeType == Node.ELEMENT_NODE) {
       // See if the user clicked on an image. This check mirrors
