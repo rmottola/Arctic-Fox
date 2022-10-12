f7c819c6ae9aa8eee279606f3f3ab1c35d106236
Riccardo Mottola <rm@gnu.org>
2022-10-13 01:17:21 +0200

    Bug 1210408 - make nsMaiInterfaceAction work with proxies, r=tbsaunde

--- accessible/ipc/ProxyAccessible.cpp
+++ accessible/ipc/ProxyAccessible.cpp
@@ -933,6 +933,12 @@ ProxyAccessible::KeyboardShortcut()
   return KeyBinding(key, modifierMask);
 }
 
+void
+ProxyAccessible::AtkKeyBinding(nsString& aBinding)
+{
+  unused << mDoc->SendAtkKeyBinding(mID, &aBinding);
+}
+
 double
 ProxyAccessible::CurValue()
 {
