587979ca8a6fd983415889c3152645750298f2e6
Riccardo Mottola <rm@gnu.org>
2022-10-17 13:34:27 +0200

    Bug 1201393. Remove usage of FLAG_BLOCK_INPUT from MediaRecorder. r=jwwang
    FLAG_BLOCK_INPUT is not needed on mPipeStream because nothing should cause
    mPipeStream to block.

--- dom/media/MediaRecorder.cpp
+++ dom/media/MediaRecorder.cpp
@@ -777,7 +777,7 @@ MediaRecorder::MediaRecorder(AudioNode& aSrcAudioNode,
     AudioNodeStream* ns = aSrcAudioNode.GetStream();
     if (ns) {
       mInputPort = mPipeStream->AllocateInputPort(aSrcAudioNode.GetStream(),
-                                                  MediaInputPort::FLAG_BLOCK_INPUT,
+                                                  0,
                                                   0,
                                                   aSrcOutput);
     }
