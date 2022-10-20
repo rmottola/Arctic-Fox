bbd192727e1006ac41e3ac00ce4c6ee6ad12a313
Riccardo Mottola <rm@gnu.org>
2022-10-20 10:43:40 +0200

    Bug 1070216 - Properly manage lifetime of allocated CaptureDevices. r=jib
    We currently avoid Deallocating a CaptureDevice used for multiple gUMStreams
    when one of them calls Deallocate() by keeping track of how many called
    Start().
    
    The normal lifetime sequence however, is:
    Allocate()
    Start()
    Stop()
    Deallocate()
    
    This patches fixes the lifetime management by keeping track of how many
    users of the CaptureDevice called Allocate().

--- dom/media/webrtc/MediaEngineWebRTC.h
+++ dom/media/webrtc/MediaEngineWebRTC.h
@@ -135,6 +135,7 @@ public:
     , mThread(aThread)
     , mCapIndex(aIndex)
     , mChannel(-1)
+    , mNrAllocations(0)
     , mInitDone(false)
     , mStarted(false)
     , mSampleFrequency(MediaEngine::DEFAULT_SAMPLE_RATE)
@@ -215,10 +216,11 @@ private:
   // from kStarted to kStopped (which are combined with EndTrack()).
   // mSources[] is accessed from webrtc threads.
   Monitor mMonitor;
-  nsTArray<RefPtr<SourceMediaStream>> mSources; // When this goes empty, we shut down HW
+  nsTArray<RefPtr<SourceMediaStream>> mSources;
   nsCOMPtr<nsIThread> mThread;
   int mCapIndex;
   int mChannel;
+  int mNrAllocations; // When this becomes 0, we shut down HW
   TrackID mTrackID;
   bool mInitDone;
   bool mStarted;
@@ -226,7 +228,6 @@ private:
   nsString mDeviceName;
   nsCString mDeviceUUID;
 
-  uint32_t mSampleFrequency;
   bool mEchoOn, mAgcOn, mNoiseOn;
   webrtc::EcModes  mEchoCancel;
   webrtc::AgcModes mAGC;
