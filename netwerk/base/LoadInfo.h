15089bb64ba27f237fde1ba9e3f51a068046d5be
Riccardo Mottola <rm@gnu.org>
2022-10-25 12:27:57 +0200

    Bug 1182571: Make nsXMLHttpRequest use AsyncOpen2. r=ehsan

--- netwerk/base/LoadInfo.h
+++ netwerk/base/LoadInfo.h
@@ -17,6 +17,7 @@
 #include "mozilla/BasePrincipal.h"
 
 class nsINode;
+class nsXMLHttpRequest;
 
 namespace mozilla {
 
@@ -77,6 +78,12 @@ private:
     nsILoadInfo** outLoadInfo);
 
   ~LoadInfo();
+
+  // This function is the *only* function which can change the securityflags
+  // of a loadinfo. It only exists because of the XHR code. Don't call it
+  // from anywhere else!
+  void SetWithCredentialsSecFlag();
+  friend class ::nsXMLHttpRequest;
 
   nsCOMPtr<nsIPrincipal>           mLoadingPrincipal;
   nsCOMPtr<nsIPrincipal>           mTriggeringPrincipal;
