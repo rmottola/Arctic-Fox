1b2d1d154e3fa939d86e620f2b302e8da531e8a8
Riccardo Mottola <rm@gnu.org>
2022-11-02 13:33:00 +0100

    Bug 1192921: Add an install location for system add-ons. r=rhelmer
    This adds two new directory install locations. One contains the default system
    add-ons that ship with the application, the other contains system add-on that
    will eventually be updatable at runtime.
    
    The updatable location tracks the expected list of add-ons in a pref. and only
    returns add-ons from that list when asked for its list of add-ons.
    
    After processFileChanges has scanned all add-ons and updated the database it
    checks if the updated system add-ons match the expected set. If not we ignore
    those add-ons when working out which add-ons should be visible. If they do match
    then we ignore the app-shipped system add-ons when working out which are
    visible.

--- toolkit/mozapps/extensions/test/xpcshell/head_addons.js
+++ toolkit/mozapps/extensions/test/xpcshell/head_addons.js
@@ -1027,7 +1027,7 @@ function getFileForAddon(aDir, aId) {
 function registerDirectory(aKey, aDir) {
   var dirProvider = {
     getFile: function(aProp, aPersistent) {
-      aPersistent.value = true;
+      aPersistent.value = false;
       if (aProp == aKey)
         return aDir.clone();
       return null;
