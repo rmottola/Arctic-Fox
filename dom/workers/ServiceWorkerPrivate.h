475c185ee421765e8b821e9064f8923239e74c89
Riccardo Mottola <rm@gnu.org>
2022-11-17 01:05:27 +0100

    Bug 1207727 - Rework updating service workers according to the latest spec. r=bkelly

--- dom/workers/ServiceWorkerPrivate.h
+++ dom/workers/ServiceWorkerPrivate.h
@@ -82,7 +82,8 @@ public:
                      nsIRunnable* aLoadFailure);
 
   nsresult
-  SendPushEvent(const Maybe<nsTArray<uint8_t>>& aData);
+  SendPushEvent(const Maybe<nsTArray<uint8_t>>& aData,
+                ServiceWorkerRegistrationInfo* aRegistration);
 
   nsresult
   SendPushSubscriptionChangeEvent();
