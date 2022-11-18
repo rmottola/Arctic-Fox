03abe63e67637b150837b01dafb8f7c0216891be
Riccardo Mottola <rm@gnu.org>
2022-11-18 01:38:22 +0100

    Bug 1209765 (Part 2) - Support sync decoding and track draw results in nsDisplayBorder. r=tn

--- layout/base/nsDisplayListInvalidation.h
+++ layout/base/nsDisplayListInvalidation.h
@@ -171,7 +171,9 @@ public:
   bool mHasRoundedCorners;
 };
 
-class nsDisplayBorderGeometry : public nsDisplayItemGeometry
+class nsDisplayBorderGeometry
+  : public nsDisplayItemGeometry
+  , public nsImageGeometryMixin<nsDisplayBorderGeometry>
 {
 public:
   nsDisplayBorderGeometry(nsDisplayItem* aItem, nsDisplayListBuilder* aBuilder);
