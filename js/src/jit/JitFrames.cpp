6ed0eebc651a269a47b330ab531c044e1ed62a9d
Riccardo Mottola <rm@gnu.org>
2022-11-20 20:41:20 +0100

    Bug 1172076 - Assert that SnapshotIterator::initInstructionResults always match the recover instructions data. r=jandem

--- js/src/jit/JitFrames.cpp
+++ js/src/jit/JitFrames.cpp
@@ -1744,6 +1744,14 @@ RInstructionResults::isInitialized() const
     return initialized_;
 }
 
+#ifdef DEBUG
+size_t
+RInstructionResults::length() const
+{
+    return results_->length();
+}
+#endif
+
 JitFrameLayout*
 RInstructionResults::frame() const
 {
@@ -2210,6 +2218,7 @@ SnapshotIterator::initInstructionResults(MaybeReadFallback& fallback)
     }
 
     MOZ_ASSERT(results->isInitialized());
+    MOZ_ASSERT(results->length() == recover_.numInstructions() - 1);
     instructionResults_ = results;
     return true;
 }
