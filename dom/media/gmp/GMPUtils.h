3f8ba45b9fff63a3e6c8934c5d225fc44dabd7fa
Riccardo Mottola <rm@gnu.org>
2022-11-23 10:23:13 +0100

    Bug 1155178 - Part 1: Convert GMPUnique into a template alias; r=cpearce

--- dom/media/gmp/GMPUtils.h
+++ dom/media/gmp/GMPUtils.h
@@ -15,18 +15,15 @@ class nsCString;
 namespace mozilla {
 
 template<typename T>
-struct GMPUniqueDestroyPolicy
+struct DestroyPolicy
 {
   void operator()(T* aGMPObject) const {
     aGMPObject->Destroy();
   }
 };
 
-// Ideally, this would be a template alias, but GCC 4.6 doesn't support them.  See bug 1124021.
 template<typename T>
-struct GMPUnique {
-  typedef mozilla::UniquePtr<T, GMPUniqueDestroyPolicy<T>> Ptr;
-};
+using GMPUnique = mozilla::UniquePtr<T, DestroyPolicy<T>>;
 
 bool GetEMEVoucherPath(nsIFile** aPath);
 
