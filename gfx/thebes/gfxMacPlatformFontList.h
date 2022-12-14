da06064b8604d86d1caa7d3b4b14ed5450934bf1
Riccardo Mottola <rm@gnu.org>
2022-12-14 20:32:16 +0100

    Bug 1201403 - streamline MacOSFontEntry::HasFontTable implementation. r=jfkthame

--- gfx/thebes/gfxMacPlatformFontList.h
+++ gfx/thebes/gfxMacPlatformFontList.h
@@ -68,6 +68,7 @@ protected:
     bool mRequiresAAT;
     bool mIsCFF;
     bool mIsCFFInitialized;
+    nsTHashtable<nsUint32HashKey> mAvailableTables;
 };
 
 class gfxMacPlatformFontList : public gfxPlatformFontList {
