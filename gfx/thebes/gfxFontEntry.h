f56f0507d07253f8a965c575d4da4a7f1cffa418
Riccardo Mottola <rm@gnu.org>
2022-12-15 01:37:50 +0100

    Bug 1160506 - support intra-family font fallback. r=heycam
    Fontconfig allows for fonts to be arbitrarily mapped into another font
    family. Under some versions of Debian and Fedora, families like Droid
    Sans Japanese were mapped into the Droid Sans family. To match these
    font families properly, it's necessary to test multiple fonts within a
    single given family. Since this is a relatively time consuming
    procedure, only do this when there are multiple regular faces for a
    given family.

--- gfx/thebes/gfxFontEntry.h
+++ gfx/thebes/gfxFontEntry.h
@@ -664,7 +664,8 @@ public:
         mIsSimpleFamily(false),
         mIsBadUnderlineFamily(false),
         mFamilyCharacterMapInitialized(false),
-        mSkipDefaultFeatureSpaceCheck(false)
+        mSkipDefaultFeatureSpaceCheck(false),
+        mCheckForFallbackFaces(false)
         { }
 
     const nsString& Name() { return mName; }
@@ -768,6 +769,7 @@ public:
     }
 
     bool IsBadUnderlineFamily() const { return mIsBadUnderlineFamily; }
+    bool CheckForFallbackFaces() const { return mCheckForFallbackFaces; }
 
     // sort available fonts to put preferred (standard) faces towards the end
     void SortAvailableFonts();
@@ -823,6 +825,7 @@ protected:
     bool mIsBadUnderlineFamily : 1;
     bool mFamilyCharacterMapInitialized : 1;
     bool mSkipDefaultFeatureSpaceCheck : 1;
+    bool mCheckForFallbackFaces : 1;  // check other faces for character
 
     enum {
         // for "simple" families, the faces are stored in mAvailableFonts
