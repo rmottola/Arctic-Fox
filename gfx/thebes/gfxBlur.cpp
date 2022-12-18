335259200c6e1ca5ec7d5443756e781f8e7f0294
Riccardo Mottola <rm@gnu.org>
2022-12-18 18:15:18 +0100

    Bug 1216506. Properly initialize min inset box shadow color to black. r=mstange

--- gfx/thebes/gfxBlur.cpp
+++ gfx/thebes/gfxBlur.cpp
@@ -933,7 +933,8 @@ gfxAlphaBoxBlur::GetInsetBlur(IntMargin& aExtendDestBy,
                                                 ToRect(innerRect), aHasBorderRadius,
                                                 aInnerClipRadii);
 
-  minGfxContext->SetColor(aShadowColor);
+  Color black(0.f, 0.f, 0.f, 1.f);
+  minGfxContext->SetColor(black);
   minGfxContext->SetPath(maskPath);
   minGfxContext->Fill();
 
