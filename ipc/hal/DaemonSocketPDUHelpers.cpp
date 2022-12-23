4c745de79c966bf963bddfb294f15c580998938c
Riccardo Mottola <rm@gnu.org>
2022-12-23 13:32:20 +0100

    Bug 1202704: Move Bluetooth IPC pack functions to generic HAL IPC, r=shuang

--- ipc/hal/DaemonSocketPDUHelpers.cpp
+++ ipc/hal/DaemonSocketPDUHelpers.cpp
@@ -209,6 +209,12 @@ Convert(size_t aIn, uint16_t& aOut)
 //
 
 nsresult
+PackPDU(bool aIn, DaemonSocketPDU& aPDU)
+{
+  return PackPDU(PackConversion<bool, uint8_t>(aIn), aPDU);
+}
+
+nsresult
 PackPDU(const DaemonSocketPDUHeader& aIn, DaemonSocketPDU& aPDU)
 {
   nsresult rv = PackPDU(aIn.mService, aPDU);
