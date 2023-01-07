55b60edfc20ecd57e3066966f25bf67fd974a160
Riccardo Mottola <rm@gnu.org>
2023-01-07 21:09:28 +0100

    Bug 980014 - Allow new tab grid layout to reduce rows/columns if the window can't fit 3x3 [r=adw]
    Remove newtab-rows and convert the grid to wrap/overflow tiles as necessary by setting the appropriate width and heights. Compute grid dimensions on load/preload and adjust for resizes.

--- browser/base/content/newtab/grid.js
+++ browser/base/content/newtab/grid.js
@@ -66,6 +66,26 @@ var gGrid = {
   },
 
   /**
+   * Handles all grid events.
+   */
+  handleEvent: function Grid_handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "load":
+        // Save the cell's computed height/width including margin and border
+        let refCell = document.querySelector(".newtab-cell");
+        this._cellMargin = parseFloat(getComputedStyle(refCell).marginTop) * 2;
+        this._cellHeight = refCell.offsetHeight + this._cellMargin;
+        this._cellWidth = refCell.offsetWidth + this._cellMargin;
+        this._resizeGrid();
+        break;
+
+      case "resize":
+        this._resizeGrid();
+        break;
+    }
+  },
+
+  /**
    * Refreshes the grid and re-creates all sites.
    */
   refresh: function Grid_refresh() {
@@ -80,6 +100,7 @@ var gGrid = {
 
     // Render the grid again.
     this._render();
+    this._resizeGrid();
   },
 
   /**
@@ -100,26 +121,30 @@ var gGrid = {
    * Creates the newtab grid.
    */
   _renderGrid: function Grid_renderGrid() {
-    let row = document.createElementNS(HTML_NAMESPACE, "div");
     let cell = document.createElementNS(HTML_NAMESPACE, "div");
-    row.classList.add("newtab-row");
     cell.classList.add("newtab-cell");
 
     // Clear the grid
     this._node.innerHTML = "";
 
-    // Creates the structure of one row
-    for (let i = 0; i < gGridPrefs.gridColumns; i++) {
-      row.appendChild(cell.cloneNode(true));
-    }
-    // Creates the grid
-    for (let j = 0; j < gGridPrefs.gridRows; j++) {
-      this._node.appendChild(row.cloneNode(true));
+    // Creates all the cells up to the maximum
+    for (let i = 0; i < gGridPrefs.gridColumns * gGridPrefs.gridRows; i++) {
+      this._node.appendChild(cell.cloneNode(true));
     }
 
     // (Re-)initialize all cells.
     let cellElements = this.node.querySelectorAll(".newtab-cell");
     this._cells = [new Cell(this, cell) for (cell of cellElements)];
+  },
+
+  /**
+   * Calculate the height for a number of rows up to the maximum rows
+   * @param rows Number of rows defaulting to the max
+   */
+  _computeHeight: function Grid_computeHeight(aRows) {
+    let {gridRows} = gGridPrefs;
+    aRows = aRows === undefined ? gridRows : Math.min(gridRows, aRows);
+    return aRows * this._cellHeight + GRID_BOTTOM_EXTRA;
   },
 
   /**
@@ -171,11 +196,22 @@ var gGrid = {
     this._renderSites();
   },
 
+  /**
+   * Make sure the correct number of rows and columns are visible
+   */
+  _resizeGrid: function Grid_resizeGrid() {
+    let availSpace = document.documentElement.clientHeight - this._cellMargin -
+                     document.querySelector("#newtab-margin-top").offsetHeight;
+    let visibleRows = Math.floor(availSpace / this._cellHeight);
+    this._node.style.height = this._computeHeight() + "px";
+    this._node.style.maxHeight = this._computeHeight(visibleRows) + "px";
+    this._node.style.maxWidth = gGridPrefs.gridColumns * this._cellWidth +
+                                GRID_WIDTH_EXTRA + "px";
+  },
+
   _shouldRenderGrid : function Grid_shouldRenderGrid() {
-    let rowsLength = this._node.querySelectorAll(".newtab-row").length;
     let cellsLength = this._node.querySelectorAll(".newtab-cell").length;
 
-    return (rowsLength != gGridPrefs.gridRows ||
-            cellsLength != (gGridPrefs.gridRows * gGridPrefs.gridColumns));
+    return cellsLength != (gGridPrefs.gridRows * gGridPrefs.gridColumns);
   }
 };
