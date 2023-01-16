2c84b88ff1a1e5a45c2282f5a0350400dd9e42ae
Riccardo Mottola <rm@gnu.org>
2023-01-16 12:18:14 +0100

    Bug 1092125 - part 2 - add non-scaling-stroke support to SVGLineElement::GetGeometryBounds. r=jwatt

--- layout/svg/nsSVGUtils.cpp
+++ layout/svg/nsSVGUtils.cpp
@@ -1200,7 +1200,7 @@ PathExtentsToMaxStrokeExtents(const gfxRect& aPathExtents,
   gfxMatrix outerSVGToUser;
   if (nsSVGUtils::GetNonScalingStrokeTransform(aFrame, &outerSVGToUser)) {
     outerSVGToUser.Invert();
-    matrix *= outerSVGToUser;
+    matrix.PreMultiply(outerSVGToUser);
   }
 
   double dx = style_expansion * (fabs(matrix._11) + fabs(matrix._21));
