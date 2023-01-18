742aa54a285f381e0367bde423d70a9f6506e688
Riccardo Mottola <rm@gnu.org>
2023-01-18 15:40:38 +0100

    Bug 1224403 (part 12) - Remove WidgetToScreenOffsetUntyped(). r=kats.
    There are few enough calls now that using WidgetToScreenOffset() +
    ToUnknownPoint() is a better approach.

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -1118,7 +1118,7 @@ PuppetWidget::GetWindowPosition()
 
 NS_METHOD
 PuppetWidget::GetScreenBoundsUntyped(nsIntRect &aRect) {
-  aRect.MoveTo(WidgetToScreenOffsetUntyped());
+  aRect.MoveTo(WidgetToScreenOffset().ToUnknownPoint());
   aRect.SizeTo(mBounds.Size());
   return NS_OK;
 }
