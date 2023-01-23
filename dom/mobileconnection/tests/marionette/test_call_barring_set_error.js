872c2cc056bc71e99d3d8253bcb40a31c30af098
Riccardo Mottola <rm@gnu.org>
2023-01-23 10:45:52 +0100

    Bug 1169160 - [MobileConnection] Support more call barring program: all service, outgoing service and incoming service. r=hsinyi,aknow

--- dom/mobileconnection/tests/marionette/test_call_barring_set_error.js
+++ dom/mobileconnection/tests/marionette/test_call_barring_set_error.js
@@ -1,5 +1,5 @@
 /* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = "head.js";
