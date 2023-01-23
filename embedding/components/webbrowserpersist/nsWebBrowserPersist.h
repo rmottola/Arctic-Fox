60b476c31347d26fa33e7c83cbbb4296899b61e6
Riccardo Mottola <rm@gnu.org>
2023-01-23 23:53:17 +0100

    Bug 1186794 (part 2) - Replace nsBaseHashtable::EnumerateRead() calls in embedding/ with iterators. r=bz.

--- embedding/components/webbrowserpersist/nsWebBrowserPersist.h
+++ embedding/components/webbrowserpersist/nsWebBrowserPersist.h
@@ -148,8 +148,6 @@ private:
         nsISupports *aKey, UploadData *aData, void* aClosure);
     static PLDHashOperator EnumFixRedirect(
         nsISupports *aKey, OutputData *aData, void* aClosure);
-    static PLDHashOperator EnumCountURIsToPersist(
-        const nsACString &aKey, URIData *aData, void* aClosure);
     static PLDHashOperator EnumCopyURIsToFlatMap(
         const nsACString &aKey, URIData *aData, void* aClosure);
 
