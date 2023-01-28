9bea51f31848596a6c096c33cafd042189a49473
Riccardo Mottola <rm@gnu.org>
2023-01-28 22:15:09 +0100

    Bug 1177023 - Log the IsRootContent flag when dumping framemetrics. r=botond

--- gfx/layers/LayersLogging.cpp
+++ gfx/layers/LayersLogging.cpp
@@ -164,6 +164,9 @@ AppendToString(std::stringstream& aStream, const FrameMetrics& m,
     if (m.GetScrollParentId() != FrameMetrics::NULL_SCROLL_ID) {
       AppendToString(aStream, m.GetScrollParentId(), "] [scrollParent=");
     }
+    if (m.IsRootContent()) {
+      aStream << "] [rcd";
+    }
     if (m.HasClipRect()) {
       AppendToString(aStream, m.ClipRect(), "] [clip=");
     }
@@ -183,8 +186,8 @@ AppendToString(std::stringstream& aStream, const FrameMetrics& m,
             m.GetScrollOffsetUpdated(), m.GetDoSmoothScroll(),
             m.GetScrollGeneration()).get();
     AppendToString(aStream, m.GetScrollParentId(), "] [p=");
-    aStream << nsPrintfCString("] [i=(%ld %lld)] }",
-            m.GetPresShellId(), m.GetScrollId()).get();
+    aStream << nsPrintfCString("] [i=(%ld %lld %d)] }",
+            m.GetPresShellId(), m.GetScrollId(), m.IsRootContent()).get();
   }
   aStream << sfx;
 }
