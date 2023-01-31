dd570d1e71a2a75cd9c2f60c9bd286ff8e09f97f
Riccardo Mottola <rm@gnu.org>
2023-01-31 08:52:52 +0100

    First version of patch 1469116, this helps fix patch 1209812 without skia.

--- gfx/2d/Types.h
+++ gfx/2d/Types.h
@@ -61,15 +61,15 @@ enum class SurfaceFormat : int8_t {
   // The following values are endian-independent synonyms. The _UINT32 suffix
   // indicates that the name reflects the layout when viewed as a uint32_t
   // value.
-#if MOZ_LITTLE_ENDIAN
+//#if MOZ_LITTLE_ENDIAN
   A8R8G8B8_UINT32 = B8G8R8A8,       // 0xAARRGGBB
   X8R8G8B8_UINT32 = B8G8R8X8        // 0x00RRGGBB
-#elif MOZ_BIG_ENDIAN
-  A8R8G8B8_UINT32 = A8R8G8B8,       // 0xAARRGGBB
-  X8R8G8B8_UINT32 = X8R8G8B8        // 0x00RRGGBB
-#else
-# error "bad endianness"
-#endif
+//#elif MOZ_BIG_ENDIAN
+//  A8R8G8B8_UINT32 = A8R8G8B8,       // 0xAARRGGBB
+//  X8R8G8B8_UINT32 = X8R8G8B8        // 0x00RRGGBB
+//#else
+//# error "bad endianness"
+//#endif
 };
 
 inline bool IsOpaque(SurfaceFormat aFormat)
