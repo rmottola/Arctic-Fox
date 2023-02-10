6b12c6d1210efd1eadefb39a7f0998f4e6a33f25
Riccardo Mottola <rm@gnu.org>
2023-02-10 15:36:51 +0100

    Bug 1114554 - Patch 9 - Fixed crash in b2g-desktop tests. r=nsm

--- dom/notification/Notification.cpp
+++ dom/notification/Notification.cpp
@@ -1238,6 +1238,15 @@ MainThreadNotificationObserver::Observe(nsISupports* aSubject, const char* aTopi
       }
     }
   } else if (!strcmp("alertfinished", aTopic)) {
+    // In b2g-desktop, if the app is closed, closing a notification still
+    // triggers the observer which might be alive even though the owner window
+    // was closed. Keeping this until we remove the close event (Bug 1139363)
+    // from implementation.
+    nsCOMPtr<nsPIDOMWindow> window = notification->GetOwner();
+    if (NS_WARN_IF(!window || !window->IsCurrentInnerWindow())) {
+      return NS_ERROR_FAILURE;
+    }
+
     notification->UnpersistNotification();
     notification->mIsClosed = true;
     notification->DispatchTrustedEvent(NS_LITERAL_STRING("close"));
