29df9ffb2d62ccbc0172ca6e5059cb82281c34d8
Riccardo Mottola <rm@gnu.org>
2023-02-12 10:36:40 +0100

    Bug 1166356 - Properly detect double-caching in nsXULPrototypeCache; r=ehsan

--- dom/xul/nsXULPrototypeCache.cpp
+++ dom/xul/nsXULPrototypeCache.cpp
@@ -322,7 +322,7 @@ nsXULPrototypeCache::AbortCaching()
     Flush();
 
     // Clear the cache set
-    mCacheURITable.Clear();
+    mStartupCacheURITable.Clear();
 }
 
 
@@ -427,7 +427,7 @@ nsXULPrototypeCache::FinishOutputStream(nsIURI* uri)
                                     &len);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    if (!mCacheURITable.GetEntry(uri)) {
+    if (!mStartupCacheURITable.GetEntry(uri)) {
         nsAutoCString spec(kXULCachePrefix);
         rv = PathifyURI(uri, spec);
         if (NS_FAILED(rv))
@@ -435,7 +435,7 @@ nsXULPrototypeCache::FinishOutputStream(nsIURI* uri)
         rv = sc->PutBuffer(spec.get(), buf, len);
         if (NS_SUCCEEDED(rv)) {
             mOutputStreamTable.Remove(uri);
-            mCacheURITable.RemoveEntry(uri);
+            mStartupCacheURITable.PutEntry(uri);
         }
     }
 
@@ -558,6 +558,7 @@ nsXULPrototypeCache::BeginCaching(nsIURI* aURI)
             // XXX This blows away work that other consumers (like
             // mozJSComponentLoader) have done, need more fine-grained control.
             startupCache->InvalidateCache();
+            mStartupCacheURITable.Clear();
             rv = NS_ERROR_UNEXPECTED;
         }
     } else if (rv != NS_ERROR_NOT_AVAILABLE)
@@ -612,13 +613,10 @@ nsXULPrototypeCache::BeginCaching(nsIURI* aURI)
         // Failed again, just bail.
         if (NS_FAILED(rv)) {
             startupCache->InvalidateCache();
+            mStartupCacheURITable.Clear();
             return NS_ERROR_FAILURE;
         }
     }
-
-    // Success!  Insert this URI into the mCacheURITable
-    // and commit locals to globals.
-    mCacheURITable.PutEntry(aURI);
 
     return NS_OK;
 }
