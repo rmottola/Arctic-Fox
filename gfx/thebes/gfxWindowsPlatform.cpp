72e6a9b98e12b013db37eaa9965bbc2e7b12b23c
Riccardo Mottola <rm@gnu.org>
2023-02-22 11:39:51 +0100

    some window fixes by Roy

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -1913,7 +1913,7 @@ bool DoesD3D11TextureSharingWorkInternal(ID3D11Device *device, DXGI_FORMAT forma
   data.pSysMem = color;
   data.SysMemPitch = texture_size * 4;
   data.SysMemSlicePitch = 0;
-  if (FAILED(device->CreateTexture2D(&desc, &data, byRef(texture)))) {
+  if (FAILED(device->CreateTexture2D(&desc, &data, getter_AddRefs(texture)))) {
     return false;
   }
 
@@ -1950,12 +1950,12 @@ bool DoesD3D11TextureSharingWorkInternal(ID3D11Device *device, DXGI_FORMAT forma
   desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
   desc.MiscFlags = 0;
   desc.BindFlags = 0;
-  if (FAILED(device->CreateTexture2D(&desc, nullptr, byRef(cpuTexture)))) {
+  if (FAILED(device->CreateTexture2D(&desc, nullptr, getter_AddRefs(cpuTexture)))) {
     return false;
   }
 
-  nsRefPtr<IDXGIKeyedMutex> sharedMutex;
-  nsRefPtr<ID3D11DeviceContext> deviceContext;
+  RefPtr<IDXGIKeyedMutex> sharedMutex;
+  RefPtr<ID3D11DeviceContext> deviceContext;
   sharedResource->QueryInterface(__uuidof(IDXGIKeyedMutex), (void**)getter_AddRefs(sharedMutex));
   device->GetImmediateContext(getter_AddRefs(deviceContext));
   if (FAILED(sharedMutex->AcquireSync(0, 30*1000))) {
@@ -2118,7 +2118,7 @@ gfxWindowsPlatform::AttemptD3D11DeviceCreation()
     gfxCriticalError() << "D3D11 device creation failed: " << hexa(hr);
     return FeatureStatus::Failed;
   }
-  if (!DoesD3D11DeviceWork(device)) {
+  if (!DoesD3D11DeviceWork(mD3D11Device)) {
     mD3D11Device = nullptr;
     return FeatureStatus::Blocked;
   }
