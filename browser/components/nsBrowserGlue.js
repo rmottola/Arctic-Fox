9495f1c66c14c5fec68acb60413ab0c0aa5a1662
Riccardo Mottola <rm@gnu.org>
2023-02-23 22:16:22 +0100

    Bug 1175300 - Switch password manager to using displayOrigin to display the origin in the capture doorhanger. r=MattN

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2361,9 +2361,7 @@ ContentPermissionPrompt.prototype = {
 
     if (!aOptions)
       aOptions = {};
-    aOptions.displayOrigin = (requestPrincipal.URI instanceof Ci.nsIFileURL) ?
-                             requestPrincipal.URI.file.path :
-                             requestPrincipal.URI.host;
+    aOptions.displayOrigin = requestPrincipal.URI;
 
     return chromeWin.PopupNotifications.show(browser, aNotificationId, aMessage, aAnchorId,
                                              mainAction, secondaryActions, aOptions);
