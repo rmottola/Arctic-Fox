194d629af069923bb514423447536e0b4947ae9f
Riccardo Mottola <rm@gnu.org>
2023-02-24 08:27:13 +0100

    Bug 1210444 - Set DEALLOCATE_CLIENT flag for EGLImage SharedSurfaceTextureClients. r=nical
    SharedSurface_EGLImages are deallocated on the client side and are
    unable to be recycled. This led to a race condition where CanvasClient
    would free a TextureClient, destroying its underlying data, but
    ImageHost might attempt to composite the corresponding TextureHost
    before receiving a replacement to use instead.
    
    Give SharedSurface an interface for its backends to specify any texture
    flags that they require a TextureClient to set, and ensure that
    DEALLOCATE_CLIENT is set for SharedSurface_EGLImage
    SharedSurfaceTextureClients. This ensures that texture data is destroyed
    synchronously and, importantly, after any outstanding ipdl messages have
    been delivered. This guarantees that ImageHost has received a new
    TextureHost to composite before the previous TextureHost's data is
    destroyed.

--- gfx/layers/client/TextureClientSharedSurface.cpp
+++ gfx/layers/client/TextureClientSharedSurface.cpp
@@ -29,7 +29,9 @@ SharedSurfaceTextureClient::SharedSurfaceTextureClient(ISurfaceAllocator* aAlloc
                                                        gl::SurfaceFactory* factory)
   : TextureClient(aAllocator, aFlags | TextureFlags::RECYCLE)
   , mSurf(Move(surf))
-{ }
+{
+  AddFlags(mSurf->GetTextureFlags());
+}
 
 SharedSurfaceTextureClient::~SharedSurfaceTextureClient()
 {
