015ee4994486ca16ada921676fb26b6c38a41a68
Riccardo Mottola <rm@gnu.org>
2023-03-20 01:23:28 +0100

    crash reporter

--- accessible/windows/msaa/IUnknownImpl.cpp
+++ accessible/windows/msaa/IUnknownImpl.cpp
@@ -9,6 +9,10 @@
 
 #include "nsDebug.h"
 
+#ifdef MOZ_CRASHREPORTER
+#include "nsExceptionHandler.h"
+#endif
+
 namespace mozilla {
 namespace a11y {
 
@@ -37,6 +41,12 @@ int
 FilterExceptions(unsigned int aCode, EXCEPTION_POINTERS* aExceptionInfo)
 {
   if (aCode == EXCEPTION_ACCESS_VIOLATION) {
+#ifdef MOZ_CRASHREPORTER
+    // MSAA swallows crashes (because it is COM-based) but we still need to
+    // learn about those crashes so we can fix them. Make sure to pass them to
+    // the crash reporter.
+    CrashReporter::WriteMinidumpForException(aExceptionInfo);
+#endif
   } else {
     NS_NOTREACHED("We should only be catching crash exceptions");
   }
