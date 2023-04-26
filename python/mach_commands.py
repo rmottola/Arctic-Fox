fa334fc2c9555c959363d4c68d25e679923a9b82
Riccardo Mottola <rm@gnu.org>
2023-04-27 01:00:46 +0200

    Bug 1228628: Move all eslintignore rules to the top-level of the tree. r=mconley

--- python/mach_commands.py
+++ python/mach_commands.py
@@ -212,11 +212,10 @@ class MachCommands(MachCommandBase):
         cmd_args = [binary,
             '--ext', ext,  # This keeps ext as a single argument.
         ] + args
-        # Path must come after arguments.  Path is '.' due to cwd below.
-        cmd_args += ['.']
+        # Path must come after arguments.
+        cmd_args += [path]
 
         return self.run_process(cmd_args,
-            cwd=path,
             pass_thru=True,  # Allow user to run eslint interactively.
             ensure_exit_code=False,  # Don't throw on non-zero exit code.
             require_unix_environment=True # eslint is not a valid Win32 binary.
