831bfac4a41863c5bd0e35c891769ddd53564df7
Riccardo Mottola <rm@gnu.org>
2023-04-28 10:00:35 +0200

    Bug 1187357 - rename referrer policy attribute in frontend code. r=Gijs

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5660,7 +5660,7 @@ function handleLinkClick(event, href, linkNode) {
   if (Services.prefs.getBoolPref("network.http.enablePerElementReferrer") &&
       linkNode) {
     let referrerAttrValue = Services.netUtils.parseAttributePolicyString(linkNode.
-                            getAttribute("referrer"));
+                            getAttribute("referrerpolicy"));
     if (referrerAttrValue != Ci.nsIHttpChannel.REFERRER_POLICY_DEFAULT) {
       referrerPolicy = referrerAttrValue;
     }
