e010cd17046a809075e08ed95952d325580bea64
Riccardo Mottola <rm@gnu.org>
2023-05-09 15:29:23 +0200

    bug 147419 remove ununsed plexName colorspace resolutionName and downloadFonts r=roc
    This was only used to write to and read from otherwise unused preferences.

--- widget/nsPrintSettingsImpl.h
+++ widget/nsPrintSettingsImpl.h
@@ -80,7 +80,6 @@ protected:
   nsString      mFooterStrs[NUM_HEAD_FOOT];
 
   nsString      mPaperName;
-  nsString      mPlexName;
   int16_t       mPaperData;
   int16_t       mPaperSizeType;
   double        mPaperWidth;
@@ -90,11 +89,8 @@ protected:
   bool          mPrintReversed;
   bool          mPrintInColor; // a false means grayscale
   int32_t       mOrientation;  // see orientation consts
-  nsString      mColorspace;
-  nsString      mResolutionName;
   int32_t       mResolution;
   int32_t       mDuplex;
-  bool          mDownloadFonts;
   int32_t       mNumCopies;
   nsXPIDLString mPrinter;
   bool          mPrintToFile;
