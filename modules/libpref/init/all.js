4b3488c7ed023b75a8b1bd3ab9ef415f36ec803e
Riccardo Mottola <rm@gnu.org>
2023-05-12 01:28:52 +0200

    Bug 1218918 - Issues with devtools "work offline" when connectivity changes r=mcmanus,dao
    * Flip the network.offline-mirrors-connectivity pref, so that ioservice.offline means the same as devtools work offline
    * ioService.manageOfflineStatus shouldn't be changed when going to offline mode.
    * Add logging to nsIOService in order to track offline/connectivity issues

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1578,7 +1578,7 @@ pref("network.warnOnAboutNetworking", true);
 // Whether IOService.connectivity and NS_IsOffline depends on connectivity status
 pref("network.manage-offline-status", false);
 // If set to true, IOService.offline depends on IOService.connectivity
-pref("network.offline-mirrors-connectivity", true);
+pref("network.offline-mirrors-connectivity", false);
 
 // <http>
 pref("network.http.version", "1.1");      // default
