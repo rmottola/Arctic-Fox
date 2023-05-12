3f67dd7c3f308364957963c1a4687a03634baf04
Riccardo Mottola <rm@gnu.org>
2023-05-12 17:52:07 +0200

    Bug 1202940 part 2 - Remove layout.css.ruby.enabled pref. r=dbaron

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2693,9 +2693,6 @@ pref("layout.css.grid-template-subgrid-value.enabled", false);
 // Is support for CSS contain enabled?
 pref("layout.css.contain.enabled", false);
 
-// Is support for CSS Ruby enabled?
-pref("layout.css.ruby.enabled", true);
-
 // Is support for CSS display:contents enabled?
 pref("layout.css.display-contents.enabled", true);
 
