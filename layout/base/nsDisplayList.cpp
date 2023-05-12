3db2cd1452c745413a9878b75d5477b4242478b7
Riccardo Mottola <rm@gnu.org>
2023-05-12 08:26:55 +0200

    Bug 1069417 - Remove the explicit template argument of TransformTo() and related functions. r=kats
    In this process, TransformTo() and UntransformTo() are renamed TransformedBy()
    and UntransformBy() so calls to them continue to read sensibly.

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1222,7 +1222,7 @@ nsDisplayListBuilder::AdjustWindowDraggingRegion(nsIFrame* aFrame)
     LayoutDeviceRect devPixelBorderBox =
       LayoutDevicePixel::FromAppUnits(borderBox, aFrame->PresContext()->AppUnitsPerDevPixel());
     LayoutDeviceRect transformedDevPixelBorderBox =
-      TransformTo<LayoutDevicePixel>(referenceFrameToRootReferenceFrame, devPixelBorderBox);
+      TransformBy(referenceFrameToRootReferenceFrame, devPixelBorderBox);
     transformedDevPixelBorderBox.Round();
     LayoutDeviceIntRect transformedDevPixelBorderBoxInt;
     if (transformedDevPixelBorderBox.ToIntRect(&transformedDevPixelBorderBoxInt)) {
