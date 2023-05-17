309dd1a92b5d991a63db95d123338482a1ce33db
Riccardo Mottola <rm@gnu.org>
2023-05-18 01:32:57 +0200

    Bug 1166469 - Add HandleBackendError for hfp-fallback product, r=btian

--- dom/bluetooth/bluedroid/hfp-fallback/BluetoothHfpManager.cpp
+++ dom/bluetooth/bluedroid/hfp-fallback/BluetoothHfpManager.cpp
@@ -208,6 +208,17 @@ BluetoothHfpManager::ConnectSco()
   return false;
 }
 
+void
+BluetoothHfpManager::HandleBackendError()
+{
+  /**
+   * TODO: 
+   *   Reset connection state and audio state to DISCONNECTED to handle backend
+   *   error. The state change triggers UI status bar update as ordinary
+   *   bluetooth turn-off sequence.
+   */
+}
+
 bool
 BluetoothHfpManager::DisconnectSco()
 {
