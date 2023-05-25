cb3bea30fc9364d27587138688462068ff0e545f
Riccardo Mottola <rm@gnu.org>
2023-05-25 16:19:43 +0200

    Bug 1227028: TraceLogger - Fix when to keep the payload of a TraceLogger event, r=bbouvier

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -1263,6 +1263,8 @@ void
 IonScript::Destroy(FreeOp* fop, IonScript* script)
 {
     script->unlinkFromRuntime(fop);
+    // Frees the potential event we have set.
+    script->traceLoggerScriptEvent_ = TraceLoggerEvent();
     fop->free_(script);
 }
 
