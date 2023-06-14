11e58dfe79948da581c70236566f43428e14b534
Riccardo Mottola <rm@gnu.org>
2023-06-14 13:18:07 +0200

    Bug 1197932 - Remove Nightly-only restriction from ES6 Classes. (r=jorendorff)

--- js/src/jit-test/tests/ion/bug1185957.js
+++ js/src/jit-test/tests/ion/bug1185957.js
@@ -1,15 +1,9 @@
 // |jit-test| error: TypeError
 
-load(libdir + "class.js");
-
-var test = `
 class test {
     constructor() {};
 }
+
 (function() {
     test()
 })();
-`;
-
-if (classesEnabled())
-    eval(test);
--- js/src/tests/ecma_6/Class/derivedConstructorTDZExplicitThis.js
+++ js/src/tests/ecma_6/Class/derivedConstructorTDZExplicitThis.js
@@ -1,12 +1,6 @@
-function pleaseRunMyCode() { }
-
 class foo extends null {
     constructor() {
-        // Just bareword |this| is DCEd by the BytecodeEmitter. Your guess as
-        // to why we think this is a good idea is as good as mine. In order to
-        // combat this inanity, make it a function arg, so we have to compute
-        // it.
-        pleaseRunMyCode(this);
+        this;
         assertEq(false, true);
     }
 }
