0ee0e3db798d5b5316c4fae1d4a1a6d0bffdaad1
Riccardo Mottola <rm@gnu.org>
2023-06-20 08:29:34 +0200

    Bug 1163911 - Make responsive images block the document load event while the load task is queued. r=jst

--- dom/html/HTMLImageElement.cpp
+++ dom/html/HTMLImageElement.cpp
@@ -82,7 +82,10 @@ class ImageLoadTask : public nsRunnable
 public:
   explicit ImageLoadTask(HTMLImageElement *aElement) :
     mElement(aElement)
-  {}
+  {
+    mDocument = aElement->OwnerDoc();
+    mDocument->BlockOnload();
+  }
 
   NS_IMETHOD Run()
   {
@@ -90,12 +93,14 @@ public:
       mElement->mPendingImageLoadTask = nullptr;
       mElement->LoadSelectedImage(true, true);
     }
+    mDocument->UnblockOnload(false);
     return NS_OK;
   }
 
 private:
   ~ImageLoadTask() {}
   RefPtr<HTMLImageElement> mElement;
+  nsCOMPtr<nsIDocument> mDocument;
 };
 
 HTMLImageElement::HTMLImageElement(already_AddRefed<mozilla::dom::NodeInfo>& aNodeInfo)
