23f7032244d28b1e29acbf7a99583dbb4f44972b
Riccardo Mottola <rm@gnu.org>
2023-06-22 08:24:00 +0200

    pointer style

--- js/src/gdb/tests/test-JSObject.cpp
+++ js/src/gdb/tests/test-JSObject.cpp
@@ -2,19 +2,19 @@
 #include "jsapi.h"
 
 FRAGMENT(JSObject, simple) {
-  JS::Rooted<JSObject *> glob(cx, JS::CurrentGlobalOrNull(cx));
-  JS::Rooted<JSObject *> plain(cx, JS_NewPlainObject(cx));
-  JS::Rooted<JSObject *> global(cx, JS::CurrentGlobalOrNull(cx));
-  JS::Rooted<JSObject *> func(cx, (JSObject *) JS_NewFunction(cx, (JSNative) 1, 0, 0,
+  JS::Rooted<JSObject*> glob(cx, JS::CurrentGlobalOrNull(cx));
+  JS::Rooted<JSObject*> plain(cx, JS_NewPlainObject(cx));
+  JS::Rooted<JSObject*> global(cx, JS::CurrentGlobalOrNull(cx));
+  JS::Rooted<JSObject*> func(cx, (JSObject*) JS_NewFunction(cx, (JSNative) 1, 0, 0,
                                                               "dys"));
-  JS::Rooted<JSObject *> anon(cx, (JSObject *) JS_NewFunction(cx, (JSNative) 1, 0, 0, nullptr));
-  JS::Rooted<JSFunctionu *> funcPtr(cx, JS_NewFunction(cx, (JSNative) 1, 0, 0,
+  JS::Rooted<JSObject*> anon(cx, (JSObject*) JS_NewFunction(cx, (JSNative) 1, 0, 0, nullptr));
+  JS::Rooted<JSFunction*> funcPtr(cx, JS_NewFunction(cx, (JSNative) 1, 0, 0,
                                                       "formFollows"));
 
-  JSObject &plainRef = *plain;
-  JSFunction &funcRef = *funcPtr;
-  JSObject *plainRaw = plain;
-  JSObject *funcRaw = func;
+  JSObject& plainRef = *plain;
+  JSFunction& funcRef = *funcPtr;
+  JSObject* plainRaw = plain;
+  JSObject* funcRaw = func;
 
   breakpoint();
 
