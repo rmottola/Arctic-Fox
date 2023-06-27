721df0036168285bcc0d8a8fb36b3d663ad7744f
Riccardo Mottola <rm@gnu.org>
2023-06-28 00:46:30 +0200

    Bug 1229987: P4. Stop pre-rolling when encountering WAITING_FOR_DATA. r=cpearce
    This allows to start displaying frames even when we have less than 5 frames.

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -787,6 +787,7 @@ MediaDecoder::Seek(double aTime, SeekTarget::Type aSeekType)
 
   UpdateDormantState(false /* aDormantTimeout */, true /* aActivity */);
 
+  MOZ_ASSERT(!mIsDormant, "should be out of dormant by now");
   MOZ_ASSERT(aTime >= 0.0, "Cannot seek to a negative value.");
 
   int64_t timeUsecs = 0;
