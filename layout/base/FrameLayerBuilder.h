1fb9a583ed6121ea4e799469996ecc5d544b124f
Riccardo Mottola <rm@gnu.org>
2023-07-10 12:01:39 +0200

    Bug 1183085 - Correct a typo in FrameLayerBuilder.h; r=roc

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -474,7 +474,7 @@ public:
      * longer than the transaction.
      *
      * Updates the geometry, frame list and clip.
-     * For items within a PaintedLayer, a geometry object must be specifed to retain
+     * For items within a PaintedLayer, a geometry object must be specified to retain
      * until the next transaction.
      *
      */
