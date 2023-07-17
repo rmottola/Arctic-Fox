c041817b7e9035981fb55075266dc8e40ab0e1f9
Riccardo Mottola <rm@gnu.org>
2023-07-17 13:27:10 +0200

    Bug 1172383 - Stop mDNS properly during destruction. r=mcmanus

--- netwerk/dns/mdns/libmdns/nsDNSServiceDiscovery.cpp
+++ netwerk/dns/mdns/libmdns/nsDNSServiceDiscovery.cpp
@@ -155,6 +155,13 @@ RegisterRequest::Cancel(nsresult aReason)
 
 NS_IMPL_ISUPPORTS(nsDNSServiceDiscovery, nsIDNSServiceDiscovery)
 
+nsDNSServiceDiscovery::~nsDNSServiceDiscovery()
+{
+#ifdef MOZ_WIDGET_GONK
+  StopService();
+#endif
+}
+
 nsresult
 nsDNSServiceDiscovery::Init()
 {
