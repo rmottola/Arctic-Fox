baa7b3da17ca7a823922351e76825e14bca2d634
Riccardo Mottola <rm@gnu.org>
2023-08-01 10:26:47 +0200

    Bug 1234402 - Crash on OOM in AlternativeGenerationList constructor. r=bbouvier

--- js/src/irregexp/RegExpEngine.cpp
+++ js/src/irregexp/RegExpEngine.cpp
@@ -4234,13 +4234,14 @@ class AlternativeGenerationList
     AlternativeGenerationList(LifoAlloc* alloc, size_t count)
       : alt_gens_(*alloc)
     {
+        AutoEnterOOMUnsafeRegion oomUnsafe;
         alt_gens_.reserve(count);
         for (size_t i = 0; i < count && i < kAFew; i++)
             alt_gens_.infallibleAppend(a_few_alt_gens_ + i);
         for (size_t i = kAFew; i < count; i++) {
             AlternativeGeneration* gen = js_new<AlternativeGeneration>();
             if (!gen)
-                MOZ_CRASH("AlternativeGenerationList js_new");
+                oomUnsafe.crash("AlternativeGenerationList js_new");
             alt_gens_.infallibleAppend(gen);
         }
     }
