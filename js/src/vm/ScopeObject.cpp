6c474eb5b03045208132ff275260d6f940107285
Riccardo Mottola <rm@gnu.org>
2023-08-02 19:54:12 +0200

    Bug 1234845 part 1 - Remove fun() method from frames and remove some dead code. r=luke

--- js/src/vm/ScopeObject.cpp
+++ js/src/vm/ScopeObject.cpp
@@ -1451,7 +1451,7 @@ ScopeIter::settle()
 {
     // Check for trying to iterate a function frame before the prologue has
     // created the CallObject, in which case we have to skip.
-    if (frame_ && frame_.isNonEvalFunctionFrame() && frame_.fun()->needsCallObject() &&
+    if (frame_ && frame_.isNonEvalFunctionFrame() && frame_.callee()->needsCallObject() &&
         !frame_.hasCallObj())
     {
         MOZ_ASSERT(ssi_.type() == StaticScopeIter<CanGC>::Function);
@@ -2608,7 +2608,7 @@ DebugScopes::onPopCall(AbstractFramePtr frame, JSContext* cx)
 
     Rooted<DebugScopeObject*> debugScope(cx, nullptr);
 
-    if (frame.fun()->needsCallObject()) {
+    if (frame.callee()->needsCallObject()) {
         /*
          * The frame may be observed before the prologue has created the
          * CallObject. See ScopeIter::settle.
@@ -2616,7 +2616,7 @@ DebugScopes::onPopCall(AbstractFramePtr frame, JSContext* cx)
         if (!frame.hasCallObj())
             return;
 
-        if (frame.fun()->isGenerator())
+        if (frame.callee()->isGenerator())
             return;
 
         CallObject& callobj = frame.scopeChain()->as<CallObject>();
