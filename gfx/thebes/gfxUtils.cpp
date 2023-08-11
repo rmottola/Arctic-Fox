84c95c7f353f260124d677965c5f501998f81638
Riccardo Mottola <rm@gnu.org>
2023-08-11 21:37:13 +0200

    Bug 1238438 (part 1) - Remove dead function gfxUtils::PathFromRegion(). r=nical.

--- gfx/thebes/gfxUtils.cpp
+++ gfx/thebes/gfxUtils.cpp
@@ -808,13 +808,6 @@ gfxUtils::ClampToScaleFactor(gfxFloat aVal)
   return scale;
 }
 
-
-/*static*/ void
-gfxUtils::PathFromRegion(gfxContext* aContext, const nsIntRegion& aRegion)
-{
-  PathFromRegionInternal(aContext, aRegion);
-}
-
 gfxMatrix
 gfxUtils::TransformRectToRect(const gfxRect& aFrom, const gfxPoint& aToTopLeft,
                               const gfxPoint& aToTopRight, const gfxPoint& aToBottomRight)
