6166363541454d1f4cb863204d5ebb452bdeff36
Riccardo Mottola <rm@gnu.org>
2023-08-11 23:04:52 +0200

    Bug 1232561 part 2 - Move the animation style rules from AnimationCollection to EffectSet; r=heycam
    This is needed in order to support script-generated animations since they do not
    belong to any AnimationCollection.
    
    This patch adopts the naming "animation rule" over "style rule". Currently we
    are inconsistent about this (e.g. GetAnimationRule vs EnsureStyleRuleFor).
    We don't do a mass rename here but just a few places near where we're touching.
    Many of the other references to "style rule" will be revised in this bug or
    related bugs so we can fix those references when we come to them.

--- layout/style/nsAnimationManager.cpp
+++ layout/style/nsAnimationManager.cpp
@@ -395,7 +395,6 @@ nsAnimationManager::CheckAnimationRule(nsStyleContext* aStyleContext,
   }
 
   if (collection) {
-    collection->mStyleRule = nullptr;
     collection->mStyleRuleRefreshTime = TimeStamp();
     EffectSet* effectSet =
       EffectSet::GetEffectSet(aElement, aStyleContext->GetPseudoType());
