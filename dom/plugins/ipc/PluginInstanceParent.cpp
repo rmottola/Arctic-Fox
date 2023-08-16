1f74728aec17961dc8d6834789f6a6ddc7072190
Riccardo Mottola <rm@gnu.org>
2023-08-16 22:43:24 +0200

    Bug 1233619 (part 1) - Remove unneeded gfxContext argument from EndUpdate() and EndUpdateBackground() functions. r=roc.

--- dom/plugins/ipc/PluginInstanceParent.cpp
+++ dom/plugins/ipc/PluginInstanceParent.cpp
@@ -1183,8 +1183,7 @@ PluginInstanceParent::BeginUpdateBackground(const nsIntRect& aRect,
 }
 
 nsresult
-PluginInstanceParent::EndUpdateBackground(gfxContext* aCtx,
-                                          const nsIntRect& aRect)
+PluginInstanceParent::EndUpdateBackground(const nsIntRect& aRect)
 {
     PLUGIN_LOG_DEBUG(
         ("[InstanceParent][%p] EndUpdateBackground for <x=%d,y=%d, w=%d,h=%d>",
