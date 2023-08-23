9f7553561793cc80f340ffc1a1838308a89a1149
Riccardo Mottola <rm@gnu.org>
2023-08-23 18:16:35 +0200

    Bug 1237720: Put "-webkit-min-device-pixel-ratio"/"-webkit-max-device-pixel-ratio" behind its own disabled pref. r=heycam

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2666,6 +2666,11 @@ pref("layout.css.prefixes.gradients", true);
 // Are webkit-prefixed properties & property-values supported?
 pref("layout.css.prefixes.webkit", false);
 
+// Are "-webkit-{min|max}-device-pixel-ratio" media queries supported?
+// (Note: this pref has no effect if the master 'layout.css.prefixes.webkit'
+// pref is set to false.)
+pref("layout.css.prefixes.device-pixel-ratio-webkit", false);
+
 // Is the CSS Unprefixing Service enabled? (This service emulates support
 // for certain vendor-prefixed properties & values, for sites on a "fixlist".)
 pref("layout.css.unprefixing-service.enabled", true);
