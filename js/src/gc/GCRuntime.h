8d90fd103e4e4ea6b90c12adefa7d8cabfbd369c
Riccardo Mottola <rm@gnu.org>
2023-08-25 17:37:48 +0200

    Bug 1245767 - Allow combining different gczeal modes. r=terrence

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -585,7 +585,8 @@ class GCRuntime
     void finishRoots();
     void finish();
 
-    inline int zeal();
+    inline bool hasZealMode(ZealMode mode);
+    inline void clearZealMode(ZealMode mode);
     inline bool upcomingZealousGC();
     inline bool needZealousGC();
 
@@ -641,8 +642,8 @@ class GCRuntime
     void onOutOfMallocMemory(const AutoLockGC& lock);
 
 #ifdef JS_GC_ZEAL
-    const void* addressOfZealMode() { return &zealMode; }
-    void getZeal(uint8_t* zeal, uint32_t* frequency, uint32_t* nextScheduled);
+    const void* addressOfZealModeBits() { return &zealModeBits; }
+    void getZealBits(uint32_t* zealBits, uint32_t* frequency, uint32_t* nextScheduled);
     void setZeal(uint8_t zeal, uint32_t frequency);
     bool parseAndSetZeal(const char* str);
     void setNextScheduled(uint32_t count);
@@ -1264,7 +1265,7 @@ class GCRuntime
      * zeal_ value 14 performs periodic shrinking collections.
      */
 #ifdef JS_GC_ZEAL
-    int zealMode;
+    uint32_t zealModeBits;
     int zealFrequency;
     int nextScheduled;
     bool deterministicOnly;
@@ -1352,9 +1353,20 @@ class MOZ_RAII AutoEnterIteration {
 };
 
 #ifdef JS_GC_ZEAL
-inline int
-GCRuntime::zeal() {
-    return zealMode;
+
+inline bool
+GCRuntime::hasZealMode(ZealMode mode)
+{
+    static_assert(size_t(ZealMode::Limit) < sizeof(zealModeBits) * 8,
+                  "Zeal modes must fit in zealModeBits");
+    return zealModeBits & (1 << uint32_t(mode));
+}
+
+inline void
+GCRuntime::clearZealMode(ZealMode mode)
+{
+    zealModeBits &= ~(1 << uint32_t(mode));
+    MOZ_ASSERT(!hasZealMode(mode));
 }
 
 inline bool
@@ -1365,11 +1377,12 @@ GCRuntime::upcomingZealousGC() {
 inline bool
 GCRuntime::needZealousGC() {
     if (nextScheduled > 0 && --nextScheduled == 0) {
-        if (zealMode == ZealAllocValue ||
-            zealMode == ZealGenerationalGCValue ||
-            (zealMode >= ZealIncrementalRootsThenFinish &&
-             zealMode <= ZealIncrementalMultipleSlices) ||
-            zealMode == ZealCompactValue)
+        if (hasZealMode(ZealMode::Alloc) ||
+            hasZealMode(ZealMode::GenerationalGC) ||
+            hasZealMode(ZealMode::IncrementalRootsThenFinish) ||
+            hasZealMode(ZealMode::IncrementalMarkAllThenFinish) ||
+            hasZealMode(ZealMode::IncrementalMultipleSlices) ||
+            hasZealMode(ZealMode::Compact))
         {
             nextScheduled = zealFrequency;
         }
@@ -1378,7 +1391,8 @@ GCRuntime::needZealousGC() {
     return false;
 }
 #else
-inline int GCRuntime::zeal() { return 0; }
+inline bool GCRuntime::hasZealMode(ZealMode mode) { return false; }
+inline void GCRuntime::clearZealMode(ZealMode mode) { }
 inline bool GCRuntime::upcomingZealousGC() { return false; }
 inline bool GCRuntime::needZealousGC() { return false; }
 #endif
