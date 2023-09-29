5117e4df60ae2a58b6838f91674ea5569b7a2573
Riccardo Mottola <rm@gnu.org>
2023-09-29 18:01:57 +0200

    disable location services

--- dom/geolocation/nsGeolocation.cpp
+++ dom/geolocation/nsGeolocation.cpp
@@ -827,9 +827,12 @@ nsresult nsGeolocationService::Init()
 #endif
 
 #ifdef MOZ_WIDGET_COCOA
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
+
   if (Preferences::GetBool("geo.provider.use_corelocation", true)) {
     mProvider = new CoreLocationLocationProvider();
   }
+#endif
 #endif
 
 #ifdef XP_WIN
