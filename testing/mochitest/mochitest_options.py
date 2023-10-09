edccfcf9a2695cfe81e63fda17e7fdd51670c6c2
Riccardo Mottola <rm@gnu.org>
2023-10-09 23:48:56 +0200

    Bug 1188730 - Select tests in the plugins subsuite by manifest rather than path to fix unintended interaction with mach try. r=ahal
    This prevents mach try's filtering of the master mochitest manifest from
    interacting with the plugins subsuite by selecting by manifest rather than
    test path. The way we run mochitest other could use a lot of cleanup --
    this is a quick fix to prevent misleading output on treeherder when using
    mach try.

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -178,13 +178,6 @@ class MochitestArguments(ArgumentContainer):
           "help": "Run chrome mochitests.",
           "suppress": True,
           }],
-        [["--ipcplugins"],
-         {"action": "store_true",
-          "dest": "ipcplugins",
-          "help": "Run ipcplugins mochitests.",
-          "default": False,
-          "suppress": True,
-          }],
         [["--bisect-chunk"],
          {"dest": "bisectChunk",
           "default": None,
@@ -590,9 +583,6 @@ class MochitestArguments(ArgumentContainer):
             )
             options.gmp_path = os.pathsep.join(
                 os.path.join(build_obj.bindir, *p) for p in gmp_modules)
-
-        if options.ipcplugins:
-            options.test_paths.append('dom/plugins/test/mochitest')
 
         if options.totalChunks is not None and options.thisChunk is None:
             parser.error(
