86f4a6f4c0474022b4a962c2240642d55b8754b9
Riccardo Mottola <rm@gnu.org>
2023-10-16 15:32:33 +0200

    Bug 1193215 - Support for passing test directories through mach try, r=chmanchester
    This adds support for web-platform-tests to mach try. It changes the implementation
    so that instead of passing paths to manifests, the user passes arbitary paths in the
    source tree, and tests under that path are run, with test discovery mainly left to
    the harness.

--- testing/mozharness/mozharness/mozilla/testing/try_tools.py
+++ testing/mozharness/mozharness/mozilla/testing/try_tools.py
@@ -8,17 +8,43 @@
 import argparse
 import os
 import re
+from collections import defaultdict
 
 from mozharness.base.script import PostScriptAction
 from mozharness.base.transfer import TransferMixin
 
+try_config_options = [
+    [["--try-message"],
+     {"action": "store",
+     "dest": "try_message",
+     "default": None,
+     "help": "try syntax string to select tests to run",
+      }],
+]
+
+test_flavors = {
+    'browser-chrome': {},
+    'chrome': {},
+    'devtools-chrome': {},
+    'mochitest': {},
+    'xpcshell' :{},
+    'reftest': {
+        "path": lambda x: os.path.join("tests", "reftest", "tests", x)
+    },
+    'crashtest': {
+        "path": lambda x: os.path.join("tests", "reftest", "tests", x)
+    },
+    'web-platform-tests': {
+        "path": lambda x: os.path.join("tests", x.split("testing" + os.path.sep)[1])
+    }
+}
 
 class TryToolsMixin(TransferMixin):
     """Utility functions for an interface between try syntax and out test harnesses.
     Requires log and script mixins."""
 
     harness_extra_args = None
-    try_test_paths = []
+    try_test_paths = {}
     known_try_arguments = {
         '--tag': {
             'action': 'append',
@@ -110,7 +136,7 @@ class TryToolsMixin(TransferMixin):
 
         parser.add_argument('--try-test-paths', nargs='*')
         (args, _) = parser.parse_known_args(all_try_args)
-        self.try_test_paths = args.try_test_paths
+        self.try_test_paths = self._group_test_paths(args.try_test_paths)
         del args.try_test_paths
 
         out_args = []
@@ -130,91 +156,35 @@ class TryToolsMixin(TransferMixin):
 
         self.harness_extra_args = out_args
 
-    def _resolve_specified_manifests(self):
-        if not self.try_test_paths:
-            return None
+    def _group_test_paths(self, args):
+        rv = defaultdict(list)
 
-        target_manifests = set(self.try_test_paths)
+        if args is None:
+            return rv
 
-        def filter_ini_manifest(line):
-            # Lines are formatted as [include:<path>], we care about <path>.
-            parts = line.split(':')
-            term = line
-            if len(parts) == 2:
-                term = parts[1]
-            if term.endswith(']'):
-                term = term[:-1]
-            if (term in target_manifests or
-                any(term.startswith(l) for l in target_manifests)):
-                return True
-            return False
+        for item in args:
+            suite, path = item.split(":", 1)
+            rv[suite].append(path)
+        return rv
 
-        def filter_list_manifest(line):
-            # Lines are usually formatted as "include <path>", we care about <path>.
-            parts = line.split()
-            term = line
-            if len(parts) == 2:
-                term = parts[1]
-            # Reftest master manifests also include skip-if lines and relative
-            # paths we aren't doing to resolve here, so unlike above this is just
-            # a substring check.
-            if (term in target_manifests or
-                any(l in term for l in target_manifests)):
-                return True
-            return False
-
-        # The master manifests we need to filter for target manifests.
-        # TODO: All this needs to go in a config file somewhere and get sewn
-        # into the job definition so its less likely to break as things are
-        # modified. One straightforward way to achieve this would be with a key
-        # in the tree manifest for the master manifest path, however the current
-        # tree manifests don't distinguish between flavors of mochitests to this
-        # isn't straightforward.
-        master_manifests = [
-            ('mochitest/chrome/chrome.ini', filter_ini_manifest),
-            ('mochitest/browser/browser-chrome.ini', filter_ini_manifest),
-            ('mochitest/tests/mochitest.ini', filter_ini_manifest),
-            ('xpcshell/tests/all-test-dirs.list', filter_ini_manifest),
-            ('xpcshell/tests/xpcshell.ini', filter_ini_manifest),
-            ('reftest/tests/layout/reftests/reftest.list', filter_list_manifest),
-            ('reftest/tests/testing/crashtest/crashtests.list', filter_list_manifest),
-        ]
-
-        dirs = self.query_abs_dirs()
-        tests_dir = dirs.get('abs_test_install_dir',
-                             os.path.join(dirs['abs_work_dir'], 'tests'))
-        master_manifests = [(os.path.join(tests_dir, name), filter_fn) for (name, filter_fn) in
-                            master_manifests]
-        for m, filter_fn in master_manifests:
-            if not os.path.isfile(m):
-                continue
-
-            self.info("Filtering master manifest at: %s" % m)
-            lines = self.read_from_file(m).splitlines()
-
-            out_lines = [line for line in lines if filter_fn(line)]
-
-            self.rmtree(m)
-            self.write_to_file(m, '\n'.join(out_lines))
-
-
-    def append_harness_extra_args(self, cmd):
-        """Append arguments derived from try syntax to a command."""
+    def try_args(self, flavor):
+        """Get arguments, test_list derived from try syntax to apply to a command"""
         # TODO: Detect and reject incompatible arguments
-        extra_args = self.harness_extra_args[:] if self.harness_extra_args else []
-        if self.try_test_paths:
-            self._resolve_specified_manifests()
+        args = self.harness_extra_args[:] if self.harness_extra_args else []
+
+        if self.try_test_paths.get(flavor):
             self.info('TinderboxPrint: Tests will be run from the following '
-                      'manifests: %s.' % ','.join(self.try_test_paths))
-            extra_args.extend(['--this-chunk=1', '--total-chunks=1'])
+                      'files: %s.' % ','.join(self.try_test_paths[flavor]))
+            args.extend(['--this-chunk=1', '--total-chunks=1'])
 
-        if not extra_args:
-            return cmd
+            path_func = test_flavors[flavor].get("path", lambda x:x)
+            tests = [path_func(item) for item in self.try_test_paths[flavor]]
+        else:
+            tests = []
 
-        out_cmd = cmd[:]
-        out_cmd.extend(extra_args)
-        self.info('TinderboxPrint: The following arguments were forwarded from mozharness '
-                  'to the test command:\nTinderboxPrint: \t%s' %
-                  extra_args)
+        if args or tests:
+            self.info('TinderboxPrint: The following arguments were forwarded from mozharness '
+                      'to the test command:\nTinderboxPrint: \t%s -- %s' %
+                      (" ".join(args), " ".join(tests)))
 
-        return out_cmd
+        return args, tests
