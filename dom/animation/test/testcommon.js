8c9b92f2d441952aebea04738ac2260e5a70cb76
Riccardo Mottola <rm@gnu.org>
2023-10-31 14:21:27 +0100

    Bug 1212720 - Part 4: Remove all cancelAllAnimationsOnEnd. It is not needed any more. r=bbirtles

--- dom/animation/test/testcommon.js
+++ dom/animation/test/testcommon.js
@@ -18,18 +18,6 @@ function addDiv(t) {
 }
 
 /**
- * Some tests cause animations to continue to exist even after their target
- * element has been removed from the document tree. To ensure that these
- * animations do not impact other tests we should cancel them when the test
- * is complete.
- */
-function cancelAllAnimationsOnEnd(t) {
-  t.add_cleanup(function() {
-    document.getAnimations().forEach(animation => animation.cancel());
-  });
-}
-
-/**
  * Promise wrapper for requestAnimationFrame.
  */
 function waitForFrame() {
