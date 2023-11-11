bab75927035faeb0bcf08547e1e834bb2072a494
Riccardo Mottola <rm@gnu.org>
2023-11-11 23:18:20 +0100

    bits of 1178215

--- media/libvpx/update.py
+++ media/libvpx/update.py
@@ -592,8 +592,6 @@ def apply_patches():
     # Cherry pick https://chromium-review.googlesource.com/#/c/276889/
     # to fix crash on 32bit
     os.system("patch -p1 < vp9_filter_restore_aligment.patch")
-    # Patch to allow MSVC 2015 to compile libvpx
-    os.system("patch -p1 < msvc2015.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:
