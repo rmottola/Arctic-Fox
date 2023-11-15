81dfcf036a362c1177ed498555fda8620ffabe41
Riccardo Mottola <rm@gnu.org>
2023-11-16 00:39:20 +0100

    Bug 1007136 - Ensure malloc/free always match when using JSAutoByteString; r=bz

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -4606,7 +4606,7 @@ class MOZ_RAII JSAutoByteString
     }
 
     ~JSAutoByteString() {
-        js_free(mBytes);
+        JS_free(nullptr, mBytes);
     }
 
     /* Take ownership of the given byte array. */
@@ -4651,7 +4651,7 @@ class MOZ_RAII JSAutoByteString
     }
 
   private:
-    char*       mBytes;
+    char* mBytes;
     MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
 
     /* Copy and assignment are not supported. */
