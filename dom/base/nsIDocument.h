d0c256c29ff317a003c93c8822d0ca707b7a6641
Riccardo Mottola <rm@gnu.org>
2023-11-21 21:31:38 +0100

    Bug 1220160 - part 1: add chromeonly getter for documenturi that returns the original document when on an error page, r=bz

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2622,6 +2622,8 @@ public:
 
   void ObsoleteSheet(const nsAString& aSheetURI, mozilla::ErrorResult& rv);
 
+  already_AddRefed<nsIURI> GetMozDocumentURIIfNotForErrorPages();
+
   // ParentNode
   nsIHTMLCollection* Children();
   uint32_t ChildElementCount();
