c923e5e9fd55f5a12e52074b7e63ac3dabf1cb2c
Riccardo Mottola <rm@gnu.org>
2023-11-26 14:13:39 +0100

    Bug 1168861 - Make mach bootstrap install GConf2-devel on Fedora, r=glandium

--- python/mozboot/mozboot/centos.py
+++ python/mozboot/mozboot/centos.py
@@ -34,6 +34,7 @@ class CentOSBootstrapper(BaseBootstrapper):
         self.browser_packages = [
             'alsa-lib-devel',
             'dbus-glib-devel',
+            'GConf2-devel',
             'glibc-static',
             'gtk2-devel',
             'libstdc++-static',
