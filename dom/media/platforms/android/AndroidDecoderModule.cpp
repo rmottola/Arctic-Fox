35e0427a2b69ad57d99799bd1f8d1b3dd64c3ba5
Riccardo Mottola <rm@gnu.org>
2023-11-30 16:24:01 +0100

    Bug 1192539 - [2.4] Use MediaCodecList to check for decoder support. r=snorp

--- dom/media/platforms/android/AndroidDecoderModule.cpp
+++ dom/media/platforms/android/AndroidDecoderModule.cpp
@@ -301,7 +301,7 @@ bool
 AndroidDecoderModule::SupportsMimeType(const nsACString& aMimeType) const
 {
   if (!AndroidBridge::Bridge() ||
-      (AndroidBridge::Bridge()->GetAPIVersion() < 16)) {
+      AndroidBridge::Bridge()->GetAPIVersion() < 16) {
     return false;
   }
 
@@ -310,12 +310,8 @@ AndroidDecoderModule::SupportsMimeType(const nsACString& aMimeType) const
     return true;
   }
 
-  MediaCodec::LocalRef ref = mozilla::CreateDecoder(aMimeType);
-  if (!ref) {
-    return false;
-  }
-  ref->Release();
-  return true;
+  return widget::HardwareCodecCapabilityUtils::FindDecoderCodecInfoForMimeType(
+      nsCString(TranslateMimeType(aMimeType)));
 }
 
 already_AddRefed<MediaDataDecoder>
