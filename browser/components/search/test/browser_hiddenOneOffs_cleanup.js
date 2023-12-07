86fb6ff36433263bde5cad21f77cfb19b47dc952
Riccardo Mottola <rm@gnu.org>
2023-12-07 11:47:59 +0100

    Bug 1121417: Change hiddenOneOffs search pref to use unichar type. r=gavin

--- browser/components/search/test/browser_hiddenOneOffs_cleanup.js
+++ browser/components/search/test/browser_hiddenOneOffs_cleanup.js
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-const cachedPref = Services.prefs.getCharPref("browser.search.hiddenOneOffs");
 const testPref = "Foo,FooDupe";
 
 function promiseNewEngine(basename) {
@@ -10,7 +9,7 @@ function promiseNewEngine(basename) {
     Services.search.init({
       onInitComplete: function() {
         let url = getRootDirectory(gTestPath) + basename;
-        Services.search.addEngine(url, Ci.nsISearchEngine.TYPE_MOZSEARCH, "", false, {
+        Services.search.addEngine(url, null, "", false, {
           onSuccess: function (engine) {
             info("Search engine added: " + basename);
             resolve(engine);
@@ -38,9 +37,9 @@ add_task(function* test_remove() {
 
   is(hiddenOneOffs.length, 1,
      "hiddenOneOffs has the correct engine count post removal.");
-  is(hiddenOneOffs.includes("FooDupe"), false,
+  is(hiddenOneOffs.some(x => x == "FooDupe"), false,
      "Removed Engine is not in hiddenOneOffs after removal");
-  is(hiddenOneOffs.includes("Foo"), true,
+  is(hiddenOneOffs.some(x => x == "Foo"), true,
      "Current hidden engine is not affected by removal.");
 
   info("Removing testEngine.xml");
@@ -61,10 +60,34 @@ add_task(function* test_add() {
 
   is(hiddenOneOffs.length, 1,
      "hiddenOneOffs has the correct number of hidden engines present post add.");
-  is(hiddenOneOffs.includes("FooDupe"), false,
+  is(hiddenOneOffs.some(x => x == "FooDupe"), false,
      "Added engine is not present in hidden list.");
-  is(hiddenOneOffs.includes("Foo"), true,
+  is(hiddenOneOffs.some(x => x == "Foo"), true,
      "Adding an engine does not remove engines from hidden list.");
+});
+
+add_task(function* test_diacritics() {
+  const diacritic_engine = "Foo \u2661";
+  let Preferences =
+    Cu.import("resource://gre/modules/Preferences.jsm", {}).Preferences;
+
+  Preferences.set("browser.search.hiddenOneOffs", diacritic_engine);
+  yield promiseNewEngine("testEngine_diacritics.xml");
+
+  let hiddenOneOffs =
+    Preferences.get("browser.search.hiddenOneOffs").split(",");
+  is(hiddenOneOffs.some(x => x == diacritic_engine), false,
+     "Observer cleans up added hidden engines that include a diacritic.");
+
+  Preferences.set("browser.search.hiddenOneOffs", diacritic_engine);
+
+  info("Removing testEngine_diacritics.xml");
+  Services.search.removeEngine(Services.search.getEngineByName(diacritic_engine));
+
+  hiddenOneOffs =
+    Preferences.get("browser.search.hiddenOneOffs").split(",");
+  is(hiddenOneOffs.some(x => x == diacritic_engine), false,
+     "Observer cleans up removed hidden engines that include a diacritic.");
 });
 
 registerCleanupFunction(() => {
@@ -72,5 +95,5 @@ registerCleanupFunction(() => {
   Services.search.removeEngine(Services.search.getEngineByName("Foo"));
   info("Removing testEngine_dupe.xml");
   Services.search.removeEngine(Services.search.getEngineByName("FooDupe"));
-  Services.prefs.setCharPref("browser.search.hiddenOneOffs", cachedPref);
+  Services.prefs.clearUserPref("browser.search.hiddenOneOffs");
 });
