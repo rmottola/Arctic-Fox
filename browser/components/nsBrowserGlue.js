e93e96d6642967953979954d8f1f8697d609bfde
Riccardo Mottola <rm@gnu.org>
2023-12-07 13:53:06 +0100

    Bug 1138503 - Remove the SEARCH_DEFAULT_ENGINE histogram. r=gfritzsche

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -162,28 +162,6 @@ const BOOKMARKS_BACKUP_MIN_INTERVAL_DAYS = 1;
 // days we will try to create a new one more aggressively.
 const BOOKMARKS_BACKUP_MAX_INTERVAL_DAYS = 3;
 
-// Record the current default search engine in Telemetry.
-function recordDefaultSearchEngine() {
-  let engine;
-  try {
-    engine = Services.search.defaultEngine;
-  } catch (e) {}
-  let name;
-
-  if (!engine) {
-    name = "NONE";
-  } else if (engine.identifier) {
-    name = engine.identifier;
-  } else if (engine.name) {
-    name = "other-" + engine.name;
-  } else {
-    name = "UNDEFINED";
-  }
-
-  let engines = Services.telemetry.getKeyedHistogramById("SEARCH_DEFAULT_ENGINE");
-  engines.add(name, true)
-}
-
 // Factory object
 const BrowserGlueServiceFactory = {
   _instance: null,
