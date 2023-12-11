03d6f690daee29f40e599c361ea5a2b9c16493fb
Riccardo Mottola <rm@gnu.org>
2023-12-12 00:48:11 +0100

    Bug 1246136: Check for dead code in assertion in EmitIfElse; r=luke

--- js/src/asmjs/WasmIonCompile.cpp
+++ js/src/asmjs/WasmIonCompile.cpp
@@ -2589,7 +2589,7 @@ EmitIfElse(FunctionCompiler& f, bool hasElse, ExprType expected, MDefinition** d
     if (expected != ExprType::Void)
         *def = f.popPhiOutput();
 
-    MOZ_ASSERT((expected != ExprType::Void) == !!*def);
+    MOZ_ASSERT_IF(!f.inDeadCode(), (expected != ExprType::Void) == !!*def);
     return true;
 }
 
