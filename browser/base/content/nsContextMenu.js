90aa8b2b6ea1db49fb0f3c431b958139c72777c1
Riccardo Mottola <rm@gnu.org>
2023-12-13 01:09:47 +0100

    Bug 1244462 set isContentSelected before using it for the on-build-contextmenu notification, r=gijs

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -35,6 +35,7 @@ nsContextMenu.prototype = {
       return;
 
     this.hasPageMenu = false;
+    this.isContentSelected = !this.selectionInfo.docSelectionIsCollapsed;
     if (!aIsShift) {
       if (this.isRemote) {
         this.hasPageMenu =
@@ -75,6 +76,8 @@ nsContextMenu.prototype = {
                                                    Ci.nsIPrefLocalizedString).data;
     } catch (e) { }
 
+    // Reset after "on-build-contextmenu" notification in case selection was
+    // changed during the notification.
     this.isContentSelected = !this.selectionInfo.docSelectionIsCollapsed;
     this.onPlainTextLink = false;
 
