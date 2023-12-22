b22caa112111f8e7d13ef5563c54ca02fe0b9019
Riccardo Mottola <rm@gnu.org>
2023-12-22 09:58:09 +0100

    Bug 1218782 - use fallible allocations in Downscaler.cpp; r=seth
    MakeUnique and its underlying |new| call will crash the program on
    failure.  This code was clearly written with fallible allocations in
    mind, so let's make the allocations actually be fallible.

--- image/Downscaler.cpp
+++ image/Downscaler.cpp
@@ -113,8 +113,8 @@ Downscaler::BeginFrame(const nsIntSize& aOriginalSize,
 
   // Allocate the buffer, which contains scanlines of the original image.
   // pad by 15 to handle overreads by the simd code
-  size_t bufferLen = mOriginalSize.width * sizeof(uint32_t);
-  mRowBuffer = MakeUnique<uint8_t[]>(mOriginalSize.width * sizeof(uint32_t) + 15);
+  size_t bufferLen = mOriginalSize.width * sizeof(uint32_t) + 15;
+  mRowBuffer.reset(new (fallible) uint8_t[bufferLen]);
   if (MOZ_UNLIKELY(!mRowBuffer)) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
@@ -122,12 +122,11 @@ Downscaler::BeginFrame(const nsIntSize& aOriginalSize,
   // Zero buffer to keep valgrind happy.
   memset(mRowBuffer.get(), 0, bufferLen);
 
-
   // Allocate the window, which contains horizontally downscaled scanlines. (We
   // can store scanlines which are already downscale because our downscaling
   // filter is separable.)
   mWindowCapacity = mYFilter->max_filter();
-  mWindow = MakeUnique<uint8_t*[]>(mWindowCapacity);
+  mWindow.reset(new (fallible) uint8_t*[mWindowCapacity]);
   if (MOZ_UNLIKELY(!mWindow)) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
@@ -136,7 +135,7 @@ Downscaler::BeginFrame(const nsIntSize& aOriginalSize,
   // pad by 15 to handle overreads by the simd code
   const int rowSize = mTargetSize.width * sizeof(uint32_t) + 15;
   for (int32_t i = 0; i < mWindowCapacity; ++i) {
-    mWindow[i] = new uint8_t[rowSize];
+    mWindow[i] = new (fallible) uint8_t[rowSize];
     anyAllocationFailed = anyAllocationFailed || mWindow[i] == nullptr;
   }
 
