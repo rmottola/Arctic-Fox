751082c8baab535111d1cc119db3adb0d3d5f5a8
Riccardo Mottola <rm@gnu.org>
2024-01-15 00:11:36 +0100

    Bug 1238213 - Make FetchEvent.request non-nullable; r=baku

--- dom/workers/ServiceWorkerPrivate.cpp
+++ dom/workers/ServiceWorkerPrivate.cpp
@@ -1234,8 +1234,7 @@ private:
                   request->Redirect() == RequestRedirect::Manual);
 
     RootedDictionary<FetchEventInit> init(aCx);
-    init.mRequest.Construct();
-    init.mRequest.Value() = request;
+    init.mRequest = request;
     init.mBubbles = false;
     init.mCancelable = true;
     if (!mClientId.IsEmpty()) {
