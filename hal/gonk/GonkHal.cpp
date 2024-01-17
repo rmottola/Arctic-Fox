4ae357d5871f3c4ab33a96a037de80111eed0b13
Riccardo Mottola <rm@gnu.org>
2024-01-17 01:30:29 +0100

    Bug 1123628 - mozilla::hal_impl::PriorityClass::~PriorityClass() closes not-open files. r=dhylands.

--- hal/gonk/GonkHal.cpp
+++ hal/gonk/GonkHal.cpp
@@ -1634,8 +1634,12 @@ PriorityClass::PriorityClass(ProcessPriority aPriority)
 
 PriorityClass::~PriorityClass()
 {
-  close(mCpuCGroupProcsFd);
-  close(mMemCGroupProcsFd);
+  if (mCpuCGroupProcsFd != -1) {
+    close(mCpuCGroupProcsFd);
+  }
+  if (mMemCGroupProcsFd != -1) {
+    close(mMemCGroupProcsFd);
+  }
 }
 
 PriorityClass::PriorityClass(const PriorityClass& aOther)
