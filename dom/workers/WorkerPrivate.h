6f4fc13e1ee890990adb5519b1baba691b6e79d3
Riccardo Mottola <rm@gnu.org>
2024-01-26 01:18:42 +0100

    Bug 1119490 - Expose the URL constructor to WorkerDebuggerGlobalScope;r=khuey

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -1263,6 +1263,9 @@ public:
   bool
   RegisterBindings(JSContext* aCx, JS::Handle<JSObject*> aGlobal);
 
+  bool
+  RegisterDebuggerBindings(JSContext* aCx, JS::Handle<JSObject*> aGlobal);
+
 #define WORKER_SIMPLE_PREF(name, getter, NAME)                                \
   bool                                                                        \
   getter() const                                                              \
