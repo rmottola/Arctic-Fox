f59232736bbaf37c07479016dd253393bb53c897
Riccardo Mottola <rm@gnu.org>
2024-02-01 14:57:28 +0100

    Bug 1218646 - Skip to check whether property can run on compositor or not if mWinsInCascade is false. r=birtles

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -2101,6 +2101,11 @@ KeyframeEffectReadOnly::ShouldBlockCompositorAnimations(const nsIFrame*
   bool shouldLog = nsLayoutUtils::IsAnimationLoggingEnabled();
 
   for (const AnimationProperty& property : mProperties) {
+    // If a property is overridden in the CSS cascade, it should not block other
+    // animations from running on the compositor.
+    if (!property.mWinsInCascade) {
+      continue;
+    }
     // Check for geometric properties
     if (IsGeometricProperty(property.mProperty)) {
       if (shouldLog) {
