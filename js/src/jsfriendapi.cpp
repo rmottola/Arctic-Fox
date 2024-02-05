e8bd53b44e2a66a7d55c78327487e5165656b0df
Riccardo Mottola <rm@gnu.org>
2024-02-05 18:50:05 +0100

    Bug 1251833 - Part 3: Merge ArenaHeader into Arena. r=terrence

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -1005,7 +1005,7 @@ DumpHeapVisitArena(JSRuntime* rt, void* data, gc::Arena* arena,
 {
     DumpHeapTracer* dtrc = static_cast<DumpHeapTracer*>(data);
     fprintf(dtrc->output, "# arena allockind=%u size=%u\n",
-            unsigned(arena->aheader.getAllocKind()), unsigned(thingSize));
+            unsigned(arena->getAllocKind()), unsigned(thingSize));
 }
 
 static void
