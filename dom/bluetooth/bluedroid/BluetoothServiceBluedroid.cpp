0eeddf875e854ef99591e4b7a306aeea25a8a7ac
Riccardo Mottola <rm@gnu.org>
2024-02-07 01:22:18 +0100

    Bug 1223729 - HID Connection Implementation, r=jocelyn, sr=mrbkap

--- dom/bluetooth/bluedroid/BluetoothServiceBluedroid.cpp
+++ dom/bluetooth/bluedroid/BluetoothServiceBluedroid.cpp
@@ -2623,6 +2623,7 @@ BluetoothServiceBluedroid::BackendErrorNotification(bool aCrashed)
    * Reset following profile manager states for unexpected backend crash.
    * - HFP: connection state and audio state
    * - A2DP: connection state
+   * - HID: connection state
    */
   BluetoothHfpManager* hfp = BluetoothHfpManager::Get();
   NS_ENSURE_TRUE_VOID(hfp);
@@ -2630,6 +2631,9 @@ BluetoothServiceBluedroid::BackendErrorNotification(bool aCrashed)
   BluetoothA2dpManager* a2dp = BluetoothA2dpManager::Get();
   NS_ENSURE_TRUE_VOID(a2dp);
   a2dp->HandleBackendError();
+  BluetoothHidManager* hid = BluetoothHidManager::Get();
+  NS_ENSURE_TRUE_VOID(hid);
+  hid->HandleBackendError();
 
   mIsRestart = true;
   BT_LOGR("Recovery step2: stop bluetooth");
