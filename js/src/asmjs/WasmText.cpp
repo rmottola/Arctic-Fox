cd3e204373dd4227b0ae7f4bf20c8a6c19dfdd2d
Riccardo Mottola <rm@gnu.org>
2024-02-09 01:20:27 +0100

    Bug 1253137 - Baldr: update section header structure to match BinaryEncoding.md, part 1 (r=sunfish)

--- js/src/asmjs/WasmText.cpp
+++ js/src/asmjs/WasmText.cpp
@@ -3708,11 +3708,8 @@ EncodeSignatureSection(Encoder& e, WasmAstModule& module)
     if (module.sigs().empty())
         return true;
 
-    if (!e.writeCString(SigLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(SigLabel, &offset))
         return false;
 
     if (!e.writeVarU32(module.sigs().length()))
@@ -3741,11 +3738,8 @@ EncodeDeclarationSection(Encoder& e, WasmAstModule& module)
     if (module.funcs().empty())
         return true;
 
-    if (!e.writeCString(DeclLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(DeclLabel, &offset))
         return false;
 
     if (!e.writeVarU32(module.funcs().length()))
@@ -3789,11 +3783,8 @@ EncodeImportSection(Encoder& e, WasmAstModule& module)
     if (module.imports().empty())
         return true;
 
-    if (!e.writeCString(ImportLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(ImportLabel, &offset))
         return false;
 
     for (WasmAstImport* imp : module.imports()) {
@@ -3816,11 +3807,8 @@ EncodeMemorySection(Encoder& e, WasmAstModule& module)
     if (!module.maybeMemory())
         return true;
 
-    if (!e.writeCString(MemoryLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(MemoryLabel, &offset))
         return false;
 
     WasmAstMemory& memory = *module.maybeMemory();
@@ -3865,11 +3853,8 @@ EncodeExportSection(Encoder& e, WasmAstModule& module)
     if (module.exports().empty())
         return true;
 
-    if (!e.writeCString(ExportLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(ExportLabel, &offset))
         return false;
 
     for (WasmAstExport* exp : module.exports()) {
@@ -3902,11 +3887,8 @@ EncodeTableSection(Encoder& e, WasmAstModule& module)
     if (!module.maybeTable())
         return true;
 
-    if (!e.writeCString(TableLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(TableLabel, &offset))
         return false;
 
     if (!e.writeVarU32(module.maybeTable()->elems().length()))
@@ -3922,15 +3904,8 @@ EncodeTableSection(Encoder& e, WasmAstModule& module)
 }
 
 static bool
-EncodeFunctionSection(Encoder& e, WasmAstFunc& func)
+EncodeFunctionBody(Encoder& e, WasmAstFunc& func)
 {
-    if (!e.writeCString(FuncLabel))
-        return false;
-
-    size_t offset;
-    if (!e.startSection(&offset))
-        return false;
-
     if (!e.writeVarU32(func.vars().length()))
         return false;
 
@@ -3947,8 +3922,25 @@ EncodeFunctionSection(Encoder& e, WasmAstFunc& func)
             return false;
     }
 
-    e.finishSection(offset);
+    return true;
+}
 
+static bool
+EncodeFunctionBodiesSection(Encoder& e, WasmAstModule& module)
+{
+    if (module.funcs().empty())
+        return true;
+
+    size_t offset;
+    if (!e.startSection(FuncLabel, &offset))
+        return false;
+
+    for (WasmAstFunc* func : module.funcs()) {
+        if (!EncodeFunctionBody(e, *func))
+            return false;
+    }
+
+    e.finishSection(offset);
     return true;
 }
 
@@ -3989,11 +3981,8 @@ EncodeDataSection(Encoder& e, WasmAstModule& module)
 
     const WasmAstSegmentVector& segments = module.maybeMemory()->segments();
 
-    if (!e.writeCString(DataLabel))
-        return false;
-
     size_t offset;
-    if (!e.startSection(&offset))
+    if (!e.startSection(DataLabel, &offset))
         return false;
 
     for (WasmAstSegment* segment : segments) {
@@ -4043,10 +4032,8 @@ EncodeModule(WasmAstModule& module)
     if (!EncodeExportSection(e, module))
         return nullptr;
 
-    for (WasmAstFunc* func : module.funcs()) {
-        if (!EncodeFunctionSection(e, *func))
-            return nullptr;
-    }
+    if (!EncodeFunctionBodiesSection(e, module))
+        return nullptr;
 
     if (!EncodeDataSection(e, module))
         return nullptr;
