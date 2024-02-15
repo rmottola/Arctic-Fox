034e08ef838417f21ebd29961221c6545fa98993
Riccardo Mottola <rm@gnu.org>
2024-02-15 18:57:15 +0100

    Bug 1159219: Make ContentSender a separate module

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -27,6 +27,7 @@ Cu.import("chrome://marionette/content/elements.js");
 Cu.import("chrome://marionette/content/emulator.js");
 Cu.import("chrome://marionette/content/error.js");
 Cu.import("chrome://marionette/content/modal.js");
+Cu.import("chrome://marionette/content/proxy.js");
 Cu.import("chrome://marionette/content/simpletest.js");
 
 loader.loadSubScript("chrome://marionette/content/common.js");
@@ -79,111 +80,6 @@ this.Context.fromString = function(s) {
     return this[s];
   }
   return null;
-};
-
-/**
- * Creates a transparent interface between the chrome- and content
- * processes.
- *
- * Calls to this object will  be proxied via the message manager to the active
- * browsing context (content) and responses will be provided back as
- * promises.
- *
- * The argument sequence is serialised and passed as an array, unless it
- * consists of a single object type that isn't null, in which case it's
- * passed literally.  The latter specialisation is temporary to achieve
- * backwards compatibility with listener.js.
- *
- * @param {function(): nsIMessageManager} mmFn
- *     Function returning the current message manager.
- * @param {function(string, Object, number)} sendAsyncFn
- *     Callback for sending async messages to the current listener.
- * @param {function(): BrowserObj} curBrowserFn
- *     Function that returns the current browser.
- */
-let ListenerProxy = function(mmFn, sendAsyncFn, curBrowserFn) {
-  this.curCmdId = null;
-  this.sendAsync = sendAsyncFn;
-
-  this.mmFn_ = mmFn;
-  this.curBrowserFn_ = curBrowserFn;
-};
-
-Object.defineProperty(ListenerProxy.prototype, "mm", {
-  get: function() { return this.mmFn_(); }
-});
-
-Object.defineProperty(ListenerProxy.prototype, "curBrowser", {
-  get: function() { return this.curBrowserFn_(); }
-});
-
-ListenerProxy.prototype.__noSuchMethod__ = function*(name, args) {
-  const ok = "Marionette:ok";
-  const val = "Marionette:done";
-  const err = "Marionette:error";
-
-  let proxy = new Promise((resolve, reject) => {
-    let removeListeners = (name, fn) => {
-      let rmFn = msg => {
-        if (this.isOutOfSync(msg.json.command_id)) {
-          logger.warn("Skipping out-of-sync response from listener: " +
-              msg.name + msg.json.toSource());
-          return;
-        }
-
-        listeners.remove();
-        modal.removeHandler(handleDialog);
-
-        fn(msg);
-        this.curCmdId = null;
-      };
-
-      listeners.push([name, rmFn]);
-      return rmFn;
-    };
-
-    let listeners = [];
-    listeners.add = () => {
-      this.mm.addMessageListener(ok, removeListeners(ok, okListener));
-      this.mm.addMessageListener(val, removeListeners(val, valListener));
-      this.mm.addMessageListener(err, removeListeners(err, errListener));
-    };
-    listeners.remove = () =>
-        listeners.map(l => this.mm.removeMessageListener(l[0], l[1]));
-
-    let okListener = () => resolve();
-    let valListener = msg => resolve(msg.json.value);
-    let errListener = msg => reject(
-        "error" in msg.objects ? msg.objects.error : msg.json);
-
-    let handleDialog = function(subject, topic) {
-      listeners.remove();
-      modal.removeHandler(handleDialog);
-      this.sendAsync("cancelRequest");
-      resolve();
-    }.bind(this);
-
-    // start content process listeners, and install observers for global-
-    // and tab modal dialogues
-    listeners.add();
-    modal.addHandler(handleDialog);
-
-    // convert to array if passed arguments
-    let msg;
-    if (args.length == 1 && typeof args[0] == "object" && args[0] !== null) {
-      msg = args[0];
-    } else {
-      msg = Array.prototype.slice.call(args);
-    }
-
-    this.sendAsync(name, msg, this.curCmdId);
-  });
-
-  return proxy;
-};
-
-ListenerProxy.prototype.isOutOfSync = function(id) {
-  return this.curCmdId !== id;
 };
 
 /**
@@ -266,10 +162,7 @@ this.GeckoDriver = function(appName, device, emulator) {
   };
 
   this.mm = globalMessageManager;
-  this.listener = new ListenerProxy(
-      () => this.mm,
-      this.sendAsync.bind(this),
-      () => this.curBrowser);
+  this.listener = proxy.toListener(() => this.mm, this.sendAsync.bind(this));
 
   this.dialog = null;
   let handleDialog = (subject, topic) => {
