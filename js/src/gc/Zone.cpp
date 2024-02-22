3b98615a8bc2b319e808f38c941a18923613ca03
Riccardo Mottola <rm@gnu.org>
2024-02-22 14:47:48 +0100

    Bug 1244412 - Remove ZoneCellIterUnderGC now it's no longer needed and refactor r=terrence

--- js/src/gc/Zone.cpp
+++ js/src/gc/Zone.cpp
@@ -145,7 +145,7 @@ Zone::sweepBreakpoints(FreeOp* fop)
      */
 
     MOZ_ASSERT(isGCSweepingOrCompacting());
-    for (ZoneCellIterUnderGC i(this, AllocKind::SCRIPT); !i.done(); i.next()) {
+    for (ZoneCellIter i(this, AllocKind::SCRIPT); !i.done(); i.next()) {
         JSScript* script = i.get<JSScript>();
         if (!script->hasAnyBreakpointsOrStepMode())
             continue;
