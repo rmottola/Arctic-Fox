609b2c0a2c710a7b4721fc60d4e7f4cbd7b5b0f6
Riccardo Mottola <rm@gnu.org>
2024-02-23 13:08:29 +0100

    Bug 518805 - Don't do meta refreshes when backgrounded r=bz
    This is controlled by browser.meta_refresh_when_inactive.disabled, which
    is false (allow refreshes) on desktop, and true (disable refreshes) on Fennec

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -940,6 +940,7 @@ protected:
   bool mAllowKeywordFixup;
   bool mIsOffScreenBrowser;
   bool mIsActive;
+  bool mDisableMetaRefreshWhenInactive;
   bool mIsPrerendered;
   bool mIsAppTab;
   bool mUseGlobalHistory;
