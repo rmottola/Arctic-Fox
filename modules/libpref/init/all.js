9085644f4e07ef4349de84fe0da3437b8a01163d
Riccardo Mottola <rm@gnu.org>
2024-02-23 15:26:02 +0100

    Bug 1242926 - Increase the max retry attempt number. r=mcmanus

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1636,7 +1636,7 @@ pref("network.http.max-persistent-connections-per-proxy", 16);
 pref("network.http.request.max-start-delay", 10);
 
 // If a connection is reset, we will retry it max-attempts times.
-pref("network.http.request.max-attempts", 3);
+pref("network.http.request.max-attempts", 6);
 
 // Headers
 pref("network.http.accept.default", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
