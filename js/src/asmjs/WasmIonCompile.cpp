0fdb365e82fc3dfaa4ff9f26daad990b1ace4b68
Riccardo Mottola <rm@gnu.org>
2024-03-05 18:17:18 +0100

    Bug 1256637: Set definition before returning early in EmitBrTable; r=luke

--- js/src/asmjs/WasmIonCompile.cpp
+++ js/src/asmjs/WasmIonCompile.cpp
@@ -2632,6 +2632,8 @@ EmitBrTable(FunctionCompiler& f, MDefinition** def)
     if (!EmitExpr(f, &index))
         return false;
 
+    *def = nullptr;
+
     // Empty table
     if (!numCases)
         return f.br(defaultDepth);
@@ -2663,7 +2665,6 @@ EmitBrTable(FunctionCompiler& f, MDefinition** def)
     if (!f.joinSwitch(switchBlock, cases, defaultBlock))
         return false;
 
-    *def = nullptr;
     return true;
 }
 
