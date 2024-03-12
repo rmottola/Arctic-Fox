8d5770a31cc012885c2b4c3f69b4888acffc8cbe
Riccardo Mottola <rm@gnu.org>
2024-03-12 23:19:01 +0100

    Bug 963945 - Add about:addons URI as whitelisted so that it can be refocused instead of opening duplicate tabs in private browsing. r=jdm

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -7567,6 +7567,7 @@ function switchToTabHavingURI(aURI, aOpenNew, aOpenParams={}) {
   // Certain URLs can be switched to irrespective of the source or destination
   // window being in private browsing mode:
   const kPrivateBrowsingWhitelist = new Set([
+    "about:addons",
     "about:customizing",
   ]);
 
