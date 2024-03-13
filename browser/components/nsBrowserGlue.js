78dbd44c98c3e8dbeedbb9d0f9e9e4ee85592c14
Riccardo Mottola <rm@gnu.org>
2024-03-13 11:18:55 +0100

    Bug 1199805 - Fix displayURI typo on site permission prompts. r=bgrins
    One instance of displayOrigin wasn't renamed to displayURI.

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2436,7 +2436,7 @@ ContentPermissionPrompt.prototype = {
 
     if (!aOptions)
       aOptions = {};
-    aOptions.displayOrigin = requestPrincipal.URI;
+    aOptions.displayURI = requestPrincipal.URI;
 
     return chromeWin.PopupNotifications.show(browser, aNotificationId, aMessage, aAnchorId,
                                              mainAction, secondaryActions, aOptions);
