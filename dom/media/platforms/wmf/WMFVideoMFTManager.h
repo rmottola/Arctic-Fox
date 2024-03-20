cb459d971c07db8ea76e7d88cda9020a0dc480bf
Riccardo Mottola <rm@gnu.org>
2024-03-20 08:29:47 +0100

    Bug 1249706: Added telemetry for the proportion of frames dropped keyed by several details. r=jya

--- dom/media/platforms/wmf/WMFVideoMFTManager.h
+++ dom/media/platforms/wmf/WMFVideoMFTManager.h
@@ -39,14 +39,9 @@ public:
     return TrackInfo::kVideoTrack;
   }
 
-  const char* GetDescriptionName() const override
-  {
-    nsCString failureReason;
-    return IsHardwareAccelerated(failureReason)
-      ? "wmf hardware video decoder" : "wmf software video decoder";
-  }
-
   void ConfigurationChanged(const TrackInfo& aConfig) override;
+
+  const char* GetDescriptionName() const override;
 
 private:
 
