8f389cb0c76338f3c146a39d94af064f94687555
Riccardo Mottola <rm@gnu.org>
2024-03-25 13:02:13 +0100

    Bug 1169674 - Use originNoSuffix for permission event. r=fabrice, f=bholley

--- b2g/components/ContentPermissionPrompt.js
+++ b2g/components/ContentPermissionPrompt.js
@@ -32,7 +32,7 @@ Cu.import("resource://gre/modules/PermissionsTable.jsm");
 var permissionManager = Cc["@mozilla.org/permissionmanager;1"].getService(Ci.nsIPermissionManager);
 var secMan = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
 
-let permissionSpecificChecker = {};
+var permissionSpecificChecker = {};
 
 XPCOMUtils.defineLazyServiceGetter(this,
                                    "TelephonyService",
@@ -413,7 +413,10 @@ ContentPermissionPrompt.prototype = {
       type: type,
       permissions: permissions,
       id: requestId,
-      origin: principal.origin,
+      // This system app uses the origin from permission events to
+      // compare against the mozApp.origin of app windows, so we
+      // are not concerned with origin suffixes here (appId, etc).
+      origin: principal.originNoSuffix,
       isApp: isApp,
       remember: remember,
       isGranted: isGranted,
