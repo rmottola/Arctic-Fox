d8e1193adcf8db6bca19fcd07cee277771322828
Riccardo Mottola <rm@gnu.org>
2024-03-25 07:54:42 +0100

    Bug 1250333 - do not create accessibles for trailing BRs, r=davidb, roc

--- accessible/tests/mochitest/editabletext/editabletext.js
+++ accessible/tests/mochitest/editabletext/editabletext.js
@@ -62,6 +62,7 @@ function editableTextTest(aID)
 
     function setTextContentsInvoke()
     {
+      dump(`\nsetTextContents '${aValue}'\n`);
       var acc = getAccessible(aID, nsIAccessibleEditableText);
       acc.setTextContents(aValue);
     }
@@ -84,6 +85,7 @@ function editableTextTest(aID)
 
     function insertTextInvoke()
     {
+      dump(`\ninsertText '${aStr}' at ${aPos} pos\n`);
       var acc = getAccessible(aID, nsIAccessibleEditableText);
       acc.insertText(aStr, aPos);
     }
