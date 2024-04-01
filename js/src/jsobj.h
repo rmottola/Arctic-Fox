ad0540cad846d55bc463b35f037e2a045c7d7498
Riccardo Mottola <rm@gnu.org>
2024-04-01 02:08:44 +0200

    Bug 1165053 - Part 5: Add native SpeciesConstructor wrapper. r=lth

--- js/src/jsobj.h
+++ js/src/jsobj.h
@@ -1349,6 +1349,9 @@ FreezeObject(JSContext* cx, HandleObject obj)
 extern bool
 TestIntegrityLevel(JSContext* cx, HandleObject obj, IntegrityLevel level, bool* resultp);
 
+extern bool
+SpeciesConstructor(JSContext* cx, HandleObject obj, HandleValue defaultCtor, MutableHandleValue pctor);
+
 }  /* namespace js */
 
 #endif /* jsobj_h */
