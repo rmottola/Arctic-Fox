c4c2799b94c5f55f6b2c0c8d1e9710e9b53dcd9a
Riccardo Mottola <rm@gnu.org>
2024-04-18 16:14:20 +0200

    Bug 1247700: Avoid crash from invalid fonts. r=bas

--- gfx/2d/DrawTargetCairo.cpp
+++ gfx/2d/DrawTargetCairo.cpp
@@ -614,7 +614,8 @@ DrawTargetCairo::~DrawTargetCairo()
 bool
 DrawTargetCairo::IsValid() const
 {
-  return mSurface && !cairo_surface_status(mSurface) && !cairo_surface_status(cairo_get_group_target(mContext));
+  return mSurface && !cairo_surface_status(mSurface) &&
+         mContext && !cairo_surface_status(cairo_get_group_target(mContext));
 }
 
 DrawTargetType
@@ -1286,6 +1287,10 @@ DrawTargetCairo::FillGlyphs(ScaledFont *aFont,
   if (!IsValid()) {
     gfxDebug() << "FillGlyphs bad surface " << cairo_surface_status(cairo_get_group_target(mContext));
     return;
+  }
+
+  if (!aFont) {
+    gfxDevCrash(LogReason::InvalidFont) << "Invalid scaled font";
   }
 
   AutoPrepareForDrawing prep(this, mContext);
