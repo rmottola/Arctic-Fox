c1aa5d1d6257708aa6d2d43e1fe74cddcace0902
Riccardo Mottola <rm@gnu.org>
2024-04-20 13:16:23 +0200

    Bug 1253010 - part 3 - create all nsIDateTimeFormat instances directly; r=smontagu

--- security/manager/ssl/TransportSecurityInfo.cpp
+++ security/manager/ssl/TransportSecurityInfo.cpp
@@ -12,7 +12,6 @@
 #include "nsNSSCertificate.h"
 #include "nsIX509CertValidity.h"
 #include "nsIDateTimeFormat.h"
-#include "nsDateTimeFormatCID.h"
 #include "nsICertOverrideService.h"
 #include "nsIObjectInputStream.h"
 #include "nsIObjectOutputStream.h"
@@ -791,9 +790,10 @@ GetDateBoundary(nsIX509Cert* ix509,
     trueExpired_falseNotYetValid = false;
   }
 
-  nsCOMPtr<nsIDateTimeFormat> dateTimeFormat(do_CreateInstance(NS_DATETIMEFORMAT_CONTRACTID, &rv));
-  if (NS_FAILED(rv))
+  nsCOMPtr<nsIDateTimeFormat> dateTimeFormat = nsIDateTimeFormat::Create();
+  if (!dateTimeFormat) {
     return;
+  }
 
   dateTimeFormat->FormatPRTime(nullptr, kDateFormatLong, kTimeFormatNoSeconds,
                                timeToUse, formattedDate);
