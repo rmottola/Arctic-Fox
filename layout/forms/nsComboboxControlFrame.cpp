7dd43472158b9bcdbae7e33596a1c6dfa6ddb11f
Riccardo Mottola <rm@gnu.org>
2024-04-21 00:23:55 +0200

    Bug 1097499 part 3 - Add a separate anonbox for text nodes. r=heycam

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -1370,7 +1370,8 @@ nsComboboxControlFrame::CreateFrameFor(nsIContent*      aContent)
                              nsStyleSet::eSkipParentDisplayBasedStyleFixup);
 
   RefPtr<nsStyleContext> textStyleContext;
-  textStyleContext = styleSet->ResolveStyleForNonElement(mStyleContext);
+  textStyleContext = styleSet->
+    ResolveStyleForNonElement(mStyleContext, nsCSSAnonBoxes::mozText);
 
   // Start by creating our anonymous block frame
   mDisplayFrame = new (shell) nsComboboxDisplayFrame(styleContext, this);
