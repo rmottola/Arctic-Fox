99dfb99af44fa0f566da78877da32af24ffca2a4
Riccardo Mottola <rm@gnu.org>
2024-04-21 18:15:10 +0200

    Bug 1261578 - Part 3. Correct MathML text color; r=jfkthame

--- layout/mathml/nsMathMLChar.cpp
+++ layout/mathml/nsMathMLChar.cpp
@@ -2123,7 +2123,8 @@ nsMathMLChar::PaintForeground(nsPresContext* aPresContext,
   RefPtr<gfxContext> thebesContext = aRenderingContext.ThebesContext();
 
   // Set color ...
-  nscolor fgColor = styleContext->GetVisitedDependentColor(eCSSProperty_color);
+  nsCSSProperty colorProp = styleContext->GetTextFillColorProp();
+  nscolor fgColor = styleContext->GetVisitedDependentColor(colorProp);
   if (aIsSelected) {
     // get color to use for selection from the look&feel object
     fgColor = LookAndFeel::GetColor(LookAndFeel::eColorID_TextSelectForeground,
