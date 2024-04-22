4b7ee565e7095cf072b2f187979d6c7f0111d769
Riccardo Mottola <rm@gnu.org>
2024-04-23 00:46:29 +0200

    Bug 1259662 part.2 Rename WidgetDragEvent::userCancelled to WidgetDragEvent::mUserCancelled r=smaug

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -5296,7 +5296,7 @@ nsContentUtils::SetDataTransferInEvent(WidgetDragEvent* aDragEvent)
 
   // each event should use a clone of the original dataTransfer.
   initialDataTransfer->Clone(aDragEvent->target, aDragEvent->mMessage,
-                             aDragEvent->userCancelled,
+                             aDragEvent->mUserCancelled,
                              isCrossDomainSubFrameDrop,
                              getter_AddRefs(aDragEvent->mDataTransfer));
   if (NS_WARN_IF(!aDragEvent->mDataTransfer)) {
