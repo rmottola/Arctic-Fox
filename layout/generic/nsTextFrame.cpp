8745a7566ee6417310b9d8b90db4aba3f1603615
Riccardo Mottola <rm@gnu.org>
2024-04-22 18:11:18 +0200

    Bug 1265648 followup - Use an array of UniquePtr<> for mTextRunsToDelete. r=mats

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -977,7 +977,7 @@ private:
   AutoTArray<MappedFlow,10>   mMappedFlows;
   AutoTArray<nsTextFrame*,50> mLineBreakBeforeFrames;
   AutoTArray<nsAutoPtr<BreakSink>,10> mBreakSinks;
-  AutoTArray<gfxTextRun*,5>   mTextRunsToDelete;
+  AutoTArray<UniquePtr<gfxTextRun>,5> mTextRunsToDelete;
   nsLineBreaker                 mLineBreaker;
   gfxTextRun*                   mCurrentFramesAllSameTextRun;
   DrawTarget*                   mDrawTarget;
@@ -1491,11 +1491,6 @@ void BuildTextRunsScanner::FlushLineBreaks(gfxTextRun* aTrailingTextRun)
     mBreakSinks[i]->Finish(mMissingFonts);
   }
   mBreakSinks.Clear();
-
-  for (uint32_t i = 0; i < mTextRunsToDelete.Length(); ++i) {
-    gfxTextRun* deleteTextRun = mTextRunsToDelete[i];
-    delete deleteTextRun;
-  }
   mTextRunsToDelete.Clear();
 }
 
@@ -2243,7 +2238,7 @@ BuildTextRunsScanner::BuildTextRunForFrames(void* aTextBuffer)
     DestroyUserData(userDataToDestroy);
     // Arrange for this textrun to be deleted the next time the linebreaker
     // is flushed out
-    mTextRunsToDelete.AppendElement(textRun.release());
+    mTextRunsToDelete.AppendElement(Move(textRun));
     return nullptr;
   }
 
