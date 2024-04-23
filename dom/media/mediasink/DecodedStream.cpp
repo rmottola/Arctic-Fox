0662c26a97f814e8f41a17edd936704a0db99967
Riccardo Mottola <rm@gnu.org>
2024-04-23 20:04:31 +0200

    Bug 1208371 - Add PrincipalHandle to MediaChunks. r=mt,jesup
    PrincipalHandle is a thread safe pointer to a holder of (the main-thread-only
    nsIPrincipal) that can be passed around the MSG.
    
    A MediaStreamTrack whose source has just updated its principal, sets the new
    principal aside (as its "pending principal"), and combines the new principal
    into its current principal.
    
    Then the source starts passing the new principal to the MediaStreamGraph as
    a PrincipalHandle.
    
    Changes to a track's PrincipalHandle on the MSG will be surfaced through the
    MediaStreamTrackListener API. These changes are dispatched to main thread
    and compared to a MediaStreamTrack's pending principal. In case of a match
    the track knows the correct principal is flowing and can move the pending
    principal to be the current principal and update any main thread principal
    observers.

--- dom/media/mediasink/DecodedStream.cpp
+++ dom/media/mediasink/DecodedStream.cpp
@@ -491,7 +491,7 @@ SendStreamAudio(DecodedStreamData* aStream, int64_t aStartTime,
   for (uint32_t i = 0; i < audio->mChannels; ++i) {
     channels.AppendElement(bufferData + i * audio->mFrames);
   }
-  aOutput->AppendFrames(buffer.forget(), channels, audio->mFrames);
+  aOutput->AppendFrames(buffer.forget(), channels, audio->mFrames, PRINCIPAL_HANDLE_NONE /* Fixed in later patch */);
   aStream->mAudioFramesWritten += audio->mFrames;
 
   aStream->mNextAudioTime = audio->GetEndTime();
@@ -550,7 +550,7 @@ WriteVideoToMediaStream(MediaStream* aStream,
   StreamTime duration =
       aStream->MicrosecondsToStreamTimeRoundDown(aEndMicroseconds) -
       aStream->MicrosecondsToStreamTimeRoundDown(aStartMicroseconds);
-  aOutput->AppendFrame(image.forget(), duration, aIntrinsicSize);
+  aOutput->AppendFrame(image.forget(), duration, aIntrinsicSize, PRINCIPAL_HANDLE_NONE /* Fixed in later patch */);
 }
 
 static bool
