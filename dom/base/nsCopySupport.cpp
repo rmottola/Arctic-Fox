5ad3c180fe3850368b4ee97962a184d36a51ddbe
Riccardo Mottola <rm@gnu.org>
2024-04-23 08:36:50 +0200

    Bug 1259673 rename from InternalClipboardEvent.clipboardData to InternalClipboardEvent.mClipboardData. r=masayuki

--- dom/base/nsCopySupport.cpp
+++ dom/base/nsCopySupport.cpp
@@ -697,7 +697,7 @@ nsCopySupport::FireClipboardEvent(EventMessage aEventMessage,
 
     nsEventStatus status = nsEventStatus_eIgnore;
     InternalClipboardEvent evt(true, aEventMessage);
-    evt.clipboardData = clipboardData;
+    evt.mClipboardData = clipboardData;
     EventDispatcher::Dispatch(content, presShell->GetPresContext(), &evt,
                               nullptr, &status);
     // If the event was cancelled, don't do the clipboard operation
