5ffce18e3317c2ab4b4aad08001720305f1280c8
Riccardo Mottola <rm@gnu.org>
2024-04-24 09:15:22 +0200

    Bug 1225327 - fix |mEncoder| null-dereference. r=jwwang

--- dom/media/MediaRecorder.cpp
+++ dom/media/MediaRecorder.cpp
@@ -584,8 +584,7 @@ public:
   size_t
   SizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf) const
   {
-    size_t amount = mEncoder->SizeOfExcludingThis(aMallocSizeOf);
-    return amount;
+    return (mEncoder ?  mEncoder->SizeOfExcludingThis(aMallocSizeOf) : 0);
   }
 
 
