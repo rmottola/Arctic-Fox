860c16847dbec3e0d0d250e3cd1651299d47c4ae
Riccardo Mottola <rm@gnu.org>
2024-04-29 14:12:54 +0200

    Bug 937586 - make Size column in network monitor accurate; r=vporof

--- devtools/server/actors/webconsole.js
+++ devtools/server/actors/webconsole.js
@@ -2123,7 +2123,7 @@ NetworkEventActor.prototype =
       type: "networkEventUpdate",
       updateType: "responseContent",
       mimeType: aContent.mimeType,
-      contentSize: aContent.text.length,
+      contentSize: aContent.size,
       transferredSize: aContent.transferredSize,
       discardResponseBody: aDiscardedResponseBody,
     };
