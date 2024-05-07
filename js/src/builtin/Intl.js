999911b0b3a621472cbcd58c4343792186fc472c
Riccardo Mottola <rm@gnu.org>
2024-05-07 12:17:03 +0200

    Bug 1263040 - Add Intl.getCanonicalLocales. r=jwalden

--- js/src/builtin/Intl.js
+++ js/src/builtin/Intl.js
@@ -2885,3 +2885,17 @@ function resolveICUPattern(pattern, result) {
         }
     }
 }
+
+function Intl_getCanonicalLocales(locales) {
+  let codes = CanonicalizeLocaleList(locales);
+  let result = [];
+
+  let len = codes.length;
+  let k = 0;
+
+  while (k < len) {
+    _DefineDataProperty(result, k, codes[k]);
+    k++;
+  }
+  return result;
+}
