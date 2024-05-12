553274c0496eeda23e38f52a9af3b054709d4fc0
Riccardo Mottola <rm@gnu.org>
2024-05-12 22:05:33 +0200

    Bug 1251680 - get container accessible computation should take into account the HTML select, r=marcoz

--- accessible/generic/DocAccessible.cpp
+++ accessible/generic/DocAccessible.cpp
@@ -1254,6 +1254,12 @@ DocAccessible::GetAccessibleOrContainer(nsINode* aNode) const
     if (!(currNode = parent)) break;
   }
 
+  // HTML comboboxes have no-content list accessible as an intermediate
+  // containing all options.
+  if (accessible && accessible->IsHTMLCombobox()) {
+    return accessible->FirstChild();
+  }
+
   return accessible;
 }
 
@@ -1701,11 +1707,6 @@ DocAccessible::ProcessContentInserted(Accessible* aContainer,
       // For example, elements may be inserted under the document element while
       // there is no HTML body element.
     }
-
-    // HTML comboboxes have no-content list accessible as an intermidiate
-    // containing all options.
-    if (container->IsHTMLCombobox())
-      container = container->FirstChild();
 
     // We have a DOM/layout change under the container accessible, and its tree
     // might need an update. Since DOM/layout change of the element may affect
