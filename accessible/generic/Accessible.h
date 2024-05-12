7108ee2e06be39f9de2453895de71ad072773fcf
Riccardo Mottola <rm@gnu.org>
2024-05-12 22:45:48 +0200

    Bug 1252260 - get rid of HTML table CacheChildren, r=marcoz

--- accessible/generic/Accessible.h
+++ accessible/generic/Accessible.h
@@ -585,6 +585,7 @@ public:
   HyperTextAccessible* AsHyperText();
 
   bool IsHTMLBr() const { return mType == eHTMLBRType; }
+  bool IsHTMLCaption() const { return mType == eHTMLCaptionType; }
   bool IsHTMLCombobox() const { return mType == eHTMLComboboxType; }
   bool IsHTMLFileInput() const { return mType == eHTMLFileInputType; }
 
