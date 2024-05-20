8cd61ca5b571c12a88d0f5f4ccfa540c897c08c2
Riccardo Mottola <rm@gnu.org>
2024-05-20 23:19:53 +0200

    Bug 1243681 - Query port base and calculate index with it. r=alfredo

--- dom/media/platforms/omx/GonkOmxPlatformLayer.cpp
+++ dom/media/platforms/omx/GonkOmxPlatformLayer.cpp
@@ -361,7 +361,7 @@ GonkOmxPlatformLayer::AllocateOmxBuffer(OMX_DIRTYPE aType,
   // Get port definition.
   OMX_PARAM_PORTDEFINITIONTYPE def;
   nsTArray<uint32_t> portindex;
-  GetOmxPortIndex(portindex);
+  GetPortIndices(portindex);
   for (auto idx : portindex) {
     InitOmxParameter(&def);
     def.nPortIndex = idx;
