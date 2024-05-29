6b5d2856b639e0de6fdb2d2183c378cdefafccff
Riccardo Mottola <rm@gnu.org>
2024-05-29 22:47:31 +0200

    guard for non-linux system again or mac & FreeBSD fail to compile

--- xpcom/io/nsLocalFileUnix.cpp
+++ xpcom/io/nsLocalFileUnix.cpp
@@ -13,7 +13,9 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#if !defined(XP_MACOSX) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
 #include <sys/sysmacros.h>
+#endif
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
