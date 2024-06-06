7a38717c47d000e82595e13458a10b4fefa2607c
Riccardo Mottola <rm@gnu.org>
2024-06-06 20:28:19 +0200

    Bug 1249098: Support maxplaybackrate for opus. r=jesup

--- media/webrtc/signaling/src/peerconnection/MediaPipelineFactory.cpp
+++ media/webrtc/signaling/src/peerconnection/MediaPipelineFactory.cpp
@@ -75,6 +75,7 @@ JsepCodecDescToCodecConfig(const JsepCodecDescription& aCodec,
                                   desc.mPacketSize,
                                   desc.mChannels,
                                   desc.mBitrate);
+  (*aConfig)->mMaxPlaybackRate = desc.mMaxPlaybackRate;
 
   return NS_OK;
 }
