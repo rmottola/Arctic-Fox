1087add7d020a4639e3bba330b6416f69214fb2c
Riccardo Mottola <rm@gnu.org>
2024-07-10 14:57:07 +0200

    Bug 1251808 (Followup) - Mark the new SourceSurfaceImage constructor explicit. r=me

--- gfx/layers/ImageContainer.h
+++ gfx/layers/ImageContainer.h
@@ -825,7 +825,7 @@ public:
   virtual gfx::IntSize GetSize() override { return mSize; }
 
   SourceSurfaceImage(const gfx::IntSize& aSize, gfx::SourceSurface* aSourceSurface);
-  SourceSurfaceImage(gfx::SourceSurface* aSourceSurface);
+  explicit SourceSurfaceImage(gfx::SourceSurface* aSourceSurface);
   ~SourceSurfaceImage();
 
 private:
