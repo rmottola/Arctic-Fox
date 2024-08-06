336f161d21d9b6f3d77bc6120d7cdb5be382f5c1
Riccardo Mottola <rm@gnu.org>
2024-08-06 18:38:24 +0200

    Bug 1239955 - Let DNSService rely on IOService::Offline, r=bagder

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -1072,9 +1072,6 @@ nsIOService::SetOffline(bool offline)
                                                  NS_IOSERVICE_GOING_OFFLINE_TOPIC,
                                                  offlineString.get());
 
-            if (mDNSService)
-                mDNSService->SetOffline(true);
-
             if (mSocketTransportService)
                 mSocketTransportService->SetOffline(true);
 
@@ -1087,7 +1084,6 @@ nsIOService::SetOffline(bool offline)
         else if (!offline && mOffline) {
             // go online
             if (mDNSService) {
-                mDNSService->SetOffline(false);
                 DebugOnly<nsresult> rv = mDNSService->Init();
                 NS_ASSERTION(NS_SUCCEEDED(rv), "DNS service init failed");
             }
