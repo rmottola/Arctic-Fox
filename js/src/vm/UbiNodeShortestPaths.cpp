1b858d7a0ca19d064661945098d7108e231a755c
Riccardo Mottola <rm@gnu.org>
2024-08-22 11:07:38 +0200

    Bug 1267737 - Request edge names from JS::ubi::RootList before calling `init`; r=jimb
    Setting the `wantNames` flag must happen before calling `init` or else it will
    not have any effect.

--- js/src/vm/UbiNodeShortestPaths.cpp
+++ js/src/vm/UbiNodeShortestPaths.cpp
@@ -49,9 +49,8 @@ dumpPaths(JSRuntime* rt, Node node, uint32_t maxNumPaths /* = 10 */)
 {
     mozilla::Maybe<AutoCheckCannotGC> nogc;
 
-    JS::ubi::RootList rootList(rt, nogc);
+    JS::ubi::RootList rootList(rt, nogc, true);
     MOZ_ASSERT(rootList.init());
-    rootList.wantNames = true;
 
     NodeSet targets;
     bool ok = targets.init() && targets.putNew(node);
