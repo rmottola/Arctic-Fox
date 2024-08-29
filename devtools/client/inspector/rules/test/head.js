964270c8c3a89d2efb6170980ae9097ecc7e33b7
Riccardo Mottola <rm@gnu.org>
2024-08-29 14:40:33 +0200

    Bug 1241527 - 3 - Fix typo in hideTooltipAndWaitForRuleviewChanged; r=gl

--- devtools/client/inspector/rules/test/head.js
+++ devtools/client/inspector/rules/test/head.js
@@ -206,7 +206,7 @@ function assertHoverTooltipOn(tooltip, element) {
  * @param {Tooltip} tooltip
  * @param {CSSRuleView} view
  */
-function* hideTooltipAndWaitForRuleviewChanged(tooltip, view) {
+function* hideTooltipAndWaitForRuleViewChanged(tooltip, view) {
   let onModified = view.once("ruleview-changed");
   tooltip.hide();
   yield onModified;
