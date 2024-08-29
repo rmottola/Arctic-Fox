d21e70b8118e460af8a42e62da7d025c9ef7fb69
Riccardo Mottola <rm@gnu.org>
2024-08-29 11:28:46 +0200

    Bug 1155465 - part1: layout-view: use content-type CSS_VALUE for editors;r=miker
    Inplace editors for the layout view should use the CSS_VALUE content-type since they
    allow to edit a CSS value, and not just plain text.

--- devtools/client/inspector/layout/layout.js
+++ devtools/client/inspector/layout/layout.js
@@ -318,7 +318,10 @@ LayoutView.prototype = {
     let editor = new InplaceEditor({
       element: element,
       initial: initialValue,
-
+      contentType: InplaceEditor.CONTENT_TYPES.CSS_VALUE,
+      property: {
+        name: dimension.property
+      },
       start: self => {
         self.elt.parentNode.classList.add("layout-editing");
       },
