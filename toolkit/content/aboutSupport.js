9b85c113e6c584bea835a1c7ceac3f4ff2fec38e
Riccardo Mottola <rm@gnu.org>
2024-09-06 14:19:15 +0200

    Bug 1145171 - Show the detailed version in about:support (usefull for beta, no impact for the rest) r=dolske

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -10,6 +10,7 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/Troubleshoot.jsm");
 Cu.import("resource://gre/modules/ResetProfile.jsm");
+Cu.import("resource://gre/modules/AppConstants.jsm");
 
 XPCOMUtils.defineLazyModuleGetter(this, "PluralForm",
                                   "resource://gre/modules/PluralForm.jsm");
@@ -40,7 +41,7 @@ var snapshotFormatters = {
     $("useragent-box").textContent = data.userAgent;
     $("os-box").textContent = data.osVersion;
     $("supportLink").href = data.supportURL;
-    let version = data.version;
+    let version = AppConstants.MOZ_APP_VERSION_DISPLAY;
     if (data.vendor)
       version += " (" + data.vendor + ")";
     $("version-box").textContent = version;
