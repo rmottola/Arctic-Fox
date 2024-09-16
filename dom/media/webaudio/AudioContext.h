5332c8b42b8d021051a11c193c0e185062bc5998
Riccardo Mottola <rm@gnu.org>
2024-09-16 08:57:58 +0200

    bug 1255618 remove AudioContext from global window at unlink r=Ehsan
    mDestination is cleared during unlink, which means that after that point the
    window can't do much with the AudioContext, nor should need to do so.

--- dom/media/webaudio/AudioContext.h
+++ dom/media/webaudio/AudioContext.h
@@ -314,6 +314,7 @@ public:
   IMPL_EVENT_HANDLER(mozinterruptend)
 
 private:
+  void DisconnectFromWindow();
   void RemoveFromDecodeQueue(WebAudioDecodeJob* aDecodeJob);
   void ShutdownDecoder();
 
