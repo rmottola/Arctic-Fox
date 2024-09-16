f43e3f17bad8212cacd0790cd7f576079152633d
Riccardo Mottola <rm@gnu.org>
2024-09-16 09:45:07 +0200

    Bug 1265405 - Use a dictionary to specify how PeriodicWave should be normalized (or not); r=padenot

--- dom/media/webaudio/AudioContext.cpp
+++ dom/media/webaudio/AudioContext.cpp
@@ -532,7 +532,8 @@ AudioContext::CreatePeriodicWave(const Float32Array& aRealData,
 
   RefPtr<PeriodicWave> periodicWave =
     new PeriodicWave(this, aRealData.Data(), aImagData.Data(),
-                     aImagData.Length(), aRv);
+                     aImagData.Length(), aConstraints.mDisableNormalization,
+                     aRv);
   if (aRv.Failed()) {
     return nullptr;
   }
