10ab6df542770452fc466df1866a87b562505bf1
Riccardo Mottola <rm@gnu.org>
2024-09-18 15:03:11 +0200

    Bug 1147562 - Update remaining callsites of newChannel before landing the shim in b2g/ (r=sicking)

--- b2g/components/MailtoProtocolHandler.js
+++ b2g/components/MailtoProtocolHandler.js
@@ -31,12 +31,15 @@ MailtoProtocolHandler.prototype = {
     return uri;
   },
 
-  newChannel: function Proto_newChannel(aURI) {
-    cpmm.sendAsyncMessage("mail-handler", {
-      URI: aURI.spec,
-      type: "mail" });
+  newChannel2: function Proto_newChannel2(aURI, aLoadInfo) {
+    return new ActivityChannel(aURI, aLoadInfo,
+                               "mail-handler",
+                               { URI: aURI.spec,
+                                 type: "mail" });
+  },
 
-    throw Components.results.NS_ERROR_ILLEGAL_VALUE;
+  newChannel: function Proto_newChannel(aURI) {
+    return this.newChannel2(aURI, null);
   },
 
   classID: Components.ID("{50777e53-0331-4366-a191-900999be386c}"),
