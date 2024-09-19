dd738033b1470d0e88d115b60a8e7726c4f5acf4
Riccardo Mottola <rm@gnu.org>
2024-09-19 05:03:01 +0200

    Bug 1178005 - Fix unintended octal literals (00 => 0). r=Mossop

--- toolkit/components/telemetry/tests/unit/test_TelemetrySession.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetrySession.js
@@ -980,7 +980,7 @@ add_task(function* test_dailyOverdue() {
 
   // Simulate an overdue ping: we're not close to midnight, but the last daily ping
   // time is too long ago.
-  let dailyOverdue = new Date(2030, 1, 2, 13, 00, 0);
+  let dailyOverdue = new Date(2030, 1, 2, 13, 0, 0);
   fakeNow(dailyOverdue);
 
   // Run a scheduler tick: it should trigger the daily ping.
@@ -1442,7 +1442,7 @@ add_task(function* test_abortedDailyCoalescing() {
   let schedulerTickCallback = null;
   PingServer.clearRequests();
 
-  let nowDate = new Date(2009, 10, 18, 00, 00, 0);
+  let nowDate = new Date(2009, 10, 18, 0, 0, 0);
   fakeNow(nowDate);
 
   // Fake scheduler functions to control aborted-session flow in tests.
@@ -1495,7 +1495,7 @@ add_task(function* test_schedulerComputerSleep() {
   yield OS.File.removeDir(DATAREPORTING_PATH, { ignoreAbsent: true });
 
   // Set a fake current date and start Telemetry.
-  let nowDate = new Date(2060, 10, 18, 0, 00, 0);
+  let nowDate = new Date(2009, 10, 18, 0, 0, 0);
   fakeNow(nowDate);
   let schedulerTickCallback = null;
   fakeSchedulerTimer(callback => schedulerTickCallback = callback, () => {});
@@ -1534,7 +1534,7 @@ add_task(function* test_schedulerEnvironmentReschedules() {
   PingServer.clearRequests();
 
   // Set a fake current date and start Telemetry.
-  let nowDate = new Date(2009, 10, 18, 0, 00, 0);
+  let nowDate = new Date(2060, 10, 18, 0, 0, 0);
   fakeNow(nowDate);
   let schedulerTickCallback = null;
   fakeSchedulerTimer(callback => schedulerTickCallback = callback, () => {});
