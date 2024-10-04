d67b8a22368cc21719a585725cea6f301880f2b6
Riccardo Mottola <rm@gnu.org>
2024-10-04 17:48:10 +0200

    Bug 1275538: P1. Abort early if a skip request is in progress. r=gerald,kamidphish
    We would only abort early if new data had been received. Which may not always be the case.

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1070,6 +1070,11 @@ MediaFormatReader::Update(TrackType aTrack)
     return;
   }
 
+  if (aTrack == TrackType::kVideoTrack && mSkipRequest.Exists()) {
+    LOGV("Skipping in progress, nothing more to do");
+    return;
+  }
+
   if (UpdateReceivedNewData(aTrack)) {
     LOGV("Nothing more to do");
     return;
