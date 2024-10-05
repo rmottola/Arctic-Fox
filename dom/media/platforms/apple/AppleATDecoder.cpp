ed25bc636f93c6717a4703b4686f85053d7a6258
Riccardo Mottola <rm@gnu.org>
2024-10-05 12:50:04 +0200

    Bug 1272225. Part 3 - remove use of FlushableTaskQueue. r=jya.

--- dom/media/platforms/apple/AppleATDecoder.cpp
+++ dom/media/platforms/apple/AppleATDecoder.cpp
@@ -19,11 +19,11 @@
 namespace mozilla {
 
 AppleATDecoder::AppleATDecoder(const AudioInfo& aConfig,
-                               FlushableTaskQueue* aAudioTaskQueue,
+                               TaskQueue* aTaskQueue,
                                MediaDataDecoderCallback* aCallback)
   : mConfig(aConfig)
   , mFileStreamError(false)
-  , mTaskQueue(aAudioTaskQueue)
+  , mTaskQueue(aTaskQueue)
   , mCallback(aCallback)
   , mConverter(nullptr)
   , mStream(nullptr)
