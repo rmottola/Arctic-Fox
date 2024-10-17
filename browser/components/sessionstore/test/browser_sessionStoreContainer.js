a145421b4d7312d1484219bdf5f7fea047e72661
Riccardo Mottola <rm@gnu.org>
2024-10-17 17:11:38 +0200

    Bug 1250063 - Part 1: Pass userContextId in duplicateTab. r=ttaubert

--- browser/components/sessionstore/test/browser_sessionStoreContainer.js
+++ browser/components/sessionstore/test/browser_sessionStoreContainer.js
@@ -2,24 +2,70 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function retrieveUserContextId(browser) {
-  return ContentTask.spawn(browser, null, function* () {
-    let loadContext = docShell.QueryInterface(Ci.nsILoadContext);
-    return loadContext.originAttributes.userContextId;
-  });
-}
+"use strict";
 
-add_task(function() {
+add_task(function* () {
   for (let i = 0; i < 3; ++i) {
-    let tab = gBrowser.addTab("about:blank");
+    let tab = gBrowser.addTab("http://example.com/", { userContextId: i });
     let browser = tab.linkedBrowser;
 
     yield promiseBrowserLoaded(browser);
-    yield promiseTabState(tab, { userContextId: i, entries: [{ url: "http://example.com/" }] });
 
-    let userContextId = yield retrieveUserContextId(browser);
-    is(userContextId, i, "The docShell has the correct userContextId");
+    let tab2 = gBrowser.duplicateTab(tab);
+    Assert.equal(tab2.getAttribute("usercontextid"), i);
+    let browser2 = tab2.linkedBrowser;
+    yield promiseTabRestored(tab2)
+
+    yield ContentTask.spawn(browser2, { expectedId: i }, function* (args) {
+      let loadContext = docShell.QueryInterface(Ci.nsILoadContext);
+      Assert.equal(loadContext.originAttributes.userContextId,
+        args.expectedId, "The docShell has the correct userContextId");
+    });
 
     yield promiseRemoveTab(tab);
+    yield promiseRemoveTab(tab2);
   }
 });
+
+add_task(function* () {
+  let tab = gBrowser.addTab("http://example.com/", { userContextId: 1 });
+  let browser = tab.linkedBrowser;
+
+  yield promiseBrowserLoaded(browser);
+
+  gBrowser.selectedTab = tab;
+
+  let tab2 = gBrowser.duplicateTab(tab);
+  let browser2 = tab2.linkedBrowser;
+  yield promiseTabRestored(tab2)
+
+  yield ContentTask.spawn(browser2, { expectedId: 1 }, function* (args) {
+    Assert.equal(docShell.getOriginAttributes().userContextId,
+                 args.expectedId,
+                 "The docShell has the correct userContextId");
+  });
+
+  yield promiseRemoveTab(tab);
+  yield promiseRemoveTab(tab2);
+});
+
+add_task(function* () {
+  let tab = gBrowser.addTab("http://example.com/", { userContextId: 1 });
+  let browser = tab.linkedBrowser;
+
+  yield promiseBrowserLoaded(browser);
+
+  gBrowser.removeTab(tab);
+
+  let tab2 = ss.undoCloseTab(window, 0);
+  Assert.equal(tab2.getAttribute("usercontextid"), 1);
+  yield promiseTabRestored(tab2);
+  yield ContentTask.spawn(tab2.linkedBrowser, { expectedId: 1 }, function* (args) {
+    Assert.equal(docShell.getOriginAttributes().userContextId,
+                 args.expectedId,
+                 "The docShell has the correct userContextId");
+  });
+
+  yield promiseRemoveTab(tab2);
+});
+
