efafe839b9c019a73cffb93f3ce045ba0c98ff18
Riccardo Mottola <rm@gnu.org>
2024-10-25 17:34:16 +0200

    Bug 1229332 - Implement EXT_color_buffer_float. - r=jrmuizel

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -94,6 +94,7 @@ enum class GLFeature {
     element_index_uint,
     ES2_compatibility,
     ES3_compatibility,
+    EXT_color_buffer_float,
     frag_color_float,
     frag_depth,
     framebuffer_blit,
