c976587ba684fa1ea08b646712b6cc262596c569
Riccardo Mottola <rm@gnu.org>
2024-10-27 12:14:14 +0100

    Bug 1278925 - micro-optimize nsCOMArray appending with already_AddRefed AppendElement; r=erahm In all of the places touched by this patch, the smart pointer we're appending is about to become unused, so simply .forget()'ing its reference into the appropriate nsCOMArray works just fine.

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -1521,7 +1521,7 @@ EventListenerManager::GetListenerInfo(nsCOMArray<nsIEventListenerInfo>* aList)
                             listener.mFlags.mCapture,
                             listener.mFlags.mAllowUntrustedEvents,
                             listener.mFlags.mInSystemGroup);
-    aList->AppendObject(info);
+    aList->AppendElement(info.forget());
   }
   return NS_OK;
 }
