ad0ef09f8632bb3db7b0c39657131dc04cac1c6a
Riccardo Mottola <rm@gnu.org>
2024-10-29 15:01:39 +0100

    Bug 1274999 - Stop GetFilesHelper runnable when the operation is canceled, r=smaug

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -329,6 +329,10 @@ public:
     mGlobal = nullptr;
     mFiles.Clear();
     mPromises.Clear();
+    mCallbacks.Clear();
+
+    MutexAutoLock lock(mMutex);
+    mCanceled = true;
   }
 
   void Traverse(nsCycleCollectionTraversalCallback &cb)
@@ -345,6 +349,8 @@ private:
     , mRecursiveFlag(aRecursiveFlag)
     , mListingCompleted(false)
     , mErrorResult(NS_OK)
+    , mMutex("GetFilesHelper::mMutex")
+    , mCanceled(false)
   {
     MOZ_ASSERT(aGlobal);
   }
@@ -353,6 +359,13 @@ private:
   SetDirectoryPath(const nsAString& aDirectoryPath)
   {
     mDirectoryPath = aDirectoryPath;
+  }
+
+  bool
+  IsCanceled()
+  {
+    MutexAutoLock lock(mMutex);
+    return mCanceled;
   }
 
   NS_IMETHOD
@@ -365,7 +378,20 @@ private:
     // This happens in the I/O thread.
     if (!NS_IsMainThread()) {
       RunIO();
+
+      // If this operation has been canceled, we don't have to go back to
+      // main-thread.
+      if (IsCanceled()) {
+        return NS_OK;
+      }
+
       return NS_DispatchToMainThread(this);
+    }
+
+    // We are here, but we should not do anything on this thread because, in the
+    // meantime, the operation has been canceled.
+    if (IsCanceled()) {
+      return NS_OK;
     }
 
     RunMainThread();
@@ -454,6 +480,11 @@ private:
   {
     MOZ_ASSERT(!NS_IsMainThread());
     MOZ_ASSERT(aFile);
+
+    // We check if this operation has to be terminated at each recursion.
+    if (IsCanceled()) {
+      return NS_OK;
+    }
 
     nsCOMPtr<nsISimpleEnumerator> entries;
     nsresult rv = aFile->GetDirectoryEntries(getter_AddRefs(entries));
@@ -578,6 +609,11 @@ private:
 
   nsTArray<RefPtr<Promise>> mPromises;
   nsTArray<RefPtr<GetFilesCallback>> mCallbacks;
+
+  Mutex mMutex;
+
+  // This variable is protected by mutex.
+  bool mCanceled;
 };
 
 // An helper class for the dispatching of the 'change' event.
