acd8d72758698ba1fd3bb6c44322d0542cd654d1
Riccardo Mottola <rm@gnu.org>
2024-11-08 13:20:03 +0100

    Bug 1278236 - Convert GetSurfaceForGdkDrawable to return a DrawTarget. r=Bas

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -71,6 +71,7 @@ class CurrentX11TimeGetter;
 class nsWindow : public nsBaseWidget
 {
 public:
+    typedef mozilla::gfx::DrawTarget DrawTarget;
     typedef mozilla::WidgetEventTime WidgetEventTime;
 
     nsWindow();
@@ -319,8 +320,8 @@ public:
                                                              mozilla::layers::BufferMode* aBufferMode);
 
 #if (MOZ_WIDGET_GTK == 2)
-    static already_AddRefed<gfxASurface> GetSurfaceForGdkDrawable(GdkDrawable* aDrawable,
-                                                                  const nsIntSize& aSize);
+    static already_AddRefed<DrawTarget> GetDrawTargetForGdkDrawable(GdkDrawable* aDrawable,
+                                                                    const IntSize& aSize);
 #endif
     NS_IMETHOD         ReparentNativeWidget(nsIWidget* aNewParent) override;
 
