e4500adfd51de9b498ad78dc08720b0a366e9f6e
Riccardo Mottola <rm@gnu.org>
2024-11-11 10:53:30 +0100

    remove PM prefs

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -838,8 +838,6 @@ pref("accessibility.typeaheadfind.autostart", true);
 //     1 - "always" (case-sensitive)
 // other - "auto"   (case-sensitive for mixed-case input, insensitive otherwise)
 pref("accessibility.typeaheadfind.casesensitive", 0);
-pref("accessibility.typeaheadfind.highlightallbydefault", false);
-pref("accessibility.typeaheadfind.highlightallremember", false);
 pref("accessibility.typeaheadfind.linksonly", true);
 pref("accessibility.typeaheadfind.startlinksonly", false);
 pref("accessibility.typeaheadfind.timeout", 4000);
