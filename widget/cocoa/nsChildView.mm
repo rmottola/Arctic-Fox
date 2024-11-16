afbebb43b7bd94ebb635f3f12f02067bc6a9e969
Riccardo Mottola <rm@gnu.org>
2024-11-17 00:25:04 +0100

    Bug 1274282 - Add exception guards to nsChildView::PrepareWindowEffects(). r=spohl

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -2015,6 +2015,8 @@ nsChildView::RectContainingTitlebarControls()
 void
 nsChildView::PrepareWindowEffects()
 {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
+
   MutexAutoLock lock(mEffectsLock);
   mShowsResizeIndicator = ShowsResizeIndicator(&mResizeIndicatorRect);
   mHasRoundedBottomCorners = [(ChildView*)mView hasRoundedBottomCorners];
@@ -2032,6 +2034,8 @@ nsChildView::PrepareWindowEffects()
     mTitlebarRect = RectContainingTitlebarControls();
     UpdateTitlebarCGContext();
   }
+
+  NS_OBJC_END_TRY_ABORT_BLOCK;
 }
 
 void
