4135589fa21fb0c70496be3d49f617bc7093573d
Riccardo Mottola <rm@gnu.org>
2024-12-09 16:55:02 +0100

    Bug 1186139 - Don't use a sync message where we don't have to. r=felipe

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3501,7 +3501,7 @@ const DOMLinkHandler = {
         break;
 
       case "Link:SetIcon":
-        return this.setIcon(aMsg.target, aMsg.data.url, aMsg.data.loadingPrincipal);
+        this.setIcon(aMsg.target, aMsg.data.url, aMsg.data.loadingPrincipal);
         break;
 
       case "Link:AddSearch":
