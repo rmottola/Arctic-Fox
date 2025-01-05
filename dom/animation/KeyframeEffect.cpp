d25c87847034e2183f7f1c3970d4608e38af961b
Riccardo Mottola <rm@gnu.org>
2025-01-05 18:29:20 +0100

    Bug 1263488 - Make AnimValuesStyleRule re-use PropertyStyleAnimationValuePair; r=hiro
    Rather than defining the same type redundantly but with a name that overlaps
    with a struct in KeyframeEffect.h (that has a different type).

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -842,15 +842,6 @@ DumpAnimationProperties(nsTArray<AnimationProperty>& aAnimationProperties)
 }
 #endif
 
-/**
- * A property and StyleAnimationValue pair.
- */
-struct KeyframeValue
-{
-  nsCSSProperty mProperty;
-  StyleAnimationValue mValue;
-};
-
 /* static */ already_AddRefed<KeyframeEffectReadOnly>
 KeyframeEffectReadOnly::Constructor(
     const GlobalObject& aGlobal,
