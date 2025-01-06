f75fc438b419cf9f3e64780868b37a16c7221425
Riccardo Mottola <rm@gnu.org>
2025-01-06 11:58:51 +0100

    Bug 1031184 - Introduce a NativeDescription method to more easily customize accessible descriptions for various classes, r=MarcoZ
    Implemented a NativeDescription method

--- accessible/generic/Accessible.h
+++ accessible/generic/Accessible.h
@@ -965,6 +965,12 @@ protected:
   virtual mozilla::a11y::ENameValueFlag NativeName(nsString& aName);
 
   /**
+   * Return the accessible description provided by native markup. It doesn't take
+   * into account ARIA markup used to specify the description.
+   */
+  virtual void NativeDescription(nsString& aDescription);
+
+  /**
    * Return object attributes provided by native markup. It doesn't take into
    * account ARIA.
    */
