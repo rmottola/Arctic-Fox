d69dad2d1c14784196cde1d1d9b9d2baa7b61ec0
Riccardo Mottola <rm@gnu.org>
2025-01-07 21:53:21 +0100

    Bug 1241485 - Increase the limit for workers per-origin, r=khuey

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -147,7 +147,7 @@ pref("dom.select_events.textcontrols.enabled", true);
 // Whether or not Web Workers are enabled.
 pref("dom.workers.enabled", true);
 // The number of workers per domain allowed to run concurrently.
-pref("dom.workers.maxPerDomain", 20);
+pref("dom.workers.maxPerDomain", 50);
 
 pref("dom.serviceWorkers.enabled", false);
 
