51c09350cd3b23a8046d908326b2bf3e2cdd5979
Riccardo Mottola <rm@gnu.org>
2025-01-08 09:21:02 +0100

    Bug 1276310 part 4.  Store an XPCJSRuntime* in XPCCallContext instead of storing an XPCContext*.  r=bholley

--- js/xpconnect/src/XPCCallContext.cpp
+++ js/xpconnect/src/XPCCallContext.cpp
@@ -27,7 +27,7 @@ XPCCallContext::XPCCallContext(JSContext* cx,
     :   mAr(cx),
         mState(INIT_FAILED),
         mXPC(nsXPConnect::XPConnect()),
-        mXPCContext(nullptr),
+        mXPCJSRuntime(nullptr),
         mJSContext(cx),
         mWrapper(nullptr),
         mTearOff(nullptr),
@@ -39,12 +39,12 @@ XPCCallContext::XPCCallContext(JSContext* cx,
     if (!mXPC)
         return;
 
-    mXPCContext = XPCContext::GetXPCContext(mJSContext);
+    mXPCJSRuntime = XPCJSRuntime::Get();
 
     // hook into call context chain.
-    mPrevCallContext = XPCJSRuntime::Get()->SetCallContext(this);
+    mPrevCallContext = mXPCJSRuntime->SetCallContext(this);
 
-    mState = HAVE_CONTEXT;
+    mState = HAVE_RUNTIME;
 
     if (!obj)
         return;
@@ -125,7 +125,7 @@ void
 XPCCallContext::SetCallInfo(XPCNativeInterface* iface, XPCNativeMember* member,
                             bool isSetter)
 {
-    CHECK_STATE(HAVE_CONTEXT);
+    CHECK_STATE(HAVE_RUNTIME);
 
     // We are going straight to the method info and need not do a lookup
     // by id.
@@ -197,7 +197,7 @@ XPCCallContext::SystemIsBeingShutDown()
     // can be making this call on one thread for call contexts on another
     // thread.
     NS_WARNING("Shutting Down XPConnect even through there is a live XPCCallContext");
-    mXPCContext = nullptr;
+    mXPCJSRuntime = nullptr;
     mState = SYSTEM_SHUTDOWN;
     if (mPrevCallContext)
         mPrevCallContext->SystemIsBeingShutDown();
@@ -205,8 +205,8 @@ XPCCallContext::SystemIsBeingShutDown()
 
 XPCCallContext::~XPCCallContext()
 {
-    if (mXPCContext) {
-        DebugOnly<XPCCallContext*> old = XPCJSRuntime::Get()->SetCallContext(mPrevCallContext);
+    if (mXPCJSRuntime) {
+        DebugOnly<XPCCallContext*> old = mXPCJSRuntime->SetCallContext(mPrevCallContext);
         MOZ_ASSERT(old == this, "bad pop from per thread data");
     }
 }
--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -744,7 +744,6 @@ public:
     inline bool                         IsValid() const ;
 
     inline XPCJSRuntime*                GetRuntime() const ;
-    inline XPCContext*                  GetXPCContext() const ;
     inline JSContext*                   GetJSContext() const ;
     inline bool                         GetContextPopRequired() const ;
     inline XPCCallContext*              GetPrevCallContext() const ;
@@ -803,7 +802,7 @@ private:
     enum State {
         INIT_FAILED,
         SYSTEM_SHUTDOWN,
-        HAVE_CONTEXT,
+        HAVE_RUNTIME,
         HAVE_OBJECT,
         HAVE_NAME,
         HAVE_ARGS,
@@ -823,7 +822,7 @@ private:
 
     RefPtr<nsXPConnect>           mXPC;
 
-    XPCContext*                     mXPCContext;
+    XPCJSRuntime*                   mXPCJSRuntime;
     JSContext*                      mJSContext;
 
     // ctor does not necessarily init the following. BEWARE!
