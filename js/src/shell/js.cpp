565834e7abdbfee22b9ec03a2a63180681d5bf2b
Riccardo Mottola <rm@gnu.org>
2025-01-08 11:41:36 +0100

    Bug 1283855 part 18 - Make JS_DropPrincipals take JSContext instead of JSRuntime. r=luke

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -4353,7 +4353,7 @@ NewGlobal(JSContext* cx, unsigned argc, Value* vp)
 
     RootedObject global(cx, NewGlobalObject(cx, options, principals));
     if (principals)
-        JS_DropPrincipals(cx->runtime(), principals);
+        JS_DropPrincipals(cx, principals);
     if (!global)
         return false;
 
