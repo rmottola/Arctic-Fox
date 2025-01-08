e0720351c1c1339c24a70934ea164dc1e71c9a56
Riccardo Mottola <rm@gnu.org>
2025-01-09 00:40:35 +0100

    Bug 1280930 - pushJoinReg must push void values, not ignore them. r=bbouvier

--- js/src/asmjs/WasmBaselineCompile.cpp
+++ js/src/asmjs/WasmBaselineCompile.cpp
@@ -1528,6 +1528,7 @@ class BaseCompiler
     void pushJoinReg(AnyReg r) {
         switch (r.tag) {
           case AnyReg::NONE:
+            pushVoid();
             break;
           case AnyReg::I32:
             pushI32(r.i32());
