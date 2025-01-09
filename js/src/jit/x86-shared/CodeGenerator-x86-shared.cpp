d8274ae1d750f58401dfda5995c51c3a86f88d26
Riccardo Mottola <rm@gnu.org>
2025-01-09 19:32:57 +0100

    Bug 1278283 - Expose max and min in the MacroAssembler + move ARM code. r=nbp

--- js/src/jit/x86-shared/CodeGenerator-x86-shared.cpp
+++ js/src/jit/x86-shared/CodeGenerator-x86-shared.cpp
@@ -697,10 +697,12 @@ CodeGeneratorX86Shared::visitMinMaxD(LMinMaxD* ins)
     MOZ_ASSERT(first == output);
 #endif
 
-    bool specialHandling = !ins->mir()->range() || ins->mir()->range()->canBeNaN();
-    bool isMax = ins->mir()->isMax();
+    bool handleNaN = !ins->mir()->range() || ins->mir()->range()->canBeNaN();
 
-    masm.minMaxDouble(first, second, specialHandling, isMax);
+    if (ins->mir()->isMax())
+        masm.maxDouble(second, first, handleNaN);
+    else
+        masm.minDouble(second, first, handleNaN);
 }
 
 void
@@ -713,10 +715,12 @@ CodeGeneratorX86Shared::visitMinMaxF(LMinMaxF* ins)
     MOZ_ASSERT(first == output);
 #endif
 
-    bool specialHandling = !ins->mir()->range() || ins->mir()->range()->canBeNaN();
-    bool isMax = ins->mir()->isMax();
+    bool handleNaN = !ins->mir()->range() || ins->mir()->range()->canBeNaN();
 
-    masm.minMaxFloat32(first, second, specialHandling, isMax);
+    if (ins->mir()->isMax())
+        masm.maxFloat32(second, first, handleNaN);
+    else
+        masm.minFloat32(second, first, handleNaN);
 }
 
 void
