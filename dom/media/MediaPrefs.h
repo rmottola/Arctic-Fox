403b3861b5863620b3b45e9343407212a55ba607
Riccardo Mottola <rm@gnu.org>
2025-01-31 12:03:13 +0100

    Bug 1271490: Use MediaPrefs to read preferences. r=cpearce

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -114,6 +114,8 @@ private:
   DECL_MEDIA_PREF("media.webspeech.recognition.enable",       WebSpeechRecognitionEnabled, bool, false);
   DECL_MEDIA_PREF("media.webspeech.recognition.force_enable", WebSpeechRecognitionForceEnabled, bool, false);
 
+  DECL_MEDIA_PREF("media.num-decode-threads",                 MediaThreadPoolDefaultCount, uint32_t, 4);
+
 public:
   // Manage the singleton:
   static MediaPrefs& GetSingleton();
