beddccdcbd6354fa1d9e52c00732eb0d74b95745
Riccardo Mottola <rm@gnu.org>
2025-01-31 01:40:51 +0100

    Bug 1266646 - Rename MediaStreamDirectListener and MediaStreamTrackDirectListener to DirectMediaStreamListener and DirectMediaStreamTrackListener respectively. r=pehrsons
    Rename those two function to better name alignment with AddDirectListener and AddDirectTrackListener.

--- dom/media/DOMMediaStream.cpp
+++ dom/media/DOMMediaStream.cpp
@@ -752,7 +752,7 @@ DOMMediaStream::OwnsTrack(const MediaStreamTrack& aTrack) const
 }
 
 bool
-DOMMediaStream::AddDirectListener(MediaStreamDirectListener* aListener)
+DOMMediaStream::AddDirectListener(DirectMediaStreamListener* aListener)
 {
   if (GetInputStream() && GetInputStream()->AsSourceStream()) {
     GetInputStream()->AsSourceStream()->AddDirectListener(aListener);
@@ -762,7 +762,7 @@ DOMMediaStream::AddDirectListener(MediaStreamDirectListener* aListener)
 }
 
 void
-DOMMediaStream::RemoveDirectListener(MediaStreamDirectListener* aListener)
+DOMMediaStream::RemoveDirectListener(DirectMediaStreamListener* aListener)
 {
   if (GetInputStream() && GetInputStream()->AsSourceStream()) {
     GetInputStream()->AsSourceStream()->RemoveDirectListener(aListener);
