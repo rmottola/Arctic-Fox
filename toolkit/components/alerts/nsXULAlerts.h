6546a15a7c737ba9e84dc139e73ccf4f77a34a92
Riccardo Mottola <rm@gnu.org>
2025-02-19 23:44:27 +0100

    Bug 1267342 Cycle collect nsXULAlertObserver and its window reference. r=mccr8

--- toolkit/components/alerts/nsXULAlerts.h
+++ toolkit/components/alerts/nsXULAlerts.h
@@ -44,8 +44,9 @@ protected:
  */
 class nsXULAlertObserver : public nsIObserver {
 public:
-  NS_DECL_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_NSIOBSERVER
+  NS_DECL_CYCLE_COLLECTION_CLASS(nsXULAlertObserver)
 
   nsXULAlertObserver(nsXULAlerts* aXULAlerts, const nsAString& aAlertName,
                      nsIObserver* aObserver)
