624e786c0722b7c9fcf2948a5e75d2cc93f930b4
Riccardo Mottola <rm@gnu.org>
2025-02-26 13:12:48 +0100

    bit of Bug 1217591 - Run autocomplete tests

--- devtools/shared/webconsole/test/common.js
+++ devtools/shared/webconsole/test/common.js
@@ -94,11 +94,26 @@ function _attachConsole(aListeners, aCallback, aAttachToTab, aAttachToWorker)
           return;
         }
         let tab = aResponse.tabs[aResponse.selected];
-        let consoleActor = tab.consoleActor;
-        aState.dbgClient.attachTab(tab.actor, function () {
-          aState.actor = consoleActor;
-          aState.dbgClient.attachConsole(consoleActor, aListeners,
-                                         _onAttachConsole.bind(null, aState));
+        aState.dbgClient.attachTab(tab.actor, function (response, tabClient) {
+          if (aAttachToWorker) {
+            var worker = new Worker("console-test-worker.js");
+            worker.addEventListener("message", function listener() {
+              worker.removeEventListener("message", listener);
+              tabClient.listWorkers(function (response) {
+                tabClient.attachWorker(response.workers[0].actor, function (response, workerClient) {
+                  workerClient.attachThread({}, function(aResponse) {
+                    aState.actor = workerClient.consoleActor;
+                    aState.dbgClient.attachConsole(workerClient.consoleActor, aListeners,
+                                                   _onAttachConsole.bind(null, aState));
+                  });
+                });
+              });
+            });
+          } else {
+            aState.actor = tab.consoleActor;
+            aState.dbgClient.attachConsole(tab.consoleActor, aListeners,
+                                           _onAttachConsole.bind(null, aState));
+          }
         });
       });
     } else {
