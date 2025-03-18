5293a41d52d0549fea097ecf4b51a49b1b6e958e
Riccardo Mottola <rm@gnu.org>
2025-03-18 14:22:26 +0100

    Bug 1287351 - OdinMonkey: MIPS: Implement many simple operations in the MacroAssembler. r=lth

--- js/src/jit/mips-shared/MacroAssembler-mips-shared.cpp
+++ js/src/jit/mips-shared/MacroAssembler-mips-shared.cpp
@@ -1171,6 +1171,13 @@ MacroAssembler::Pop(Register reg)
 }
 
 void
+MacroAssembler::Pop(FloatRegister f)
+{
+    ma_pop(f);
+    adjustFrame(-sizeof(double));
+}
+
+void
 MacroAssembler::Pop(const ValueOperand& val)
 {
     popValue(val);
