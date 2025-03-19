c527a06a871e903597312134ed2771c6a6535dde
Riccardo Mottola <rm@gnu.org>
2025-03-20 00:49:35 +0100

    Bug 1207233 - Part 2: Make URLSearchParams nsIXHRSendable as well as clonable, and have XHRs set the correct request content type for them. r=baku

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -15,6 +15,7 @@
 #include "mozilla/dom/File.h"
 #include "mozilla/dom/FetchUtil.h"
 #include "mozilla/dom/FormData.h"
+#include "mozilla/dom/URLSearchParams.h"
 #include "mozilla/dom/XMLDocument.h"
 #include "mozilla/EventDispatcher.h"
 #include "mozilla/EventListenerManager.h"
@@ -2244,6 +2245,15 @@ GetRequestBodyInternal(nsIInputStream* aStream, nsIInputStream** aResult,
 }
 
 static nsresult
+GetRequestBodyInternal(URLSearchParams* aURLSearchParams,
+                       nsIInputStream** aResult, uint64_t* aContentLength,
+                       nsACString& aContentType, nsACString& aCharset)
+{
+  return aURLSearchParams->GetSendInfo(aResult, aContentLength,
+                                       aContentType, aCharset);
+}
+
+static nsresult
 GetRequestBodyInternal(nsIXHRSendable* aSendable, nsIInputStream** aResult,
                        uint64_t* aContentLength, nsACString& aContentType,
                        nsACString& aCharset)
@@ -2422,6 +2432,12 @@ XMLHttpRequestMainThread::GetRequestBody(nsIVariant* aVariant,
       MOZ_ASSERT(value.mFormData);
       return GetRequestBodyInternal(value.mFormData, aResult, aContentLength,
                                     aContentType, aCharset);
+    }
+    case XMLHttpRequestMainThread::RequestBody::eURLSearchParams:
+    {
+      MOZ_ASSERT(value.mURLSearchParams);
+      return GetRequestBodyInternal(value.mURLSearchParams, aResult,
+                                    aContentLength, aContentType, aCharset);
     }
     case XMLHttpRequestMainThread::RequestBody::eInputStream:
     {
