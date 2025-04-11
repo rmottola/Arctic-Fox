044c09e7971b814b0c967d0798fa8047631b1ee9
Riccardo Mottola <rm@gnu.org>
2025-04-11 22:19:36 +0200

    Bug 1160784 - Remove a misleading declaration r=smontagu

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -3182,11 +3182,6 @@ nsBlockFrame::ReflowBlockFrame(BlockReflowInput& aState,
     nscoord curBCoord = aState.mBCoord + aState.mPrevBEndMargin.get();
     nscoord clearBCoord = aState.ClearFloats(curBCoord, breakType, replacedBlock);
     if (clearBCoord != curBCoord) {
-      // Looks like that assumption was invalid, we do need
-      // clearance. Tell our ancestor so it can reflow again. It is
-      // responsible for actually setting our clearance flag before
-      // the next reflow.
-      treatWithClearance = true;
       // Only record the first frame that requires clearance
       if (!*aState.mReflowState.mDiscoveredClearance) {
         *aState.mReflowState.mDiscoveredClearance = frame;
