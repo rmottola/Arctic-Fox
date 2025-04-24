474bf1f5834fd022a18c3f89b8e08e6872953ec4
Riccardo Mottola <rm@gnu.org>
2025-04-24 08:01:32 +0200

    Bug 1281708: remove assert for unknown stun server transports. r=mjf

--- media/mtransport/nricectx.cpp
+++ media/mtransport/nricectx.cpp
@@ -6,8 +6,6 @@
 
 
 // Original author: ekr@rtfm.com
-// Contributors:
-//   Moonchild <moonchild@palemoon.org>
 
 // Some of this code is cut-and-pasted from nICEr. Copyright is:
 
@@ -214,7 +212,7 @@ nsresult NrIceStunServer::ToNicerStunStruct(nr_ice_stun_server *server) const {
   } else if (transport_ == kNrIceTransportTcp) {
     server->transport = IPPROTO_TCP;
   } else {
-    MOZ_ASSERT(false);
+    MOZ_MTLOG(ML_ERROR, "Unsupported STUN server transport: " << transport_);
     return NS_ERROR_FAILURE;
   }
 
