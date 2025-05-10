c621eb0367568bef9ae49aae1f9cc8a702f62eb2
Riccardo Mottola <rm@gnu.org>
2025-05-10 23:58:50 +0200

    Bug 1262439 - 1 - Introduce a new eye-dropper highlighter; r=zer0
    This is mostly porting code from the XUL eye-dropper over to a new
    highlighter file.
    While I have tested this locally, this highlighter isn't yet used
    in devtools.
    This will come in later patches.

--- devtools/server/actors/highlighters/utils/markup.js
+++ devtools/server/actors/highlighters/utils/markup.js
@@ -333,6 +333,10 @@ CanvasFrameAnonymousContentHelper.prototype = {
     return typeof this.getAttributeForElement(id, name) === "string";
   },
 
+  getCanvasContext: function (id, type = "2d") {
+    return this.content ? this.content.getCanvasContext(id, type) : null;
+  },
+
   /**
    * Add an event listener to one of the elements inserted in the canvasFrame
    * native anonymous container.
@@ -468,6 +472,7 @@ CanvasFrameAnonymousContentHelper.prototype = {
       getAttribute: name => this.getAttributeForElement(id, name),
       removeAttribute: name => this.removeAttributeForElement(id, name),
       hasAttribute: name => this.hasAttributeForElement(id, name),
+      getCanvasContext: type => this.getCanvasContext(id, type),
       addEventListener: (type, handler) => {
         return this.addEventListenerForElement(id, type, handler);
       },
