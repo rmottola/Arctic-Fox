68afcda036ce41af976c8c32705da93746f67315
Riccardo Mottola <rm@gnu.org>
2025-06-17 08:08:28 +0200

    Bug 1291293: Don't take the same register twice in i64 mod/umod on x86/ARM; r=h4writer

--- js/src/jit/arm/CodeGenerator-arm.cpp
+++ js/src/jit/arm/CodeGenerator-arm.cpp
@@ -3116,8 +3116,10 @@ CodeGeneratorARM::visitDivOrModI64(LDivOrModI64* lir)
     AllocatableGeneralRegisterSet regs(GeneralRegisterSet::All());
     regs.take(lhs.low);
     regs.take(lhs.high);
-    regs.take(rhs.low);
-    regs.take(rhs.high);
+    if (lhs != rhs) {
+        regs.take(rhs.low);
+        regs.take(rhs.high);
+    }
     Register temp = regs.takeAny();
 
     Label done;
@@ -3169,8 +3171,10 @@ CodeGeneratorARM::visitUDivOrModI64(LUDivOrModI64* lir)
     AllocatableGeneralRegisterSet regs(GeneralRegisterSet::All());
     regs.take(lhs.low);
     regs.take(lhs.high);
-    regs.take(rhs.low);
-    regs.take(rhs.high);
+    if (lhs != rhs) {
+        regs.take(rhs.low);
+        regs.take(rhs.high);
+    }
     Register temp = regs.takeAny();
 
     // Prevent divide by zero.
