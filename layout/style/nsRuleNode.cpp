bc48d372e4da9e7194b554722b3ea2c5d118b38c
Riccardo Mottola <rm@gnu.org>
2025-06-30 17:51:24 +0200

    Bug 652991 - Part 7. Using FragmentOrURL to represent SVG filter url. r=heycam

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -9749,11 +9749,7 @@ SetStyleFilterToCSSValue(nsStyleFilter* aStyleFilter,
 {
   nsCSSUnit unit = aValue.GetUnit();
   if (unit == eCSSUnit_URL) {
-    nsIURI* url = aValue.GetURLValue();
-    if (!url)
-      return false;
-    aStyleFilter->SetURL(url);
-    return true;
+    return aStyleFilter->SetURL(&aValue);
   }
 
   MOZ_ASSERT(unit == eCSSUnit_Function, "expected a filter function");
