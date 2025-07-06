1ee341421d7d62e2aa2555bcb337e3420ea4c48c
Riccardo Mottola <rm@gnu.org>
2025-07-06 22:59:32 +0200

    Bug 1288860 - Change GLContextProvider to use CompositorWidget. r=jrmuizel

--- gfx/layers/opengl/CompositorOGL.cpp
+++ gfx/layers/opengl/CompositorOGL.cpp
@@ -121,7 +121,7 @@ CompositorOGL::CreateContext()
 #ifdef XP_WIN
   if (gfxEnv::LayersPreferEGL()) {
     printf_stderr("Trying GL layers...\n");
-    context = gl::GLContextProviderEGL::CreateForWindow(mWidget->RealWidget(), false);
+    context = gl::GLContextProviderEGL::CreateForCompositorWidget(mWidget, false);
   }
 #endif
 
@@ -138,7 +138,7 @@ CompositorOGL::CreateContext()
   }
 
   if (!context) {
-    context = gl::GLContextProvider::CreateForWindow(mWidget->RealWidget(),
+    context = gl::GLContextProvider::CreateForCompositorWidget(mWidget,
                 gfxPlatform::GetPlatform()->RequiresAcceleratedGLContextForCompositorOGL());
   }
 
