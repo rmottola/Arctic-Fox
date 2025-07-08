a3aa2583ba75636749f7f25760f5237b373e4d81
Riccardo Mottola <rm@gnu.org>
2025-07-08 09:53:00 +0200

    Bug 1289435 - Ensure that the synthesized native touch events have sane timestamps. r=mstange

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -1210,8 +1210,9 @@ nsresult nsChildView::SynthesizeNativeTouchPoint(uint32_t aPointerId,
 
   LayoutDeviceIntPoint pointInWindow = aPoint - WidgetToScreenOffset();
   MultiTouchInput inputToDispatch = UpdateSynthesizedTouchState(
-      mSynthesizedTouchInput.get(), aPointerId, aPointerState,
-      pointInWindow, aPointerPressure, aPointerOrientation);
+      mSynthesizedTouchInput.get(), PR_IntervalNow(), TimeStamp::Now(),
+      aPointerId, aPointerState, pointInWindow, aPointerPressure,
+      aPointerOrientation);
 
   if (mAPZC) {
     uint64_t inputBlockId = 0;
