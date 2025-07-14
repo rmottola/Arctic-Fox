c479253da8cd27cfa7ef7a81220fc4a06a7b9e98
Riccardo Mottola <rm@gnu.org>
2025-07-14 13:18:53 +0200

    backout changeset 1a1d7ef3cb0e (bug 1279479) for causing WebRTC compatibility issues
    The WebRTC implementation inherits cipher suite preferences from PSM and then
    enables a few mandatory ones and disables a number of undesirable ones. If PSM
    makes a change to a cipher suite preference that isn't in WebRTC's whitelist or
    blacklist, compatibility issues can arise. See bug 1288246 for an example.

--- security/manager/ssl/nsNSSComponent.cpp
+++ security/manager/ssl/nsNSSComponent.cpp
@@ -1249,7 +1249,7 @@ typedef struct {
   const char* pref;
   long id;
   bool enabledByDefault;
-  bool fallback;
+  bool weak;
 } CipherPref;
 
 // Update the switch statement in AccumulateCipherSuite in nsNSSCallbacks.cpp
@@ -1281,12 +1281,12 @@ static const CipherPref sCipherPrefs[] = {
    TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA, true },
 
  { "security.ssl3.dhe_rsa_aes_128_sha",
-   TLS_DHE_RSA_WITH_AES_128_CBC_SHA, true, true },
+   TLS_DHE_RSA_WITH_AES_128_CBC_SHA, true },
  { "security.ssl3.dhe_rsa_camellia_128_sha", 
    TLS_DHE_RSA_WITH_CAMELLIA_128_CBC_SHA, true },
 
  { "security.ssl3.dhe_rsa_aes_256_sha",
-   TLS_DHE_RSA_WITH_AES_256_CBC_SHA, true, true },
+   TLS_DHE_RSA_WITH_AES_256_CBC_SHA, true },
  { "security.ssl3.dhe_rsa_camellia_256_sha", 
    TLS_DHE_RSA_WITH_CAMELLIA_256_CBC_SHA, true}, 
 
@@ -1316,26 +1316,26 @@ static const CipherPref sCipherPrefs[] = {
  { nullptr, 0 } // end marker
 };
 
-// Bit flags indicating what fallback ciphers are enabled.
+// Bit flags indicating what weak ciphers are enabled.
 // The bit index will correspond to the index in sCipherPrefs.
 // Wrtten by the main thread, read from any threads.
-static uint64_t sEnabledFallbackCiphers;
+static uint64_t sEnabledWeakCiphers;
 static_assert(MOZ_ARRAY_LENGTH(sCipherPrefs) - 1 <= sizeof(uint64_t) * CHAR_BIT,
               "too many cipher suites");
 
 /*static*/ bool
-nsNSSComponent::AreAnyFallbackCiphersEnabled()
+nsNSSComponent::AreAnyWeakCiphersEnabled()
 {
-  return !!sEnabledFallbackCiphers;
+  return !!sEnabledWeakCiphers;
 }
 
 /*static*/ void
-nsNSSComponent::UseFallbackCiphersOnSocket(PRFileDesc* fd)
+nsNSSComponent::UseWeakCiphersOnSocket(PRFileDesc* fd)
 {
-  const uint64_t enabledFallbackCiphers = sEnabledFallbackCiphers;
+  const uint64_t enabledWeakCiphers = sEnabledWeakCiphers;
   const CipherPref* const cp = sCipherPrefs;
   for (size_t i = 0; cp[i].pref; ++i) {
-    if (enabledFallbackCiphers & ((uint64_t)1 << i)) {
+    if (enabledWeakCiphers & ((uint64_t)1 << i)) {
       SSL_CipherPrefSet(fd, cp[i].id, true);
     }
   }
@@ -1449,18 +1449,18 @@ CipherSuiteChangeObserver::Observe(nsISupports* aSubject,
       if (prefName.Equals(cp[i].pref)) {
         bool cipherEnabled = Preferences::GetBool(cp[i].pref,
                                                   cp[i].enabledByDefault);
-        if (cp[i].fallback) {
-          // Fallback ciphers will not be used by default even if they
+        if (cp[i].weak) {
+          // Weak ciphers will not be used by default even if they
           // are enabled in prefs. They are only used on specific
           // sockets as a part of a fallback mechanism.
-          // Only the main thread will change sEnabledFallbackCiphers.
-          uint32_t enabledFallbackCiphers = sEnabledFallbackCiphers;
+          // Only the main thread will change sEnabledWeakCiphers.
+          uint32_t enabledWeakCiphers = sEnabledWeakCiphers;
           if (cipherEnabled) {
-            enabledFallbackCiphers |= ((uint64_t)1 << i);
+            enabledWeakCiphers |= ((uint64_t)1 << i);
           } else {
-            enabledFallbackCiphers &= ~((uint64_t)1 << i);
+            enabledWeakCiphers &= ~((uint64_t)1 << i);
           }
-          sEnabledFallbackCiphers = enabledFallbackCiphers;
+          sEnabledWeakCiphers = enabledWeakCiphers;
         } else {
           SSL_CipherPrefSetDefault(cp[i].id, cipherEnabled);
           SSL_ClearSessionCache();
@@ -2312,22 +2312,22 @@ InitializeCipherSuite()
   }
 
   // Now only set SSL/TLS ciphers we knew about at compile time
-  uint64_t enabledFallbackCiphers = 0;
+  uint64_t enabledWeakCiphers = 0;
   const CipherPref* const cp = sCipherPrefs;
   for (size_t i = 0; cp[i].pref; ++i) {
     bool cipherEnabled = Preferences::GetBool(cp[i].pref,
                                               cp[i].enabledByDefault);
-    if (cp[i].fallback) {
-      // Fallback ciphers are not used by default. See the comment
+    if (cp[i].weak) {
+      // Weak ciphers are not used by default. See the comment
       // in CipherSuiteChangeObserver::Observe for details.
       if (cipherEnabled) {
-        enabledFallbackCiphers |= ((uint64_t)1 << i);
+        enabledWeakCiphers |= ((uint64_t)1 << i);
       }
     } else {
       SSL_CipherPrefSetDefault(cp[i].id, cipherEnabled);
     }
   }
-  sEnabledFallbackCiphers = enabledFallbackCiphers;
+  sEnabledWeakCiphers = enabledWeakCiphers;
 
   // Enable ciphers for PKCS#12
   SEC_PKCS12EnableCipher(PKCS12_RC4_40, 1);
--- security/manager/ssl/tests/unit/test_weak_crypto.js
+++ security/manager/ssl/tests/unit/test_weak_crypto.js
@@ -5,13 +5,10 @@
 
 // Tests the weak crypto override
 
-const TLS_DHE_RSA_WITH_AES_128_CBC_SHA     = 0x0033;
-const TLS_DHE_RSA_WITH_AES_256_CBC_SHA     = 0x0039;
-
-const fallback_cipher_prefs = [
-  "security.ssl3.dhe_rsa_aes_128_sha",
-  "security.ssl3.dhe_rsa_aes_256_sha"
-];
+const TLS_RSA_WITH_RC4_128_MD5         = 0x0004;
+const TLS_RSA_WITH_RC4_128_SHA         = 0x0005;
+const TLS_ECDHE_ECDSA_WITH_RC4_128_SHA = 0xC007;
+const TLS_ECDHE_RSA_WITH_RC4_128_SHA   = 0xC011;
 
 // Need profile dir to store the key / cert
 do_get_profile();
@@ -28,13 +25,9 @@ const socketTransportService =
   Cc["@mozilla.org/network/socket-transport-service;1"]
   .getService(Ci.nsISocketTransportService);
 
-function getCert(keyType = Ci.nsILocalCertService.KEY_TYPE_EC) {
+function getCert() {
   let deferred = Promise.defer();
-  let nickname = "tls-test";
-  if (keyType == Ci.nsILocalCertService.KEY_TYPE_RSA) {
-    nickname = "tls-test-rsa";
-  }
-  certService.getOrCreateCert(nickname, {
+  certService.getOrCreateCert("tls-test", {
     handleCert: function(c, rv) {
       if (rv) {
         deferred.reject(rv);
@@ -42,11 +35,11 @@ function getCert(keyType = Ci.nsILocalCertService.KEY_TYPE_EC) {
       }
       deferred.resolve(c);
     }
-  }, keyType);
+  });
   return deferred.promise;
 }
 
-function startServer(cert, fallbackOnly) {
+function startServer(cert, rc4only) {
   let tlsServer = Cc["@mozilla.org/network/tls-server-socket;1"]
                   .createInstance(Ci.nsITLSServerSocket);
   tlsServer.init(-1, true, -1);
@@ -68,12 +61,12 @@ function startServer(cert, fallbackOnly) {
 
       equal(status.tlsVersionUsed, Ci.nsITLSClientStatus.TLS_VERSION_1_2,
             "Using TLS 1.2");
-      let expectedCipher = fallbackOnly ? "TLS_DHE_RSA_WITH_AES_128_CBC_SHA"
-                                        : "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256";
+      let expectedCipher = rc4only ? "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
+                                   : "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256";
       equal(status.cipherName, expectedCipher,
             "Using expected cipher");
       equal(status.keyLength, 128, "Using 128-bit key");
-      equal(status.macLength, fallbackOnly ? 160 : 128, "Using MAC of expected length");
+      equal(status.macLength, rc4only ? 160 : 128, "Using MAC of expected length");
 
       input.asyncWait({
         onInputStreamReady: function(input) {
@@ -87,10 +80,12 @@ function startServer(cert, fallbackOnly) {
   tlsServer.setSessionCache(false);
   tlsServer.setSessionTickets(false);
   tlsServer.setRequestClientCertificate(Ci.nsITLSServerSocket.REQUEST_NEVER);
-  if (fallbackOnly) {
+  if (rc4only) {
     let cipherSuites = [
-      TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
-      TLS_DHE_RSA_WITH_AES_256_CBC_SHA
+      TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+      TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+      TLS_RSA_WITH_RC4_128_SHA,
+      TLS_RSA_WITH_RC4_128_MD5
     ];
     tlsServer.setCipherSuites(cipherSuites, cipherSuites.length);
   }
@@ -107,7 +102,7 @@ function storeCertOverride(port, cert) {
                                                overrideBits, true);
 }
 
-function startClient(name, port, expectedResult, options = {}) {
+function startClient(port, expectedResult, options = {}) {
   let transport =
     socketTransportService.createTransport(["ssl"], 1, "127.0.0.1", port, null);
   if (options.isPrivate) {
@@ -129,13 +124,17 @@ function startClient(name, port, expectedResult, options = {}) {
     onInputStreamReady: function(input) {
       try {
         let data = NetUtil.readInputStreamToString(input, input.available());
+        equal(Cr.NS_OK, expectedResult, "Connection should succeed");
         equal(data, "HELLO", "Echoed data received");
-        input.close();
-        output.close();
-        deferred.resolve();
       } catch (e) {
-        deferred.reject(e);
+        if (!((e.result == Cr.NS_ERROR_NET_RESET) && options.allowReset) &&
+            (e.result != expectedResult)) {
+          deferred.reject(e);
+        }
       }
+      input.close();
+      output.close();
+      deferred.resolve();
     },
 
     onOutputStreamReady: function(output) {
@@ -147,13 +146,15 @@ function startClient(name, port, expectedResult, options = {}) {
             output.asyncWait(handler, 0, 0, Services.tm.currentThread);
             return;
           }
-          equal(e.result, expectedResult,
-                "Actual and expected connection result should match");
-          output.close();
-          deferred.resolve();
-          return;
+          if (e.result != Cr.NS_OK) {
+            ok((e.result === expectedResult) ||
+               (options.allowReset && (e.result === Cr.NS_ERROR_NET_RESET)),
+               "Actual and expected connection result should match");
+            output.close();
+            deferred.resolve();
+            return;
+          }
         }
-        equal(Cr.NS_OK, expectedResult, "Connection should succeed");
         do_print("Output to server written");
         input = transport.openInputStream(0, 0, 0);
         input.asyncWait(handler, 0, 0, Services.tm.currentThread);
@@ -203,7 +204,8 @@ add_task(function* () {
   // The auto-retry on connection reset is implemented in our HTTP layer.
   // So we will see the crafted NS_ERROR_NET_RESET when we use
   // nsISocketTransport directly.
-  yield startClient(port, Cr.NS_ERROR_NET_RESET, {isPrivate: true});
+  yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
+                    {isPrivate: true, allowReset: true});
   // retry manually to simulate the HTTP layer
   yield startClient(port, Cr.NS_OK, {isPrivate: true});
 
@@ -218,7 +220,8 @@ add_task(function* () {
   // temporary override should not change the pref.
   equal(Services.prefs.getCharPref("security.tls.insecure_fallback_hosts"),
         "");
-  yield startClient(port, Cr.NS_ERROR_NET_RESET);
+  yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
+                    {allowReset: true});
   yield startClient(port, Cr.NS_OK);
   yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
                     {isPrivate: true});
@@ -234,7 +237,8 @@ add_task(function* () {
   // permanent override should change the pref.
   equal(Services.prefs.getCharPref("security.tls.insecure_fallback_hosts"),
         "127.0.0.1");
-  yield startClient(port, Cr.NS_ERROR_NET_RESET);
+  yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
+                    {allowReset: true});
   yield startClient(port, Cr.NS_OK);
   yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
                     {isPrivate: true});
@@ -248,7 +252,8 @@ add_task(function* () {
 
   // add a host to the pref to prepare the next test
   weakCryptoOverride.addWeakCryptoOverride("127.0.0.1", false);
-  yield startClient(port, Cr.NS_ERROR_NET_RESET);
+  yield startClient(port, getXPCOMStatusFromNSS(SSL_ERROR_NO_CYPHER_OVERLAP),
+                    {allowReset: true});
   yield startClient(port, Cr.NS_OK);
   equal(Services.prefs.getCharPref("security.tls.insecure_fallback_hosts"),
         "127.0.0.1");
