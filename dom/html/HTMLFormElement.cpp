337eb7747f81cc4e83b87ca95cda65abb908dc9b
Riccardo Mottola <rm@gnu.org>
2025-07-21 17:09:00 +0200

    Bug 1259672 Rename InternalFormEvent::originator to mOriginator r=smaug

--- dom/html/HTMLFormElement.cpp
+++ dom/html/HTMLFormElement.cpp
@@ -689,7 +689,7 @@ HTMLFormElement::BuildSubmission(HTMLFormSubmission** aFormSubmission,
   if (aEvent) {
     InternalFormEvent* formEvent = aEvent->AsFormEvent();
     if (formEvent) {
-      nsIContent* originator = formEvent->originator;
+      nsIContent* originator = formEvent->mOriginator;
       if (originator) {
         if (!originator->IsHTMLElement()) {
           return NS_ERROR_UNEXPECTED;
