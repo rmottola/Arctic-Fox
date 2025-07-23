bb2fbda22cf44e76131a35766a93357e9d13ff7b
Riccardo Mottola <rm@gnu.org>
2025-07-23 02:28:28 +0200

    Bug 1197125: [webm]. Don't load the entire webm in RAM. r=kinetik
    Instead we parse it in chunks of 1MiB max.

--- dom/media/webm/WebMBufferedParser.cpp
+++ dom/media/webm/WebMBufferedParser.cpp
@@ -440,9 +440,16 @@ void WebMBufferedState::UpdateIndex(const MediaByteRangeSet& aRanges, MediaResou
         }
       }
     }
-    RefPtr<MediaByteBuffer> bytes = aResource->MediaReadAt(offset, length);
-    if(bytes) {
+    while (length > 0) {
+      static const uint32_t BLOCK_SIZE = 1048576;
+      uint32_t block = std::min(length, BLOCK_SIZE);
+      RefPtr<MediaByteBuffer> bytes = aResource->MediaReadAt(offset, block);
+      if (!bytes) {
+        break;
+      }
       NotifyDataArrived(bytes->Elements(), bytes->Length(), offset);
+      length -= bytes->Length();
+      offset += bytes->Length();
     }
   }
 }
