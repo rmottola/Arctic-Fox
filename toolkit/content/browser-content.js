e78cf456edf790b076a8963f93b33646044d275c
Riccardo Mottola <rm@gnu.org>
2025-08-04 16:20:37 +0200

    Bug 1190382 - fix reference to KeyboardEvent in content process, rs=bustage

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -794,7 +794,7 @@ var FindBar = {
     let fakeEvent = {};
     for (let k in event) {
       if (typeof event[k] != "object" && typeof event[k] != "function" &&
-          !(k in KeyboardEvent)) {
+          !(k in content.KeyboardEvent)) {
         fakeEvent[k] = event[k];
       }
     }
