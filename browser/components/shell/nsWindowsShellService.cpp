ef03d86918ef1e8d7eeb619c6a5e4d42e9ac7e86
Riccardo Mottola <rm@gnu.org>
2025-08-17 22:31:45 +0200

    fix Pale Moon leftover

--- browser/components/shell/nsWindowsShellService.cpp
+++ browser/components/shell/nsWindowsShellService.cpp
@@ -188,7 +188,7 @@ typedef struct {
   const char* oldValueData;
 } SETTING;
 
-#define APP_REG_NAME L"Pale Moon"
+#define APP_REG_NAME L"ArcticFox"
 #define VAL_FILE_ICON "%APPPATH%,1"
 #define VAL_OPEN "\"%APPPATH%\" -osint -url \"%1\""
 #define OLD_VAL_OPEN "\"%APPPATH%\" -requestPending -osint -url \"%1\""
