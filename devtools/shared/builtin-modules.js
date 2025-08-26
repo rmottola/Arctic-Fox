17c7694b5dcf89fd359261884df262fddea1e191
Riccardo Mottola <rm@gnu.org>
2025-08-26 15:17:28 +0200

    Bug 1292574 - make CSS a global rather than a module in devtools loader; r=gregtatum

--- devtools/shared/builtin-modules.js
+++ devtools/shared/builtin-modules.js
@@ -217,13 +217,6 @@ defineLazyGetter(exports.modules, "indexedDB", () => {
   }
 });
 
-defineLazyGetter(exports.modules, "CSS", () => {
-  let sandbox
-    = Cu.Sandbox(CC("@mozilla.org/systemprincipal;1", "nsIPrincipal")(),
-                 {wantGlobalProperties: ["CSS"]});
-  return sandbox.CSS;
-});
-
 defineLazyGetter(exports.modules, "FileReader", () => {
   let sandbox
     = Cu.Sandbox(CC("@mozilla.org/systemprincipal;1", "nsIPrincipal")(),
@@ -292,6 +285,12 @@ defineLazyGetter(globals, "setInterval", () => {
 defineLazyGetter(globals, "CSSRule", () => Ci.nsIDOMCSSRule);
 defineLazyGetter(globals, "DOMParser", () => {
   return CC("@mozilla.org/xmlextras/domparser;1", "nsIDOMParser");
+});
+defineLazyGetter(globals, "CSS", () => {
+  let sandbox
+    = Cu.Sandbox(CC("@mozilla.org/systemprincipal;1", "nsIPrincipal")(),
+                 {wantGlobalProperties: ["CSS"]});
+  return sandbox.CSS;
 });
 defineLazyGetter(globals, "WebSocket", () => {
   return Services.appShell.hiddenDOMWindow.WebSocket;
