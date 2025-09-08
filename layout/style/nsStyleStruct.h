7dd3172611d1801110fe863de16463f54304c2e4
Riccardo Mottola <rm@gnu.org>
2025-09-08 19:33:08 +0200

    Bug 1295662 - stylo: Add bindings for StyleClipPath; r=heycam

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -2602,7 +2602,7 @@ public:
     return !(*this == aOther);
   }
 
-  NS_INLINE_DECL_REFCOUNTING(StyleBasicShape);
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(StyleBasicShape);
 
 private:
   ~StyleBasicShape() {}
