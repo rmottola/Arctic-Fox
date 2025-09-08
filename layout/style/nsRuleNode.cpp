c36dfb1b356ada9c1c2daa385ffbeab6e17d4785
Riccardo Mottola <rm@gnu.org>
2025-09-08 22:31:31 +0200

    Bug 1291280 - Part 4. Fix compile error after enable-mask-as-shorthand. r=me

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -9993,7 +9993,7 @@ nsRuleNode::ComputeSVGResetData(void* aStartStruct,
                     svgReset->mMask.mLayers,
                     parentSVGReset->mMask.mLayers,
                     &nsStyleImageLayers::Layer::mSourceURI,
-                    nsCOMPtr<nsIURI>(), parentSVGReset->mMask.mImageCount,
+                    FragmentOrURL(), parentSVGReset->mMask.mImageCount,
                     svgReset->mMask.mImageCount,
                     maxItemCount, rebuild, conditions);
 
