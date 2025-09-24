31a12ba3f2b56133c3b8b385a5f25dd55329a9ef
Riccardo Mottola <rm@gnu.org>
2025-09-24 10:15:53 +0200

    Bug 1262310 - Fix opening synced tabs from the sidebar. r=markh

--- browser/components/syncedtabs/TabListComponent.js
+++ browser/components/syncedtabs/TabListComponent.js
@@ -104,7 +104,7 @@ TabListComponent.prototype = {
   },
 
   onOpenTab(url, where, params) {
-    this._window.openLinkIn(url, where, params);
+    this._window.openUILinkIn(url, where, params);
   },
 
   onCopyTabLocation(url) {
