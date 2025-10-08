de42390d862cc88410de97dcecd8a77a6f9868b5
Riccardo Mottola <rm@gnu.org>
2025-10-08 20:08:50 +0200

    Bug 1258808 - Part 1: Passing PointerId (Gecko). r=smaug, f=btseng

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -6909,6 +6909,8 @@ DispatchPointerFromMouseOrTouch(PresShell* aShell,
     }
 
     WidgetPointerEvent event(*mouseEvent);
+    event.pointerId = mouseEvent->pointerId;
+    event.inputSource = mouseEvent->inputSource;
     event.mMessage = pointerMessage;
     event.button = button;
     event.buttons = mouseEvent->buttons;
