8b7529dd2b868439ba46eb620366069591d035bf
Riccardo Mottola <rm@gnu.org>
2025-10-15 10:58:42 +0200

    Bug 1249739 - Improve performance in XHR in workers - part 1 - XHR.responseText must be cached, r=smaug

--- dom/xhr/XMLHttpRequestMainThread.h
+++ dom/xhr/XMLHttpRequestMainThread.h
@@ -579,6 +579,8 @@ protected:
   already_AddRefed<nsIHttpChannel> GetCurrentHttpChannel();
   already_AddRefed<nsIJARChannel> GetCurrentJARChannel();
 
+  void TruncateResponseText();
+
   bool IsSystemXHR() const;
   bool InUploadPhase() const;
 
