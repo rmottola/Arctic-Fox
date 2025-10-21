5bc8ba6f5b74116e9f82dab16d9d46240bb8f5b6
Riccardo Mottola <rm@gnu.org>
2025-10-21 09:17:29 +0200

    Bug 1290614, part 4 - Stop storing mPosition in XPCNativeSetKey. r=mrbkap
    There are three cases for a key, represented by the three ctors:
    1. mBaseSet is non-null, mAddition is null.
    2. mBaseSet is null, mAddition is non-null.
    3. Both mBaseSet and mAddition are non-null.
    
    In the three places that use the value of mPosition, condition 3
    holds, so the key must have been constructed using the third ctor. For
    this ctor, mPosition is equal to mBaseSet->GetInterfaceCount(), so I
    substitute the value and eliminate the field.
    
    This makes a check in NewInstanceMutate() trivially false, so I
    eliminated that, too.

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -1283,7 +1283,7 @@ class XPCNativeSetKey final
 public:
     // This represents an existing set |baseSet|.
     explicit XPCNativeSetKey(XPCNativeSet* baseSet)
-        : mBaseSet(baseSet), mAddition(nullptr), mPosition(0)
+        : mBaseSet(baseSet), mAddition(nullptr)
     {
         MOZ_ASSERT(baseSet);
     }
@@ -1291,7 +1291,7 @@ public:
     // This represents a new set containing only nsISupports and
     // |addition|.
     explicit XPCNativeSetKey(XPCNativeInterface* addition)
-        : mBaseSet(nullptr), mAddition(addition), mPosition(0)
+        : mBaseSet(nullptr), mAddition(addition)
     {
         MOZ_ASSERT(addition);
     }
@@ -1305,7 +1305,6 @@ public:
 
     XPCNativeSet* GetBaseSet() const {return mBaseSet;}
     XPCNativeInterface* GetAddition() const {return mAddition;}
-    uint16_t GetPosition() const {return mPosition;}
 
     PLDHashNumber Hash() const;
 
@@ -1314,7 +1313,6 @@ public:
 private:
     XPCNativeSet* mBaseSet;
     XPCNativeInterface* mAddition;
-    uint16_t mPosition;
 };
 
 /***************************************************************************/
