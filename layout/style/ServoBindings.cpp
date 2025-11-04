ce2cdc626ee0bf925a8851369a4edf0a2bb3b08d
Riccardo Mottola <rm@gnu.org>
2025-11-04 20:09:49 +0100

    Bug 1297064 - stylo: Add bindings for filling nsStyleImageLayers lists; r=heycam

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -20,6 +20,7 @@
 #include "nsINode.h"
 #include "nsIPrincipal.h"
 #include "nsNameSpaceManager.h"
+#include "nsRuleNode.h"
 #include "nsString.h"
 #include "nsStyleStruct.h"
 #include "nsStyleUtil.h"
@@ -319,6 +320,12 @@ Gecko_GetServoDeclarationBlock(RawGeckoElementBorrowed aElement)
     return nullptr;
   }
   return attr->GetServoCSSDeclarationValue();
+}
+
+void
+Gecko_FillAllBackgroundLists(nsStyleImageLayers* aLayers, uint32_t aMaxLen)
+{
+  nsRuleNode::FillAllBackgroundLists(*aLayers, aMaxLen);
 }
 
 template <typename Implementor>
