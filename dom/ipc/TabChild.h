f0e01c60b6cc9a250f513d59b29151e804dea9ca
Riccardo Mottola <rm@gnu.org>
2025-11-07 00:01:54 +0100

    Bug 1297549: Part 2 - Propagate changes in tab native window down to content for a11y; r=jimm

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -594,6 +594,8 @@ public:
   virtual bool RecvPrint(const uint64_t& aOuterWindowID,
                          const PrintData& aPrintData) override;
 
+  virtual bool RecvUpdateNativeWindowHandle(const uintptr_t& aNewHandle) override;
+
   /**
    * Native widget remoting protocol for use with windowed plugins with e10s.
    */
@@ -651,6 +653,10 @@ public:
   {
       mAPZChild = aAPZChild;
   }
+
+#if defined(XP_WIN) && defined(ACCESSIBILITY)
+  uintptr_t GetNativeWindowHandle() const { return mNativeWindowHandle; }
+#endif
 
 protected:
   virtual ~TabChild();
@@ -785,6 +791,11 @@ private:
   // APZChild clears this pointer from its destructor, so it shouldn't be a
   // dangling pointer.
   layers::APZChild* mAPZChild;
+
+#if defined(XP_WIN) && defined(ACCESSIBILITY)
+  // The handle associated with the native window that contains this tab
+  uintptr_t mNativeWindowHandle;
+#endif // defined(XP_WIN)
 
   DISALLOW_EVIL_CONSTRUCTORS(TabChild);
 };
