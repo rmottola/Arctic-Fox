85a43a357db3a3c3a91c02e8da91facfb73926e5
Riccardo Mottola <rm@gnu.org>
2025-11-19 17:08:26 +0100

    Bug 1264562 - Part 2: Test firstPartyDomain in test_ocsp_caching.js r=keeler

--- security/manager/ssl/tests/unit/test_ocsp_caching.js
+++ security/manager/ssl/tests/unit/test_ocsp_caching.js
@@ -42,7 +42,8 @@ function generateGoodOCSPResponse() {
   return responses[0];
 }
 
-function add_ocsp_test(aHost, aExpectedResult, aResponses, aMessage) {
+function add_ocsp_test(aHost, aExpectedResult, aResponses, aMessage,
+                       aFirstPartyDomain) {
   add_connection_test(aHost, aExpectedResult,
       function() {
         clearSessionCache();
@@ -55,7 +56,7 @@ function add_ocsp_test(aHost, aExpectedResult, aResponses, aMessage) {
         equal(gFetchCount, aResponses.length,
               "should have made " + aResponses.length +
               " OCSP request" + (aResponses.length == 1 ? "" : "s"));
-      });
+      }, null, aFirstPartyDomain);
 }
 
 function run_test() {
