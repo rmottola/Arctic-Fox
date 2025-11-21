57c76faac992bf62fab6762a75c41e4ccc6da7e9
Riccardo Mottola <rm@gnu.org>
2025-11-21 11:23:59 +0100

    Bug 1289050 - Part 8.1: Use %lu and %lx instead of PRIu32 and PRIx32 in WasmBinary.cpp. r=jwalden

--- js/src/asmjs/WasmBinary.cpp
+++ js/src/asmjs/WasmBinary.cpp
@@ -56,7 +56,7 @@ wasm::DecodePreamble(Decoder& d)
         return d.fail("failed to match magic number");
 
     if (!d.readFixedU32(&u32) || u32 != EncodingVersion)
-        return d.fail("binary version 0x%" PRIx32 " does not match expected version 0x%" PRIx32,
+        return d.fail("binary version 0x%lx does not match expected version 0x%lx",
                       u32, EncodingVersion);
 
     return true;
@@ -148,7 +148,7 @@ wasm::DecodeResizable(Decoder& d, ResizableLimits* limits)
         return d.fail("expected flags");
 
     if (flags & ~uint32_t(ResizableFlags::AllowedMask))
-        return d.fail("unexpected bits set in flags: %" PRIu32,
+        return d.fail("unexpected bits set in flags: %lu",
                       (flags & ~uint32_t(ResizableFlags::AllowedMask)));
 
     if (!(flags & uint32_t(ResizableFlags::Default)))
@@ -164,7 +164,7 @@ wasm::DecodeResizable(Decoder& d, ResizableLimits* limits)
 
         if (limits->initial > maximum) {
             return d.fail("memory size minimum must not be greater than maximum; "
-                          "maximum length %" PRIu32 " is less than initial length %" PRIu32,
+                          "maximum length %lu is less than initial length %lu",
                           maximum, limits->initial);
         }
 
