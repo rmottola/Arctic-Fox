e9abbf73d8a0f1cb00ba8cf497b4e4297d7e37e3
Riccardo Mottola <rm@gnu.org>
2025-11-25 09:15:56 +0100

    Bug 1297590. Part 10 - remove some mSentFirstFrameLoadedEvent checks for it is surely true when mState is DECODING. r=kaku

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -793,8 +793,7 @@ MediaDecoderStateMachine::OnVideoDecoded(MediaData* aVideoSample,
         return;
       }
       TimeDuration decodeTime = TimeStamp::Now() - aDecodeStartTime;
-      if (mSentFirstFrameLoadedEvent &&
-          THRESHOLD_FACTOR * DurationToUsecs(decodeTime) > mLowAudioThresholdUsecs &&
+      if (THRESHOLD_FACTOR * DurationToUsecs(decodeTime) > mLowAudioThresholdUsecs &&
           !HasLowUndecodedData())
       {
         mLowAudioThresholdUsecs =
@@ -2296,10 +2295,6 @@ MediaDecoderStateMachine::RunStateMachine()
       return;
 
     case DECODER_STATE_DECODING: {
-      // Can't start playback until having decoded first frames.
-      if (!mSentFirstFrameLoadedEvent) {
-        return;
-      }
       if (mPlayState != MediaDecoder::PLAY_STATE_PLAYING && IsPlaying())
       {
         // We're playing, but the element/decoder is in paused state. Stop
