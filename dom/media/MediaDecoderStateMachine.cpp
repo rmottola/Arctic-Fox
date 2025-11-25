b98a267d7a117e880af99ce8718307903c18e563
Riccardo Mottola <rm@gnu.org>
2025-11-25 08:11:01 +0100

    Bug 1297590. Part 1 - add DECODER_STATE_DECODING_FIRSTFRAME to MDSM. r=kaku
    When separating decoding frist frames from the DECODING state, we need
    to change the test `mState == DECODER_STATE_DECODING` to
    `mState == DECODER_STATE_DECODING || mState == DECODER_STATE_DECODING_FIRSTFRAME`.
    However, we don't make changes for those where mSentFirstFrameLoadedEvent is
    proven to be true. Because there is no way for mState to be DECODING_FIRSTFRAME
    when mSentFirstFrameLoadedEvent is true.

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -676,7 +676,9 @@ MediaDecoderStateMachine::OnNotDecoded(MediaData::Type aType,
     } else {
       StopPrerollingVideo();
     }
-    if (mState == DECODER_STATE_BUFFERING || mState == DECODER_STATE_DECODING) {
+    if (mState == DECODER_STATE_BUFFERING ||
+        mState == DECODER_STATE_DECODING ||
+        mState == DECODER_STATE_DECODING_FIRSTFRAME) {
         MaybeFinishDecodeFirstFrame();
     }
     return;
@@ -827,6 +829,8 @@ bool
 MediaDecoderStateMachine::CheckIfDecodeComplete()
 {
   MOZ_ASSERT(OnTaskQueue());
+  // DecodeComplete is possible only after decoding first frames.
+  MOZ_ASSERT(mSentFirstFrameLoadedEvent);
   MOZ_ASSERT(mState == DECODER_STATE_DECODING ||
              mState == DECODER_STATE_BUFFERING);
   return !IsVideoDecoding() && !IsAudioDecoding();
@@ -938,6 +942,8 @@ void MediaDecoderStateMachine::StopPlayback()
 void MediaDecoderStateMachine::MaybeStartPlayback()
 {
   MOZ_ASSERT(OnTaskQueue());
+  // Should try to start playback only after decoding first frames.
+  MOZ_ASSERT(mSentFirstFrameLoadedEvent);
   MOZ_ASSERT(mState == DECODER_STATE_DECODING ||
              mState == DECODER_STATE_COMPLETED);
 
@@ -973,6 +979,8 @@ void
 MediaDecoderStateMachine::MaybeStartBuffering()
 {
   MOZ_ASSERT(OnTaskQueue());
+  // Buffering makes senses only after decoding first frames.
+  MOZ_ASSERT(mSentFirstFrameLoadedEvent);
   MOZ_ASSERT(mState == DECODER_STATE_DECODING);
 
   if (mPlayState == MediaDecoder::PLAY_STATE_PLAYING &&
@@ -1020,14 +1028,15 @@ void MediaDecoderStateMachine::UpdatePlaybackPosition(int64_t aTime)
 MediaDecoderStateMachine::ToStateStr(State aState)
 {
   switch (aState) {
-    case DECODER_STATE_DECODING_METADATA: return "DECODING_METADATA";
-    case DECODER_STATE_WAIT_FOR_CDM:      return "WAIT_FOR_CDM";
-    case DECODER_STATE_DORMANT:           return "DORMANT";
-    case DECODER_STATE_DECODING:          return "DECODING";
-    case DECODER_STATE_SEEKING:           return "SEEKING";
-    case DECODER_STATE_BUFFERING:         return "BUFFERING";
-    case DECODER_STATE_COMPLETED:         return "COMPLETED";
-    case DECODER_STATE_SHUTDOWN:          return "SHUTDOWN";
+    case DECODER_STATE_DECODING_METADATA:   return "DECODING_METADATA";
+    case DECODER_STATE_WAIT_FOR_CDM:        return "WAIT_FOR_CDM";
+    case DECODER_STATE_DORMANT:             return "DORMANT";
+    case DECODER_STATE_DECODING_FIRSTFRAME: return "DECODING_FIRSTFRAME";
+    case DECODER_STATE_DECODING:            return "DECODING";
+    case DECODER_STATE_SEEKING:             return "SEEKING";
+    case DECODER_STATE_BUFFERING:           return "BUFFERING";
+    case DECODER_STATE_COMPLETED:           return "COMPLETED";
+    case DECODER_STATE_SHUTDOWN:            return "SHUTDOWN";
     default: MOZ_ASSERT_UNREACHABLE("Invalid state.");
   }
   return "UNKNOWN";
@@ -1252,6 +1261,8 @@ void
 MediaDecoderStateMachine::StartDecoding()
 {
   MOZ_ASSERT(OnTaskQueue());
+  // Should transition to DECODING only after decoding first frames.
+  MOZ_ASSERT(mSentFirstFrameLoadedEvent);
   MOZ_ASSERT(mState == DECODER_STATE_DECODING);
 
   // Handle the pending seek now if we've decoded first frames. Otherwise it
@@ -2252,6 +2263,7 @@ MediaDecoderStateMachine::RunStateMachine()
     case DECODER_STATE_DORMANT:
     case DECODER_STATE_WAIT_FOR_CDM:
     case DECODER_STATE_DECODING_METADATA:
+    case DECODER_STATE_DECODING_FIRSTFRAME:
       return;
 
     case DECODER_STATE_DECODING: {
