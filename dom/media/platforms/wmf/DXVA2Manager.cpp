ef0c6fe569fdc75a2ab34c011ff27a0377b61a8b
Riccardo Mottola <rm@gnu.org>
2025-11-25 16:54:18 +0100

    Bug 1288618 - Part 3: Avoid accessing MediaPrefs from the GPU process within WMF code. r=cpearce

--- dom/media/platforms/wmf/DXVA2Manager.cpp
+++ dom/media/platforms/wmf/DXVA2Manager.cpp
@@ -489,7 +489,12 @@ DXVA2Manager::CreateD3D9DXVA(nsACString& aFailureReason)
 
   // DXVA processing takes up a lot of GPU resources, so limit the number of
   // videos we use DXVA with at any one time.
-  const uint32_t dxvaLimit = MediaPrefs::PDMWMFMaxDXVAVideos();
+  uint32_t dxvaLimit = 4;
+  // TODO: Sync this value across to the GPU process.
+  if (XRE_GetProcessType() != GeckoProcessType_GPU) {
+    dxvaLimit = MediaPrefs::PDMWMFMaxDXVAVideos();
+  }
+
   if (sDXVAVideosCount == dxvaLimit) {
     aFailureReason.AssignLiteral("Too many DXVA videos playing");
     return nullptr;
@@ -897,7 +902,12 @@ DXVA2Manager::CreateD3D11DXVA(nsACString& aFailureReason)
 {
   // DXVA processing takes up a lot of GPU resources, so limit the number of
   // videos we use DXVA with at any one time.
-  const uint32_t dxvaLimit = MediaPrefs::PDMWMFMaxDXVAVideos();
+  uint32_t dxvaLimit = 4;
+  // TODO: Sync this value across to the GPU process.
+  if (XRE_GetProcessType() != GeckoProcessType_GPU) {
+    dxvaLimit = MediaPrefs::PDMWMFMaxDXVAVideos();
+  }
+
   if (sDXVAVideosCount == dxvaLimit) {
     aFailureReason.AssignLiteral("Too many DXVA videos playing");
     return nullptr;
