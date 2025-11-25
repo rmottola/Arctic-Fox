f3dbfb2a16dc1b90906a63ac6d3162c23c216465
Riccardo Mottola <rm@gnu.org>
2025-11-25 08:17:38 +0100

    Bug 1297590. Part 5 - change some SetState(DECODER_STATE_DECODING) to SetState(DECODER_STATE_DECODING_FIRSTFRAME). r=kaku MDSM used to transition to DECODING in the following places: 1. BUFFERING 2. OnMetadataRead() 3. WAIT_FOR_CDM 4. SeekCompleted()
    We will transition to DECODING_FIRSTFRAME in case 2 and 3.
    
    For case 1, BUFFERING makes sense only after decoding first frames. So BUFFERING
    should never transition to DECODING_FIRSTFRAME.
    
    For case 4, we always finish decoding first frames before completing seek. So
    It should not transition to DECODING_FIRSTFRAME either.

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2018,7 +2018,7 @@ MediaDecoderStateMachine::OnMetadataRead(MetadataHolder* aMetadata)
     return;
   }
 
-  SetState(DECODER_STATE_DECODING);
+  SetState(DECODER_STATE_DECODING_FIRSTFRAME);
 
   ScheduleStateMachine();
 }
@@ -2749,7 +2749,7 @@ MediaDecoderStateMachine::OnCDMProxyReady(RefPtr<CDMProxy> aProxy)
   mCDMProxy = aProxy;
   mReader->SetCDMProxy(aProxy);
   if (mState == DECODER_STATE_WAIT_FOR_CDM) {
-    SetState(DECODER_STATE_DECODING);
+    SetState(DECODER_STATE_DECODING_FIRSTFRAME);
   }
 }
 
