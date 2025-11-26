f2c2a3d3f1b04c131d64962db13cc584cdb0e4a1
Riccardo Mottola <rm@gnu.org>
2025-11-26 01:08:33 +0100

    Bug 1309799 - Generate StringFromIPCMessageType and use it in MessageChannel::MaybeHandleError. r=billm

--- ipc/glue/ProtocolUtils.h
+++ ipc/glue/ProtocolUtils.h
@@ -358,7 +358,7 @@ enum class MessageDirection {
 MOZ_NEVER_INLINE void
 LogMessageForProtocol(const char* aTopLevelProtocol, base::ProcessId aOtherPid,
                       const char* aContextDescription,
-                      const char* aMessageDescription,
+                      uint32_t aMessageId,
                       MessageDirection aDirection);
 
 MOZ_NEVER_INLINE void
@@ -591,6 +591,8 @@ CreateEndpoints(const PrivateIPDLInterface& aPrivate,
 void
 TableToArray(const nsTHashtable<nsPtrHashKey<void>>& aTable,
              nsTArray<void*>& aArray);
+
+const char* StringFromIPCMessageType(uint32_t aMessageType);
 
 } // namespace ipc
 
