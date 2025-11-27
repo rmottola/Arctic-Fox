f104f725ed2fca0f7e8aa2d72b843f89bc2f2acd
Riccardo Mottola <rm@gnu.org>
2025-11-27 17:32:20 +0100

    Bug 1309868 part 2 - Use const nsIContent pointer in some layout utils. r=heycam

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -2844,7 +2844,7 @@ public:
    * @param aParentContent the content node corresponding to the parent frame
    * @return whether the frame is a pseudo frame
    */   
-  bool IsPseudoFrame(nsIContent* aParentContent) {
+  bool IsPseudoFrame(const nsIContent* aParentContent) {
     return mContent == aParentContent;
   }
 
