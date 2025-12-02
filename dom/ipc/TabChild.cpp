9624dd0f2716979fb171407b394fc3e043ac3c8d
Riccardo Mottola <rm@gnu.org>
2025-12-02 23:22:41 +0100

    Bug 1303967 - Don't set mIPCOpen until TabChild is initialized (r=mrbkap)

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -542,7 +542,7 @@ TabChild::TabChild(nsIContentChild* aManager,
   , mDPI(0)
   , mDefaultScale(0)
   , mIsTransparent(false)
-  , mIPCOpen(true)
+  , mIPCOpen(false)
   , mParentIsActive(false)
   , mDidSetRealShowInfo(false)
   , mDidLoadURLInit(false)
@@ -825,6 +825,8 @@ TabChild::Init()
         }
       });
   mAPZEventState = new APZEventState(mPuppetWidget, Move(callback));
+
+  mIPCOpen = true;
 
   return NS_OK;
 }
@@ -2672,8 +2674,8 @@ TabChild::RecvSetDocShellIsActive(const bool& aIsActive,
       // updated its epoch). ForcePaintNoOp does that.
       if (IPCOpen()) {
         Unused << SendForcePaintNoOp(aLayerObserverEpoch);
+        return true;
       }
-      return true;
     }
 
     docShell->SetIsActive(aIsActive);
@@ -3360,6 +3362,12 @@ TabChild::GetOuterRect()
 void
 TabChild::ForcePaint(uint64_t aLayerObserverEpoch)
 {
+  if (!IPCOpen()) {
+    // Don't bother doing anything now. Better to wait until we receive the
+    // message on the PContent channel.
+    return;
+  }
+
   nsAutoScriptBlocker scriptBlocker;
   RecvSetDocShellIsActive(true, false, aLayerObserverEpoch);
 }
