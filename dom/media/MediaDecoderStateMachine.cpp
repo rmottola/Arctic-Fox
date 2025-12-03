ffa01f6ee95d9ab25d922e4226690737c87b61b2
Riccardo Mottola <rm@gnu.org>
2025-12-03 18:42:47 +0100

    Bug 1307699. Part 5 - move mSeekTask into SeekingState. r=kaku

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -661,11 +661,11 @@ public:
     // Create a new SeekTask instance for the incoming seek task.
     if (mSeekJob.mTarget.IsAccurate() ||
         mSeekJob.mTarget.IsFast()) {
-      mMaster->mSeekTask = new AccurateSeekTask(
+      mSeekTask = new AccurateSeekTask(
         mMaster->mDecoderID, OwnerThread(), Reader(), mSeekJob.mTarget,
         mMaster->mInfo, mMaster->Duration(), mMaster->GetMediaTime());
     } else if (mSeekJob.mTarget.IsNextFrame()) {
-      mMaster->mSeekTask = new NextFrameSeekTask(
+      mSeekTask = new NextFrameSeekTask(
         mMaster->mDecoderID, OwnerThread(), Reader(), mSeekJob.mTarget,
         mMaster->mInfo, mMaster->Duration(),mMaster->GetMediaTime(),
         mMaster->AudioQueue(), mMaster->VideoQueue());
@@ -683,7 +683,7 @@ public:
     // seek target to [0, duration]. We want to update the playback position to
     // the clamped value.
     mMaster->UpdatePlaybackPositionInternal(
-      mMaster->mSeekTask->GetSeekTarget().GetTime().ToMicroseconds());
+      mSeekTask->GetSeekTarget().GetTime().ToMicroseconds());
 
     if (mSeekJob.mTarget.mEventVisibility ==
         MediaDecoderEventVisibility::Observable) {
@@ -691,7 +691,7 @@ public:
     }
 
     // Reset our state machine and decoding pipeline before seeking.
-    if (mMaster->mSeekTask->NeedToResetMDSM()) {
+    if (mSeekTask->NeedToResetMDSM()) {
       if (mSeekJob.mTarget.IsVideoOnly()) {
         mMaster->Reset(TrackInfo::kVideoTrack);
       } else {
@@ -700,7 +700,7 @@ public:
     }
 
     // Do the seek.
-    mSeekTaskRequest.Begin(mMaster->mSeekTask->Seek(mMaster->Duration())
+    mSeekTaskRequest.Begin(mSeekTask->Seek(mMaster->Duration())
       ->Then(OwnerThread(), __func__,
              [this] (const SeekTaskResolveValue& aValue) {
                OnSeekTaskResolved(aValue);
@@ -718,10 +718,10 @@ public:
   {
     mSeekTaskRequest.DisconnectIfExists();
 
-    if (mMaster->mSeekTask) {
+    if (mSeekTask) {
       mMaster->mCurrentSeek.RejectIfExists(__func__);
-      mMaster->mSeekTask->Discard();
-      mMaster->mSeekTask = nullptr;
+      mSeekTask->Discard();
+      mSeekTask = nullptr;
 
       // Reset the MediaDecoderReaderWrapper's callbask.
       mMaster->SetMediaDecoderReaderWrapperCallback();
@@ -821,7 +821,7 @@ private:
 
   void SeekCompleted()
   {
-    int64_t seekTime = mMaster->mSeekTask->GetSeekTarget().GetTime().ToMicroseconds();
+    int64_t seekTime = mSeekTask->GetSeekTarget().GetTime().ToMicroseconds();
     int64_t newCurrentTime = seekTime;
 
     // Setup timestamp state.
@@ -895,6 +895,7 @@ private:
 
   SeekJob mSeekJob;
   MozPromiseRequestHolder<SeekTask::SeekTaskPromise> mSeekTaskRequest;
+  RefPtr<SeekTask> mSeekTask;
 };
 
 class MediaDecoderStateMachine::BufferingState
@@ -2156,7 +2157,7 @@ void MediaDecoderStateMachine::VisibilityChanged()
 
     // If an existing seek is in flight don't bother creating a new
     // one to catch up.
-    if (mSeekTask || mQueuedSeek.Exists()) {
+    if (mState == DECODER_STATE_SEEKING || mQueuedSeek.Exists()) {
       return;
     }
 
