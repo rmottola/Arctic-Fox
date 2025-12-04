4d601081cb02c3b1a846bd3919f5bd21a5b6ebce
Riccardo Mottola <rm@gnu.org>
2025-12-05 00:09:21 +0100

    Bug 1304314. Part 3 - take mPlaybackRate into account. r=kikuo

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2595,7 +2595,7 @@ MediaDecoderStateMachine::HasLowDecodedAudio()
 {
   MOZ_ASSERT(OnTaskQueue());
   return IsAudioDecoding() &&
-         GetDecodedAudioDuration() < EXHAUSTED_DATA_MARGIN_USECS;
+         GetDecodedAudioDuration() < EXHAUSTED_DATA_MARGIN_USECS * mPlaybackRate;
 }
 
 bool
@@ -2603,7 +2603,7 @@ MediaDecoderStateMachine::HasLowDecodedVideo()
 {
   MOZ_ASSERT(OnTaskQueue());
   return IsVideoDecoding() &&
-         static_cast<uint32_t>(VideoQueue().GetSize()) < LOW_VIDEO_FRAMES;
+         VideoQueue().GetSize() < LOW_VIDEO_FRAMES * mPlaybackRate;
 }
 
 bool
