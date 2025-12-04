4f7df1e45486d92c2788bc35a374d77e63436c57
Riccardo Mottola <rm@gnu.org>
2025-12-04 14:39:36 +0100

    Bug 1310859 - Remove DOM/nfc and related code. r=jst

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -294,9 +294,6 @@ public:
                                JSObject* aGlobal);
   static bool HasWifiManagerSupport(JSContext* /* unused */,
                                   JSObject* aGlobal);
-#ifdef MOZ_NFC
-  static bool HasNFCSupport(JSContext* /* unused */, JSObject* aGlobal);
-#endif // MOZ_NFC
   static bool HasUserMediaSupport(JSContext* /* unused */,
                                   JSObject* /* unused */);
 
