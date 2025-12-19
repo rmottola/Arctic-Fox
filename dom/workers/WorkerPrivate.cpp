a4e46fa2fd93027434dbb0620947da34edbcfde1
Riccardo Mottola <rm@gnu.org>
2025-12-19 14:46:08 +0100

    Bug 1302036 - DOM changes to conform to new JS engine API for cloning. r=baku

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -2876,7 +2876,7 @@ WorkerPrivateParent<Derived>::PostMessageInternal(
       MarkerTracingType::START);
   }
 
-  runnable->Write(aCx, aMessage, transferable, aRv);
+  runnable->Write(aCx, aMessage, transferable, JS::CloneDataPolicy(), aRv);
 
   if (isTimelineRecording) {
     end = MakeUnique<WorkerTimelineMarker>(NS_IsMainThread()
@@ -5602,7 +5602,7 @@ WorkerPrivate::PostMessageToParentInternal(
       MarkerTracingType::START);
   }
 
-  runnable->Write(aCx, aMessage, transferable, aRv);
+  runnable->Write(aCx, aMessage, transferable, JS::CloneDataPolicy(), aRv);
 
   if (isTimelineRecording) {
     end = MakeUnique<WorkerTimelineMarker>(NS_IsMainThread()
