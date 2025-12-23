cd5329bb20ed588d178dd24904cf7d88f51c4562
Riccardo Mottola <rm@gnu.org>
2025-12-23 23:12:51 +0100

    Bug 1309909 - Give each mutex an order and check the order of aquisition r=fitzgen r=luke

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -871,6 +871,7 @@ GCRuntime::GCRuntime(JSRuntime* rt) :
     noNurseryAllocationCheck(0),
     arenasEmptyAtShutdown(true),
 #endif
+    lock(mutexid::GCLock),
     allocTask(rt, emptyChunks_),
     decommitTask(rt),
     helperState(rt)
