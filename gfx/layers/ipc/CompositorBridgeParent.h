0a282de06fa0820b9bb93286c9db8f3dae634287
Riccardo Mottola <rm@gnu.org>
2026-01-14 12:35:34 +0100

    Bug 1312059 - Stop passing the CompositorBridgeParent into the APZCTreeManager; instead the necessary subinterfaces can be obtained via the LayerTreeState. r=botond

--- gfx/layers/ipc/CompositorBridgeParent.h
+++ gfx/layers/ipc/CompositorBridgeParent.h
@@ -454,7 +454,9 @@ public:
     // acknowledged by the child.
     uint32_t mPendingCompositorUpdates;
 
+    CompositorController* GetCompositorController() const;
     MetricsSharingController* CrossProcessSharingController() const;
+    MetricsSharingController* InProcessSharingController() const;
   };
 
   /**
