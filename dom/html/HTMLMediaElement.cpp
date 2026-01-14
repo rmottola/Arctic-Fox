f2d569dfc2abb2f3eb9b2413f2f4d6079f42c5d6
Riccardo Mottola <rm@gnu.org>
2026-01-15 00:38:29 +0100

    Bug 1314529. Part 1 - remove the call to MediaDecoder::SetMediaSeekable(). r=cpearce

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -3753,7 +3753,6 @@ nsresult HTMLMediaElement::InitializeDecoderAsClone(MediaDecoder* aOriginal)
 
   LOG(LogLevel::Debug, ("%p Cloned decoder %p from %p", this, decoder.get(), aOriginal));
 
-  decoder->SetMediaSeekable(aOriginal->IsMediaSeekable());
   decoder->SetMediaSeekableOnlyInBufferedRanges(
     aOriginal->IsMediaSeekableOnlyInBufferedRanges());
 
