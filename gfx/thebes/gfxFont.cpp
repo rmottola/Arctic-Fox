82d8300a1272d0a7c30233e67b679c24723bf095
Riccardo Mottola <rm@gnu.org>
2026-01-20 23:08:36 +0100

    Bug 1304699 - Pass the proper unicode-range when looking up a font in the global font cache. r=jrmuizel

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -3111,6 +3111,10 @@ gfxFont::InitFakeSmallCapsRun(DrawTarget     *aDrawTarget,
     bool ok = true;
 
     RefPtr<gfxFont> smallCapsFont = GetSmallCapsFont();
+    if (!smallCapsFont) {
+        NS_WARNING("failed to get reduced-size font for smallcaps!");
+        smallCapsFont = this;
+    }
 
     enum RunCaseAction {
         kNoChange,
