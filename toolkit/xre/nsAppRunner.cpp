9394fb44bad5edc9e489bb478b99726ac5da9fc8
Riccardo Mottola <rm@gnu.org>
2026-01-21 19:18:16 +0100

    Bug 1304829 - Rename nsXULAppInfo's isReleaseBuild to isReleaseOrBeta. r=ted

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -992,7 +992,7 @@ nsXULAppInfo::GetLastRunCrashID(nsAString &aLastRunCrashID)
 }
 
 NS_IMETHODIMP
-nsXULAppInfo::GetIsReleaseBuild(bool* aResult)
+nsXULAppInfo::GetIsReleaseOrBeta(bool* aResult)
 {
 #ifdef RELEASE_OR_BETA
   *aResult = true;
