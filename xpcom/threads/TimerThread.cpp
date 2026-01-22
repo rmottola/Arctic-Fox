ed92b35a1cd601b823e438e5780356c1b972eb9b
Riccardo Mottola <rm@gnu.org>
2026-01-22 02:33:55 +0100

    Bug 1307350: Compensate for repeating timers that have not been appropriately cancelled. r=froydnj

--- xpcom/threads/TimerThread.cpp
+++ xpcom/threads/TimerThread.cpp
@@ -567,6 +567,10 @@ TimerThread::AddTimer(nsTimerImpl* aTimer)
 {
   MonitorAutoLock lock(mMonitor);
 
+  if (!aTimer->mEventTarget) {
+    return NS_ERROR_NOT_INITIALIZED;
+  }
+
   // Add the timer to our list.
   int32_t i = AddTimerInternal(aTimer);
   if (i < 0) {
@@ -583,7 +587,7 @@ TimerThread::AddTimer(nsTimerImpl* aTimer)
 }
 
 nsresult
-TimerThread::RemoveTimer(nsTimerImpl* aTimer)
+TimerThread::RemoveTimer(nsTimerImpl* aTimer, bool aDisable)
 {
   MonitorAutoLock lock(mMonitor);
 
@@ -592,6 +596,10 @@ TimerThread::RemoveTimer(nsTimerImpl* aTimer)
 
   if (!RemoveTimerInternal(aTimer)) {
     return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  if (aDisable) {
+    aTimer->mEventTarget = nullptr;
   }
 
   // Awaken the timer thread.
