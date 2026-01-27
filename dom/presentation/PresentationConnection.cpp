e36ea42ada67b6aea4da05b671b641190c6b2b5a
Riccardo Mottola <rm@gnu.org>
2026-01-27 11:37:00 +0100

    Bug 1291971 - Part 1, enable controller idlharness test. r=smaug.

--- dom/presentation/PresentationConnection.cpp
+++ dom/presentation/PresentationConnection.cpp
@@ -12,7 +12,7 @@
 #include "mozilla/dom/File.h"
 #include "mozilla/dom/MessageEvent.h"
 #include "mozilla/dom/MessageEventBinding.h"
-#include "mozilla/dom/PresentationConnectionClosedEvent.h"
+#include "mozilla/dom/PresentationConnectionCloseEvent.h"
 #include "mozilla/ErrorNames.h"
 #include "mozilla/DebugOnly.h"
 #include "nsContentUtils.h"
@@ -429,7 +429,7 @@ PresentationConnection::ProcessStateChanged(nsresult aReason)
       }
 
       Unused <<
-        NS_WARN_IF(NS_FAILED(DispatchConnectionClosedEvent(reason, errorMsg)));
+        NS_WARN_IF(NS_FAILED(DispatchConnectionCloseEvent(reason, errorMsg)));
 
       return RemoveFromLoadGroup();
     }
@@ -539,7 +539,7 @@ PresentationConnection::NotifyReplaced()
 }
 
 nsresult
-PresentationConnection::DispatchConnectionClosedEvent(
+PresentationConnection::DispatchConnectionCloseEvent(
   PresentationConnectionClosedReason aReason,
   const nsAString& aMessage,
   bool aDispatchNow)
@@ -549,12 +549,12 @@ PresentationConnection::DispatchConnectionClosedEvent(
     return NS_ERROR_FAILURE;
   }
 
-  PresentationConnectionClosedEventInit init;
+  PresentationConnectionCloseEventInit init;
   init.mReason = aReason;
   init.mMessage = aMessage;
 
-  RefPtr<PresentationConnectionClosedEvent> closedEvent =
-    PresentationConnectionClosedEvent::Constructor(this,
+  RefPtr<PresentationConnectionCloseEvent> closedEvent =
+    PresentationConnectionCloseEvent::Constructor(this,
                                                    NS_LITERAL_STRING("close"),
                                                    init);
   closedEvent->SetTrusted(true);
@@ -748,7 +748,7 @@ PresentationConnection::AsyncCloseConnectionWithErrorMsg(const nsAString& aMessa
       // Make sure dispatching the event and closing the connection are invoked
       // at the same time by setting |aDispatchNow| to true.
       Unused << NS_WARN_IF(NS_FAILED(
-        DispatchConnectionClosedEvent(PresentationConnectionClosedReason::Error,
+        DispatchConnectionCloseEvent(PresentationConnectionClosedReason::Error,
                                       message,
                                       true)));
 
