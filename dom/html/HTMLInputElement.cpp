eda5ea047ccb7dd9565d252b5d1a160cadcae894
Riccardo Mottola <rm@gnu.org>
2026-01-30 10:38:52 +0100

    Bug 1317367 part 2.  Make HTMLInputElement::GetValue infallible again; just return empty string on OOM.  To a first approximation no one checks the return value anyway.  r=smaug

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -1751,10 +1751,7 @@ HTMLInputElement::SetWidth(uint32_t aWidth)
 NS_IMETHODIMP
 HTMLInputElement::GetValue(nsAString& aValue)
 {
-  nsresult rv = GetValueInternal(aValue);
-  if (NS_FAILED(rv)) {
-    return rv;
-  }
+  GetValueInternal(aValue);
 
   // Don't return non-sanitized value for types that are experimental on mobile
   // or datetime types
@@ -1765,7 +1762,7 @@ HTMLInputElement::GetValue(nsAString& aValue)
   return NS_OK;
 }
 
-nsresult
+void
 HTMLInputElement::GetValueInternal(nsAString& aValue) const
 {
   switch (GetValueMode()) {
@@ -1773,9 +1770,9 @@ HTMLInputElement::GetValueInternal(nsAString& aValue) const
       if (IsSingleLineTextControl(false)) {
         mInputData.mState->GetValue(aValue, true);
       } else if (!aValue.Assign(mInputData.mValue, fallible)) {
-        return NS_ERROR_OUT_OF_MEMORY;
+        aValue.Truncate();
       }
-      return NS_OK;
+      return;
 
     case VALUE_MODE_FILENAME:
       if (nsContentUtils::LegacyIsCallerChromeOrNativeCode()) {
@@ -1789,23 +1786,20 @@ HTMLInputElement::GetValueInternal(nsAString& aValue) const
         }
       }
 
-      return NS_OK;
+      return;
 
     case VALUE_MODE_DEFAULT:
       // Treat defaultValue as value.
       GetAttr(kNameSpaceID_None, nsGkAtoms::value, aValue);
-      return NS_OK;
+      return;
 
     case VALUE_MODE_DEFAULT_ON:
       // Treat default value as value and returns "on" if no value.
       if (!GetAttr(kNameSpaceID_None, nsGkAtoms::value, aValue)) {
         aValue.AssignLiteral("on");
       }
-      return NS_OK;
+      return;
   }
-
-  // This return statement is required for some compilers.
-  return NS_OK;
 }
 
 bool
@@ -1937,15 +1931,6 @@ HTMLInputElement::GetValueAsDecimal() const
 
   return !ConvertStringToNumber(stringValue, decimalValue) ? Decimal::nan()
                                                            : decimalValue;
-}
-
-void
-HTMLInputElement::GetValue(nsAString& aValue, ErrorResult& aRv)
-{
-  nsresult rv = GetValue(aValue);
-  if (NS_FAILED(rv)) {
-    aRv.Throw(rv);
-  }
 }
 
 void
@@ -7424,7 +7409,7 @@ HTMLInputElement::HasTypeMismatch() const
   }
 
   nsAutoString value;
-  NS_ENSURE_SUCCESS(GetValueInternal(value), false);
+  GetValueInternal(value);
 
   if (value.IsEmpty()) {
     return false;
@@ -7467,7 +7452,7 @@ HTMLInputElement::HasPatternMismatch() const
   GetAttr(kNameSpaceID_None, nsGkAtoms::pattern, pattern);
 
   nsAutoString value;
-  NS_ENSURE_SUCCESS(GetValueInternal(value), false);
+  GetValueInternal(value);
 
   if (value.IsEmpty()) {
     return false;
@@ -7626,7 +7611,7 @@ HTMLInputElement::HasBadInput() const
     nsAutoString value;
     nsAutoCString unused;
     uint32_t unused2;
-    NS_ENSURE_SUCCESS(GetValueInternal(value), false);
+    GetValueInternal(value);
     HTMLSplitOnSpacesTokenizer tokenizer(value, ',');
     while (tokenizer.hasMoreTokens()) {
       if (!PunycodeEncodeEmailAddress(tokenizer.nextToken(), unused, &unused2)) {
