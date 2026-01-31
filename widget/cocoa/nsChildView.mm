02765c2bd7766d74a4f4fb1031032dc9bc7fac89
Riccardo Mottola <rm@gnu.org>
2026-01-31 21:40:51 +0100

    Bug 1315477 - Remove some instances of static_cast'ing to ClientLayerManager. r=mattwoodrow

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -4011,9 +4011,8 @@ NSEvent* gLastDragMouseDownEvent = nil;
     }
 
     if ([self isUsingOpenGL]) {
-      if (mGeckoChild->GetLayerManager()->GetBackendType() == LayersBackend::LAYERS_CLIENT) {
-        ClientLayerManager *manager = static_cast<ClientLayerManager*>(mGeckoChild->GetLayerManager());
-        manager->AsShadowForwarder()->WindowOverlayChanged();
+      if (ShadowLayerForwarder* slf = mGeckoChild->GetLayerManager()->AsShadowForwarder()) {
+        slf->WindowOverlayChanged();
       }
     }
 
