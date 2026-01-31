bff92c7a3c8e3206639ce509f6dd2e1bd4a90285
Riccardo Mottola <rm@gnu.org>
2026-01-31 21:39:21 +0100

    Bug 1304199 - Fix nsWindow::GetNativeData called with bad value. r=erahm

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1761,6 +1761,8 @@ nsWindow::GetNativeData(uint32_t aDataType)
         }
         return mIMContext.get();
     }
+    case NS_NATIVE_OPENGL_CONTEXT:
+      return nullptr;
 #ifdef MOZ_X11
     case NS_NATIVE_COMPOSITOR_DISPLAY:
         return gfxPlatformGtk::GetPlatform()->GetCompositorDisplay();
