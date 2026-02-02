4e20ae2d821a9128156b045ad58eb0bb85cc8a66
Riccardo Mottola <rm@gnu.org>
2026-02-02 15:52:22 +0100

    Bug 1297314 - Initialize IPC union parameters to void_t for the case when RecvKeywordToURI() returns early. r=kanru

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -4141,6 +4141,9 @@ ContentParent::RecvKeywordToURI(const nsCString& aKeyword,
                                 OptionalInputStreamParams* aPostData,
                                 OptionalURIParams* aURI)
 {
+  *aPostData = void_t();
+  *aURI = void_t();
+
   nsCOMPtr<nsIURIFixup> fixup = do_GetService(NS_URIFIXUP_CONTRACTID);
   if (!fixup) {
     return true;
