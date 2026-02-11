96322de59c0d334926c5803e1a54fbf33691cb5b
Riccardo Mottola <rm@gnu.org>
2026-02-11 12:55:08 +0100

    Bug 1311642 - Give GLUploadHelpers some love. r=nical
    GLUploadHelpers was trying to be too flexible, so remove some unused
    options it provided. Require that the full surface and texture size is
    passed in rather than only the updated subregion. This prevents textures
    being initialized to an incorrect size when partial texture uploads are
    disallowed.

--- gfx/gl/TextureImageEGL.cpp
+++ gfx/gl/TextureImageEGL.cpp
@@ -214,10 +214,10 @@ TextureImageEGL::DirectUpdate(gfx::DataSourceSurface* aSurf, const nsIntRegion& 
                              aSurf,
                              region,
                              mTexture,
+                             mSize,
                              &uploadSize,
                              needInit,
-                             bounds.TopLeft() + gfx::IntPoint(aFrom.x, aFrom.y),
-                             false);
+                             aFrom);
     if (uploadSize > 0) {
         UpdateUploadSize(uploadSize);
     }
