b31589447345b82d1fbb9c5cd1bba397f2a940c3
Riccardo Mottola <rm@gnu.org>
2026-02-22 21:27:39 +0100

    Bug 1302148 - Fix potential race in toolbox.destroy. r=bgrins

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -2056,6 +2056,8 @@ Toolbox.prototype = {
     if (this._destroyer) {
       return this._destroyer;
     }
+    let deferred = defer();
+    this._destroyer = deferred.promise;
 
     this.emit("destroy");
 
@@ -2141,7 +2143,7 @@ Toolbox.prototype = {
     // Finish all outstanding tasks (which means finish destroying panels and
     // then destroying the host, successfully or not) before destroying the
     // target.
-    this._destroyer = settleAll(outstanding)
+    deferred.resolve(settleAll(outstanding)
         .catch(console.error)
         .then(() => this.destroyHost())
         .catch(console.error)
@@ -2175,7 +2177,7 @@ Toolbox.prototype = {
               .getInterface(Ci.nsIDOMWindowUtils)
               .garbageCollect();
           }
-        }).then(null, console.error);
+        }).then(null, console.error));
 
     let leakCheckObserver = ({wrappedJSObject: barrier}) => {
       // Make the leak detector wait until this toolbox is properly destroyed.
