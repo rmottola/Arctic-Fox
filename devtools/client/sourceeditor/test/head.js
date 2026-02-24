c35ef43728536b23779b4ca182c7a14d6860cbf5
Riccardo Mottola <rm@gnu.org>
2026-02-24 18:24:53 +0100

    Bug 1292592 - Convert sourceeditor to use key shortcuts module;r=ochameau

--- devtools/client/sourceeditor/test/head.js
+++ devtools/client/sourceeditor/test/head.js
@@ -1,14 +1,16 @@
 /* vim: set ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
-
+/* import-globals-from ../../framework/test/shared-head.js */
 "use strict";
 
-const { require } = Cu.import("resource://devtools/shared/Loader.jsm", {});
+// shared-head.js handles imports, constants, and utility functions
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/framework/test/shared-head.js",
+  this);
+
 const { NetUtil } = require("resource://gre/modules/NetUtil.jsm");
 const Editor = require("devtools/client/sourceeditor/editor");
-const promise = require("promise");
-const flags = require("devtools/shared/flags");
 const {getClientCssProperties} = require("devtools/shared/fronts/css-properties");
 
 flags.testing = true;
