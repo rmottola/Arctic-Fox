e72b39316f6a722f49baa7f709292141bda94211
Riccardo Mottola <rm@gnu.org>
2026-03-06 22:35:42 +0100

    Bug 1296934: Prevent background pages from showing up in global history. r=gabor

--- toolkit/components/extensions/ext-backgroundPage.js
+++ toolkit/components/extensions/ext-backgroundPage.js
@@ -61,6 +61,7 @@ BackgroundPage.prototype = {
                                        .getInterface(Ci.nsIDocShell)
                                        .QueryInterface(Ci.nsIWebNavigation);
 
+    chromeShell.useGlobalHistory = false;
     chromeShell.createAboutBlankContentViewer(system);
     chromeShell.loadURI(XUL_URL, 0, null, null, null);
 
