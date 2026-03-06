71995d1b6d7bf974a42a12ac7f096c0ff064b783
Riccardo Mottola <rm@gnu.org>
2026-03-06 22:39:24 +0100

    Bug 1268773 - Notify Addon Listeners when the preferred addon debug global is changed. r=aswan

--- toolkit/components/extensions/ext-backgroundPage.js
+++ toolkit/components/extensions/ext-backgroundPage.js
@@ -104,6 +104,11 @@ BackgroundPage.prototype = {
   }),
 
   shutdown() {
+    if (this.extension.addonData.instanceID) {
+      AddonManager.getAddonByInstanceID(this.extension.addonData.instanceID)
+                  .then(addon => addon.setDebugGlobal(null));
+    }
+
     // Navigate away from the background page to invalidate any
     // setTimeouts or other callbacks.
     if (this.webNav) {
@@ -114,11 +119,6 @@ BackgroundPage.prototype = {
     this.windowlessBrowser.loadURI("about:blank", 0, null, null, null);
     this.windowlessBrowser.close();
     this.windowlessBrowser = null;
-
-    if (this.extension.addonData.instanceID) {
-      AddonManager.getAddonByInstanceID(this.extension.addonData.instanceID)
-                  .then(addon => addon.setDebugGlobal(null));
-    }
   },
 };
 
