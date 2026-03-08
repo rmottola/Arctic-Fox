a1e7168eb9f1c59ff2986998c4573ca10b78f783
Riccardo Mottola <rm@gnu.org>
2026-03-08 20:22:39 +0100

    Bug 1310912 - Part 3. The selection into PlaceholderTransaction should be updated via RangeUpdater. r=masayuki
    Since the selection into PlaceholderTransaction isn't updated via RangeUpdater, UndoTransaction may return error when selection/caret position is changed.  Then, redo is failed.
    
    So we should add selection into PlaceholderTrancation to RangeUpdater.

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -932,6 +932,7 @@ EditorBase::BeginPlaceHolderTransaction(nsIAtom* aName)
     if (selection) {
       mSelState = new SelectionState();
       mSelState->SaveSelection(selection);
+      mRangeUpdater.RegisterSelectionState(*mSelState);
     }
   }
   mPlaceHolderBatch++;
@@ -983,6 +984,7 @@ EditorBase::EndPlaceHolderTransaction()
     if (mSelState) {
       // we saved the selection state, but never got to hand it to placeholder
       // (else we ould have nulled out this pointer), so destroy it to prevent leaks.
+      mRangeUpdater.DropSelectionState(*mSelState);
       delete mSelState;
       mSelState = nullptr;
     }
