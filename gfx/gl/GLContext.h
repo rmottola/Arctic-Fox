fd87878d76ec14326b6a853800292454aca6b08b
Riccardo Mottola <rm@gnu.org>
2026-03-14 21:33:04 +0100

    Bug 1288446 - Call glCheckFramebufferStatus after glFramebufferTexture2D to avoid driver crash; r=jrmuizel
    Some PowerVR SGX540 devices running Android 4.0 were crashing when
    uploading texture data with glTexSubImage2D. This was unearthed by a
    Skia upgrade which removed what should have been an unnecessary OpenGL
    call. This reinstates the OpenGL call which prevents the crash, but at a
    GLContext level rather than in Skia.

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -1972,6 +1972,9 @@ public:
         BEFORE_GL_CALL;
         mSymbols.fFramebufferTexture2D(target, attachmentPoint, textureTarget, texture, level);
         AFTER_GL_CALL;
+        if (mNeedsCheckAfterAttachTextureToFb) {
+            fCheckFramebufferStatus(target);
+        }
     }
 
     void fFramebufferTextureLayer(GLenum target, GLenum attachment, GLuint texture, GLint level, GLint layer) {
@@ -3536,6 +3539,7 @@ protected:
     bool mNeedsTextureSizeChecks;
     bool mNeedsFlushBeforeDeleteFB;
     bool mTextureAllocCrashesOnMapFailure;
+    bool mNeedsCheckAfterAttachTextureToFb;
     bool mWorkAroundDriverBugs;
 
     bool IsTextureSizeSafeToPassToDriver(GLenum target, GLsizei width, GLsizei height) const {
