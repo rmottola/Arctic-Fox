5aed4cdb3feea57eeb053e075966c389479c9e63
Riccardo Mottola <rm@gnu.org>
2026-03-15 20:39:07 +0100

    Bug 1308260 - Part 6: Toggling the grid highlighter should emit the node that the grid highlighter was shown or hidden for and update the grid's highlighted state. r=pbro

--- devtools/client/inspector/rules/views/text-property-editor.js
+++ devtools/client/inspector/rules/views/text-property-editor.js
@@ -735,7 +735,7 @@ TextPropertyEditor.prototype = {
     }
 
     if (this.isDisplayGrid()) {
-      this.ruleView.highlighters._hideGridHighlighter();
+      this.ruleView.highlighters.hideGridHighlighter();
     }
 
     // First, set this property value (common case, only modified a property)
