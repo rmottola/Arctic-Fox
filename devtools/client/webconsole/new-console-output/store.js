9bd2e3d3d6bb33dc6429d7c72812f7b5a077c6cf
Riccardo Mottola <rm@gnu.org>
2026-03-26 00:09:35 +0100

    Bug 1304900 - New console frontend: Add network messages filtering in the filter bar. r=linclark

--- devtools/client/webconsole/new-console-output/store.js
+++ devtools/client/webconsole/new-console-output/store.js
@@ -26,6 +26,8 @@ function configureStore() {
       warn: Services.prefs.getBoolPref("devtools.webconsole.filter.warn"),
       info: Services.prefs.getBoolPref("devtools.webconsole.filter.info"),
       log: Services.prefs.getBoolPref("devtools.webconsole.filter.log"),
+      network: Services.prefs.getBoolPref("devtools.webconsole.filter.network"),
+      netxhr: Services.prefs.getBoolPref("devtools.webconsole.filter.netxhr"),
     })
   };
 
