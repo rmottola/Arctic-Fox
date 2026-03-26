47bc09b1976bd07d2c0e56297129da8882812e42
Riccardo Mottola <rm@gnu.org>
2026-03-26 01:14:01 +0100

    Bug 1303590 - Remove Luciddream test suite r=aobreja

--- testing/mozharness/configs/unittests/linux_unittest.py
+++ testing/mozharness/configs/unittests/linux_unittest.py
@@ -95,27 +95,6 @@ config = {
             "run_filename": "jit_test.py",
             "testsdir": "jit-test/jit-test"
         },
-        "luciddream-emulator": {
-            "options": [
-                "--startup-timeout=300",
-                "--log-raw=%(raw_log_file)s",
-                "--log-errorsummary=%(error_summary_file)s",
-                "--browser-path=%(browser_path)s",
-                "--b2gpath=%(emulator_path)s",
-                "%(test_manifest)s"
-            ],
-        },
-        "luciddream-b2gdt": {
-            "options": [
-                "--startup-timeout=300",
-                "--log-raw=%(raw_log_file)s",
-                "--log-errorsummary=%(error_summary_file)s",
-                "--browser-path=%(browser_path)s",
-                "--b2g-desktop-path=%(fxos_desktop_path)s",
-                "--gaia-profile=%(gaia_profile)s",
-                "%(test_manifest)s"
-            ],
-        },
         "mochitest": {
             "options": [
                 "--appname=%(binary_path)s",
