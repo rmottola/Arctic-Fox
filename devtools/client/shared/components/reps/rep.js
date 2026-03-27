a34190fe1c216da3fccc9a8164facab3fdca8f2f
Riccardo Mottola <rm@gnu.org>
2026-03-27 10:11:52 +0100

    Bug 1298935 - Add a Rep mode proptype. r=linclark

--- devtools/client/shared/components/reps/rep.js
+++ devtools/client/shared/components/reps/rep.js
@@ -90,6 +90,7 @@ define(function (require, exports, module) {
     propTypes: {
       object: React.PropTypes.any,
       defaultRep: React.PropTypes.object,
+      mode: React.PropTypes.string
     },
 
     render: function () {
