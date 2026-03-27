8ddf396959636acb68e1aa392d882d17fd23e95f
Riccardo Mottola <rm@gnu.org>
2026-03-27 01:39:15 +0100

    Bug 1311246 - Remove support for the -moz-samplesize media fragment; r=jrmuizel
    This removes the feature added in bug 854795 for Tarako b2g devices.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5493,9 +5493,6 @@ pref("dom.wakelock.enabled", false);
 // The URL of the Firefox Accounts auth server backend
 pref("identity.fxaccounts.auth.uri", "https://api.accounts.firefox.com/v1");
 
-// disable mozsample size for now
-pref("image.mozsamplesize.enabled", false);
-
 // Enable navigator.sendBeacon on all platforms except b2g because it doesn't
 // play nicely with Firefox OS apps yet.
 #ifndef MOZ_WIDGET_GONK
