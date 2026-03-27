2acd072b4a3b544ac4d2eeaeb4d7544ac3db6bc5
Riccardo Mottola <rm@gnu.org>
2026-03-27 02:08:05 +0100

    Bug 1311669 - replace string literals containing escaped characters with raw string literals. r=valentin

--- netwerk/ipc/NeckoParent.cpp
+++ netwerk/ipc/NeckoParent.cpp
@@ -147,7 +147,7 @@ GetRequestingPrincipal(const FTPChannelCreationArgs& aArgs)
 static MOZ_NORETURN MOZ_COLD
 void CrashWithReason(const char * reason)
 {
-#ifndef RELEASE_BUILD
+#ifndef RELEASE_OR_BETA
   MOZ_CRASH_ANNOTATE(reason);
   MOZ_REALLY_CRASH();
 #endif
