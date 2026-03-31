da829b42b2619aa5e4fab40bfb44bdc58cbdfa92
Riccardo Mottola <rm@gnu.org>
2026-03-31 16:38:55 +0200

    Bug 1316736 - Clean up RDM typos. r=ochameau

--- devtools/client/responsive.html/browser/tunnel.js
+++ devtools/client/responsive.html/browser/tunnel.js
@@ -425,7 +425,7 @@ MessageManagerTunnel.prototype = {
     "InlineSpellChecker:",
     // Messages sent from pageinfo.js
     "PageInfo:",
-    // Messsage sent from printUtils.js
+    // Messages sent from printUtils.js
     "Printing:",
     // Messages sent from browser-social.js
     "Social:",
@@ -443,7 +443,7 @@ MessageManagerTunnel.prototype = {
     "Finder:",
     // Messages sent to pageinfo.js
     "PageInfo:",
-    // Messsage sent from printUtils.js
+    // Messages sent to printUtils.js
     "Printing:",
     // Messages sent to browser-social.js
     "Social:",
