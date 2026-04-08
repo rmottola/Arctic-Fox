72d1665fdb72d5b917bca20d66e476c84d0ecd93
Riccardo Mottola <rm@gnu.org>
2026-04-08 11:27:39 +0200

    Bug 1291375 - 5. Use Fennec bindings where needed; r=snorp
    Switch include statements from GeckoView bindings to Fennec bindings
    where needed.

--- toolkit/components/jsdownloads/src/DownloadPlatform.cpp
+++ toolkit/components/jsdownloads/src/DownloadPlatform.cpp
@@ -31,7 +31,7 @@
 #endif
 
 #ifdef MOZ_WIDGET_ANDROID
-#include "GeneratedJNIWrappers.h"
+#include "FennecJNIWrappers.h"
 #endif
 
 #ifdef MOZ_WIDGET_GTK
@@ -113,7 +113,7 @@ nsresult DownloadPlatform::DownloadDone(nsIURI* aSource, nsIURI* aReferrer, nsIF
       bool addToRecentDocs = Preferences::GetBool(PREF_BDM_ADDTORECENTDOCS);
 #ifdef MOZ_WIDGET_ANDROID
       if (addToRecentDocs) {
-        java::DownloadsIntegration::ScanMedia(path, NS_ConvertUTF8toUTF16(aContentType));
+        java::DownloadsIntegration::ScanMedia(path, aContentType);
       }
 #else
       if (addToRecentDocs && !aIsPrivate) {
