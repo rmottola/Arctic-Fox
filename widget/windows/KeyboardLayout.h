906655cfda8d5e404a8138b230b999419b64296f
Riccardo Mottola <rm@gnu.org>
2026-04-09 17:08:24 +0200

    Bug 1306549 part.6 Create KeyboardLayout::ActivateDeadKeyState() r=m_kato
    There is DeactivateDeadKeyState() but not ActivateDeadKeyState().  There should be both of them and hides the dead key state management into them.

--- widget/windows/KeyboardLayout.h
+++ widget/windows/KeyboardLayout.h
@@ -657,7 +657,13 @@ private:
                                   uint16_t aShiftStatesWithBaseChars,
                                   DeadKeyEntry* aDeadKeyArray,
                                   uint32_t aMaxEntries);
+  /**
+   * Activates or deactivates dead key state.
+   */
+  void ActivateDeadKeyState(const NativeKey& aNativeKey,
+                            const ModifierKeyState& aModKeyState);
   void DeactivateDeadKeyState();
+
   const DeadKeyTable* AddDeadKeyTable(const DeadKeyEntry* aDeadKeyArray,
                                       uint32_t aEntries);
   void ReleaseDeadKeyTables();
