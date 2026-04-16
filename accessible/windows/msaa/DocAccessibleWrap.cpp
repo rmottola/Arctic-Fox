ecec9aa0f592eb95a9f87f1408106e84bde1204f
Riccardo Mottola <rm@gnu.org>
2026-04-16 21:08:56 +0200

    Bug 1206711 - remove unused ipc bits from DocAccessibleWrap::DoInitialUpdate. r=surkov

--- accessible/windows/msaa/DocAccessibleWrap.cpp
+++ accessible/windows/msaa/DocAccessibleWrap.cpp
@@ -9,7 +9,6 @@
 #include "Compatibility.h"
 #include "DocAccessibleChild.h"
 #include "nsWinUtils.h"
-#include "mozilla/dom/TabChild.h"
 #include "Role.h"
 #include "RootAccessible.h"
 #include "sdnDocAccessible.h"
@@ -136,18 +135,7 @@ DocAccessibleWrap::DoInitialUpdate()
   if (nsWinUtils::IsWindowEmulationStarted()) {
     // Create window for tab document.
     if (mDocFlags & eTabDocument) {
-      mozilla::dom::TabChild* tabChild =
-        mozilla::dom::TabChild::GetFrom(mDocumentNode->GetShell());
-
       a11y::RootAccessible* rootDocument = RootAccessible();
-
-      mozilla::WindowsHandle nativeData = 0;
-      if (tabChild)
-        tabChild->SendGetWidgetNativeData(&nativeData);
-      else
-        nativeData = reinterpret_cast<mozilla::WindowsHandle>(
-          rootDocument->GetNativeWindow());
-
       bool isActive = true;
       nsIntRect rect(CW_USEDEFAULT, CW_USEDEFAULT, 0, 0);
       if (Compatibility::IsDolphin()) {
@@ -161,7 +149,7 @@ DocAccessibleWrap::DoInitialUpdate()
         docShell->GetIsActive(&isActive);
       }
 
-      HWND parentWnd = reinterpret_cast<HWND>(nativeData);
+      HWND parentWnd = reinterpret_cast<HWND>(rootDocument->GetNativeWindow());
       mHWND = nsWinUtils::CreateNativeWindow(kClassNameTabContent, parentWnd,
                                              rect.x, rect.y,
                                              rect.width, rect.height, isActive);
