a55c4ee348ba8461f1552fe28de1dea9141aad67
Riccardo Mottola <rm@gnu.org>
2026-04-25 00:56:30 +0200

    Bug 1306210 - Expose the principal of the device request. r=smaug

--- dom/presentation/PresentationRequest.cpp
+++ dom/presentation/PresentationRequest.cpp
@@ -227,6 +227,7 @@ PresentationRequest::StartWithDevice(const nsAString& aDeviceId,
   // process.
   nsCOMPtr<nsIDOMEventTarget> handler =
     do_QueryInterface(GetOwner()->GetChromeEventHandler());
+  nsCOMPtr<nsIPrincipal> principal = doc->NodePrincipal();
   nsCOMPtr<nsIPresentationServiceCallback> callback =
     new PresentationRequesterCallback(this, id, promise);
   nsCOMPtr<nsIPresentationTransportBuilderConstructor> constructor =
@@ -237,6 +238,7 @@ PresentationRequest::StartWithDevice(const nsAString& aDeviceId,
                              aDeviceId,
                              GetOwner()->WindowID(),
                              handler,
+                             principal,
                              callback,
                              constructor);
   if (NS_WARN_IF(NS_FAILED(rv))) {
