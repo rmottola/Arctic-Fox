5fffa4c4cf709ecb65ab51ce3d2a982b03b9b312
Riccardo Mottola <rm@gnu.org>
2026-04-29 23:23:53 +0200

    Bug 1318971 - Update !MOZ_WEBSPEECH case in ContentParent::RecvPSpeechSynthesisConstructor to return mozilla::ipc::IPCResult. r=kanru

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3153,7 +3153,7 @@ ContentParent::RecvPSpeechSynthesisConstructor(PSpeechSynthesisParent* aActor)
 #ifdef MOZ_WEBSPEECH
   return IPC_OK();
 #else
-  return false;
+  return IPC_FAIL_NO_REASON(this);
 #endif
 }
 
