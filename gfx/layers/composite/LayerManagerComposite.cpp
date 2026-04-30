fab0b0b09d673dc44dfda1fde37bdf2bea33b59f
Riccardo Mottola <rm@gnu.org>
2026-04-30 15:04:56 +0200

    Bug 1300121 - Flush the D3D11 immediate context if a composition is cancelled to avoid resources queing up in the driver. r=Bas

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -157,6 +157,7 @@ LayerManagerComposite::Destroy()
     if (mRoot) {
       RootLayer()->Destroy();
     }
+    mCompositor->CancelFrame();
     mRoot = nullptr;
     mClonedLayerTreeProperties = nullptr;
     mDestroyed = true;
@@ -1337,6 +1338,9 @@ LayerManagerComposite::AutoAddMaskEffect::~AutoAddMaskEffect()
 void
 LayerManagerComposite::ChangeCompositor(Compositor* aNewCompositor)
 {
+  if (mCompositor) {
+    mCompositor->CancelFrame();
+  }
   mCompositor = aNewCompositor;
   mTextRenderer = new TextRenderer(aNewCompositor);
   mTwoPassTmpTarget = nullptr;
