90f8282a4d3dbf77a3c239b5e3c614fe525df945
Riccardo Mottola <rm@gnu.org>
2026-05-09 22:19:13 +0200

    Bug 1313635 part 1 - don't go through MediaDecoderReaderWrapper to set start time; r=jwwang,jya

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -772,6 +772,9 @@ MediaFormatReader::MaybeResolveMetadataPromise()
     mInfo.mStartTime = startTime; // mInfo.mStartTime is initialized to 0.
   }
 
+  mHasStartTime = true;
+  UpdateBuffered();
+
   RefPtr<MetadataHolder> metadata = new MetadataHolder();
   metadata->mInfo = mInfo;
   metadata->mTags = mTags->Count() ? mTags.release() : nullptr;
@@ -2167,16 +2170,10 @@ MediaFormatReader::GetBuffered()
   media::TimeIntervals audioti;
   media::TimeIntervals intervals;
 
-  if (!mInitDone) {
+  if (!mInitDone || !mHasStartTime) {
     return intervals;
   }
-  int64_t startTime = 0;
-  if (!ForceZeroStartTime()) {
-    if (!HaveStartTime()) {
-      return intervals;
-    }
-    startTime = StartTime();
-  }
+
   // Ensure we have up to date buffered time range.
   if (HasVideo()) {
     UpdateReceivedNewData(TrackType::kVideoTrack);
@@ -2203,7 +2200,7 @@ MediaFormatReader::GetBuffered()
     // IntervalSet already starts at 0 or is empty, nothing to shift.
     return intervals;
   }
-  return intervals.Shift(media::TimeUnit::FromMicroseconds(-startTime));
+  return intervals.Shift(media::TimeUnit() - mInfo.mStartTime);
 }
 
 // For the MediaFormatReader override we need to force an update to the
