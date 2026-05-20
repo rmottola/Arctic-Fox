020f6d19b100ecb93d7a64eb9874c6167861daaa
Riccardo Mottola <rm@gnu.org>
2026-05-20 23:04:21 +0200

    Bug 1304883: Modify mscom::MainThreadInvoker and Win32 widget to use atomic boolean to flag pending APCs; r=jimm

--- widget/windows/WinUtils.h
+++ widget/windows/WinUtils.h
@@ -510,6 +510,10 @@ public:
    */
   static bool GetAppInitDLLs(nsAString& aOutput);
 
+#ifdef ACCESSIBILITY
+  static void SetAPCPending();
+#endif
+
 private:
   typedef HRESULT (WINAPI * SHCreateItemFromParsingNamePtr)(PCWSTR pszPath,
                                                             IBindCtx *pbc,
