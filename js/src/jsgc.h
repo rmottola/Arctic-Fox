ce33640dfb5fbac0d2b1b048e95558e682f76f4a
Riccardo Mottola <rm@gnu.org>
2026-05-27 20:08:29 +0200

    Bug 1318627 - Remove Zone::active and related infrastructure. r=jonco, f=bhackett

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -835,9 +835,6 @@ const size_t MAX_EMPTY_CHUNK_AGE = 4;
 class InterpreterFrame;
 
 extern void
-MarkCompartmentActive(js::InterpreterFrame* fp);
-
-extern void
 TraceRuntime(JSTracer* trc);
 
 extern void
