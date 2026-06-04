d88c9652f9d5b8cd0e8e2bfd5b9eba259098eee3
Valery Ushakov <uwe@stderr.spb.ru>
2026-06-04 19:03:12 +0300

    mozconfig: ignore more shell variables
    NetBSD shell provides LINENO and RANDOM variables that are volatile
    and should be ignored.

--- python/mozbuild/mozbuild/mozconfig.py
+++ python/mozbuild/mozbuild/mozconfig.py
@@ -74,7 +74,7 @@ class MozconfigLoader(object):
     DEPRECATED_TOPSRCDIR_PATHS = ('mozconfig.sh', 'myconfig.sh')
     DEPRECATED_HOME_PATHS = ('.mozconfig', '.mozconfig.sh', '.mozmyconfig.sh')
 
-    IGNORE_SHELL_VARIABLES = {'_'}
+    IGNORE_SHELL_VARIABLES = {'_', 'LINENO', 'RANDOM'}
 
     ENVIRONMENT_VARIABLES = {
         'CC', 'CXX', 'CFLAGS', 'CXXFLAGS', 'LDFLAGS', 'MOZ_OBJDIR',
