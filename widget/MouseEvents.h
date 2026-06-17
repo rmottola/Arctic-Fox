ea39a36993dd26535405b1551178e20ae4beb8ac
Riccardo Mottola <rm@gnu.org>
2026-06-17 08:24:28 +0200

    Bug 1304315 - [Pointer Event] Make all pointer events's default width/height to 1. f=bevistseng. r=smaug,masayuki.

--- widget/MouseEvents.h
+++ widget/MouseEvents.h
@@ -671,8 +671,8 @@ class WidgetPointerEvent : public WidgetMouseEvent
   friend class mozilla::dom::PBrowserChild;
 
   WidgetPointerEvent()
-    : mWidth(0)
-    , mHeight(0)
+    : mWidth(1)
+    , mHeight(1)
     , mIsPrimary(true)
   {
   }
@@ -682,8 +682,8 @@ public:
 
   WidgetPointerEvent(bool aIsTrusted, EventMessage aMsg, nsIWidget* w)
     : WidgetMouseEvent(aIsTrusted, aMsg, w, ePointerEventClass, eReal)
-    , mWidth(0)
-    , mHeight(0)
+    , mWidth(1)
+    , mHeight(1)
     , mIsPrimary(true)
   {
   }
