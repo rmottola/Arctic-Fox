9688be42699eb8702c4a0dfddac28cce8386c45f
Riccardo Mottola <rm@gnu.org>
2026-06-26 14:18:40 +0200

    Bug 1329857 - Remove NS_METHOD and NS_CALLBACK. r=froydnj.
    Bug 1295053 removed most uses of NS_METHOD and NS_CALLBACK, but one use was
    unintentionally left behind (in the XPIDL parser) and another has since crept
    in (in MediaDrmCDMProxy.h).
    
    So this patch removes NS_METHOD and NS_CALLBACK. NS_METHOD_(nsresult) and
    NS_CALLBACK_(nsresult, T) can still be used for the same purpose, but those
    alternatives are less likely to be used unintentionally.

--- xpcom/threads/nsTimerImpl.cpp
+++ xpcom/threads/nsTimerImpl.cpp
@@ -245,7 +245,7 @@ nsTimerImpl::InitWithFuncCallbackCommon(nsTimerCallbackFunc aFunc,
   return InitCommon(aDelay, aType);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::InitWithFuncCallback(nsTimerCallbackFunc aFunc,
                                   void* aClosure,
                                   uint32_t aDelay,
@@ -255,7 +255,7 @@ nsTimerImpl::InitWithFuncCallback(nsTimerCallbackFunc aFunc,
   return InitWithFuncCallbackCommon(aFunc, aClosure, aDelay, aType, name);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::InitWithNamedFuncCallback(nsTimerCallbackFunc aFunc,
                                        void* aClosure,
                                        uint32_t aDelay,
@@ -266,7 +266,7 @@ nsTimerImpl::InitWithNamedFuncCallback(nsTimerCallbackFunc aFunc,
   return InitWithFuncCallbackCommon(aFunc, aClosure, aDelay, aType, name);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::InitWithNameableFuncCallback(nsTimerCallbackFunc aFunc,
                                           void* aClosure,
                                           uint32_t aDelay,
@@ -277,7 +277,7 @@ nsTimerImpl::InitWithNameableFuncCallback(nsTimerCallbackFunc aFunc,
   return InitWithFuncCallbackCommon(aFunc, aClosure, aDelay, aType, name);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::InitWithCallback(nsITimerCallback* aCallback,
                               uint32_t aDelay,
                               uint32_t aType)
@@ -294,7 +294,7 @@ nsTimerImpl::InitWithCallback(nsITimerCallback* aCallback,
   return InitCommon(aDelay, aType);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::Init(nsIObserver* aObserver, uint32_t aDelay, uint32_t aType)
 {
   if (NS_WARN_IF(!aObserver)) {
@@ -309,7 +309,7 @@ nsTimerImpl::Init(nsIObserver* aObserver, uint32_t aDelay, uint32_t aType)
   return InitCommon(aDelay, aType);
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::Cancel()
 {
 
@@ -336,7 +336,7 @@ nsTimerImpl::Neuter()
   ++mGeneration;
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::SetDelay(uint32_t aDelay)
 {
   if (mCallbackType == CallbackType::Unknown && mType == nsITimer::TYPE_ONE_SHOT) {
@@ -361,14 +361,14 @@ nsTimerImpl::SetDelay(uint32_t aDelay)
   return NS_OK;
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::GetDelay(uint32_t* aDelay)
 {
   *aDelay = mDelay;
   return NS_OK;
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::SetType(uint32_t aType)
 {
   mType = (uint8_t)aType;
@@ -378,7 +378,7 @@ nsTimerImpl::SetType(uint32_t aType)
   return NS_OK;
 }
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::GetType(uint32_t* aType)
 {
   *aType = mType;
@@ -386,7 +386,7 @@ nsTimerImpl::GetType(uint32_t* aType)
 }
 
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::GetClosure(void** aClosure)
 {
   *aClosure = mClosure;
@@ -394,7 +394,7 @@ nsTimerImpl::GetClosure(void** aClosure)
 }
 
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::GetCallback(nsITimerCallback** aCallback)
 {
   if (mCallbackType == CallbackType::Interface) {
@@ -407,7 +407,7 @@ nsTimerImpl::GetCallback(nsITimerCallback** aCallback)
 }
 
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::GetTarget(nsIEventTarget** aTarget)
 {
   NS_IF_ADDREF(*aTarget = mEventTarget);
@@ -415,7 +415,7 @@ nsTimerImpl::GetTarget(nsIEventTarget** aTarget)
 }
 
 
-NS_IMETHODIMP
+nsresult
 nsTimerImpl::SetTarget(nsIEventTarget* aTarget)
 {
   if (NS_WARN_IF(mCallbackType != CallbackType::Unknown)) {
