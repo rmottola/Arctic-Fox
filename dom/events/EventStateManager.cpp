33e88767ea12464c5b1fc6cac367897b212e145e
Riccardo Mottola <rm@gnu.org>
2026-07-10 00:22:50 +0200

    Bug 1313169 - Remove dom/inputmethod/ and related code. r=Ehsan

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -2839,8 +2839,7 @@ EventStateManager::PostHandleKeyboardEvent(WidgetKeyboardEvent* aKeyboardEvent,
                                            nsEventStatus& aStatus,
                                            bool dispatchedToContentProcess)
 {
-  if (aStatus == nsEventStatus_eConsumeNoDefault ||
-      aKeyboardEvent->mInputMethodAppState == WidgetKeyboardEvent::eHandling) {
+  if (aStatus == nsEventStatus_eConsumeNoDefault) {
     return;
   }
 
