5c7484caf2e39e14e05aa3cbb647aaf2202b3252
Riccardo Mottola <rm@gnu.org>
2026-07-10 12:44:51 +0200

    Bug 1325028 part 5.  Change nsGlonalWindow::CanMoveResizeWindows to take CallerType.  r=bkelly

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -6872,12 +6872,12 @@ nsGlobalWindow::MakeScriptDialogTitle(nsAString& aOutTitle,
 }
 
 bool
-nsGlobalWindow::CanMoveResizeWindows(bool aCallerIsChrome)
+nsGlobalWindow::CanMoveResizeWindows(CallerType aCallerType)
 {
   MOZ_ASSERT(IsOuterWindow());
 
   // When called from chrome, we can avoid the following checks.
-  if (!aCallerIsChrome) {
+  if (aCallerType != CallerType::System) {
     // Don't allow scripts to move or resize windows that were not opened by a
     // script.
     if (!mHadOriginalOpener) {
@@ -7531,7 +7531,7 @@ nsGlobalWindow::MoveToOuter(int32_t aXPos, int32_t aYPos,
    * prevent window.moveTo() by exiting early
    */
 
-  if (!CanMoveResizeWindows(aCallerType == CallerType::System) || IsFrame()) {
+  if (!CanMoveResizeWindows(aCallerType) || IsFrame()) {
     return;
   }
 
@@ -7599,7 +7599,7 @@ nsGlobalWindow::MoveByOuter(int32_t aXDif, int32_t aYDif,
    * prevent window.moveBy() by exiting early
    */
 
-  if (!CanMoveResizeWindows(aCallerType == CallerType::System) || IsFrame()) {
+  if (!CanMoveResizeWindows(aCallerType) || IsFrame()) {
     return;
   }
 
@@ -7678,7 +7678,7 @@ nsGlobalWindow::ResizeToOuter(int32_t aWidth, int32_t aHeight,
    * prevent window.resizeTo() by exiting early
    */
 
-  if (!CanMoveResizeWindows(aCallerType == CallerType::System) || IsFrame()) {
+  if (!CanMoveResizeWindows(aCallerType) || IsFrame()) {
     return;
   }
 
@@ -7737,7 +7737,7 @@ nsGlobalWindow::ResizeByOuter(int32_t aWidthDif, int32_t aHeightDif,
    * prevent window.resizeBy() by exiting early
    */
 
-  if (!CanMoveResizeWindows(aCallerType == CallerType::System) || IsFrame()) {
+  if (!CanMoveResizeWindows(aCallerType) || IsFrame()) {
     return;
   }
 
@@ -7794,7 +7794,7 @@ nsGlobalWindow::SizeToContentOuter(CallerType aCallerType, ErrorResult& aError)
    * prevent window.sizeToContent() by exiting early
    */
 
-  if (!CanMoveResizeWindows(aCallerType == CallerType::System) || IsFrame()) {
+  if (!CanMoveResizeWindows(aCallerType) || IsFrame()) {
     return;
   }
 
@@ -14057,7 +14057,7 @@ nsGlobalWindow::SetReplaceableWindowCoord(JSContext* aCx,
    */
   nsGlobalWindow* outer = GetOuterWindowInternal();
   if (!outer ||
-      !outer->CanMoveResizeWindows(aCallerType == CallerType::System) ||
+      !outer->CanMoveResizeWindows(aCallerType) ||
       outer->IsFrame()) {
     RedefineProperty(aCx, aPropName, aValue, aError);
     return;
