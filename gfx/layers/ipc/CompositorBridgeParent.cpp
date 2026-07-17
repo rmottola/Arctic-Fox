8723167f0a554eca324d528887790191a6f27960
Riccardo Mottola <rm@gnu.org>
2026-07-17 13:29:37 +0200

    Bug 1330037 - Propagate the CompositorOptions to all CompositorWidget instances. r=dvander

--- gfx/layers/ipc/CompositorBridgeParent.cpp
+++ gfx/layers/ipc/CompositorBridgeParent.cpp
@@ -1661,7 +1661,7 @@ CompositorBridgeParent::AllocPCompositorWidgetParent(const CompositorWidgetInitD
   }
 
   widget::CompositorWidgetParent* widget =
-    new widget::CompositorWidgetParent(aInitData);
+    new widget::CompositorWidgetParent(aInitData, mOptions);
   widget->AddRef();
 
   // Sending the constructor acts as initialization as well.
