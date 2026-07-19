e356eb11fe6a347ae20c69eb90d6ef7447479bad
Riccardo Mottola <rm@gnu.org>
2026-07-19 23:42:31 +0200

    Bug 1331193 P2 Don't leave TabChild in inconsistent state if init fails. r=billm

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -2468,7 +2468,6 @@ TabChild::InitTabChildGlobal()
     NS_ENSURE_TRUE(chromeHandler, false);
 
     RefPtr<TabChildGlobal> scope = new TabChildGlobal(this);
-    mTabChildGlobal = scope;
 
     nsISupports* scopeSupports = NS_ISUPPORTS_CAST(EventTarget*, scope);
 
@@ -2480,6 +2479,8 @@ TabChild::InitTabChildGlobal()
     nsCOMPtr<nsPIWindowRoot> root = do_QueryInterface(chromeHandler);
     NS_ENSURE_TRUE(root, false);
     root->SetParentTarget(scope);
+
+    mTabChildGlobal = scope.forget();;
   }
 
   if (!mTriedBrowserInit) {
