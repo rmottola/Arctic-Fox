23aab507d67c7f3394dded2c960f980655e73077
Riccardo Mottola <rm@gnu.org>
2026-07-20 11:41:45 +0200

    Bug 1331484 - Removed mentions of Adobe from browser-media.js - r=Gijs
    We do not support Adobe Primetime anymore, so there is no need to handle
    related issues.

--- browser/base/content/browser-media.js
+++ browser/base/content/browser-media.js
@@ -194,21 +194,6 @@ let gDecoderDoctorHandler = {
   },
 
   getLabelForNotificationBox(type) {
-    if (type == "adobe-cdm-not-found" &&
-        AppConstants.platform == "win") {
-      if (AppConstants.isPlatformAndVersionAtMost("win", "5.9")) {
-        // We supply our own Learn More button so we don't need to populate the message here.
-        return gNavigatorBundle.getFormattedString("emeNotifications.drmContentDisabled.message", [""]);
-      }
-      return gNavigatorBundle.getString("decoder.noCodecs.message");
-    }
-    if (type == "adobe-cdm-not-activated" &&
-        AppConstants.platform == "win") {
-      if (AppConstants.isPlatformAndVersionAtMost("win", "5.9")) {
-        return gNavigatorBundle.getString("decoder.noCodecsXP.message");
-      }
-      return gNavigatorBundle.getString("decoder.noCodecs.message");
-    }
     if (type == "platform-decoder-not-found") {
       if (AppConstants.isPlatformAndVersionAtLeast("win", "6")) {
         return gNavigatorBundle.getString("decoder.noHWAcceleration.message");
