1d0a1ab2b5eb143c1b663170a6b1a8ceda3c534b
Riccardo Mottola <rm@gnu.org>
2026-07-20 11:58:36 +0200

    Bug 1332530 - Remove librlz and EME/GMP device binding code. r=gerald

--- dom/media/gmp/widevine-adapter/WidevineAdapter.cpp
+++ dom/media/gmp/widevine-adapter/WidevineAdapter.cpp
@@ -148,12 +148,6 @@ WidevineAdapter::GMPShutdown()
   }
 }
 
-void
-WidevineAdapter::GMPSetNodeId(const char* aNodeId, uint32_t aLength)
-{
-
-}
-
 /* static */
 bool
 WidevineAdapter::Supports(int32_t aModuleVersion,
