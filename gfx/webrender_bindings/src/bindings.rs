a939837dc57133afd694efd413ba12ee8a22b412
Riccardo Mottola <rm@gnu.org>
2026-07-22 17:12:25 +0200

    Bug 1320320 - flush and wait the last frame's data before webrender render() call. r=jmuizelaar?

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -426,6 +426,8 @@ pub extern fn wr_pop_dl_builder(state: &mut WrState, bounds: WrRect, overflow: W
 #[no_mangle]
 pub fn wr_composite_window(window: &mut WrWindowState) {
     assert!( unsafe { is_in_compositor_thread() });
+    window.api.flush();
+    wait_for_flush_notification(&window.flush_notifier_lock);
     gl::clear(gl::COLOR_BUFFER_BIT);
     window.renderer.update();
     window.renderer.render(window.size);
