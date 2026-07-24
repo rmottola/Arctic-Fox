2ee6927fca52096ee1907233e1cd6e6c959d4fc5
Riccardo Mottola <rm@gnu.org>
2026-07-24 13:00:38 +0200

    Bug 1328602 - Start implementing the render thread. r=gfx?

--- gfx/layers/wr/WebRenderBridgeParent.h
+++ gfx/layers/wr/WebRenderBridgeParent.h
@@ -108,6 +108,8 @@ public:
   void SendPendingAsyncMessages() override;
   void SetAboutToSendAsyncMessages() override;
 
+  void DidComposite(uint64_t aTransactionId, TimeStamp aStart, TimeStamp aEnd);
+
 private:
   virtual ~WebRenderBridgeParent();
 
