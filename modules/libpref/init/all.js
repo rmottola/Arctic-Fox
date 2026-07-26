e3e7deb4b205d0cbf74c05c6b5db95a90d0f1974
Riccardo Mottola <rm@gnu.org>
2026-07-26 23:42:11 +0200

    Bug 1322504 - Part3. Disable RotationBuffer when enabling WR. r=sotaro

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5007,7 +5007,11 @@ pref("layers.offmainthreadcomposition.async-animations", true);
 // Whether to log information about off main thread animations to stderr
 pref("layers.offmainthreadcomposition.log-animations", false);
 
+#ifdef MOZ_ENABLE_WEBRENDER
+pref("layers.bufferrotation.enabled", false);
+#else
 pref("layers.bufferrotation.enabled", true);
+#endif
 
 pref("layers.componentalpha.enabled", true);
 pref("layers.draw-mask-debug", false);
