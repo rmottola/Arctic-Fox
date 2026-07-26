6367fb7aed2f51914d5341a588849946c9c6e509
Riccardo Mottola <rm@gnu.org>
2026-07-27 00:03:46 +0200

    Bug 1331515 - Replace the usage of some of the WebRender ffi types by their strongly typed equivalents. r=gfx?

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -1155,7 +1155,7 @@ CompositorBridgeChild::HandleFatalError(const char* aName, const char* aMsg) con
 }
 
 PWebRenderBridgeChild*
-CompositorBridgeChild::AllocPWebRenderBridgeChild(const uint64_t& aPipelineId, TextureFactoryIdentifier*)
+CompositorBridgeChild::AllocPWebRenderBridgeChild(const wr::PipelineId& aPipelineId, TextureFactoryIdentifier*)
 {
   WebRenderBridgeChild* child = new WebRenderBridgeChild(aPipelineId);
   child->AddIPDLReference();
--- gfx/layers/wr/WebRenderLayerManager.cpp
+++ gfx/layers/wr/WebRenderLayerManager.cpp
@@ -114,10 +114,10 @@ WebRenderLayer::buildMaskLayer() {
           gfx::IntSize size = surface->GetSize();
           MOZ_RELEASE_ASSERT(surface->GetFormat() == SurfaceFormat::A8, "bad format");
           wr::ByteBuffer buf(size.height * map.GetStride(), map.GetData());
-          WrImageKey maskKey;
-          WrBridge()->SendAddImage(size.width, size.height, map.GetStride(), A8, buf, &maskKey);
+          wr::ImageKey maskKey;
+          WrBridge()->SendAddImage(size, map.GetStride(), SurfaceFormat::A8, buf, &maskKey);
 
-          imageMask.image = maskKey;
+          imageMask.image = maskKey.mHandle;
           imageMask.rect = wr::ToWrRect(Rect(0, 0, size.width, size.height));
           imageMask.repeat = false;
           WrManager()->AddImageKeyForDiscard(maskKey);
@@ -198,12 +198,12 @@ WebRenderLayerManager::Initialize(PCompositorBridgeChild* aCBChild,
   MOZ_ASSERT(aTextureFactoryIdentifier);
 
   TextureFactoryIdentifier textureFactoryIdentifier;
-  PWebRenderBridgeChild* bridge = aCBChild->SendPWebRenderBridgeConstructor(aLayersId,
+  PWebRenderBridgeChild* bridge = aCBChild->SendPWebRenderBridgeConstructor(wr::PipelineId(aLayersId),
                                                                             &textureFactoryIdentifier);
   MOZ_ASSERT(bridge);
   mWrChild = static_cast<WebRenderBridgeChild*>(bridge);
   LayoutDeviceIntSize size = mWidget->GetClientSize();
-  WrBridge()->SendCreate(size.width, size.height);
+  WrBridge()->SendCreate(size.ToUnknownSize());
   WrBridge()->IdentifyTextureHost(textureFactoryIdentifier);
   *aTextureFactoryIdentifier = textureFactoryIdentifier;
 }
@@ -287,7 +287,7 @@ WebRenderLayerManager::EndTransaction(DrawPaintedLayerCallback aCallback,
   mAnimationReadyTime = TimeStamp::Now();
 
   LayoutDeviceIntSize size = mWidget->GetClientSize();
-  if (!WrBridge()->DPBegin(size.width, size.height)) {
+  if (!WrBridge()->DPBegin(size.ToUnknownSize())) {
     return;
   }
 
@@ -368,7 +368,7 @@ WebRenderLayerManager::MakeSnapshotIfRequired(LayoutDeviceIntSize aSize)
 }
 
 void
-WebRenderLayerManager::AddImageKeyForDiscard(WrImageKey key)
+WebRenderLayerManager::AddImageKeyForDiscard(wr::ImageKey key)
 {
   mImageKeys.push_back(key);
 }
