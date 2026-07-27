43fd14bcd644be9b8e0dd9a345d8714d89d6282e
Riccardo Mottola <rm@gnu.org>
2026-07-27 23:17:07 +0200

    Bug 1331676 - Avoid doing sync IPC to the parent process for getting gfx feature status; r=dvander

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -463,6 +463,8 @@ public:
                                                             const bool& aIsAudio,
                                                             const bool& aIsVideo) override;
 
+  virtual mozilla::ipc::IPCResult RecvGetGfxInfoFeatureStatus(nsTArray<mozilla::dom::GfxInfoFeatureStatus>* aFS) override;
+
   bool CycleCollectWithLogs(bool aDumpAllTraces,
                             nsICycleCollectorLogSink* aSink,
                             nsIDumpGCAndCCLogsCallback* aCallback);
@@ -1016,11 +1018,6 @@ private:
                                                           uint64_t* aId) override;
 
   virtual mozilla::ipc::IPCResult RecvDeallocateLayerTreeId(const uint64_t& aId) override;
-
-  virtual mozilla::ipc::IPCResult RecvGetGraphicsFeatureStatus(const int32_t& aFeature,
-                                                               int32_t* aStatus,
-                                                               nsCString* aFailureId,
-                                                               bool* aSuccess) override;
 
   virtual mozilla::ipc::IPCResult RecvGraphicsError(const nsCString& aError) override;
 
