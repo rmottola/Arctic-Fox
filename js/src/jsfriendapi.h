dcaca31242227728231e4b9545b9884b0b80278c
Riccardo Mottola <rm@gnu.org>
2026-07-30 16:11:33 +0200

    Bug 1331662 part 2 - Replace nsJSUtils::EvaluateString calls by ExecutionContext scopes. r=bz

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -684,6 +684,9 @@ GetPrototypeNoProxy(JSObject* obj);
 JS_FRIEND_API(void)
 AssertSameCompartment(JSContext* cx, JSObject* obj);
 
+JS_FRIEND_API(void)
+AssertSameCompartment(JSContext* cx, JS::HandleValue v);
+
 #ifdef JS_DEBUG
 JS_FRIEND_API(void)
 AssertSameCompartment(JSObject* objA, JSObject* objB);
