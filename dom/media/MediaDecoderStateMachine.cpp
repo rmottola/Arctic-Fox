f2109820b0660f5ede9da7c0bc157e7821823ac4
Riccardo Mottola <rm@gnu.org>
2026-07-31 14:52:23 +0200

    Bug 1324339. Part 5 - remove |mSeekedVideoData|. r=kaku

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1341,7 +1341,7 @@ private:
     MOZ_ASSERT(!mSeekJob.mPromise.IsEmpty(), "Seek shouldn't be finished");
 
     if (aVideo->mTime > mCurrentTime) {
-      mSeekedVideoData = aVideo;
+      mMaster->Push(aVideo);
     }
 
     if (NeedMoreVideo()) {
@@ -1456,10 +1456,6 @@ private:
 
   void OnSeekTaskResolved()
   {
-    if (mSeekedVideoData) {
-      mMaster->Push(mSeekedVideoData);
-    }
-
     if (mIsAudioQueueFinished) {
       AudioQueue().Finish();
     }
@@ -1493,7 +1489,6 @@ private:
   {
     // Need to request video when we have none and video queue is not finished.
     return VideoQueue().GetSize() == 0 &&
-           !mSeekedVideoData &&
            !VideoQueue().IsFinished() &&
            !mIsVideoQueueFinished;
   }
@@ -1524,8 +1519,6 @@ private:
     RefPtr<MediaData> data = VideoQueue().PeekFront();
     if (data) {
       mSeekJob.mTarget->SetTime(TimeUnit::FromMicroseconds(data->mTime));
-    } else if (mSeekedVideoData) {
-      mSeekJob.mTarget->SetTime(TimeUnit::FromMicroseconds(mSeekedVideoData->mTime));
     } else if (mIsVideoQueueFinished || VideoQueue().AtEndOfStream()) {
       mSeekJob.mTarget->SetTime(mDuration);
     } else {
@@ -1557,7 +1550,6 @@ private:
   /*
    * Information which are going to be returned to MDSM.
    */
-  RefPtr<MediaData> mSeekedVideoData;
   bool mIsAudioQueueFinished = false;
   bool mIsVideoQueueFinished = false;
 };
