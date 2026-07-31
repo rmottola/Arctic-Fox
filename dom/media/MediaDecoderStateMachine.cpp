9db75972bb3da124a6afe97c405bbd29e388f48c
Riccardo Mottola <rm@gnu.org>
2026-07-31 17:16:35 +0200

    Bug 1319295. Part 2 - reject the seek promise with WAITING_FOR_DATA so MDSM has a chance to call WaitForData() and seek again. r=jya

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -855,6 +855,8 @@ public:
 
     // Disconnect MediaDecoderReaderWrapper.
     mSeekRequest.DisconnectIfExists();
+
+    mWaitRequest.DisconnectIfExists();
   }
 
   void HandleAudioDecoded(MediaData* aAudio) override
@@ -987,6 +989,19 @@ public:
   }
 
 private:
+  void DemuxerSeek()
+  {
+    // Request the demuxer to perform seek.
+    mSeekRequest.Begin(Reader()->Seek(mSeekJob.mTarget.ref())
+      ->Then(OwnerThread(), __func__,
+             [this] (const media::TimeUnit& aUnit) {
+               OnSeekResolved(aUnit);
+             },
+             [this] (const SeekRejectValue& aReject) {
+               OnSeekRejected(aReject);
+             }));
+  }
+
   void DoSeek() override
   {
     mDoneAudioSeeking = !Info().HasAudio() || mSeekJob.mTarget->IsVideoOnly();
@@ -998,15 +1013,7 @@ private:
       mMaster->Reset();
     }
 
-    // Request the demuxer to perform seek.
-    mSeekRequest.Begin(Reader()->Seek(mSeekJob.mTarget.ref())
-      ->Then(OwnerThread(), __func__,
-             [this] (media::TimeUnit aUnit) {
-               OnSeekResolved(aUnit);
-             },
-             [this] (const SeekRejectValue& aReject) {
-               OnSeekRejected(aReject);
-             }));
+    DemuxerSeek();
   }
 
   int64_t CalculateNewCurrentTime() const override
@@ -1058,6 +1065,32 @@ private:
 
   void OnSeekRejected(const SeekRejectValue& aReject) {
     mSeekRequest.Complete();
+
+    if (aReject.mError == NS_ERROR_DOM_MEDIA_WAITING_FOR_DATA) {
+      SLOG("OnSeekRejected reason=WAITING_FOR_DATA type=%d", aReject.mType);
+      MOZ_ASSERT(!mMaster->IsRequestingAudioData());
+      MOZ_ASSERT(!mMaster->IsRequestingVideoData());
+      MOZ_ASSERT(!mMaster->IsWaitingAudioData());
+      MOZ_ASSERT(!mMaster->IsWaitingVideoData());
+      // Fire 'waiting' to notify the player that we are waiting for data.
+      mMaster->UpdateNextFrameStatus(MediaDecoderOwner::NEXT_FRAME_UNAVAILABLE_SEEKING);
+      mWaitRequest.Begin(
+        Reader()->WaitForData(aReject.mType)->Then(
+          OwnerThread(), __func__,
+          [this] (MediaData::Type aType) {
+            SLOG("OnSeekRejected wait promise resolved");
+            mWaitRequest.Complete();
+            DemuxerSeek();
+          },
+          [this] (const WaitForDataRejectValue& aRejection) {
+            SLOG("OnSeekRejected wait promise rejected");
+            mWaitRequest.Complete();
+            mMaster->DecodeError(NS_ERROR_DOM_MEDIA_WAITING_FOR_DATA);
+          })
+      );
+      return;
+    }
+
     MOZ_ASSERT(NS_FAILED(aReject.mError), "Cancels should also disconnect mSeekRequest");
     mMaster->DecodeError(aReject.mError);
   }
@@ -1222,6 +1255,7 @@ private:
   media::TimeUnit mCurrentTimeBeforeSeek;
   bool mDoneAudioSeeking = false;
   bool mDoneVideoSeeking = false;
+  MozPromiseRequestHolder<WaitForDataPromise> mWaitRequest;
 
   // This temporarily stores the first frame we decode after we seek.
   // This is so that if we hit end of stream while we're decoding to reach
