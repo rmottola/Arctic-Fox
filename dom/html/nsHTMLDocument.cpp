23c0a844425d316501b5a99e1af6e0d012f5af4b
Riccardo Mottola <rm@gnu.org>
2026-08-03 09:37:58 +0200

    Bug 1336802 - Part 2: Updating the whole code base to make sure nsILoadInfo get null check. r=smaug

--- dom/html/nsHTMLDocument.cpp
+++ dom/html/nsHTMLDocument.cpp
@@ -2342,7 +2342,9 @@ nsHTMLDocument::CreateAndAddWyciwygChannel(void)
                      nsIContentPolicy::TYPE_OTHER);
   NS_ENSURE_SUCCESS(rv, rv);
   nsCOMPtr<nsILoadInfo> loadInfo = channel->GetLoadInfo();
+  NS_ENSURE_STATE(loadInfo);
   loadInfo->SetPrincipalToInherit(NodePrincipal());
+
 
   mWyciwygChannel = do_QueryInterface(channel);
 
