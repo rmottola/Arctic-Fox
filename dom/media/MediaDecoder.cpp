019e7acbd52ab57283f4fc9191c2fc5f3c1c3f01
Riccardo Mottola <rm@gnu.org>
2026-08-03 21:57:18 +0200

    Bug 1328130. Part 2 - remove MozPromiseRequestHolder::Begin(). r=gerald

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -753,10 +753,10 @@ MediaDecoder::CallSeek(const SeekTarget& aTarget, dom::Promise* aPromise)
   DiscardOngoingSeekIfExists();
 
   mSeekDOMPromise = aPromise;
-  mSeekRequest.Begin(
-    mDecoderStateMachine->InvokeSeek(aTarget)
-    ->Then(AbstractThread::MainThread(), __func__, this,
-           &MediaDecoder::OnSeekResolved, &MediaDecoder::OnSeekRejected));
+  mDecoderStateMachine->InvokeSeek(aTarget)
+  ->Then(AbstractThread::MainThread(), __func__, this,
+         &MediaDecoder::OnSeekResolved, &MediaDecoder::OnSeekRejected)
+  ->Track(mSeekRequest);
 }
 
 double
