a9c14512cd636ea503058b6542c365d49454842f
Riccardo Mottola <rm@gnu.org>
2026-08-04 08:08:09 +0200

    Bug 1328547. part 2 - remove checks for IsShutdown() which is false in DecodingState. r=kaku

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2081,13 +2081,13 @@ DecodingState::Enter()
 
   mOnAudioPopped = AudioQueue().PopEvent().Connect(
     OwnerThread(), [this] () {
-    if (!mMaster->IsShutdown() && mMaster->NeedToDecodeAudio()) {
+    if (mMaster->NeedToDecodeAudio()) {
       mMaster->EnsureAudioDecodeTaskQueued();
     }
   });
   mOnVideoPopped = VideoQueue().PopEvent().Connect(
     OwnerThread(), [this] () {
-    if (!mMaster->IsShutdown() && mMaster->NeedToDecodeVideo()) {
+    if (mMaster->NeedToDecodeVideo()) {
       mMaster->EnsureVideoDecodeTaskQueued();
     }
   });
