091c908ad97ce4c216603f3dee36849b10b94b3c
Riccardo Mottola <rm@gnu.org>
2026-08-09 17:46:13 +0200

    Bug 1310318 - Part 1: Allow access to canvas drawWindow() with web extensions permission r=bz

--- dom/canvas/CanvasUtils.cpp
+++ dom/canvas/CanvasUtils.cpp
@@ -124,5 +124,11 @@ CoerceDouble(const JS::Value& v, double* d)
     return true;
 }
 
+bool
+HasDrawWindowPrivilege(JSContext* aCx, JSObject* /* unused */)
+{
+  return nsContentUtils::CallerHasPermission(aCx, NS_LITERAL_STRING("<all_urls>"));
+}
+
 } // namespace CanvasUtils
 } // namespace mozilla
