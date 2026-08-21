b2934e38a1e02ea8693a4c1ad8b0c148c76a630f
Riccardo Mottola <rm@gnu.org>
2026-08-21 17:48:59 +0200

    Bug 1325200 part 3 - Discard regexp/constructor code on GC. r=bhackett

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -1293,6 +1293,13 @@ RegExpShared::needsSweep(JSRuntime* rt)
 }
 
 void
+RegExpShared::discardJitCode()
+{
+    for (size_t i = 0; i < ArrayLength(compilationArray); i++)
+        compilationArray[i].jitCode = nullptr;
+}
+
+void
 RegExpCompartment::sweep(JSRuntime* rt)
 {
     if (!set_.initialized())
@@ -1303,6 +1310,10 @@ RegExpCompartment::sweep(JSRuntime* rt)
         if (shared->needsSweep(rt)) {
             js_delete(shared);
             e.removeFront();
+        } else {
+            // Discard code to avoid holding onto ExecutablePools.
+            if (rt->gc.isHeapCompacting())
+                shared->discardJitCode();
         }
     }
 
