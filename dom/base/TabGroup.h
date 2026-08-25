5760d3b97b93723d2ac194d2df3795e8566d7827
Riccardo Mottola <rm@gnu.org>
2026-08-25 21:27:55 +0200

    Bug 1332790 - Don't assert mLastWindowLeft in ~TabGroup when destroying the chrome TabGroup, r=billm

--- dom/base/TabGroup.h
+++ dom/base/TabGroup.h
@@ -128,10 +128,15 @@ private:
   void EnsureThrottledEventQueues();
 
   ~TabGroup();
-  DocGroupMap mDocGroups;
+
+  // Thread-safe members
   Atomic<bool> mLastWindowLeft;
-  nsTArray<nsPIDOMWindowOuter*> mWindows;
   Atomic<bool> mThrottledQueuesInitialized;
+  const bool mIsChrome;
+
+  // Main thread only
+  DocGroupMap mDocGroups;
+  nsTArray<nsPIDOMWindowOuter*> mWindows;
   nsCOMPtr<nsIEventTarget> mEventTargets[size_t(TaskCategory::Count)];
   RefPtr<AbstractThread> mAbstractThreads[size_t(TaskCategory::Count)];
 };
