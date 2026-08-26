0cbdcfacf5edf8696db463a487ce20ee80793ff2
Riccardo Mottola <rm@gnu.org>
2026-08-26 23:03:12 +0200

    Bug 1337189 (part 8) - Move mGatherer out of Sampler. r=mstange.

--- tools/profiler/core/Sampler.cpp
+++ tools/profiler/core/Sampler.cpp
@@ -38,7 +38,6 @@
 #include "nsTArray.h"
 
 #include "mozilla/Preferences.h"
-#include "mozilla/ProfileGatherer.h"
 
 #if defined(SPS_OS_android) && !defined(MOZ_WIDGET_GONK)
   #include "FennecJNIWrappers.h"
@@ -259,8 +258,6 @@ Sampler::Sampler(double aInterval, int aEntrySize,
     mozilla::tasktracer::StartLogging();
   }
 #endif
-
-  mGatherer = new mozilla::ProfileGatherer(this);
 }
 
 Sampler::~Sampler()
@@ -287,10 +284,6 @@ Sampler::~Sampler()
       }
     }
   }
-
-  // Cancel any in-flight async profile gatherering
-  // requests
-  mGatherer->Cancel();
 
 #ifdef MOZ_TASK_TRACER
   if (mTaskTracer) {
@@ -427,41 +420,12 @@ Sampler::ToJSObject(JSContext *aCx, double aSinceTime)
   return &val.toObject();
 }
 
-void
-Sampler::GetGatherer(nsISupports** aRetVal)
-{
-  if (!aRetVal || NS_WARN_IF(!mGatherer)) {
-    return;
-  }
-  NS_ADDREF(*aRetVal = mGatherer);
-}
-
 UniquePtr<char[]>
 Sampler::ToJSON(double aSinceTime)
 {
   SpliceableChunkedJSONWriter b;
   StreamJSON(b, aSinceTime);
   return b.WriteFunc()->CopyData();
-}
-
-void
-Sampler::ToJSObjectAsync(double aSinceTime, mozilla::dom::Promise* aPromise)
-{
-  if (NS_WARN_IF(!mGatherer)) {
-    return;
-  }
-
-  mGatherer->Start(aSinceTime, aPromise);
-}
-
-void
-Sampler::ToFileAsync(const nsACString& aFileName, double aSinceTime)
-{
-  if (NS_WARN_IF(!mGatherer)) {
-    return;
-  }
-
-  mGatherer->Start(aSinceTime, aFileName);
 }
 
 struct SubprocessClosure {
