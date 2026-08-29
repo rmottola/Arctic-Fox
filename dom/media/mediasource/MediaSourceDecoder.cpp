1a7fc4a7a6dfd2f5fa4a0a21332e500ec99f1a2f
Riccardo Mottola <rm@gnu.org>
2026-08-29 16:22:11 +0200

    Bug 1325707: P4. Fix coding style of MediaDataDemuxers. r=gerald

--- dom/media/mediasource/MediaSourceDecoder.cpp
+++ dom/media/mediasource/MediaSourceDecoder.cpp
@@ -269,8 +269,8 @@ MediaSourceDecoder::NextFrameBufferedStatus()
 {
   MOZ_ASSERT(NS_IsMainThread());
 
-  if (!mMediaSource ||
-      mMediaSource->ReadyState() == dom::MediaSourceReadyState::Closed) {
+  if (!mMediaSource
+      || mMediaSource->ReadyState() == dom::MediaSourceReadyState::Closed) {
     return MediaDecoderOwner::NEXT_FRAME_UNAVAILABLE;
   }
 
