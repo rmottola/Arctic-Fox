abdfbf704b0774da49ebea44476ddbcd0470a9b7
Riccardo Mottola <rm@gnu.org>
2026-08-31 17:06:11 +0200

    Bug 1334573: Remove assertion that Intl.PluralRules.prototype is an Intl.PluralRules instance. r=Waldo

--- js/src/builtin/Intl.cpp
+++ js/src/builtin/Intl.cpp
@@ -3501,7 +3501,6 @@ js::GlobalObject::addPluralRulesConstructor(JSContext* cx, HandleObject intl)
         const HeapSlot& slot = global->getReservedSlotRef(PLURAL_RULES_PROTO);
         if (!slot.isUndefined()) {
             MOZ_ASSERT(slot.isObject());
-            MOZ_ASSERT(slot.toObject().is<PluralRulesObject>());
             JS_ReportErrorASCII(cx,
                                 "the PluralRules constructor can't be added "
                                 "multiple times in the same global");
