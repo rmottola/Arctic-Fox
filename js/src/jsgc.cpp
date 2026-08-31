29233711b9df5942ea0d6c1fff1e7badf34c700d
Riccardo Mottola <rm@gnu.org>
2026-08-31 21:19:37 +0200

    Bug 1324773 - Sweep JSCompartment::varNames_ r=sfink a=abillings * * * Bug 1324773 - Fix test bustage r=me

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -4854,6 +4854,8 @@ MAKE_GC_SWEEP_TASK(SweepMiscTask);
 SweepAtomsTask::run()
 {
     runtime->sweepAtoms();
+    for (CompartmentsIter comp(runtime, SkipAtoms); !comp.done(); comp.next())
+        comp->sweepVarNames();
 }
 
 /* virtual */ void
