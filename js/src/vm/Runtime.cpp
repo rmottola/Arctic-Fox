c61ffec481eed49cf1db1823afa41dc9153a4b68
Riccardo Mottola <rm@gnu.org>
2026-09-02 15:50:25 +0200

    Bug 1334885 - Allow wasm signal handlers to work with multiple threads in a runtime, r=luke.

--- js/src/vm/Runtime.cpp
+++ js/src/vm/Runtime.cpp
@@ -246,9 +246,6 @@ JSRuntime::init(JSContext* cx, uint32_t maxbytes, uint32_t maxNurseryBytes)
     jitSupportsUnalignedAccesses = js::jit::JitSupportsUnalignedAccesses();
     jitSupportsSimd = js::jit::JitSupportsSimd();
 
-    if (!wasm::EnsureSignalHandlers(this))
-        return false;
-
     if (!geckoProfiler().init())
         return false;
 
@@ -500,7 +497,7 @@ JSContext::requestInterrupt(InterruptMode mode)
         if (fx.isWaiting())
             fx.wake(FutexThread::WakeForJSInterrupt);
         fx.unlock();
-        InterruptRunningJitCode(runtime());
+        InterruptRunningJitCode(this);
     }
 }
 
