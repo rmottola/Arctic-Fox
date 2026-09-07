6fae6ff9ea430196eda28547f12e726b1b11e427
Riccardo Mottola <rm@gnu.org>
2026-09-07 16:44:05 +0200

    Bug 1332465. Update webrender to 6c15d14bb6b786cad979407e6ed7b86949b4ae48 r=gfx?

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -117,6 +117,7 @@ pub unsafe extern fn wr_api_set_root_display_list(api: &mut RenderApi,
                               epoch,
                               LayoutSize::new(viewport_width, viewport_height),
                               frame_builder.dl_builder);
+    api.generate_frame();
 }
 #[no_mangle]
 pub extern fn wr_window_new(window_id: u64,
@@ -225,6 +226,7 @@ pub extern fn wr_dp_end(state: &mut WrState, api: &mut RenderApi, epoch: u32) {
                               Epoch(epoch),
                               LayoutSize::new(width as f32, height as f32),
                               fb.dl_builder);
+    api.generate_frame();
 }
 
 
@@ -506,6 +508,7 @@ pub extern fn wr_init_window(root_pipeline_id: u64,
 
     let pipeline_id = u64_to_pipeline_id(root_pipeline_id);
     api.set_root_pipeline(pipeline_id);
+    api.generate_frame();
 
     let state = Box::new(WrWindowState {
         renderer: renderer,
@@ -617,6 +620,7 @@ pub extern fn wr_window_dp_end(window: &mut WrWindowState, state: &mut WrState) 
                                          *epoch,
                                          LayoutSize::new(width as f32, height as f32),
                                          fb.dl_builder);
+        window.api.generate_frame();
 
         return;
     }
@@ -698,6 +702,7 @@ pub extern fn wr_dp_push_border(state: &mut WrState, rect: WrRect, clip: WrRect,
 #[no_mangle]
 pub extern fn wr_api_set_root_pipeline(api: &mut RenderApi, pipeline_id: u64) {
     api.set_root_pipeline(u64_to_pipeline_id(pipeline_id));
+    api.generate_frame();
 }
 
 #[no_mangle]
