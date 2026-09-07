7a6e6e175da63099dc3d95e94c7da87aba2fc7fb
Riccardo Mottola <rm@gnu.org>
2026-09-07 11:19:34 +0200

    Bug 1325227 - Part 1: Remove synchronous transaction replies since they don't return any data we didn't already have. r=nical

--- gfx/layers/ipc/ShadowLayers.h
+++ gfx/layers/ipc/ShadowLayers.h
@@ -33,7 +33,6 @@ namespace layers {
 
 class ClientLayerManager;
 class CompositorBridgeChild;
-class EditReply;
 class FixedSizeSmallShmemSectionAllocator;
 class ImageContainer;
 class Layer;
@@ -421,8 +420,6 @@ protected:
 #else
   void CheckSurfaceDescriptor(const SurfaceDescriptor* aDescriptor) const {}
 #endif
-
-  void ProcessReplies(const nsTArray<EditReply>& aReplies);
 
   RefPtr<CompositableClient> FindCompositable(const CompositableHandle& aHandle);
 
--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -401,9 +401,7 @@ WebRenderBridgeParent::ProcessWebrenderCommands(InfallibleTArray<WebRenderComman
         break;
       }
       case WebRenderCommand::TCompositableOperation: {
-        EditReplyVector replyv;
-        if (!ReceiveCompositableUpdate(cmd.get_CompositableOperation(),
-                                       replyv)) {
+        if (!ReceiveCompositableUpdate(cmd.get_CompositableOperation())) {
           NS_ERROR("ReceiveCompositableUpdate failed");
         }
         break;
