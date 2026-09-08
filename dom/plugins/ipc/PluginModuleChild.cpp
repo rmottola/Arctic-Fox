5eea249d927244db1724ba561d3cd1f2e05b3423
Riccardo Mottola <rm@gnu.org>
2026-09-08 22:11:41 +0200

    Bug 1334509 - Remove support for the NPNURLVCookie NPAPI; r=bsmedberg

--- dom/plugins/ipc/PluginModuleChild.cpp
+++ dom/plugins/ipc/PluginModuleChild.cpp
@@ -1620,9 +1620,7 @@ _getvalueforurl(NPP npp, NPNURLVariable variable, const char *url,
     if (!npp || !value || !len)
         return NPERR_INVALID_PARAM;
 
-    switch (variable) {
-    case NPNURLVCookie:
-    case NPNURLVProxy:
+    if (variable == NPNURLVProxy) {
         nsCString v;
         NPError result;
         InstCast(npp)->
@@ -1650,9 +1648,7 @@ _setvalueforurl(NPP npp, NPNURLVariable variable, const char *url,
     if (!url)
         return NPERR_INVALID_URL;
 
-    switch (variable) {
-    case NPNURLVCookie:
-    case NPNURLVProxy:
+    if (variable == NPNURLVProxy) {
         NPError result;
         InstCast(npp)->CallNPN_SetValueForURL(variable, nsCString(url),
                                               nsDependentCString(value, len),
