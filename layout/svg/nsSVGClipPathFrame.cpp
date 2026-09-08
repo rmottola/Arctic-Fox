4c89cab42a601ca9fb7897d31b6b9bf0cb78f0e2
Riccardo Mottola <rm@gnu.org>
2026-09-08 17:30:27 +0200

    Bug 1318266 - Part 1. Paint trivial clip-path onto mask layer. r=mstange

--- layout/svg/nsSVGClipPathFrame.cpp
+++ layout/svg/nsSVGClipPathFrame.cpp
@@ -271,8 +271,6 @@ nsSVGClipPathFrame::GetClipMask(gfxContext& aReferenceContext,
                                 SourceSurface* aExtraMask,
                                 const Matrix& aExtraMasksTransform)
 {
-  MOZ_ASSERT(!IsTrivial(), "Caller needs to use ApplyClipPath");
-
   IntPoint offset;
   RefPtr<DrawTarget> maskDT = CreateClipMask(aReferenceContext, offset);
   if (!maskDT) {
