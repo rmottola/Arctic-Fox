3688d07fecd969714fb223331c19ade5bc92f0e5
Riccardo Mottola <rm@gnu.org>
2026-09-08 09:30:08 +0200

    Bug 1335799 - Update WR layers and FFI/bindings code for the webrender update. r=jrmuizel

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -1343,7 +1343,7 @@ void nsBaseWidget::CreateCompositor(int aWidth, int aHeight)
   if (lm->AsWebRenderLayerManager()) {
     TextureFactoryIdentifier textureFactoryIdentifier;
     lm->AsWebRenderLayerManager()->Initialize(mCompositorBridgeChild,
-                                              mCompositorSession->RootLayerTreeId(),
+                                              wr::AsPipelineId(mCompositorSession->RootLayerTreeId()),
                                               &textureFactoryIdentifier);
     ImageBridgeChild::IdentifyCompositorTextureHost(textureFactoryIdentifier);
     gfx::VRManagerChild::IdentifyTextureHost(textureFactoryIdentifier);
