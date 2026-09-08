55bbeffe4f98a675ee23e71ea891d330230c75a2
Riccardo Mottola <rm@gnu.org>
2026-09-08 19:26:33 +0200

    Bug 1337387 - Fix unified build bustage r=kats?

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -61,6 +61,7 @@
 #include "opengl/CompositorOGL.h"
 #include "GLContextEGL.h"
 #include "GLContextProvider.h"
+#include "mozilla/widget/AndroidCompositorWidget.h"
 #include "ScopedGLHelpers.h"
 #endif
 #include "GeckoProfiler.h"
