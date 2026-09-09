3a26baa1d8a7cae49bdf206127865c88c2015c8d
Riccardo Mottola <rm@gnu.org>
2026-09-10 01:02:08 +0200

    Bug 1342754 - Make WebRenderBridgeChild::mIdNamespace unique r=jrmuizel

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -1153,7 +1153,7 @@ CompositorBridgeChild::HandleFatalError(const char* aName, const char* aMsg) con
 PWebRenderBridgeChild*
 CompositorBridgeChild::AllocPWebRenderBridgeChild(const wr::PipelineId& aPipelineId, TextureFactoryIdentifier*, uint32_t *aIdNamespace)
 {
-  WebRenderBridgeChild* child = new WebRenderBridgeChild(aPipelineId, *aIdNamespace);
+  WebRenderBridgeChild* child = new WebRenderBridgeChild(aPipelineId);
   child->AddIPDLReference();
   return child;
 }
