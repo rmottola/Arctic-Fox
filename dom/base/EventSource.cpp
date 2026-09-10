d3b33bcf1b259ff88130bd89cc7c23c9918a5863
Riccardo Mottola <rm@gnu.org>
2026-09-10 23:49:38 +0200

    Bug 1332322 part 1.  Add an nsWrapperCache function to mark the wrapper as 'live' for GC purposes.  r=mccr8

--- dom/base/EventSource.cpp
+++ dom/base/EventSource.cpp
@@ -94,8 +94,7 @@ NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_BEGIN(EventSource)
       tmp->mListenerManager->MarkForCC();
     }
     if (!isBlack && tmp->PreservingWrapper()) {
-      // This marks the wrapper black.
-      tmp->GetWrapper();
+      tmp->MarkWrapperLive();
     }
     return true;
   }
