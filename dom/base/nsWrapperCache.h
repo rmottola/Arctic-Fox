ab1d6ec5ecd3ec447020b244f31f64a6e0d861cb
Riccardo Mottola <rm@gnu.org>
2026-09-11 12:26:10 +0200

    Bug 851892 part 16.  Convert CSSFontFeatureValuesRule to WebIDL.  r=peterv,heycam

--- dom/base/nsWrapperCache.h
+++ dom/base/nsWrapperCache.h
@@ -23,7 +23,6 @@ class ProcessGlobal;
 class SandboxPrivate;
 class nsInProcessTabChildGlobal;
 class nsWindowRoot;
-class nsCSSFontFeatureValuesRule;
 class nsCSSKeyframeRule;
 class nsCSSKeyframesRule;
 class nsCSSCounterStyleRule;
@@ -289,7 +288,6 @@ private:
   friend class SandboxPrivate;
   friend class nsInProcessTabChildGlobal;
   friend class nsWindowRoot;
-  friend class nsCSSFontFeatureValuesRule;
   friend class nsCSSKeyframeRule;
   friend class nsCSSKeyframesRule;
   friend class nsCSSCounterStyleRule;
