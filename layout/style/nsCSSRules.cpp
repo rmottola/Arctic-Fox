0f34841ded810ca86a2dedec1cc08ee203f5b92f
Riccardo Mottola <rm@gnu.org>
2026-09-11 12:25:18 +0200

    Bug 851892 part 14.  Convert CSSPageRule to WebIDL.  r=peterv,heycam The PutForwards bit is a new feature, but easy enough to implement here.

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -39,6 +39,7 @@
 #include "mozilla/dom/CSSMediaRuleBinding.h"
 #include "mozilla/dom/CSSSupportsRuleBinding.h"
 #include "mozilla/dom/CSSMozDocumentRuleBinding.h"
+#include "mozilla/dom/CSSPageRuleBinding.h"
 #include "StyleRule.h"
 #include "nsFont.h"
 #include "nsIURI.h"
@@ -2581,7 +2582,6 @@ nsCSSPageRule::nsCSSPageRule(const nsCSSPageRule& aCopy)
   : Rule(aCopy)
   , mDeclaration(new css::Declaration(*aCopy.mDeclaration))
 {
-  SetIsNotDOMBinding();
   mDeclaration->SetOwningRule(this);
 }
 
@@ -2627,7 +2627,6 @@ nsCSSPageRule::IsCCLeaf() const
 // QueryInterface implementation for nsCSSPageRule
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(nsCSSPageRule)
   NS_INTERFACE_MAP_ENTRY(nsIDOMCSSPageRule)
-  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(CSSPageRule)
 NS_INTERFACE_MAP_END_INHERITING(mozilla::css::Rule)
 
 #ifdef DEBUG
@@ -2673,11 +2672,17 @@ nsCSSPageRule::GetCssTextImpl(nsAString& aCssText) const
 NS_IMETHODIMP
 nsCSSPageRule::GetStyle(nsIDOMCSSStyleDeclaration** aStyle)
 {
+  NS_ADDREF(*aStyle = Style());
+  return NS_OK;
+}
+
+nsICSSDeclaration*
+nsCSSPageRule::Style()
+{
   if (!mDOMDeclaration) {
     mDOMDeclaration = new nsCSSPageStyleDeclaration(this);
   }
-  NS_ADDREF(*aStyle = mDOMDeclaration);
-  return NS_OK;
+  return mDOMDeclaration;
 }
 
 void
@@ -2704,8 +2709,7 @@ nsCSSPageRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
 nsCSSPageRule::WrapObject(JSContext* aCx,
                           JS::Handle<JSObject*> aGivenProto)
 {
-  NS_NOTREACHED("We called SetIsNotDOMBinding() in our constructor");
-  return nullptr;
+  return CSSPageRuleBinding::Wrap(aCx, this, aGivenProto);
 }
 
 namespace mozilla {
