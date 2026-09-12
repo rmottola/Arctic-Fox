22b072fbdb0f9a558f5194a98d62785502c95658
Riccardo Mottola <rm@gnu.org>
2026-09-12 22:23:00 +0200

    Bug 1316803 part 1 - fold constant rhs for add/sub/shift/rotate/bitwise-ops. r=lth

--- js/src/jit/arm64/MacroAssembler-arm64-inl.h
+++ js/src/jit/arm64/MacroAssembler-arm64-inl.h
@@ -306,6 +306,12 @@ MacroAssembler::add64(Imm32 imm, Register64 dest)
     Add(ARMRegister(dest.reg, 64), ARMRegister(dest.reg, 64), Operand(imm.value));
 }
 
+void
+MacroAssembler::add64(Imm64 imm, Register64 dest)
+{
+    Add(ARMRegister(dest.reg, 64), ARMRegister(dest.reg, 64), Operand(imm.value));
+}
+
 CodeOffset
 MacroAssembler::add32ToPtrWithPatch(Register src, Register dest)
 {
@@ -389,6 +395,12 @@ MacroAssembler::subPtr(const Address& addr, Register dest)
 
 void
 MacroAssembler::sub64(Register64 src, Register64 dest)
+{
+    MOZ_CRASH("NYI: sub64");
+}
+
+void
+MacroAssembler::sub64(Imm64 imm, Register64 dest)
 {
     MOZ_CRASH("NYI: sub64");
 }
@@ -738,7 +750,19 @@ MacroAssembler::rotateLeft64(Register count, Register64 input, Register64 dest, 
 }
 
 void
+MacroAssembler::rotateLeft64(Imm32 count, Register64 input, Register64 dest, Register temp)
+{
+    MOZ_CRASH("NYI: rotateLeft64");
+}
+
+void
 MacroAssembler::rotateRight64(Register count, Register64 input, Register64 dest, Register temp)
+{
+    MOZ_CRASH("NYI: rotateRight64");
+}
+
+void
+MacroAssembler::rotateRight64(Imm32 count, Register64 input, Register64 dest, Register temp)
 {
     MOZ_CRASH("NYI: rotateRight64");
 }
