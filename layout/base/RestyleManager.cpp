470c3aab01db3b2e3a9bc6908793b35efa0bfbda
Riccardo Mottola <rm@gnu.org>
2026-09-14 09:31:45 +0200

    Bug 1326388 part 2.  Switch nsSVGElement from storing an nsStyleRule to storing a DeclarationBlock to represent its SMIL mapped attributes.  r=dbaron

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -6,6 +6,7 @@
 
 #include "mozilla/RestyleManager.h"
 #include "mozilla/StyleSetHandleInlines.h"
+#include "mozilla/css/StyleRule.h" // For nsCSSSelector
 #include "nsIFrame.h"
 #include "nsIPresShellInlines.h"
 
