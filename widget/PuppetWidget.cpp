d7f69bb6570e0000bf44f61822ffbfc34925eda8
Riccardo Mottola <rm@gnu.org>
2026-09-14 16:14:40 +0200

    Make sure the last transaction is revoked when resetting content layer managers. (bug 1331548 part 3, r=mattwoodrow)

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -599,6 +599,10 @@ PuppetWidget::GetLayerManager(PLayerTransactionChild* aShadowManager,
 LayerManager*
 PuppetWidget::RecreateLayerManager(PLayerTransactionChild* aShadowManager)
 {
+  // Force the old LM to self destruct, otherwise if the reference dangles we
+  // could fail to revoke the most recent transaction.
+  DestroyLayerManager();
+
   MOZ_ASSERT(mTabChild);
   if (mTabChild->GetCompositorOptions().UseWebRender()) {
     mLayerManager = new WebRenderLayerManager(this);
