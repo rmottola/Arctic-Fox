51afc439c8e7c5019d13e077dcbbaafd839a5298
Riccardo Mottola <rm@gnu.org>
2026-09-15 20:01:51 +0200

    Bug 1322648 - Don't start parse task if its zone is being collected r=sfink

--- js/src/vm/Runtime.cpp
+++ js/src/vm/Runtime.cpp
@@ -754,6 +754,7 @@ void
 JSRuntime::setUsedByExclusiveThread(Zone* zone)
 {
     MOZ_ASSERT(!zone->usedByExclusiveThread);
+    MOZ_ASSERT(!zone->wasGCStarted());
     zone->usedByExclusiveThread = true;
     numExclusiveThreads++;
 }
