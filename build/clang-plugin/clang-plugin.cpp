8459230115d3e14f89b580877c04f02785bbc611
Riccardo Mottola <rm@gnu.org>
2026-09-16 14:19:05 +0200

    Bug 1316545 part 3 - Remove LLVM_EXPORT_REGISTRY which has been dropped in r277806. r=ehsan

--- build/clang-plugin/clang-plugin.cpp
+++ build/clang-plugin/clang-plugin.cpp
@@ -2236,7 +2236,3 @@ public:
 
 static FrontendPluginRegistry::Add<MozCheckAction> X("moz-check",
                                                      "check moz action");
-// Export the registry on Windows.
-#ifdef LLVM_EXPORT_REGISTRY
-LLVM_EXPORT_REGISTRY(FrontendPluginRegistry)
-#endif
