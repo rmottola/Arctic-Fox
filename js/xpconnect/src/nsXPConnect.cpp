a82a7f338896e9378f26753168d1de1accff69a0
Riccardo Mottola <rm@gnu.org>
2026-09-20 08:45:07 +0200

    Bug 1332245 - Move nsScriptError from js/xpconnect to dom/bindings. r=bz

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -32,7 +32,9 @@
 #include "nsIObjectOutputStream.h"
 #include "nsScriptSecurityManager.h"
 #include "nsIPermissionManager.h"
+#include "nsIScriptError.h"
 #include "nsContentUtils.h"
+#include "nsScriptError.h"
 #include "jsfriendapi.h"
 
 using namespace mozilla;
