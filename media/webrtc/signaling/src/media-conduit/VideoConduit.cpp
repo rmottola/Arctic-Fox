d435af2f2a088c43b00dbdbfd092398f3fb3bb99
Riccardo Mottola <rm@gnu.org>
2026-09-21 14:12:59 +0200

    Bug 1328142 - Remove comment that wants to be removed. r=pkerr

--- media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
+++ media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
@@ -1638,7 +1638,7 @@ MediaConduitErrorCode
 WebrtcVideoConduit::DeliverPacket(const void* data, int len)
 {
   // Media Engine should be receiving already.
-  if (/* XXX Remove this.... !mEngineReceiving ||*/ !mCall) {
+  if (!mCall) {
     CSFLogError(logTag, "Error: %s when not receiving", __FUNCTION__);
     return kMediaConduitSessionNotInited;
   }
