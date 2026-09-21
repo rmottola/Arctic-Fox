f41e327805d70a0e623a83f84c8d9912c1b49c38
Riccardo Mottola <rm@gnu.org>
2026-09-21 22:47:09 +0200

    Switch PContent from PCrashReporter to shmem-based CrashReporter. (bug 1338308, r=billm)

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -75,6 +75,7 @@ class OptionalURIParams;
 class PFileDescriptorSetParent;
 class URIParams;
 class TestShellParent;
+class CrashReporterHost;
 } // namespace ipc
 
 namespace jsipc {
@@ -401,14 +402,8 @@ public:
 
   virtual void OnChannelError() override;
 
-  virtual PCrashReporterParent*
-  AllocPCrashReporterParent(const NativeThreadId& tid,
-                            const uint32_t& processType) override;
-
   virtual mozilla::ipc::IPCResult
-  RecvPCrashReporterConstructor(PCrashReporterParent* actor,
-                                const NativeThreadId& tid,
-                                const uint32_t& processType) override;
+  RecvInitCrashReporter(Shmem&& aShmem, const NativeThreadId& aThreadId) override;
 
   virtual PNeckoParent* AllocPNeckoParent() override;
 
@@ -799,9 +794,6 @@ private:
   RecvPBlobConstructor(PBlobParent* aActor,
                        const BlobConstructorParams& params) override;
 
-  virtual bool
-  DeallocPCrashReporterParent(PCrashReporterParent* crashreporter) override;
-
   virtual mozilla::ipc::IPCResult RecvNSSU2FTokenIsCompatibleVersion(const nsString& aVersion,
                                                                      bool* aIsCompatible) override;
 
@@ -1179,8 +1171,6 @@ private:
   bool mShutdownPending;
   bool mIPCOpen;
 
-  friend class CrashReporterParent;
-
   RefPtr<nsConsoleService>  mConsoleService;
   nsConsoleService* GetConsoleService();
   nsCOMPtr<nsIContentProcessInfo> mScriptableHelper;
@@ -1218,6 +1208,9 @@ private:
   nsRefPtrHashtable<nsIDHashKey, GetFilesHelper> mGetFilesPendingRequests;
 
   nsTArray<nsCString> mBlobURLs;
+#ifdef MOZ_CRASHREPORTER
+  UniquePtr<mozilla::ipc::CrashReporterHost> mCrashReporter;
+#endif
 };
 
 } // namespace dom
