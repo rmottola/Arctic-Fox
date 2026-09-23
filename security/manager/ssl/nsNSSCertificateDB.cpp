f3eaab4ca19cc5cfbdec491da859a5b2e7b83528
Riccardo Mottola <rm@gnu.org>
2026-09-23 19:46:15 +0200

    bug 1239344 - remove error alert for successful PKCS12 operations r=Cykesiopka

--- security/manager/ssl/nsNSSCertificateDB.cpp
+++ security/manager/ssl/nsNSSCertificateDB.cpp
@@ -968,6 +968,9 @@ nsNSSCertificateDB::ImportCertsFromFile(nsIFile* aFile, uint32_t aType)
 NS_IMETHODIMP
 nsNSSCertificateDB::ImportPKCS12File(nsIFile* aFile)
 {
+  if (!NS_IsMainThread()) {
+    return NS_ERROR_NOT_SAME_THREAD;
+  }
   nsNSSShutDownPreventionLock locker;
   if (isAlreadyShutDown()) {
     return NS_ERROR_NOT_AVAILABLE;
@@ -982,6 +985,9 @@ NS_IMETHODIMP
 nsNSSCertificateDB::ExportPKCS12File(nsIFile* aFile, uint32_t count,
                                      nsIX509Cert** certs)
 {
+  if (!NS_IsMainThread()) {
+    return NS_ERROR_NOT_SAME_THREAD;
+  }
   nsNSSShutDownPreventionLock locker;
   if (isAlreadyShutDown()) {
     return NS_ERROR_NOT_AVAILABLE;
