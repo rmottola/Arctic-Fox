c66e8d163e119a875c2582fbe7eff1662728c1ef
Riccardo Mottola <rm@gnu.org>
2026-09-23 15:43:10 +0200

    Bug 1346078: Remove nsAString_internal and just use the nsAString name directly. r=bsmedberg

--- netwerk/base/nsSocketTransport2.cpp
+++ netwerk/base/nsSocketTransport2.cpp
@@ -2410,7 +2410,7 @@ nsSocketTransport::GetPort(int32_t *port)
 }
 
 NS_IMETHODIMP
-nsSocketTransport::GetNetworkInterfaceId(nsACString_internal &aNetworkInterfaceId)
+nsSocketTransport::GetNetworkInterfaceId(nsACString &aNetworkInterfaceId)
 {
     MOZ_ASSERT(PR_GetCurrentThread() == gSocketThread, "wrong thread");
     aNetworkInterfaceId = mNetworkInterfaceId;
@@ -2418,7 +2418,7 @@ nsSocketTransport::GetNetworkInterfaceId(nsACString_internal &aNetworkInterfaceI
 }
 
 NS_IMETHODIMP
-nsSocketTransport::SetNetworkInterfaceId(const nsACString_internal &aNetworkInterfaceId)
+nsSocketTransport::SetNetworkInterfaceId(const nsACString &aNetworkInterfaceId)
 {
     MOZ_ASSERT(PR_GetCurrentThread() == gSocketThread, "wrong thread");
     mNetworkInterfaceId = aNetworkInterfaceId;
