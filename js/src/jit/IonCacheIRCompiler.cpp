39112fbfde46361c955330c30b324d0cf1dedb8c
Riccardo Mottola <rm@gnu.org>
2026-09-24 19:00:35 +0200

    Bug 1337024 part 2 - Port Baseline stubs for adding dense/unboxed elements to CacheIR. r=evilpie

--- js/src/jit/IonCacheIRCompiler.cpp
+++ js/src/jit/IonCacheIRCompiler.cpp
@@ -916,7 +916,19 @@ IonCacheIRCompiler::emitStoreDenseElement()
 }
 
 bool
+IonCacheIRCompiler::emitStoreDenseElementHole()
+{
+    MOZ_CRASH("Baseline-specific op");
+}
+
+bool
 IonCacheIRCompiler::emitStoreUnboxedArrayElement()
+{
+    MOZ_CRASH("Baseline-specific op");
+}
+
+bool
+IonCacheIRCompiler::emitStoreUnboxedArrayElementHole()
 {
     MOZ_CRASH("Baseline-specific op");
 }
