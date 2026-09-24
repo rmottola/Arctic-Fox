b071ca15b9621442c3d1205c1f6b25cd86ee4060
Riccardo Mottola <rm@gnu.org>
2026-09-24 19:02:23 +0200

    Bug 1337024 part 3 - Port Baseline TypedArray/TypedObject SetElem stub to CacheIR. r=evilpie

--- js/src/jit/IonCacheIRCompiler.cpp
+++ js/src/jit/IonCacheIRCompiler.cpp
@@ -922,6 +922,12 @@ IonCacheIRCompiler::emitStoreDenseElementHole()
 }
 
 bool
+IonCacheIRCompiler::emitStoreTypedElement()
+{
+    MOZ_CRASH("Baseline-specific op");
+}
+
+bool
 IonCacheIRCompiler::emitStoreUnboxedArrayElement()
 {
     MOZ_CRASH("Baseline-specific op");
