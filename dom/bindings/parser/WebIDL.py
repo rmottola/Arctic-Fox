1d3cdb16a8422c77ac32f4c086d1a7683c9ffdeb
Riccardo Mottola <rm@gnu.org>
2026-09-24 11:37:01 +0200

    Bug 1332713 part 2.  Disallow [SameObject] Promise-typed attributes.  r=qdot

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -4016,7 +4016,6 @@ class IDLAttribute(IDLInterfaceMember):
                               "interface type as its type", [self.location])
 
         if (not self.type.isInterface() and
-            not self.type.isPromise() and
             self.getExtendedAttribute("SameObject")):
             raise WebIDLError("An attribute with [SameObject] must have an "
                               "interface type as its type", [self.location])
