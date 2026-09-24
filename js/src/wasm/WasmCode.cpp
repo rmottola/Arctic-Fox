2101c37773067b1cc7caaa5f07804b205448e4be
Riccardo Mottola <rm@gnu.org>
2026-09-24 09:13:33 +0200

    Bug 1341090: Implement a JIT code counter; r=jonco, r=jandem

--- js/src/wasm/WasmCode.cpp
+++ js/src/wasm/WasmCode.cpp
@@ -76,6 +76,8 @@ AllocateCodeSegment(JSContext* cx, uint32_t codeLength)
         return nullptr;
     }
 
+    cx->zone()->updateJitCodeMallocBytes(codeLength);
+
     wasmCodeAllocations++;
     return (uint8_t*)p;
 }
@@ -247,7 +249,6 @@ CodeSegment::~CodeSegment()
     if (!bytes_)
         return;
 
-
     MOZ_ASSERT(wasmCodeAllocations > 0);
     wasmCodeAllocations--;
 
@@ -259,7 +260,6 @@ CodeSegment::~CodeSegment()
     vtune::UnmarkBytes(bytes_, size);
 #endif
     DeallocateExecutableMemory(bytes_, size);
-
 }
 
 void
