838b4496bef69dbbb0c3daa7b10fc445269836c3
Riccardo Mottola <rm@gnu.org>
2026-09-25 02:28:46 +0200

    Bug 1331452 - Keep scope in a slot in WasmFunctionCallObjects. (r=yury)

--- js/src/vm/EnvironmentObject.cpp
+++ js/src/vm/EnvironmentObject.cpp
@@ -655,6 +655,7 @@ WasmFunctionCallObject::createHollowForDebug(JSContext* cx, WasmFunctionScope* s
 
     Rooted<WasmFunctionCallObject*> callobj(cx, &obj->as<WasmFunctionCallObject>());
     callobj->initEnclosingEnvironment(&cx->global()->lexicalEnvironment());
+    callobj->initReservedSlot(SCOPE_SLOT, PrivateGCThingValue(scope));
 
     return callobj;
 }
